3dfdd2e4e96baa50d71a5ad34d06ce65f38b2b4f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2013-08-14 10:52:46 -0600

    THIS FINALLY WORKS
    ...I hope.

--- bash/functions/newwin.bash
+++ bash/functions/newwin.bash
@@ -4,22 +4,31 @@
 
 newwin()
 {   # open in a new tmux/GNU screen window if applicable
-    declare bin cmd title titleregex='^-?-t(itle)?$'
+    # Usage: newwin [-t|--title TITLE] COMMAND ARGS ...
+
+    declare bin arg i=0
+    declare title titleregex='^-?-t(itle)?$'
+    declare -a args
 
     [[ $1 =~ $titleregex ]] && {
         title="$2"
         shift 2
     } || {
         title="$(basename "$1")"
+        printf -v bin "%q" "$1"
+        shift
     }
 
-    printf -v cmd "%q " $@
-
     if [[ $TMUX ]]; then
-        tmux new-window -n "$title" "$cmd"
+        until [[ $# -eq 0 ]]; do
+            printf -v args[$i] "%q" "$1"
+            ((i++))
+            shift
+        done
+        tmux new-window -n "$title" "$bin ${args[@]}"
     elif [[ $STY ]]; then
-        screen -t "$title" $cmd
+        screen -t "$title" $bin "$@"
     else
-        $cmd
+        $bin "$@"
     fi
 }
