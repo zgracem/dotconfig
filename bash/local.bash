a0e8fab031c9a867452eeb6e7678f9cc5f9030af
Z. Grace Moreau <zgracem@users.noreply.github.com>
2013-09-05 12:47:32 -0600

    decentralizing local file stuff
    or whatever

---
+++ bash/local.bash
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# ~zozo/.config/bash/local                         sources local bashrc, if any
+# say hello: printf "zozo\x40inescapable\x2eorg"
+# -----------------------------------------------------------------------------
+
+[[ -r $HOME/.local/bashrc.bash ]] &&
+    . $HOME/.local/bashrc.bash
