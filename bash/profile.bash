73e4646c443c47e23261454dcc9f16afb634f1e4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2013-10-15 11:22:25 -0600

    when did this get blanked???

--- bash/profile.bash
+++ bash/profile.bash
@@ -2,6 +2,56 @@
 # ~zozo/.config/bash/profile               executed by bash(1) for login shells
 # say hello: printf "zozo\x40inescapable\x2eorg"
 # -----------------------------------------------------------------------------
+
+# who am I?
+: ${USER:=$(id -un)}
+: ${LOGNAME:=$USER}
+
+# where am I?
+: ${HOSTNAME:=$(hostname)}
+
+# locale settings: Canadian English, UTF-8
+LANGUAGE="en_CA:en"
+LANG="$(locale -a | grep -i 'en_CA.utf')"
+LC_ALL="$LANG"
+TZ="America/Edmonton"
+
+# # if you trust your terminal/environment to set these sensibly...
+# : ${LANG:="$(locale -a | grep -i 'en_CA.utf')"}
+# : ${LANGUAGE:="en_CA:en"}
+# : ${LC_ALL:="$LANG"}
+# : ${TZ:="America/Edmonton"}
+
+export USER LOGNAME HOSTNAME LANG LANGUAGE LC_ALL TZ
+
+umask 0077              # default 'rwX------' permissions for new files
+
+# ------------------------------------------------------------------------------
+# shell options
+# ------------------------------------------------------------------------------
+
+set -o noclobber        # output redirection won't overwrite (override with >|filename)
+shopt -s dotglob        # include .dotfiles in filename expansion
+shopt -s extglob        # enable extended pattern matching
+shopt -s nocaseglob     # case-insensitive globbing (used in pathname expansion)
+shopt -s nocasematch    # case-insensitive pattern matching in `case` and `[[`
+
+# bash-4.0+ only
+if [[ ${BASH_VERSINFO[0]} -ge 4 ]]; then
+    shopt -s globstar   # '**' matches all directories and their files recursively
+fi
+
+# ------------------------------------------------------------------------------
+# other settings
+# ------------------------------------------------------------------------------
+
+export BLOCKSIZE=1024
+export COPYFILE_DISABLE=true     # exclude ._resourceforks from tarballs
+export COMP_TAR_INTERNAL_PATHS=1 # avoid flattening contents of tar files
+
+# export RSYNC_RSH="$(type -P ssh) -aq -oBatchmode=yes"
+
+# -----------------------------------------------------------------------------
 # source .bashrc
 # -----------------------------------------------------------------------------
 
