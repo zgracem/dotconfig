5db15f700472bebcf2687d511e54858674e22e68
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-02-25 14:08:07 -0700

    new function: songinfo()

--- bash/functions/misc.bash
+++ bash/functions/misc.bash
@@ -55,3 +55,21 @@ pdfcrack()
         unset file
     done
 }
+
+songinfo()
+{   # prints metadata for song files
+    declare song="$1"
+
+    case "$song" in
+        *.mp3)
+            id3v2 -l "$song"
+            ;;
+        *.m4[ap]|*.mp4|*.aac)
+            mp4info "$song"
+            ;;
+        *)
+            scold "$FUNCNAME" "$song: no metadata found"
+            return 1
+            ;;
+    esac
+}
