4ec62272a595b02631ed5bfc720cfd004bb3a14f
Zozo <zozo@inescapable.org>
2014-03-12 13:12:08 -0600

    removed unused functions

--- bash/functions/text.bash
+++ bash/functions/text.bash
@@ -40,12 +40,13 @@ _edit()
 clip()
 {   # copy a plain text file to the clipboard
     # adapted from http://brettterpstra.com/2013/01/15/clip-text-file-a-handy-dumb-service/
+
     declare textFile="$1"
     if file "$textFile" | grep -q text; then
         cat "$textFile" | pbcopy &&
             printf "Copied '%s' to clipboard\n" "$textFile"
     else
-        printf "%s: '%s' is not plain text\n" $FUNCNAME "$textFile" 1>&2
+        scold $FUNCNAME "'$textFile' is not plain text"
         return 1
     fi
 }
@@ -95,65 +96,3 @@ flip()
     rev
 }
 
-pluralize()
-{   # add trailing 's' where appropriate
-    declare count="$1" text="$2"
-
-    [[ $count -ne 1 ]] && text+='s'
-
-    printf "%'d %s\n" "$count" "$text"
-}
-
-# -----------------------------------------------------------------------------
-# Unicode
-# -----------------------------------------------------------------------------
-
-escape()
-{   # escape UTF-8 characters into their three-byte format
-    # https://github.com/mathiasbynens/dotfiles/blob/master/.functions
-
-    printf "\\\x%s" $(printf "$@" | xxd -p -c1 -u)
-
-    # print a newline if we're not piping to another program
-    [[ -t 1 ]] &&
-        echo
-}
-
-codepoint()
-{   # return the Unicode codepoint of a single character
-    declare char="$1" hexByte binByte codeBin codeHex
-
-    # get UTF-8 byte sequence
-    declare -a hexBytes=($(printf "$char" | xxd -p -c1 -u))
-
-    # convert byte sequence to binary
-    for hexByte in ${hexBytes[@]}; do
-        # zero-pad to 8 digits
-        printf -v binByte "%08d" $(hex2bin $hexByte)
-
-        # remove metadata in high bits
-        codeBin+="${binByte#*0}"
-    done
-
-    # convert to hexidecimal UTF-16 codepoint
-    printf -v codeHex "%04s" "$(bin2hex $codeBin)"
-
-    echo "U+$codeHex"
-}
-
-ugrep()
-{   # search Unicode character descriptions
-    # http://commandlinefu.com/commands/view/7535/
-
-    declare h d searchFile="$(locate -l1 CharName.pm)"
-
-    [[ -r $searchFile ]] || {
-        scold "$FUNCNAME" "CharName.pm not found"
-        return 69
-    }
-
-    egrep -i "^[0-9a-f]{4,} .*$*" "$searchFile" |
-    while read h d; do
-        /usr/bin/printf "\U$(printf "%08x" 0x$h)\tU+%s\t%s\n" $h "$d"
-    done
-}
