f9d952fd6a985a4ca0b09ecd97bd4ff13081c8f4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-03-12 13:06:23 -0600

    speed improvements

--- bash/paths.bash
+++ bash/paths.bash
@@ -3,6 +3,10 @@
 # say hello: printf "zozo\x40inescapable\x2eorg"
 # -----------------------------------------------------------------------------
 
+# only run if we need to
+[[ $SYSPATH ]] &&
+    return
+
 # get a reliable path prefix
 SYSPATH="$(command -p getconf PATH 2>/dev/null)"
 : ${SYSPATH:="/usr/bin:/bin:/usr/sbin:/sbin"}
@@ -18,55 +22,59 @@ INFOPATH=/usr/share/info
     INFOPATH=/usr/local/share/info:$INFOPATH
 }
 
-# Fink
-[[ -d /sw ]] && {
-    PATH=/sw/bin:/sw/sbin:$PATH
-    MANPATH=/sw/share/man:$MANPATH
-    INFOPATH=/sw/share/info:$INFOPATH
-}
+# # Fink
+# [[ -d /sw ]] && {
+#     PATH=/sw/bin:/sw/sbin:$PATH
+#     MANPATH=/sw/share/man:$MANPATH
+#     INFOPATH=/sw/share/info:$INFOPATH
+# }
 
-# MacPorts
-[[ -d /opt/local ]] && {
-    PATH=/opt/local/bin:/opt/local/sbin:$PATH
-    MANPATH=/opt/local/share/man:$MANPATH
-    INFOPATH=/opt/local/share/info:$INFOPATH
-}
+# # MacPorts
+# [[ -d /opt/local ]] && {
+#     PATH=/opt/local/bin:/opt/local/sbin:$PATH
+#     MANPATH=/opt/local/share/man:$MANPATH
+#     INFOPATH=/opt/local/share/info:$INFOPATH
+# }
 
-# GNU tools (from MacPorts)
-[[ -d /opt/local/libexec/gnubin ]] && {
-    PATH=/opt/local/libexec/gnubin:$PATH
-}
+# # GNU tools (from MacPorts)
+# [[ -d /opt/local/libexec/gnubin ]] && {
+#     PATH=/opt/local/libexec/gnubin:$PATH
+# }
 
 # GNU tools (from Homebrew)
 [[ -x /usr/local/bin/brew ]] && {
-    [[ -d $(brew --prefix coreutils)/libexec/gnubin ]] && {
-        PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
-        MANPATH="$(brew --prefix coreutils)/libexec/gnuman:$MANPATH"
+    core_prefix="$(brew --prefix coreutils)"
+    [[ -d $core_prefix/libexec/gnubin ]] && {
+        PATH="$core_prefix/libexec/gnubin:$PATH"
+        MANPATH="$core_prefix/libexec/gnuman:$MANPATH"
     }
 
-    [[ -d $(brew --prefix gnu-sed)/libexec/gnubin ]] && {
-        PATH="$(brew --prefix gnu-sed)/libexec/gnubin:$PATH"
-        MANPATH="$(brew --prefix gnu-sed)/libexec/gnuman:$MANPATH"
+    sed_prefix="$(brew --prefix gnu-sed)"
+    [[ -d $sed_prefix/libexec/gnubin ]] && {
+        PATH="$sed_prefix/libexec/gnubin:$PATH"
+        MANPATH="$sed_prefix/libexec/gnuman:$MANPATH"
     }
+
+    unset {core,sed}_prefix
 }
 
-# X11 apps
-[[ -d /usr/X11 ]] && {
-    PATH=$PATH:/usr/X11/bin
-    MANPATH=$MANPATH:/usr/X11/share/man
-}
+# # X11 apps
+# [[ -d /usr/X11 ]] && {
+#     PATH=$PATH:/usr/X11/bin
+#     MANPATH=$MANPATH:/usr/X11/share/man
+# }
 
-[[ -d /usr/X11R6 ]] && {
-    PATH=$PATH:/usr/X11R6/bin
-    MANPATH=$MANPATH:/usr/X11R6/share/man
-}
+# [[ -d /usr/X11R6 ]] && {
+#     PATH=$PATH:/usr/X11R6/bin
+#     MANPATH=$MANPATH:/usr/X11R6/share/man
+# }
 
-# gcc tools
-[[ -d /opt/gcc-tools/bin ]] && {
-    PATH=$PATH:/opt/gcc-tools/bin
-    MANPATH=$MANPATH:/opt/gcc-tools/epoch2/share/man
-    INFOPATH=$INFOPATH:/opt/gcc-tools/epoch2/share/info
-}
+# # gcc tools
+# [[ -d /opt/gcc-tools/bin ]] && {
+#     PATH=$PATH:/opt/gcc-tools/bin
+#     MANPATH=$MANPATH:/opt/gcc-tools/epoch2/share/man
+#     INFOPATH=$INFOPATH:/opt/gcc-tools/epoch2/share/info
+# }
 
 # ~/bin
 [[ -d $HOME/bin ]] && {
