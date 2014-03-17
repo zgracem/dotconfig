c4186ac2e0ba3210f83a5333029b2d2b4350497c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-03-17 12:14:38 -0600

    wrapper for lessc

--- bash/functions/misc.bash
+++ bash/functions/misc.bash
@@ -93,3 +93,15 @@ md5check()
 
     printf "%s %s" $checksum "$file" | md5sum -c
 }
+
+_inPath lessc && {
+    lessc()
+    {   # quick wrapper for LESS (http://lesscss.org/)
+        declare less_file css_file
+
+        for less_file in "$@"; do
+            css_file="${less_file%.less}.css"
+            command lessc "$less_file" >| "$css_file"
+        done
+    }
+}
