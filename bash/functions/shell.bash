ddc71df97a9082c8bc2fd7a10c35160eb83128e2
Zozo <zozo@inescapable.org>
2014-04-11 10:39:10 -0600

    steal better numfiles()

--- bash/functions/shell.bash
+++ bash/functions/shell.bash
@@ -113,6 +113,35 @@ pause()
 
 numfiles()
 {   # return the number of files in $PWD (or $1)
-    
-    command ls -1 "${1-$PWD}" | wc -l
+    # Based on: https://github.com/tejr/dotfiles/blob/master/bash/bashrc.d/cf.bash
+
+    declare dir="${1-$PWD}" dotglob nullglob
+    declare -a files
+
+    # check target directory
+    if [[ ! -e $dir ]]; then
+        scold "$FUNCNAME" "$dir: not found"
+        return 1
+    elif [[ ! -d $dir ]]; then
+        scold "$FUNCNAME" "$dir: not a directory"
+        return 1
+    elif [[ ! -r $dir ]]; then
+        scold "$FUNCNAME" "$dir: not readable"
+        return 1
+    fi
+
+    # capture glob settings
+    _shoptSet dotglob  && dotglob=true
+    _shoptSet nullglob && nullglob=true
+
+    # populate files array
+    shopt -s dotglob nullglob
+    files=("$dir"/*)
+
+    # reset options
+    [[ $dotglob == true ]]  || shopt -u dotglob
+    [[ $nullglob == true ]] || shopt -u nullglob
+
+    # print result
+    printf '%d\t%s\n' "${#files[@]}" "$dir"
 }
