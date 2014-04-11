677e32b2f43a891288e9521bfc1d9a3b34acecce
Zozo <zozo@inescapable.org>
2014-04-11 16:26:51 -0600

    clean up and improve path finding & setting

--- bash/paths.bash
+++ bash/paths.bash
@@ -16,74 +16,67 @@ MANPATH=/usr/share/man:/usr/man
 INFOPATH=/usr/share/info
 
 # /usr/local
-[[ -d /usr/local ]] && {
+if [[ -d /usr/local ]]; then
     PATH=/usr/local/bin:/usr/local/sbin:$PATH
     MANPATH=/usr/local/share/man:$MANPATH
     INFOPATH=/usr/local/share/info:$INFOPATH
-}
-
-# # Fink
-# [[ -d /sw ]] && {
-#     PATH=/sw/bin:/sw/sbin:$PATH
-#     MANPATH=/sw/share/man:$MANPATH
-#     INFOPATH=/sw/share/info:$INFOPATH
-# }
-
-# # MacPorts
-# [[ -d /opt/local ]] && {
-#     PATH=/opt/local/bin:/opt/local/sbin:$PATH
-#     MANPATH=/opt/local/share/man:$MANPATH
-#     INFOPATH=/opt/local/share/info:$INFOPATH
-# }
-
-# # GNU tools (from MacPorts)
-# [[ -d /opt/local/libexec/gnubin ]] && {
-#     PATH=/opt/local/libexec/gnubin:$PATH
-# }
+fi
 
 # GNU tools (from Homebrew)
-[[ -x /usr/local/bin/brew ]] && {
+if [[ -x /usr/local/bin/brew ]]; then
     core_prefix="$(brew --prefix coreutils)"
-    [[ -d $core_prefix/libexec/gnubin ]] && {
-        PATH="$core_prefix/libexec/gnubin:$PATH"
-        MANPATH="$core_prefix/libexec/gnuman:$MANPATH"
-    }
-
     sed_prefix="$(brew --prefix gnu-sed)"
-    [[ -d $sed_prefix/libexec/gnubin ]] && {
-        PATH="$sed_prefix/libexec/gnubin:$PATH"
-        MANPATH="$sed_prefix/libexec/gnuman:$MANPATH"
-    }
+    
+    if [[ -d $core_prefix ]]; then
+        PATH=$core_prefix/libexec/gnubin:$PATH
+        MANPATH=$core_prefix/libexec/gnuman:$MANPATH
+        INFOPATH=$core_prefix/share/info:$INFOPATH
+    fi
+
+    if [[ -d $sed_prefix ]]; then
+        PATH=$sed_prefix/libexec/gnubin:$PATH
+        MANPATH=$sed_prefix/libexec/gnuman:$MANPATH
+        INFOPATH=$sed_prefix/share/info:$INFOPATH
+    fi
 
     unset {core,sed}_prefix
-}
+fi
 
 # # X11 apps
-# [[ -d /usr/X11 ]] && {
+# if [[ -d /usr/X11 ]]; then
 #     PATH=$PATH:/usr/X11/bin
 #     MANPATH=$MANPATH:/usr/X11/share/man
-# }
+# fi
 
-# [[ -d /usr/X11R6 ]] && {
+# if [[ -d /usr/X11R6 ]]; then
 #     PATH=$PATH:/usr/X11R6/bin
 #     MANPATH=$MANPATH:/usr/X11R6/share/man
-# }
+# fi
 
-# # gcc tools
-# [[ -d /opt/gcc-tools/bin ]] && {
-#     PATH=$PATH:/opt/gcc-tools/bin
-#     MANPATH=$MANPATH:/opt/gcc-tools/epoch2/share/man
-#     INFOPATH=$INFOPATH:/opt/gcc-tools/epoch2/share/info
-# }
+# gcc tools (cygwin)
+if [[ -d /opt/gcc-tools/bin ]]; then
+    PATH=$PATH:/opt/gcc-tools/bin
+    MANPATH=$MANPATH:/opt/gcc-tools/epoch2/share/man
+    INFOPATH=$INFOPATH:/opt/gcc-tools/epoch2/share/info
+fi
+
+# OpenSSL
+if [[ -d /usr/ssl/man ]]; then
+    MANPATH=$MANPATH:/usr/ssl/man
+fi
 
 # ~/bin
-[[ -d $HOME/bin ]] && {
+if [[ -d $HOME/bin ]]; then
     PATH=$HOME/bin:$PATH
-}
+fi
 
-[[ -d $HOME/share/man ]] && {
+if [[ -d $HOME/share/man ]]; then
     MANPATH=$HOME/share/man:$MANPATH
-}
+fi
+
+if [[ -d $HOME/share/info ]]; then
+    INFOPATH=$HOME/share/info:$INFOPATH
+fi
 
 # remove duplicates in $PATH while preserving order
 # https://sites.google.com/site/jdisnard/etc-skel/dot-profile
@@ -94,8 +87,8 @@ INFOPATH=/usr/share/info
 CDPATH=.:$HOME      # also look in ~ when cd'ing
 
 # Python
-[[ -d /usr/local/lib/python2.7/site-packages ]] && {
+if [[ -d /usr/local/lib/python2.7/site-packages ]]; then
     PYTHONPATH=/usr/local/lib/python2.7/site-packages
-}
+fi
 
 export {,CD,MAN,INFO,PYTHON}PATH
