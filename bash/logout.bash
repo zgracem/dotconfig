ae2c120c88da963f3eb7b5170a975dcfb07a945c
Zozo <zozo@inescapable.org>
2014-05-20 11:56:52 -0600

    stop SSH & GPG agents on logout

--- bash/logout.bash
+++ bash/logout.bash
@@ -4,11 +4,40 @@
 # -----------------------------------------------------------------------------
 
 # revoke sudo privileges
-[[ $(who | grep -ch "^$USER\>") -le 1 ]] && {
+if [[ $(who | grep -ch "^$USER\>") -le 2 ]]; then
     _inPath sudo && sudo -k
-}
+fi
+
+# stop ssh-agent if it was started by this shell
+if [[ -n $SSH_AGENT_PID && $$ -eq $SSH_AGENT_PPID ]]; then
+    # delete all identities from the agent
+    ssh-add -D &>/dev/null
+
+    # terminate the agent process
+    builtin kill $SSH_AGENT_PID && {
+        # clean up
+        if [[ -d ${SSH_AUTH_SOCK%/*} ]]; then
+            command rm -rf "${SSH_AUTH_SOCK%/*}" &>/dev/null
+        fi
+
+        unset SSH_AUTH_SOCK SSH_AGENT_PID SSH_AGENT_PPID
+    }
+fi
+
+# ditto for gpg-agent
+if _gpg_agent_is_running && [[ $$ -eq $GPG_AGENT_PPID ]]; then
+    # terminate the agent process
+    builtin kill $GPG_AGENT_PID && {
+        # clean up
+        if [[ -d ${GPG_AUTH_SOCK%/*} ]]; then
+            command rm -rf "${GPG_AUTH_SOCK%/*}" &>/dev/null
+        fi
+
+        unset GPG_AUTH_SOCK GPG_AGENT_PID GPG_AGENT_PPID GPG_TTY
+    }
+fi
 
 # # clear screen
-# [[ $SHLVL -eq 1 ]] && {
+# if [[ $SHLVL -eq 1 ]]; then
 #     _inPath clear && clear
-# }
+# fi
