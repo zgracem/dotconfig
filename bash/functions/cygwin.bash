519769c9c649daad5eadecb20e0809002eff95fd
Zozo <zozo@inescapable.org>
2014-05-22 10:02:45 -0600

    new function: whatpkg()

--- bash/functions/cygwin.bash
+++ bash/functions/cygwin.bash
@@ -40,3 +40,24 @@ killall()
 {   # kill a process by name
     cygkill $(pidof $1)
 }
+
+whatpkg()
+{   # find the Cygwin package to which $1 belongs
+    
+    declare file="$1" package error
+
+    if [[ ! -f $file ]]; then
+        file="$(getPath "$file")" || {
+            scold "$1: not found"
+            return 1
+        }
+    fi
+
+    package="$(cygcheck --find-package "$file")"
+
+    if [[ -n $package ]]; then
+        echo "$package"
+    else
+        scold "$file: does not seem to belong to a Cygwin package"
+    fi
+}
