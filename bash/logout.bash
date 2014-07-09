7d71c037ecaa3d46d8d291747383c27a849bf64e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-07-09 16:31:45 -0600

    cleanup

--- bash/logout.bash
+++ bash/logout.bash
@@ -8,35 +8,6 @@ if [[ $(who | grep -ch "^$USER\>") -le 2 ]]; then
     _inPath sudo && sudo -k
 fi
 
-# stop ssh-agent if it was started by this shell
-if [[ -n $SSH_AGENT_PID && $$ -eq $SSH_AGENT_PPID ]]; then
-    # delete all identities from the agent
-    quiet ssh-add -D
-
-    # terminate the agent process
-    quiet builtin kill $SSH_AGENT_PID && {
-        # clean up
-        if [[ -d ${SSH_AUTH_SOCK%/*} ]]; then
-            quiet command rm -rf "${SSH_AUTH_SOCK%/*}"
-        fi
-
-        unset SSH_AUTH_SOCK SSH_AGENT_PID SSH_AGENT_PPID
-    }
-fi
-
-# ditto for gpg-agent
-if [[ -n $GPG_AGENT_PID && $$ -eq $GPG_AGENT_PPID ]]; then
-    # terminate the agent process
-    quiet builtin kill $GPG_AGENT_PID && {
-        # clean up
-        if [[ -d ${GPG_AUTH_SOCK%/*} ]]; then
-            quiet command rm -rf "${GPG_AUTH_SOCK%/*}"
-        fi
-
-        unset GPG_AUTH_SOCK GPG_AGENT_PID GPG_AGENT_PPID GPG_TTY
-    }
-fi
-
 # archive ~/.bash_history if it's larger than 256 KB
 if _isGNU stat; then
     flags='-c %s'
@@ -44,15 +15,9 @@ else
     flags='-f %z'
 fi
 
-history_size=$(command stat ${flags} "$HISTFILE" 2>/dev/null)
-
-if [[ $history_size -ge 262144 ]]; then
+if [[ $(command stat ${flags} "$HISTFILE" 2>/dev/null) -ge 262144 ]]; then
     mv "$HISTFILE" "${HISTFILE}_$(date +%y%m%d)"
+    touch "$HISTFILE"
 fi
 
-unset flags history_size
-
-# # clear screen
-# if [[ $SHLVL -eq 1 ]]; then
-#     _inPath clear && clear
-# fi
+unset flags
