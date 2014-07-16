7e0fb0bf7e6510830a440968781043143e018c1f
Zozo <zozo@inescapable.org>
2014-07-16 16:06:05 -0600

    clean up syntax

--- bash/functions/text.bash
+++ bash/functions/text.bash
@@ -5,46 +5,51 @@
 
 _edit()
 {   # open a file in the appropriate editor
-    # Usage: _edit FILE[@LINE]
+    # Usage: _edit FILE[:LINE]
 
-    declare file="$1" lineno_regex='@([[:digit:]]+)$' line
+    declare line file="$1"
+    declare -r lineno_regex=':([[:digit:]]+)$'
 
     # get line number (if specified)
-    [[ $file =~ $lineno_regex ]] && {
+    if [[ $file =~ $lineno_regex ]]; then
         line="${BASH_REMATCH[1]}"
-        file="${file%@*}"
-    }
+        file="${file%:*}"
+    fi
 
-    [[ $SSH_CONNECTION ]] && {
+    if [[ -n $SSH_CONNECTION ]]; then
         # working remotely; use a console editor
         declare windowTitle="$(basename "$EDITOR")"
 
-        [[ $EDITOR =~ vim && $line ]] && {
-            newwin --title "$windowTitle" "$EDITOR" +$line "$file"
-        } || {
+        if [[ $EDITOR =~ vim && -n $line ]]; then
             # see functions/newwin.bash
+            newwin --title "$windowTitle" "$EDITOR" +$line "$file"
+        else
             newwin --title "$windowTitle" "$EDITOR" "$file"
-        }
+        fi
 
         return
-    } || {
+    else
         # use a GUI editor
 
-        [[ $VISUAL =~ subl && $line ]] && 
+        if [[ $VISUAL =~ subl && $line ]]; then
             file="$file:$line" # for Sublime Text
+        fi
 
         "$VISUAL" "$file"
-    }
+    fi
 }
+
+export -f _edit
 
 clip()
 {   # copy a plain text file to the clipboard
     # adapted from http://brettterpstra.com/2013/01/15/clip-text-file-a-handy-dumb-service/
 
     declare textFile="$1"
+
     if file "$textFile" | grep -q text; then
-        cat "$textFile" | pbcopy &&
-            printf "Copied '%s' to clipboard\n" "$textFile"
+        cat "$textFile" | pbcopy \
+        && printf "Copied '%s' to clipboard\n" "$textFile"
     else
         scold $FUNCNAME "'$textFile' is not plain text"
         return 1
@@ -53,20 +58,22 @@ clip()
 
 pastenote()
 {   # paste the contents of the clipboard to a text file
-    [[ -d $dir_notes ]] || {
+    
+    if [[ ! -d $dir_notes ]]; then
         scold $FUNCNAME "can't find notes directory"
         return 1
-    }
+    fi
 
     declare fileName="${1:-"Note $(date '+%F at %H.%M.%S')"}"
     declare filePath="$dir_notes/${fileName%.txt}.txt"
 
-    [[ -f $filePath ]] && {
+    if [[ -f $filePath ]]; then
         scold $FUNCNAME "$fileName.txt: already exists"
         return 1
-    }
+    fi
 
-    pbpaste > "$filePath" && echo "$filePath"
+    pbpaste > "$filePath" \
+    && echo "$filePath"
 }
 
 rot13()
@@ -74,25 +81,21 @@ rot13()
     declare mask='a-zA-Z n-za-mN-ZA-M'
 
     # file
-    [[ -f $1 ]] && {
+    if [[ -f $1 ]]; then
+        # file
         tr $mask < "$1"
-        return
-    }
-
-    # string
-    [[ -t 0 ]] && {
+    elif [[ -t 0 ]]; then
+        # string
         echo "$1" | tr $mask
-        return
-    }
-
-    # standard input
-    tr $mask
+    else
+        # standard input
+        tr $mask
+    fi
 }
 
 flip()
 {
-    echo "$@" |
-    sed "y/abcdefghijklmnopqrstuvwxyz,'?!./ɐqɔpǝɟƃɥıɾʞlɯuodbɹsʇnΛmxλz',¿¡˙/" |
-    rev
+    echo "$@" \
+    | sed "y/abcdefghijklmnopqrstuvwxyz,'?!./ɐqɔpǝɟƃɥıɾʞlɯuodbɹsʇnΛmxλz',¿¡˙/" \
+    | rev
 }
-
