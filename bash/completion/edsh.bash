12888473d8fd6ca39d8e057487e1c9ce382a1f1d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-07-16 11:04:17 -0600

    minor completion tweaks

--- bash/completion/edsh.bash
+++ bash/completion/edsh.bash
@@ -6,10 +6,9 @@
 __complete_edsh()
 {
     declare cur=${COMP_WORDS[COMP_CWORD]}
-    declare scripts=( $(find -H "$dir_scripts" -maxdepth 2 -type f -name '*.sh' | sed -nE 's#^.*/(.*)\.sh$#\1#p') )
+    declare scripts=( $(find -H "$dir_scripts" -maxdepth 2 -type f -name '*.sh' -printf '%f\n') )
 
-    COMPREPLY=( $(compgen -W "${scripts[*]}" -- $cur) )
+    COMPREPLY=( $(compgen -W "${scripts[*]//.sh/}" -- $cur) )
 }
 
 complete -F __complete_edsh edsh
-
