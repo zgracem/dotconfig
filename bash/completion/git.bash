1afd7622226e22a59b76d227fb7269960fa04a81
Zozo <zozo@inescapable.org>
2014-07-16 11:09:29 -0600

    add completion for 'gd' git alias

---
+++ bash/completion/git.bash
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# ~zozo/.config/bash/completion/git.bash
+# For various git-related commands
+# -----------------------------------------------------------------------------
+
+# alias for 'git difftool'
+__complete_gd()
+{
+    declare cur=${COMP_WORDS[COMP_CWORD]}
+    declare files=( $(git status 2>/dev/null \
+    	| sed -nE 's/^#\s+modified:\s+(.+)$/\1/p') )
+
+    COMPREPLY=( $(compgen -W "${files[*]}" -- $cur) )
+}
+
+complete -F __complete_gd gd
