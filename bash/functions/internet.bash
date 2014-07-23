163b9badc64fda79f4f2613f500098771ff365d6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-07-23 10:47:57 -0600

    new function: safe_email()

--- bash/functions/internet.bash
+++ bash/functions/internet.bash
@@ -119,3 +119,46 @@ apachelogs()
         done
     }
 }
+
+safe_email()
+{
+    declare input="$1" url string
+    declare i inchar outchar
+
+    # url-encode email address
+    for (( i = 0; i < ${#input}; i++ )); do
+        inchar="${input:$i:1}"
+
+        case $inchar in
+            [a-zA-Z0-9])
+                outchar="$inchar"
+                ;;
+            *)
+                printf -v outchar "%%%02x" "'${inchar}"
+                ;;
+        esac
+
+        url+="$outchar"
+    done
+
+    # HTML-entity-ify same
+    for (( i = 0; i < ${#input}; i++ )); do
+        inchar="${input:$i:1}"
+
+        case $inchar in
+            [a-zA-Z0-9])
+                outchar="$inchar"
+                ;;
+            *)
+                printf -v outchar "&#x%02x;" "'${inchar}"
+                ;;
+        esac
+
+        string+="$outchar"
+    done
+
+    # echo "<a href=\"mailto:${url}\">${string}</a>"
+
+    printf '<a href="%s">%s</a>' "$url" "$string"
+    newline
+}
