60de15ed323540a93e11217fcf3821f48186ef0d
Zozo <zozo@inescapable.org>
2014-08-07 09:46:16 -0600

    unset MAILCHECK if no mail directory

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -124,6 +124,8 @@ if [[ -r $MAIL ]]; then
     shopt -s mailwarn       # alert on new mail
     MAILCHECK=300           # check mail every 5 minutes
     MAILPATH=$MAIL?'New mail in $_'
+else
+    unset MAILCHECK
 fi
 
 # -----------------------------------------------------------------------------
