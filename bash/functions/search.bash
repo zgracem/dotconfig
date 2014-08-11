e5ed7e71a100555530edd5d0c99e702ce45ec91b
Zozo <zozo@inescapable.org>
2014-08-11 13:40:07 -0600

    new function: _find_daysold()

--- bash/functions/search.bash
+++ bash/functions/search.bash
@@ -30,3 +30,34 @@ _inPath mdfind && {
         grep -i "$term"
     }
 }
+
+_find_daysold()
+{   # list all files under $PWD changed in the last $1 days
+    
+    declare days="$1" find_bin
+
+    if ! _isNumber "$days"; then
+        scold "Error: $days: not a number"
+        return 64
+    elif ! find_bin=$(getGNU find); then
+        scold "Error: GNU find(1) required"
+        return 69
+    fi
+
+    if [[ $days -gt 0 ]]; then
+        days="-${days}"
+    fi
+
+    "$find_bin" "$PWD" \
+        -maxdepth 1 \
+        -type f \
+        -daystart \
+        -mtime "$days" \
+        -print
+}
+
+# list all files under $PWD changed today
+today() { _find_daysold 0; }
+
+# list all files under $PWD changed this week
+thisweek() { _find_daysold -7; }
