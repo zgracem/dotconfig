02a1b78eb00ce3f53fb4c4acfb3dcacb22502abf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-09-05 17:29:04 -0600

    new function: quit()

--- bash/functions/osx.bash
+++ bash/functions/osx.bash
@@ -141,3 +141,16 @@ whereami()
         echo "$address"
     fi
 }
+
+quit()
+{   # "gently" quit an application
+    
+    [[ $# -ge 1 ]] || return 64
+
+    local -a apps=("$@")
+    local app
+
+    for app in "${apps[@]}"; do
+        osascript -e "quit app '${cmd}'"
+    done
+}
