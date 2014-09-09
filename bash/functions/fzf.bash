4be1274c8ed79cdd5f41ff19cccce2f46cd123d5
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-09-09 12:55:48 -0600

    add fzf functions

---
+++ bash/functions/fzf.bash
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+# ~zozo/.config/bash/functions/fzf.bash
+# ------------------------------------------------------------------------------
+
+_isFunction fzf || return 69
+
+fd()
+{   # cd to selected directory
+
+    local dir
+    local search="$1"
+
+    dir=$(find -type d -iname "*${search}*" -printf '%P\n' 2>/dev/null \
+        | fzf +m)
+
+    cd "$dir"
+}
+
+fkill()
+{   # find, then kill, a process
+
+    local sigspec="${1:-9}"
+    local ps_flags='ef'
+    local kill_cmd='builtin kill'
+
+    if [[ $OSTYPE == cygwin ]]; then
+        kill_cmd='/bin/kill --signal $sigspec'
+        ps_flags+='W' # include Windows processes 
+    fi
+    
+    command ps -ef \
+    | sed 1d \
+    | fzf -m \
+    | awk '{print $2}' \
+    | xargs kill -$sigspec
+}
