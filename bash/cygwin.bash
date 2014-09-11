0e2b5fbfa2060b75718559d89aa979f86a49a08c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2014-09-11 11:36:45 -0600

    keep Windows's %PATH%, if available

--- bash/cygwin.bash
+++ bash/cygwin.bash
@@ -4,13 +4,18 @@
 # -----------------------------------------------------------------------------
 
 if [[ ! $OSTYPE =~ cygwin ]]; then
-    scold "${BASH_SOURCE[0]}" "cannot source on this OS"
+    scold "${BASH_SOURCE[0]}" 'cannot source on this OS'
     return 1
 fi
 
 # -----------------------------------------------------------------------------
 # shell variables
 # ------------------------------------------------------------------------------
+
+# add Windows's %PATH% if available
+if [[ -n $ORIGINAL_PATH ]]; then
+	PATH=$PATH:$ORIGINAL_PATH
+fi
 
 # don't warn on using MS-DOS-style paths instead of POSIX-style
 if [[ ! $CYGWIN =~ dosfilewarning ]]; then
@@ -23,14 +28,14 @@ if [[ ! $CYGWIN =~ winsymlinks ]]; then
 fi
 
 # do not consider DLLs executable for completion purposes
-export EXECIGNORE="*.dll"
+export EXECIGNORE='*.dll'
 
 # use the short name of programs when Tab-completing
 shopt -s completion_strip_exe
 
 # define default printer
 
-pkey="/proc/registry/HKEY_CURRENT_USER/Software/Microsoft/Windows NT/CurrentVersion/Windows/Device"
+pkey='/proc/registry/HKEY_CURRENT_USER/Software/Microsoft/Windows NT/CurrentVersion/Windows/Device'
 
 if [[ -z $PRINTER && -e $pkey ]]; then
     read -r PRINTER < "$pkey"
@@ -44,9 +49,9 @@ unset pkey
 # -----------------------------------------------------------------------------
 
 alias cpcd='cygpath -aw "$PWD" > /dev/clipboard'
-alias eject="$SYSTEMROOT/system32/rundll32 shell32.dll,Control_RunDLL hotplug.dll"
+alias eject="${SYSTEMROOT}/system32/rundll32 shell32.dll,Control_RunDLL hotplug.dll"
 
-alias tar="/usr/bin/bsdtar"
+alias tar='/usr/bin/bsdtar'
 
 # be like OS X
 alias open='cygstart'
