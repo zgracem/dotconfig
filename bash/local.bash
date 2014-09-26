a426033d42b207c9ea6a6ee6c9ddb0cf90c46fc1
Zozo <zozo@inescapable.org>
2014-09-26 11:35:15 -0600

    restore local.bash

---
+++ bash/local.bash
@@ -0,0 +1,6 @@
+# -----------------------------------------------------------------------------
+# ~zozo/.config/bash/local                         sources local bashrc, if any
+# say hello: printf "zozo\x40inescapable\x2eorg"
+# -----------------------------------------------------------------------------
+
+_source "${HOME}/.local/local.bash"
