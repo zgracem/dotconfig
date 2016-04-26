df30aa85e108a1622afcb6b4216ef434762cc45d
Grace Moreau <zgm@inescapable.org>
2016-04-26 08:26:01 -0600

    change proxy password

--- local/WS144966/bashrc.d/proxy.bash
+++ local/WS144966/bashrc.d/proxy.bash
@@ -4,7 +4,7 @@ export SOCKS5_SERVER='127.0.0.1:8080'
 export ALL_PROXY="socks5h://${SOCKS5_SERVER}"
 export GIT_PROXY_COMMAND="${dir_scripts}/git-proxy.sh"
 
-export http_proxy="http://uxhj:violet0224_Delta@proxy:8080/"
+export http_proxy="http://uxhj:violet0419_Delta@proxy:8080/"
 export https_proxy="$http_proxy"
 export HTTP_PROXY="$http_proxy"
 export HTTPS_PROXY="$http_proxy"
