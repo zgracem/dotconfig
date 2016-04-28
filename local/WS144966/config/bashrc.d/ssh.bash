f77443a0504223516d465740d89e1f1d4505fa08
Grace Moreau <zgm@inescapable.org>
2016-04-28 13:10:27 -0600

    add Athena aliases

--- local/WS144966/config/bashrc.d/ssh.bash
+++ local/WS144966/config/bashrc.d/ssh.bash
@@ -1,6 +1,8 @@
 # SSH aliases to lowercase
 
-alias m='newwin --title minerva ssh minerva'
+alias a='newwin  --title athena ssh athena'
+alias aa='newwin --title athena ssh athena.remote'
+alias m='newwin  --title minerva ssh minerva'
 alias mr='newwin --title minerva ssh minerva.remote'
 alias er='newwin --title erato ssh erato'
 alias wf='newwin --title webfaction ssh webfaction'
