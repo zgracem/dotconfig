fc76b34856a4e394601ab9715ae62628e69801fd
Grace Moreau <zgm@inescapable.org>
2016-04-28 12:07:07 -0600

    tt() causes tmux to segfault and core dump w/out existing session

--- bash/bashrc.d/tmux.bash
+++ bash/bashrc.d/tmux.bash
@@ -1,6 +1,14 @@
 _inPath tmux || return
 
 tt()
-{ # attach to an existing tmux session or create a new one
-  tmux attach-session 2>&- || tmux new-session
+{ 
+  # All we want to do is attach to an existing session (if any), and otherwise
+  # create one. Unfortunately, simply calling `tmux attach || tmux new` causes
+  # a segfault and core dump if there's no existing session.
+
+  local session=${1:-main}
+
+  tmux new-session -A -s "$session"
+  #                 │  └─ session name
+  #                 └──── attach if session already exists
 }
