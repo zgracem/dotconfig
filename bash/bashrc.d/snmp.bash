20e9172ecfafbe740de2468a6778d29b3b26625f
Grace Moreau <zgm@inescapable.org>
2016-04-28 13:09:27 -0600

    tabsize=2

--- bash/bashrc.d/snmp.bash
+++ bash/bashrc.d/snmp.bash
@@ -1,18 +1,18 @@
 # SNMP access
 
 if [[ $OSTYPE =~ darwin ]]; then
-    SNMP_HOST="Oracle.local"
-    SNMP_MIB="AIRPORT-BASESTATION-3-MIB"
-    SNMP_COMMUNITY="sibyl"
-    export ${!SNMP_*}
+  SNMP_HOST="Oracle.local"
+  SNMP_MIB="AIRPORT-BASESTATION-3-MIB"
+  SNMP_COMMUNITY="sibyl"
+  export ${!SNMP_*}
 else
 	return 0
 fi
 
 airportusers()
-{   # return number of users connected to AirPort base station
+{ # return number of users connected to AirPort base station
 
-    # -Ovq removes the equal sign, type info and OID from the output
-    snmpget -v 2c -c "$SNMP_COMMUNITY" -Oqv "$SNMP_HOST" \
-        "$SNMP_MIB::wirelessNumber.0"
+  # -Ovq suppresses the equal sign, type info, and OID
+  snmpget -v 2c -c "$SNMP_COMMUNITY" -Oqv "$SNMP_HOST" \
+    "$SNMP_MIB::wirelessNumber.0"
 }
--- bash/bashrc.d/youtube-dl.bash
+++ bash/bashrc.d/youtube-dl.bash
@@ -4,11 +4,11 @@
 _inPath youtube-dl || return
 
 if [[ ! -d $HOME/var/cache/youtube-dl ]]; then
-    mkdir -p "$HOME/var/cache/youtube-dl"
+  mkdir -p "$HOME/var/cache/youtube-dl"
 fi
 
 ydl()
 {
-    local output_fmt="%(title)s.%(ext)s"
-    youtube-dl -o "$dir_downloads/$output_fmt" "$@"
+  local output_fmt="%(title)s.%(ext)s"
+  youtube-dl -o "$dir_downloads/$output_fmt" "$@"
 }
