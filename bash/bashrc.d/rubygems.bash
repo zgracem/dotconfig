32f822eefe95afb87f7760d69c70385075e428d6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-05-02 18:05:33 +0100

    don't add gems to PATH if gem bin directory doesn't exist

--- bash/bashrc.d/rubygems.bash
+++ bash/bashrc.d/rubygems.bash
@@ -1,6 +1,6 @@
 if type -P gem &>/dev/null; then
     GEM_HOME=$(gem env home 2>/dev/null) || return
-    if [[ ! $PATH =~ $GEM_HOME/bin ]]; then
+    if [[ -d $GEM_HOME/bin ]] && [[ ! :$PATH: =~ :$GEM_HOME/bin: ]]; then
         PATH=$PATH:"$GEM_HOME/bin"
     fi
     unset -v GEM_HOME
