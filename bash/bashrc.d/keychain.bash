6c6263b6c00b27ca5e9c1b5d88a31791714ef1cd
Grace Moreau <zgm@inescapable.org>
2016-05-25 13:59:06 -0600

    use ~/var/run instead of ~/.keychain

--- bash/bashrc.d/keychain.bash
+++ bash/bashrc.d/keychain.bash
@@ -3,9 +3,20 @@ _inPath keychain || return
 export GPG_TTY="$(tty)"
 
 if [[ -z $SSH_AGENT_PID ]]; then
+  if [[ ! -d ~/var/run ]]; then
+    mkdir -p ~/var/run 1>/dev/null || return
+  fi
+
   printf "\r\e[K"
-  eval $(keychain --dir "${HOME}/.keychain" \
-       --eval --ignore-missing --inherit any --quick --quiet \
-       id_rsa) \
-  && printf "\eM\e[K"
+
+  if keychain_env=$(keychain --eval --dir ~/var/run --quick --quiet \
+                    --ignore-missing --inherit any id_rsa); then
+    eval "$keychain_env"
+    printf "\eM\e[K"
+  fi
 fi
+
+unset -v keychain_env
+
+# TODO: remove later
+[[ -d ~/var/run/.keychain ]] && rm -rfv ~/.keychain
