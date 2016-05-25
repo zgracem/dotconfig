dddb520fa3c476a3907460050da8a263b6710bf3
Grace Moreau <zgm@inescapable.org>
2016-05-25 10:40:50 -0600

    avoid slow call to gem(1) if possible

--- bash/bashrc.d/rubygems.bash
+++ bash/bashrc.d/rubygems.bash
@@ -1,7 +1,22 @@
 if type -P gem &>/dev/null; then
-    GEM_HOME=$(gem env home 2>/dev/null) || return
-    if [[ -d $GEM_HOME/bin ]] && [[ ! :$PATH: =~ :$GEM_HOME/bin: ]]; then
-        PATH=$PATH:"$GEM_HOME/bin"
-    fi
-    unset -v GEM_HOME
+  case $HOSTNAME in
+    Athena|*webfaction*)
+      GEM_HOME=$HOME/.rbenv/versions/2.2.3/lib/ruby/gems/2.2.0
+      ;;
+    WS144966*)
+      GEM_HOME=$HOME/.gem/ruby
+      ;;
+    Hiroko)
+      GEM_HOME=/usr/local/lib/ruby/gems/2.2.0
+      ;;
+    *)
+      GEM_HOME=$(gem env home 2>/dev/null) || return
+      ;;
+  esac
+
+  if [[ -d $GEM_HOME/bin ]] && [[ ! :$PATH: =~ :$GEM_HOME/bin: ]]; then
+    PATH=$PATH:"$GEM_HOME/bin"
+  fi
+
+  unset -v GEM_HOME
 fi
