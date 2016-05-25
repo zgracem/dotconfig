7b371ce547314646fe2db822fa93dc4ef76b97a4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-05-25 09:43:13 -0600

    create separate clear_scrollback() function

--- bash/bashrc.d/cd.bash
+++ bash/bashrc.d/cd.bash
@@ -1,44 +1,27 @@
 # do not create function in Midnight Commander
 if [[ -z $MC_SID ]]; then
-    cd()
-    {
-        builtin pushd "${@:-$HOME}" 1>/dev/null
-    }
+  cd()
+  {
+    builtin pushd "${@:-$HOME}" 1>/dev/null
+  }
 
-    alias --  -='pushd +1 1>/dev/null'  # -  = go back 1 dir
-    alias -- --='pushd -0 1>/dev/null'  # -- = go forward 1 dir
+  alias --  -='pushd +1 1>/dev/null'  # -  = go back 1 dir
+  alias -- --='pushd -0 1>/dev/null'  # -- = go forward 1 dir
 fi
 
 cdls()
 {   # change to, and immediately list, a directory
-    cd "$@" && ls
+  cd "$@" && ls
 }
 
 cdll()
 {   # change to, and immediately list (at length), a directory
-    cd "$@" && ll
+  cd "$@" && ll
 }
 
 ccd()
-{   # reset everything
-    builtin cd      # move to $HOME
-    builtin dirs -c # clear directory stack
-    clear           # clear screen
-
-    # clear scrollback in multiplexer
-    if _inScreen; then
-        screen -S "$STY" -X eval "scrollback 0" "scrollback 4096"
-    elif _inTmux; then
-        tmux clear-history
-    fi
-
-    # clear scrollback in terminal
-    case $TERM_PROGRAM in
-        PuTTY|Apple_Terminal)
-            printf "%b" "${CSI}3J"
-            ;;
-        iTerm*)
-            printf "%b" "${OSC}50;ClearScrollback${BEL}"
-            ;;
-    esac
+{ # reset everything
+  builtin cd        # move to $HOME
+  builtin dirs -c   # clear directory stack
+  clear_scrollback  # clear screen & scrollback
 }
