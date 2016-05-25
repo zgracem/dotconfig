1969228f92e15df0c74407c76c5e08d6ac76f03a
Grace Moreau <zgm@inescapable.org>
2016-05-25 11:03:47 -0600

    new function rebash(): start a fresh environment

---
+++ bash/functions.d/rebash.bash
@@ -0,0 +1,15 @@
+rebash()
+{ # start a fresh environment
+
+  clear_scrollback
+
+  exec -c env - \
+    HOME="$HOME" TERM="$TERM" \
+    ${TERM_PROGRAM:+TERM_PROGRAM="$TERM_PROGRAM"} \
+    ${SSH_CLIENT:+SSH_CLIENT="$SSH_CLIENT"} \
+    ${SSH_CONNECTION:+SSH_CONNECTION="$SSH_CONNECTION"} \
+    ${SSH_TTY:+SSH_TTY="$SSH_TTY"} \
+    ${USERPROFILE:+USERPROFILE="$USERPROFILE"} \
+    ${Z_SOLARIZED:+Z_SOLARIZED="$Z_SOLARIZED"} \
+    "$SHELL" -l
+}
