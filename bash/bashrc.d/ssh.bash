455fb08151fb7a2fe01ab9c347f54d727d592374
Grace Moreau <zgm@inescapable.org>
2016-06-02 15:43:50 +0100

    move ssh shortcuts to their own file & automate lowercasing for cygwin

---
+++ bash/bashrc.d/ssh.bash
@@ -0,0 +1,33 @@
+# ssh shortcuts & setup
+
+alias  a='newwin --title Athena ssh Athena'
+alias aa='newwin --title Athena ssh Athena.remote'
+alias m='newwin  --title Minerva ssh Minerva'
+alias er='newwin --title Erato ssh Erato'
+alias hiroko='newwin --title Hiroko ssh Hiroko'
+
+wf()
+{ # Because WebFaction's sshd doesn't AcceptEnv
+  # Based on: http://superuser.com/a/163228
+  local host="WebFaction"
+
+  [[ $OSTYPE =~ cygwin ]] && host=${host,,}
+
+  newwin --title WebFaction \
+    env TERM="$Z_SOLARIZED:$TERM" ssh -t "$host" "Z_SOLARIZED=\${TERM%:*}; TERM=\${TERM##*:}; export Z_SOLARIZED; exec \$HOME/opt/bin/bash --login"
+  # ┌──────────────────────────────────┘
+  # `-t` is required to explicitly allocate a terminal, otherwise
+  # the remote server won't request $TERM at all, obviating our little trick.
+}
+
+# Because cygwin ssh can't handle non-lowercase hostnames(?)
+if [[ $OSTYPE =~ cygwin ]]; then
+  for a in "${!BASH_ALIASES[@]}"; do
+    if [[ ${BASH_ALIASES[$a]} =~ \ ssh\  ]]; then
+      c="${BASH_ALIASES[$a]}"
+      quietly unalias "$a"
+      eval "alias $a=\"${c,,}\""
+    fi
+  done
+  unset -v a c
+fi
--- local/Pallas/config/bashrc.d/ssh.bash
+++
@@ -1,6 +0,0 @@
-# SSH aliases to lowercase
-
-alias m='newwin --title minerva ssh minerva'
-alias mr='newwin --title minerva ssh minerva.remote'
-alias er='newwin --title erato ssh erato'
-alias wf='newwin --title webfaction ssh webfaction'
