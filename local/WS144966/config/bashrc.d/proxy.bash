363be3cee76f8218c27e49ebf6b00b4d8234183c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-06-02 19:09:39 +0100

    check that proxy works before setting related envvars

--- local/WS144966/config/bashrc.d/proxy.bash
+++ local/WS144966/config/bashrc.d/proxy.bash
@@ -1,17 +1,24 @@
 # proxy
 
-export SOCKS5_SERVER='127.0.0.1:8080'
-export ALL_PROXY="socks5h://${SOCKS5_SERVER}"
-export GIT_PROXY_COMMAND="${dir_scripts}/git-proxy.sh"
+export SOCKS5_SERVER="127.0.0.1:8080"
+
+if [[ -z $ALL_PROXY ]]; then
+  if command curl -sSf --connect-timeout 5 --socks5-hostname $SOCKS5_SERVER inescapable.org &>/dev/null; then
+    export ALL_PROXY="socks5h://${SOCKS5_SERVER}"
+    export GIT_PROXY_COMMAND="${dir_scripts}/git-proxy.sh"
+    . "${dir_config}/bash/bashrc.d/chrome.bash"
+
+    links()
+    {
+      command links -socks-proxy "$SOCKS5_SERVER" "$@"
+    }
+  else
+    scold "$BASH_SOURCE: could not find proxy at $SOCKS5_SERVER"
+    unset -v SOCKS5_SERVER
+  fi
+fi
 
 export http_proxy="http://uxhj:violet0419_Delta@proxy:8080/"
 export https_proxy="$http_proxy"
 export HTTP_PROXY="$http_proxy"
 export HTTPS_PROXY="$http_proxy"
-
-. "${dir_config}/bash/bashrc.d/chrome.bash"
-
-links()
-{
-  command links -socks-proxy "$SOCKS5_SERVER" "$@"
-}
