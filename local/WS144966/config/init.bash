afdc2053ae8e0a2a8083eb617572c7726b9041d2
Grace Moreau <zgm@inescapable.org>
2016-06-03 08:08:49 -0600

    misc. cleanup

--- local/WS144966/config/init.bash
+++ local/WS144966/config/init.bash
@@ -7,12 +7,6 @@ if [[ -x $dir_scripts/loginbanner.sh ]]; then
     "${dir_scripts}/loginbanner.sh"
 fi
 
-### ZGM disabled 2016-04-29 -- no calendar(1) on this machine
-# # this day in history...
-# if [[ -x $dir_scripts/matins.sh ]]; then
-#     "${dir_scripts}/matins.sh"
-# fi
-
 # countdown (date & function set in private.bash)
 _isFunction cl && cl
 
