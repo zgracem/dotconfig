09c6b25bdc6b234937a3c27f8195d2e42df1ca60
Grace Moreau <zgm@inescapable.org>
2016-06-07 14:59:49 -0600

    <3

--- bash/private.d/countdown.bash
+++ bash/private.d/countdown.bash
@@ -2,20 +2,15 @@
 # ~/.config/bash/private.d/countdown.bash
 # -----------------------------------------------------------------------------
 
-# export count_to='2016-05-26 23:54 -0600'
+# export liz='2016-05-26 23:54 -0600'
 
-if [[ -x $dir_scripts/countdown.sh && -n $count_to ]]; then
-  c() {
+if [[ -x $dir_scripts/countdown.sh && -n $liz ]]; then
+  liz() {
     local output
 
     printf -v output "%b " "$esc_red\xe2\x99\xa5$esc_reset"
-    output+=$("$dir_scripts/countdown.sh" "$@" "$count_to")
+    output+=$("$dir_scripts/countdown.sh" "$@" "$liz")
 
     echo "$output"
   }
-  cs() { c -s; }
-  cl() { c -l; }
-  # alias  c="$dir_scripts/countdown.sh '${count_to}'"
-  # alias cl="$dir_scripts/countdown.sh -l '${count_to}'"
-  # alias cs="$dir_scripts/countdown.sh -s '${count_to}'"
 fi
