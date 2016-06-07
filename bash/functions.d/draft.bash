7bc754a5797ba6c871d72f613e273ce013ce767c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-06-07 14:57:52 -0600

    reorganized script directory

--- bash/functions.d/draft.bash
+++ bash/functions.d/draft.bash
@@ -2,7 +2,7 @@ draft()
 {   # save a draft
     declare scriptName="${1%.sh}"
     declare script="$dir_scripts/$scriptName.sh"
-    declare draftDir="$dir_dev/drafts"
+    declare draftDir="$dir_dev/done/drafts"
     declare draft lmSeconds lmDate i=2
 
     if [[ ! -f $script ]]; then
