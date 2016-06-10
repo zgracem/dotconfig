562ebceefb62334e751b2e542e8a339a51b5e2be
Grace Moreau <zgm@inescapable.org>
2016-06-10 10:57:22 -0600

    fpp improvements

--- bash/bashrc.d/fpp.bash
+++ bash/bashrc.d/fpp.bash
@@ -1,7 +1,34 @@
 # fpp
 # https://github.com/facebook/PathPicker
 
-# don't wait for fpp
+_inPath fpp || return
+
+export FPP_DIR=~/var/spool/fpp
+
+# TODO: remove later
+[[ -d $FPP_DIR ]] || mkdir -p "$FPP_DIR"
+[[ -d ~/.fpp ]] && rm -rf ~/.fpp &>/dev/null
+
+# Don't wait for fpp if VISUAL is something like `subl --wait`
 export FPP_EDITOR=${VISUAL%%?( -)-wait}
 
-export FPP_DIR=~/var/lib/fpp
+# -----------------------------------------------------------------------------
+# tmux integration
+# Based on: https://github.com/tmux-plugins/tmux-fpp
+# -----------------------------------------------------------------------------
+
+_inTmux || return
+
+fpp_key=$(tmux show-option -gvq "@fpp_key") \
+  || fpp_key="f"
+
+fpp_buffer="$FPP_DIR/tmux-buffer"
+fpp_log="$HOME/var/log/fpp.sh.log"
+
+tmux bind-key "$fpp_key" \
+  capture-pane -J \\\; \
+  save-buffer "$fpp_buffer" \\\; delete-buffer \\\; \
+  new-window -n fpp -c "#{pane_current_path}" \
+    "sh -c 'fpp < \"$fpp_buffer\" 2>>\"$fpp_log\"; rm \"$fpp_buffer\"'"
+
+unset -v fpp_key fpp_buffer fpp_log
