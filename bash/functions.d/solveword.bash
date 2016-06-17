74f8a3e7aefe7b2fa3c3378e23968f982876c238
Grace Moreau <zgm@inescapable.org>
2016-06-17 07:17:30 -0600

    solveword() should search whole words

--- bash/functions.d/solveword.bash
+++ bash/functions.d/solveword.bash
@@ -2,6 +2,6 @@ solveword()
 {   # crossword solver
     # http://hints.macworld.com/article.php?story=20080224044840101
 
-    command grep -Es --colour=never "$@" /usr/share/dict/words \
+    command grep -Esw --colour=never "$@" /usr/share/dict/words \
     | column
 }
