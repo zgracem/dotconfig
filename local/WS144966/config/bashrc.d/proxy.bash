507c563917c9d13dc2802b3d7b1adc9c394fd303
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-09-10 11:41:36 -0600

    remove and ignore things that shouldn't be here

--- local/WS144966/config/bashrc.d/proxy.bash
+++ local/WS144966/config/bashrc.d/proxy.bash
@@ -1,24 +1,2 @@
-# proxy
-
-export SOCKS5_SERVER="127.0.0.1:8080"
-
-if [[ -z $ALL_PROXY ]]; then
-  if command curl -sSf --connect-timeout 3 --socks5-hostname $SOCKS5_SERVER inescapable.org &>/dev/null; then
-    export ALL_PROXY="socks5h://${SOCKS5_SERVER}"
-    export GIT_PROXY_COMMAND="${dir_scripts}/util/git-proxy.sh"
-    . "${dir_config}/bash/bashrc.d/chrome.bash"
-
-    links()
-    {
-      command links -socks-proxy "$SOCKS5_SERVER" "$@"
-    }
-  else
-    scold "$BASH_SOURCE: could not find proxy at $SOCKS5_SERVER"
-    unset -v SOCKS5_SERVER
-  fi
-fi
-
-export http_proxy="http://uxhj:violet0815_Delta@proxy:8080/"
-export https_proxy="$http_proxy"
-export HTTP_PROXY="$http_proxy"
-export HTTPS_PROXY="$http_proxy"
+echo >&2 "${BASH_SOURCE[0]} called"
+. ~/.local/config/bashrc.d/private.proxy.bash
