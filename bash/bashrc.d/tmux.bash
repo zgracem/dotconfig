7e6b0cbf0ae471dc5e3a9f86ae61cb95c712e95a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-09-12 09:27:22 -0600

    fix for tmux 2.2 + macOS 10.12

--- bash/bashrc.d/tmux.bash
+++ bash/bashrc.d/tmux.bash
@@ -1,5 +1,11 @@
 _inPath tmux || return
 
+if (( MACOS_VERSINFO[0] >= 12 )); then
+  # kqueue is broken on macOS, but tmux 2.2 doesn't turn it off like it should.
+  # >> github.com/tmux/tmux/issues/475
+  export EVENT_NOKQUEUE=1
+fi
+
 tt()
 { 
   # All we want to do is attach to an existing session (if any), and otherwise
