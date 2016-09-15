1bde0c185d2ea5485ae7226fb693d60b5ac06801
Grace Moreau <zgm@inescapable.org>
2016-09-15 07:36:28 -0600

    more colour-related changes

--- bash/bashrc.d/dircolors.bash
+++ bash/bashrc.d/dircolors.bash
@@ -3,29 +3,30 @@
 # -----------------------------------------------------------------------------
 
 if [[ -z $LS_COLORS || -n $Z_RELOADING ]]; then
-  dc_src="$dir_config/dircolors"
-  dc_cache="$dir_cache/dircolors"
+  dc_src_dir="$dir_config/dircolors"
+  dc_cache_dir="$dir_cache/dircolors"
 
-  if [[ -n $Z_SOLARIZED ]]; then
-    dc_stub="solarized.$Z_SOLARIZED"
-  elif [[ -n $HV_LOADED ]]; then
+  if [[ -n $HV_LOADED ]]; then
     dc_stub="500kv.dircolors"
+  elif [[ -n $Z_SOLARIZED ]]; then
+    dc_stub="solarized.$Z_SOLARIZED"
   else
     dc_stub="default"
   fi
 
-  dc_src_file="$dc_src/$dc_stub"
-  dc_cache_file="$dc_cache/$dc_stub"
+  dc_src_file="$dc_src_dir/$dc_stub"
+  dc_cache_file="$dc_cache_dir/$dc_stub"
 
-  if [[ ! -f $dc_cache_file ]] \
-    && [[ -f $dc_src_file ]] \
-    && _inPath dircolors
+  if [[ ! -f $dc_cache_file || $dc_src_file -nt $dc_cache_file || ${FUNCNAME[1]} == "rl" ]] \
+      && [[ -f $dc_src_file ]] && _inPath dircolors
   then
+    verbose 2 ">> dircolors is recreating $dc_cache_file from $dc_src_file"
+
     # create cache dir if it doesn't exist
-    [[ -d $dc_cache ]] || mkdir -p "$dc_cache" 1>/dev/null
+    [[ -d $dc_cache_dir ]] || mkdir -p "$dc_cache_dir" 1>/dev/null
 
     # create cache file
-    dircolors -b "$dc_src_file" > "$dc_cache_file"
+    dircolors -b "$dc_src_file" >| "$dc_cache_file"
   fi
 
   if [[ -f $dc_cache_file ]]; then
