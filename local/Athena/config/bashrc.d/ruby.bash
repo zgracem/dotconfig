5795868eb95a5b078b6c824abfee1b921db61cb1
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-09-22 15:24:19 -0600

    clean up old TODO tags et al.

--- local/Athena/config/bashrc.d/ruby.bash
+++ local/Athena/config/bashrc.d/ruby.bash
@@ -1,5 +1,1 @@
-### ZGM disabled 2016-02-03 -- not sure this benefits anything
-# export GEM_HOME="$HOME/.gem/ruby/2.2.0"
-# export RUBYLIB="$GEM_HOME/lib:$HOME/lib/ruby"
-
 export RUBYLIB="$HOME/lib/ruby"
--- local/web500/config/bashrc.d/solarized.bash
+++
@@ -1,5 +0,0 @@
-# Solarized
-if [[ -z $Z_SOLARIZED ]] && _isFunction solarized; then
-  ### ZGM disabled 2016-05-26 -- obsoleted by new ssh alias?
-  : quietly solarized
-fi
