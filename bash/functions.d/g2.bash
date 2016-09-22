18e7092e6997cd1210a80ba288a89ca1c70c7363
Grace Moreau <zgm@inescapable.org>
2016-09-22 14:51:19 -0600

    remove unnecessary dir_* directories

--- bash/functions.d/g2.bash
+++ bash/functions.d/g2.bash
@@ -1,35 +1,49 @@
+declare -A mydirs
+
+mydirs[defunct]="$HOME/src/_defunct"
+mydirs[dev]="$dir_scripts/dev"
+mydirs[scratch]="$HOME/tmp/_scratch"
+mydirs[vsmm]="$dir_dropbox/www/vsmm"
+
 g2()
 {
-    local name="$1" place
 
-    if [[ $name =~ / ]]; then
-        local child="${name#*/}"
-        name="${name%%/*}"
-    fi
+  local name="$1" place
 
-    # remove illegal characters or ${!checkvar} expansion below will fail
-    local checkvar="dir_${name//[^[:alnum:]_]}"
+  if [[ $name =~ / ]]; then
+    local child="${name#*/}"
+    name="${name%%/*}"
+  fi
 
-    # first, see if we're trying to call a variable directly
-    if [[ -n ${!checkvar} ]]; then
-        place="${!checkvar}${child:+/$child}"
-    # maybe it's a directory in $HOME?
-    elif [[ -d $HOME/$name${child:+/$child} ]]; then
-        place="$HOME/$name${child:+/$child}"
-    elif [[ -n $DIRSTACK ]]; then
-        local re="$name"'$'
-        local d; for d in "${DIRSTACK[@]}"; do
-            if [[ $d =~ $re ]]; then
-                place="${d/#~/$HOME}${child:+/$child}"
-            fi
-        done
-    fi
+  # remove illegal characters or ${!checkvar} expansion below will fail
+  local checkvar="dir_${name//[^[:alnum:]_]}"
 
-    # any luck?
-    if [[ -d $place ]]; then
-        cd "$place"
-    else
-        scold "$FUNCNAME: $1: not found"
-        return 1
-    fi
+  # first, see if we're trying to call a variable directly
+  if [[ -n ${!checkvar} ]]; then
+    place="${!checkvar}${child:+/$child}"
+  elif [[ -v mydirs[$name] ]]; then
+    place="${mydirs[$name]}"
+  # maybe it's a directory in $HOME?
+  elif [[ -d $HOME/$name${child:+/$child} ]]; then
+    place="$HOME/$name${child:+/$child}"
+  elif [[ -n $DIRSTACK ]]; then
+    local re="$name"'$'
+    local d; for d in "${DIRSTACK[@]}"; do
+      if [[ $d =~ $re ]]; then
+        place="${d/#~/$HOME}${child:+/$child}"
+      fi
+    done
+  fi
+
+  # any luck?
+  if [[ -d $place ]]; then
+    cd "$place"
+    return 0
+  elif [[ -n $place ]]; then
+    scold "$FUNCNAME: $place: not found"
+  else
+    scold "$FUNCNAME: $1: not found"
+  fi
+
+  return 1
 }
