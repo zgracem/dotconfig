454dd059b56aa0e21f0e04d60160eba51dea4dca
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-10-06 08:38:47 -0600

    move macOS config stuff to top level

--- macos/chrome.bash
+++ macos/chrome.bash
@@ -10,3 +10,6 @@ defaults write com.google.Chrome DisablePrintPreview -bool true
 
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+
+# Check for updates daily
+defaults write com.google.Keystone.Agent checkInterval 86400
