b99880d8707b120a8d4d6a174d7310aae38cd937
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-10-13 11:56:40 -0600

    more misc cleanup

--- bash/bashrc.d/ssh.bash
+++ bash/bashrc.d/ssh.bash
@@ -9,18 +9,19 @@ alias hiroko='newwin --title Hiroko ssh Hiroko'
 wf()
 { # Because WebFaction's sshd doesn't AcceptEnv
   # Based on: http://superuser.com/a/163228
-  local host="WebFaction"
+  local title="WebFaction"
+  local host="$title"
 
   [[ $OSTYPE =~ cygwin ]] && host=${host,,}
 
   if [[ -n $Z_SOLARIZED ]]; then
-    newwin --title WebFaction \
-      env TERM="$Z_SOLARIZED:$TERM" ssh -t "$host" "Z_SOLARIZED=\${TERM%:*}; TERM=\${TERM##*:}; export Z_SOLARIZED; exec \$HOME/opt/bin/bash --login"
-    # ┌──────────────────────────────────┘
-    # `-t` is required to explicitly allocate a terminal, otherwise
-    # the remote server won't request $TERM at all, obviating our little trick.
+    newwin --title "$title" \
+      env TERM="$Z_SOLARIZED:$TERM" \
+        ssh -t "$host" "Z_SOLARIZED=\${TERM%:*}; TERM=\${TERM##*:}; export Z_SOLARIZED; exec \$HOME/opt/bin/bash --login"
+        # `ssh -t` is required to explicitly allocate a terminal, otherwise the
+        # remote server won't request $TERM at all, obviating our little trick
   else
-    newwin --title WebFaction ssh "$host"
+    newwin --title "$title" ssh "$host"
   fi
 
 }
@@ -32,13 +33,4 @@ if [[ $OSTYPE =~ cygwin ]]; then
   alias m='newwin  --title Minerva ssh minerva'
   alias er='newwin --title Erato ssh erato'
   alias hiroko='newwin --title Hiroko ssh hiroko'
-
-  # for a in "${!BASH_ALIASES[@]}"; do
-  #   if [[ ${BASH_ALIASES[$a]} =~ \ ssh\  ]]; then
-  #     c="${BASH_ALIASES[$a]}"
-  #     quietly unalias "$a"
-  #     alias "$a"="${c,,}"
-  #   fi
-  # done
-  # unset -v a c
 fi
--- bash/bashrc.d/x11.bash
+++
@@ -1,4 +0,0 @@
-# # Delete old error logs.
-# if [[ -f ~/.xsession-errors ]]; then
-#   rm -f ~/.xsession-errors
-# fi
