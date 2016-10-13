eb8e9e5c0d69b8d409a470b965a3e4fd1d538b92
Grace Moreau <zgm@inescapable.org>
2016-10-13 10:23:57 -0600

    remove unused functions

---
+++ bash/functions.d/localip.bash
@@ -0,0 +1,10 @@
+[[ $OSTYPE =~ darwin ]] || return
+
+localip()
+{ # print local IP address
+  local card # AirPort card (e.g. en1)
+  card=$(scutil <<< "list" \
+            | sed -nE 's#^.*Setup:/Network/Interface/(en[[:digit:]])/AirPort$#\1#p') \
+    || return
+  ipconfig getifaddr "$card"
+}
--- bash/functions.d/solveword.bash
+++
@@ -1,7 +0,0 @@
-solveword()
-{   # crossword solver
-    # http://hints.macworld.com/article.php?story=20080224044840101
-
-    command grep -Esw --colour=never "$@" /usr/share/dict/words \
-    | column
-}
