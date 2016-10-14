f8d852e5faa40061ac2076e555d43e2d34f5fbd5
Grace Moreau <zgm@inescapable.org>
2016-10-14 14:30:28 -0600

    cleanup and corrections

--- sh/paths.sh
+++ sh/paths.sh
@@ -17,103 +17,45 @@ INFOPATH=/usr/local/share/info:$INFOPATH
 # -----------------------------------------------------------------------------
 
 # rbenv
-if [ -d "$HOME/.rbenv" ] && command -v rbenv >/dev/null; then
-  eval "$(rbenv init -)"
-  MANPATH="$(rbenv prefix)/share/man:$MANPATH"
+if [ -d "$HOME/.rbenv" ]; then
+  # In case it isn't installed to /usr/local/bin
+  PATH=$HOME/.rbenv/bin:$PATH
+
+  if command -v rbenv >/dev/null; then
+    eval "$(rbenv init -)"
+    MANPATH="$(rbenv prefix)/share/man:$MANPATH"
+  fi
 fi
 
-if [ -z "$HOSTNAME" ]; then
-  HOSTNAME=`hostname`
-fi
-
-# go-lang (see also bashrc.d/golang.bash)
-case $HOSTNAME in
-  Athena*)
-    PATH=$PATH:/usr/local/opt/go/libexec/bin
-    ;;
-  *.atco.com)
-    PATH=$PATH:/opt/go/bin
-    ;;
-esac
+# go-lang (cygwin location)
+PATH=$PATH:/opt/go/bin
 
 # -----------------------------------------------------------------------------
 # macOS
 # -----------------------------------------------------------------------------
 
-# Homebrew (see also bashrc.d/homebrew.bash)
+# Homebrew
 if [ -x /usr/local/bin/brew ]; then
-  # GNU coreutils
+  # GNU coreutils (w/out `g` prefix)
   PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
   MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
   INFOPATH=/usr/local/opt/coreutils/share/info:$INFOPATH
 
-  # GNU sed
-  PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
-  MANPATH=/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH
+  # GNU sed (installed --with-default-names)
+  PATH=/usr/local/opt/gnu-sed/bin:$PATH
+  MANPATH=/usr/local/opt/gnu-sed/share/man:$MANPATH
   INFOPATH=/usr/local/opt/gnu-sed/share/info:$INFOPATH
 
-  # GNU tar
+  # GNU tar (w/out `g` prefix)
   PATH=/usr/local/opt/gnu-tar/libexec/gnubin:$PATH
+  MANPATH=/usr/local/opt/gnu-tar/share/man:$MANPATH
 
   # OpenSSL
-  PATH=/usr/local/opt/openssl/bin:$PATH
   MANPATH=/usr/local/opt/openssl/share/man:$MANPATH
 
   # ncurses
   PATH=/usr/local/opt/ncurses/bin:$PATH
   MANPATH=/usr/local/opt/ncurses/share/man:$MANPATH
-fi
-
-# Xcode
-if [ -x /usr/bin/xcode-select ]; then
-  case $HOSTNAME in
-    Athena*|Minerva*)
-      DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
-      ;;
-    Erato*)
-      DEVELOPER_DIR="/Library/Developer/CommandLineTools"
-      ;;
-    Hiroko*)
-      DEVELOPER_DIR="/Developer"
-      ;;
-    *)
-      DEVELOPER_DIR=$(xcode-select --print-path)
-      ;;
-  esac
-
-  # export DEVELOPER_DIR
-
-  darwin_ver=$(uname -r)
-
-  if [ ${darwin_ver%%.*} -ge 15 ]; then
-    PATH=$PATH:$DEVELOPER_DIR/usr/bin
-    MANPATH=$MANPATH:$DEVELOPER_DIR/usr/share/man
-    PATH=$PATH:$DEVELOPER_DIR/Toolchains/XcodeDefault.xctoolchain/usr/bin
-    MANPATH=$MANPATH:$DEVELOPER_DIR/Toolchains/XcodeDefault.xctoolchain/usr/share/man
-  else
-    PATH=$DEVELOPER_DIR/usr/bin:$PATH
-    MANPATH=$DEVELOPER_DIR/usr/share/man:$MANPATH
-  fi
-
-  unset -v darwin_ver
-fi
-
-# calibre
-PATH=$PATH:$HOME/Applications/calibre.app/Contents/MacOS
-
-# X11
-PATH=$PATH:/opt/X11/bin
-MANPATH=$MANPATH:/opt/X11/share/man
-
-# -----------------------------------------------------------------------------
-# cygwin
-# -----------------------------------------------------------------------------
-
-if [ $OSTYPE = "cygwin" ]; then
-  # gcc tools
-  PATH=$PATH:/opt/gcc-tools/bin
-  MANPATH=$MANPATH:/opt/gcc-tools/epoch2/share/man
-  INFOPATH=$INFOPATH:/opt/gcc-tools/epoch2/share/info
 fi
 
 # -----------------------------------------------------------------------------
@@ -139,29 +81,22 @@ INFOPATH=$HOME/share/info:$HOME/opt/share/info:$INFOPATH
 
 fixpath()
 {
-  # The input, a colon-separated list, is split by setting IFS to a colon
-  # and using an unquoted $* in the `for` loop. Each directory is checked to
-  # ensure that it isn't already in the PATH-in-progress, and that it exists
-  # at all; if both, it's appended to the P-in-p, w/ a leading colon if
-  # necessary. Once complete, it prints the new PATH and returns 0.
-  local d="" p="" IFS=:
+  # Set IFS to a colon so the unquoted `$*` below splits the input properly.
+  local IFS=:
 
-  if [[ -n $ZSH_NAME ]]; then
-    setopt sh_word_split
-  fi
+  # Enable ZSH word splitting so the IFS change above takes effect.
+  [[ -n $ZSH_NAME ]] && setopt SH_WORD_SPLIT
 
-  for d in $*; do
+  # Check each directory: if it isn't already in the PATH, and if it exists,
+  # append it to the PATH-in-progress (w/ a leading colon if necessary).
+  local d p; for d in $*; do
     case ":$p:" in
-      *:$d:*) 
-          false
-          ;;
-      *)  if [ -d "$d" ]; then
-            p="${p:+$p:}$d"
-          fi
-          ;;
+      *:$d:*) false ;;
+      *) [ -d "$d" ] && p="${p:+$p:}$d" ;;
     esac
   done
 
+  # Print the new PATH.
   printf "$p"
 }
 
