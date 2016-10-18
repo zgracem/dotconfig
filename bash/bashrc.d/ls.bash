5cdb3831febd0b9a353717f2168526981fc6b553
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-10-18 11:51:17 -0600

    minor code cleanup

--- bash/bashrc.d/ls.bash
+++ bash/bashrc.d/ls.bash
@@ -4,21 +4,19 @@
 
 quietly unalias ls ll
 
-export flags_ls=()
-
-flags_ls+=(-A)
-#           └────── (almost) all files
+export flags_ls=(-A)
+#                 └────── list (almost) all files
 
 if [[ $OSTYPE == cygwin ]]; then
-    flags_ls+=(--append-exe)
-    #            └─ append .exe if cygwin magic was needed
+  flags_ls+=(--append-exe)
+  #            └─ append .exe if cygwin magic was needed
 fi
 
 # colourize output
 if _isGNU ls; then
-    flags_ls+=(--color=auto)
+  flags_ls+=(--color=auto)
 else
-    flags_ls+=(-G)
+  flags_ls+=(-G)
 fi
 
 ls() { command ls "${flags_ls[@]}" "$@"; }
@@ -40,45 +38,36 @@ lst() { ll -rt "$@"; }
 #           │└─── sort by time
 #           └──── reverse sort (i.e. newest files last)
 
-lsd()
-{   # list all subdirectories in $1/$PWD
-
-    ll -d "${1+$1/}"*/
-    #   └─── list directories, not their contents
-}
+lsd() { ll -d "${1+$1/}"*/; }
+#           └──── list subdirectories, not their contents
 
 lsl()
-{   # list all symbolic links in $1/$PWD
-    find "${1-.}" -maxdepth 1 -type l \
-    | xargs ls -d "${flags_ls}"
+{ # list all symbolic links in $1/$PWD
+  find "${1-.}" -maxdepth 1 -type l \
+  | xargs ls -d "${flags_ls}"
 }
 
 # -----------------------------------------------------------------------------
 
 lsf()
-{   # "full" info
+{ # "full" info
 
-    local a flags_lsf=(-i -l)
-    #                    │  └─ long-list output
-    #                    └──── print inode number
+  local -a flags_lsf=(-i -l)
+  #                    │  └─ long-list output
+  #                    └──── print inode number
 
-    if [[ $OSTYPE == darwin* ]]; then
-        flags_lsf+=(-@ -O -G)
-        #            │  │  └── colourize output
-        #            │  └───── print file flags
-        #            └──────── display extended attributes
+  if [[ $OSTYPE == darwin* ]]; then
+    flags_lsf+=(-@ -O -G)
+    #            │  │  └──── colourize output
+    #            │  └─────── print file flags
+    #            └────────── display extended attributes
+    /bin/ls "${flags_lsf[@]}" "$@"
+    return
+  elif _isGNU ls; then
+    flags_lsf+=(--color=auto)
+  else #           ├──────── colourize output
+    flags_lsf+=(-G)
+  fi
 
-        /bin/ls "${flags_lsf[@]}" "$@"
-        return
-
-    elif _isGNU ls; then
-        flags_lsf+=(--color=auto)
-        #             └─────── colourize output
-    else
-        flags_lsf+=(-G)
-        #            └──────── colourize output
-    fi
-
-    command ls "${flags_lsf[@]}" "$@"
+  command ls "${flags_lsf[@]}" "$@"
 }
-
