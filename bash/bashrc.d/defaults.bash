0811517ec7cf20a9bf20b325d7d9f205ba20f950
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-10-23 12:34:46 -0600

    add protective wrapper for strings

--- bash/bashrc.d/defaults.bash
+++ bash/bashrc.d/defaults.bash
@@ -101,6 +101,14 @@ top()
   #            └──────────────────────── ignore frameworks
 }
 
+# `-a` scans the whole file instead of using insecure libbfd
+# >> https://lcamtuf.blogspot.ca/2014/10/psa-dont-run-strings-on-untrusted-files.html
+if _inPath strings; then
+  strings() { command strings -a "$@"; }
+elif _inPath gstrings; then
+  strings() { gstrings -a "$@" ; }
+fi
+
 if [[ $OSTYPE == darwin* ]]; then
   killall() { command killall -v "$@"; }
   #                            └─ verbose
