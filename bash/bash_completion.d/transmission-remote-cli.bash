339d6882adb914c2b522c3736076203f9d400aa8
Grace Moreau <zgm@inescapable.org>
2016-10-24 07:42:30 -0600

    start using GNU stow to manage external programs in ~/opt

--- bash/bash_completion.d/transmission-remote-cli.bash
+++ bash/bash_completion.d/transmission-remote-cli.bash
@@ -1,4 +1,4 @@
 # transmission-remote-cli <https://github.com/fagga/transmission-remote-cli>
-f="$HOME/src/github/transmission-remote-cli/completion/bash/transmission-remote-cli-bash-completion.sh"
+f="$HOME/opt/etc/bash_completion.d/transmission-remote-cli-bash-completion.sh"
 [[ -f $f ]] && . "$f"
 unset -v f
