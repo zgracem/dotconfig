96a6dd03988829e15c08c345d15d58ff783a25e9
Grace Moreau <zgm@inescapable.org>
2016-10-25 09:20:19 -0600

    disable line marks in Terminal.app
    See https://github.com/mathiasbynens/dotfiles/commit/c271ffe

--- macos/terminal.bash
+++ macos/terminal.bash
@@ -7,7 +7,10 @@ defaults write -app Terminal StringEncodings -array 4
 
 # Enable Secure Keyboard Entry
 # >> https://security.stackexchange.com/a/47786/8918
-defaults write com.apple.terminal SecureKeyboardEntry -bool true
+defaults write -app Terminal SecureKeyboardEntry -bool true
+
+# Disable line marks
+defaults write -app Terminal ShowLineMarks -int 0
 
 # Always show tab bar
 defaults write -app Terminal ShowTabBar -bool true
