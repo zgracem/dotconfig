31b151c5c1af9c810d5d4cdb1cf28ffdc27209aa
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-11-16 10:40:32 -0700

    move RealVNC files around

--- bash/functions.d/vnc.bash
+++ bash/functions.d/vnc.bash
@@ -2,19 +2,24 @@
 
 vnc()
 {
-  # set `dir_drive` if it's not already
-  dir_drive=${dir_drive:-$(find_drive "$myDrive")} || return
+  # # set `dir_drive` if it's not already
+  # dir_drive=${dir_drive:-$(find_drive "$myDrive")} || return
 
-  local vnc_dir="$dir_drive/bin/RealVNC"
+  local vnc_dir="$dir_apps/RealVNC"
+  local vnc_exe="$vnc_dir/vncviewer.exe"
+  local vnc_conf="$dir_config/vnc/vnc.conf"
+  local vnc_host="10.0.1.10:1"
 
-  if [[ -x $vnc_dir/vncviewer.exe ]]; then
-    local config_file=$(cygpath -aw "$vnc_dir/vnc.conf")
-    if run "$vnc_dir/vncviewer.exe" -config "$config_file"; then
+  if [[ -x $vnc_exe ]]; then
+    vnc_conf=$(cygpath -aw "$vnc_conf")
+    if run "$vnc_exe" -config "$vnc_conf"; then
       dtterm "6t" # lower window
       rollback
+    else
+      return
     fi
   else
-    scold "$vnc_dir/vncviewer.exe not found"
+    scold "not found: $vnc_exe"
     return 1
   fi
 }
