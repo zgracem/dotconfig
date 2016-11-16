8f250eed4db6e4b4e62e7ca67056f97dc7a381b8
Grace Moreau <zgm@inescapable.org>
2016-11-16 10:35:11 -0700

    remove unused files

--- local/Pallas/config/init.bash
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------------------------------
-# Pallas:~/.local/config/init.bash
-# -----------------------------------------------------------------------------
-
-hv_arrow     -b bryellow -f black   "Reminder"
-hv_arrow -tn -b yellow   -f brwhite "GNU Stow + ~/scripts/install/kit"
