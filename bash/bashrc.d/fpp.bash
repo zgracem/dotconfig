a81f6e205210f7d23b9ce2242ba66f823d415e68
Grace Moreau <zgm@inescapable.org>
2016-11-17 07:59:32 -0700

    use XDG_RUNTIME_DIR

--- bash/bashrc.d/fpp.bash
+++ bash/bashrc.d/fpp.bash
@@ -3,11 +3,13 @@
 
 _inPath fpp || return
 
-# don't wait for fpp if VISUAL is something like `subl --wait`
-export FPP_EDITOR=${VISUAL%%?( -)-wait}
+if [[ $VISUAL != $EDITOR ]]; then
+  # don't wait for fpp if VISUAL is something like `subl --wait`
+  export FPP_EDITOR=${VISUAL%%?( -)-wait}
+fi
 
 # setup (and create) temp directory
-export FPP_DIR=~/var/spool/fpp
+export FPP_DIR=$XDG_RUNTIME_DIR/fpp
 [[ -d $FPP_DIR ]] || mkdir -pv "$FPP_DIR"
 
 # keep homedir tidy
@@ -20,16 +22,15 @@ z_tidy ~/.fpp
 
 _inTmux || return
 
-fpp_key=$(tmux show-option -gvq "@fpp_key") \
-  || fpp_key="f"
-
+fpp_key=$(tmux show-option -gvq "@fpp_key") || fpp_key="f"
 fpp_buffer="$FPP_DIR/tmux-buffer"
 fpp_log="$HOME/var/log/fpp.sh.log"
 
 tmux bind-key "$fpp_key" \
-  capture-pane -J \\\; \
-  save-buffer "$fpp_buffer" \\\; delete-buffer \\\; \
-  new-window -n fpp -c "#{pane_current_path}" \
-    "sh -c 'fpp < \"$fpp_buffer\" 2>>\"$fpp_log\"; rm \"$fpp_buffer\"'"
+     capture-pane -J \\\; \
+     save-buffer "$fpp_buffer" \\\; \
+     delete-buffer \\\; \
+     new-window -n fpp -c "#{pane_current_path}" \
+       "sh -c 'fpp < \"$fpp_buffer\" 2>>\"$fpp_log\"; rm \"$fpp_buffer\"'"
 
 unset -v fpp_key fpp_buffer fpp_log
