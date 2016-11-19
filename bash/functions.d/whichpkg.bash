c6bc27166601faa4ebf7d2aec5c2d713758372b3
Grace Moreau <zgm@inescapable.org>
2016-11-19 08:47:20 -0700

    add Pacman support to whichpkg()

--- bash/functions.d/whichpkg.bash
+++ bash/functions.d/whichpkg.bash
@@ -1,5 +1,5 @@
 whichpkg()
-{ # find the Cygwin/Homebrew package to which file $1 belongs
+{ # Find the package to which file $1 belongs
 
   if (( $# == 1 )); then
     local file="$1"
@@ -8,20 +8,28 @@ whichpkg()
     return 1
   fi
 
+  local pkg
+
   if [[ $file != */* ]]; then
-    # Search the current directory
-    if [[ -f $PWD/$file ]]; then
+    if [[ -f $PWD/$file ]]        # Search the current directory
+    then
       file="$PWD/$file"
-    # Search for an executable in PATH
-    elif file=$(type -P "$file"); then
-      : # Do nothing, we're fine
+    elif file=$(type -P "$file")  # Search for an executable in PATH
+    then
+      file="$file"
+    elif [[ -n $HOMEBREW_PREFIX ]] \
+        && pkg=$(brew which-formula "$1" 2>/dev/null) \
+        && [[ -n $pkg ]]          # Search using homebrew-command-not-found
+    then
+      printf "%s (not installed)\n" "$pkg"
+      return 0
     else
       scold "not found: $1"
       return 1
     fi
   fi
 
-  # canonicalize 
+  # Resolve symlinks to determine canonical filename
   local canon
   if [[ $OSTYPE == darwin* ]]; then
     canon=$(mdutil -t "$file")
@@ -29,29 +37,41 @@ whichpkg()
       scold "$file: could not canonicalize"
       return 1
     fi
-  elif ! canon=$(readlink -e "$file"); then
-    scold "$file: could not canonicalize"
-    return 1
+  else
+    if ! canon=$(readlink -e "$file"); then
+      scold "$file: could not canonicalize"
+      return 1
+    fi
   fi
 
-  if [[ $OSTYPE == cygwin ]]; then
-    local mgr="Cygwin"
-    local pkg=$(cygcheck --find-package "$canon")
-  elif [[ -d $HOMEBREW_PREFIX ]]; then
+  # Homebrew/Linuxbrew
+  if command -v brew >/dev/null; then
     local mgr="Homebrew"
-    [[ $OSTYPE == linux* ]] && mgr="Linuxbrew"
+    [[ $OSTYPE == *linux* ]] && mgr="Linuxbrew"
 
-    local regex="^$HOMEBREW_PREFIX/Cellar/([^/]+)/([^/]+)/.+\$"
+    local regex="^$(brew --cellar)/([^/]+)/([^/]+)/.+\$"
     if [[ $canon =~ $regex ]]; then
       local pkg="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}"
-    fi 
+    fi
+
+  # Cygwin
+  elif command -v cygcheck >/dev/null; then
+    local mgr="Cygwin"
+    local pkg=$(cygcheck --find-package "$canon")
+
+  # Pacman (Arch Linux/MSYS2)
+  elif command -v pacman >/dev/null; then
+    local mgr="Pacman"
+    local pkg=$(pacman --query --owns "$canon")
+    pkg=${pkg#* is owned by }
+
   else
     scold "no compatible package manager found"
     return 1
   fi
 
   if [[ -n $pkg ]]; then
-    printf '%s\n' "$pkg"
+    printf "%s\n" "$pkg"
   else
     scold "$file: does not seem to belong to a $mgr package"
     return 1
