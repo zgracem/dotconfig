a5fff04c9f5876b896433c1806446a12434e32f3
Grace Moreau <zgm@inescapable.org>
2016-11-25 08:54:30 -0700

    let newwin() run without arguments

--- bash/functions.d/newwin.bash
+++ bash/functions.d/newwin.bash
@@ -1,55 +1,70 @@
 newwin()
-{   # open in a new tmux/GNU screen window, if applicable
-    # Usage: newwin [-t|--title TITLE] COMMAND [ARGS]
+{ # open in a new tmux/GNU screen window, if applicable
+  # Usage: newwin [-t|--title TITLE] [COMMAND] [ARGS]
 
-    local title_regex='^-?-t(itle)?$'
-    local title cmd arg i=0
-    local -a args
+  local title_regex='^-?-t(itle)?$'
+  local title cmd arg i=0
+  local -a args
 
-    if [[ $1 =~ $title_regex ]]; then
-        title="$2"
-        shift 2
-    else
-        title="${1##*/}"
-        printf -v cmd %q "$1"
-        shift
-    fi
-
+  if [[ -z $1 ]]; then
     if _inTmux; then
-        until [[ $# -eq 0 ]]; do
-            # printf -v args[$i] %q "$1"
-            printf -v arg %q "$1"
-            args[$i]=$arg
-            ((i++))
-            shift
-        done
-
-        command tmux new-window -n "$title" "$cmd ${args[*]}"
+      command tmux new-window
     elif _inScreen; then
-        command screen -t "$title" $cmd "$@"
+      command screen
     else
-        $cmd "$@"
+      return 1
     fi
+    return
+  fi
+
+  if [[ $1 =~ $title_regex ]]; then
+    title="$2"
+    shift 2
+  else
+    title="${1##*/}"
+    printf -v cmd %q "$1"
+    shift
+  fi
+
+  if _inTmux; then
+    until [[ $# -eq 0 ]]; do
+      printf -v args[$i] %q "$1"
+      # printf -v arg %q "$1"
+      # args[$i]=$arg
+      ((i++))
+      shift
+    done
+
+    command tmux new-window -n "$title" "$cmd ${args[*]}"
+  elif _inScreen; then
+    command screen -t "$title" $cmd "$@"
+  else
+    $cmd "$@"
+  fi
 }
 
 splitwin()
-{   # open in a new tmux windowpane, if applicable
-    # Usage: splitwin COMMAND [ARGS]
+{ # open in a new tmux windowpane, if applicable
+  # Usage: splitwin COMMAND [ARGS]
 
-    local cmd arg i=0
-    local -a args
+  local cmd arg i=0
+  local -a args
 
-    if _inTmux; then
-        until [[ $# -eq 0 ]]; do
-            # printf -v args[$i] %q "$1"
-            printf -v arg %q "$1"
-            args[$i]=$arg
-            ((i++))
-            shift
-        done
-
-        command tmux split-window -h "$cmd ${args[*]}"
+  if _inTmux; then
+    if [[ -z $1 ]]; then
+      command tmux split-window -h
     else
-        $cmd "$@"
+      until [[ $# -eq 0 ]]; do
+        printf -v args[$i] %q "$1"
+        # printf -v arg %q "$1"
+        # args[$i]=$arg
+        ((i++))
+        shift
+      done
+
+      command tmux split-window -h "$cmd ${args[*]}"
     fi
+  else
+    $cmd "$@"
+  fi
 }
