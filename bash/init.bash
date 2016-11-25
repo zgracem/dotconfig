497da9c095704227a305c28ecc45a44b65f490f7
Grace Moreau <zgm@inescapable.org>
2016-11-25 08:56:11 -0700

    add astronomical info to daily login script

--- bash/init.bash
+++ bash/init.bash
@@ -15,21 +15,25 @@ fi
 
 # -----------------------------------------------------------------------------
 
-# cute login banner
-if [[ -x $dir_scripts/login/loginbanner.sh ]]; then
-    "${dir_scripts}/login/loginbanner.sh"
-fi
+if [[ -d $dir_scripts ]]; then
+  # cute banner
+  "$dir_scripts/login/loginbanner.sh"
 
-# this day in history...
-if [[ -x $dir_scripts/login/matins.sh ]]; then
-    "${dir_scripts}/login/matins.sh"
+  # run only on first login of the day:
+  if "$dir_scripts/login/matins.sh"; then
+    # astronomical info -- requires suncalc and weather_ca gems
+    [[ $HOSTNAME == Athena* ]] && "$dir_scripts/login/astro.rb"
+
+    # this day in history...
+    "$dir_scripts/login/today_in_history.sh"
+  fi
 fi
 
 # countdown (date & function set in private.d/countdown.bash)
 _isFunction liz && liz
 
 # print bash version if not the latest release (variables set in bashrc.bash)
-if (( this_bash < latest_bash )) \
+if (( this_bash < latest_bash )) || [[ -n $newer_bash ]] \
   || [[ ${BASH_VERSINFO[4]} != "release" ]]\
   || [[ $BASH != $SHELL ]]; then
   printf 'GNU bash, version %s (%s)\n' "$BASH_VERSION" "$MACHTYPE"
