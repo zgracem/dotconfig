4c73840a755970f921f2399f646a299ce69fc375
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-11-25 15:17:46 -0700

    use variables for proxy server address and port

--- bash/functions.d/start_proxy.bash
+++ bash/functions.d/start_proxy.bash
@@ -1,13 +1,15 @@
 start_proxy()
 { # open an SSH tunnel for use as a SOCKS5 proxy
+  local addr="127.0.0.1"
+  local port="8080"
 
-	ssh -fqND 8080 -o "ExitOnForwardFailure=yes" "$@" || return
-  #    ││││       └─ wait until tunnel is established before going to background
-	#    │││└───────── dynamic port forwarding on 8080
-	#    ││└────────── don't execute remote command (forward only)
-	#    │└─────────── quiet mode
-	#    └──────────── go to background
+	ssh -fqND "$port" -o "ExitOnForwardFailure=yes" "$@" || return
+  #    ││││          └─ wait until tunnel is established before going to background
+	#    │││└──────────── dynamic port forwarding on 8080
+	#    ││└───────────── don't execute remote command (forward only)
+	#    │└────────────── quiet mode
+	#    └─────────────── go to background
 
-  export SOCKS5_SERVER="127.0.0.1:8080"
+  export SOCKS5_SERVER="${addr}:${port}"
   export ALL_PROXY="socks5h://${SOCKS5_SERVER}"
 }
