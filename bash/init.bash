875b1ac2f7500c01a34334a18076545ba105eb1d
Grace Moreau <zgm@inescapable.org>
2016-11-28 14:29:03 -0700

    do all homedir-tidying at once

--- bash/init.bash
+++ bash/init.bash
@@ -2,16 +2,50 @@
 # final initialization script
 # sourced at the end of ~/.bashrc
 # -----------------------------------------------------------------------------
+# Keep homedir tidy 
+# -----------------------------------------------------------------------------
 
-# add config file symlinks if necessary (see bashrc.d/config.bash)
-
+# Add config file symlinks if necessary (see bashrc.d/config.bash)
 if [[ ! -e ~/.irbrc ]] && _inPath irb; then
   _z_config_symlink ruby/irbrc
 fi
-
 if [[ ! -e ~/.vimrc ]] && _inPath vim; then
   _z_config_symlink vim/vimrc
 fi
+
+# Remove unwanted dotfiles
+trash_files=(
+  ~/.bash_sessions
+  ~/.fpp
+  ~/.gemrc
+  ~/.lesshst
+  ~/.npmrc
+  ~/.pine-debug{1..4} ~/.pine-crash
+  ~/.screen
+  ~/.stardict ~/.sdcv_history
+  ~/.wgetrc
+)
+
+z_tidy "${trash_files[@]}"
+
+# Create important directories
+dirs=(
+  "$XDG_CACHE_HOME"
+  "$XDG_RUNTIME_DIR"
+  "$FPP_DIR"
+  "$GEM_SPEC_CACHE"
+  "$HISTDIR"
+  "$HOMEBREW_CACHE"
+  "$HOMEBREW_LOGS"
+  "$HOMEBREW_TEMP"
+  "${MBOX%/*}"
+)
+
+for dir in "${dirs[@]}"; do
+  [[ -d $dir ]] || mkdir -pv "$dir"
+done
+
+unset -v trash_files dir dirs
 
 # -----------------------------------------------------------------------------
 
