8fc4c5fcca4bf1a321ecb8631f452a829329397f
Grace Moreau <zgm@inescapable.org>
2016-11-28 10:58:56 -0700

    new ssh wrapper function to SendEnv to AcceptEnv-disabled servers

--- bash/bashrc.d/ssh.bash
+++ bash/bashrc.d/ssh.bash
@@ -1,38 +1,6 @@
-# ssh shortcuts & setup
-
-alias  a='newwin --title Athena ssh Athena'
-alias aa='newwin --title Athena ssh Athena.remote'
-alias m='newwin  --title Minerva ssh Minerva'
-alias er='newwin --title Erato ssh Erato'
-alias hiroko='newwin --title Hiroko ssh Hiroko'
-
-wf()
-{ # Because WebFaction's sshd doesn't AcceptEnv
-  # >> http://superuser.com/a/163228
-  local title="WebFaction"
-  local host="$title"
-
-  [[ $OSTYPE == cygwin ]] && host=${host,,}
-
-  if [[ -n $Z_SOLARIZED ]]; then
-    newwin --title "$title" \
-      env TERM="$Z_SOLARIZED:$TERM" \
-        ssh -t "$host" "Z_SOLARIZED=\${TERM%:*}; TERM=\${TERM##*:}; export Z_SOLARIZED; exec \$HOME/opt/bin/bash --login"
-        # `ssh -t` is required to explicitly allocate a terminal, otherwise the
-        # remote server won't request $TERM at all, obviating our little trick
-  else
-    newwin --title "$title" ssh "$host"
-  fi
-}
-
-# Because cygwin ssh can't handle non-lowercase hostnames(?)
-if [[ $OSTYPE == cygwin ]]; then
-  alias  a='newwin --title Athena ssh athena'
-  alias aa='newwin --title Athena ssh athena.remote'
-  alias m='newwin  --title Minerva ssh minerva'
-  alias er='newwin --title Erato ssh erato'
-  alias hiroko='newwin --title Hiroko ssh hiroko'
-fi
+# -----------------------------------------------------------------------------
+# SSH setup & shortcuts
+# -----------------------------------------------------------------------------
 
 # Symlink host-specific ssh_config (if any), or global file otherwise
 if [[ ! -L ~/.ssh/config ]]; then
@@ -42,3 +10,40 @@ if [[ ! -L ~/.ssh/config ]]; then
     ln -sv "../.config/ssh/config" ~/.ssh/config
   fi
 fi
+
+_ssh()
+{
+  local host="$1"
+  local title="${host%%.*}"
+
+  local send_env=(TERM_PROGRAM Z_SOLARIZED)
+
+  # Because cygwin's ssh can't handle non-lowercase hostnames(?)
+  [[ $OSTYPE == cygwin ]] && host=${host,,}
+
+  # WebFaction's sshd doesn't AcceptEnv, so set important vars manually
+  # >> http://superuser.com/a/163228
+  if [[ $host == WebFaction ]]; then
+    local cmd="exec bash --login"
+    local assign
+    local export
+
+    local var; for var in "${send_env[@]}"; do
+      if [[ -n ${!var} ]]; then
+        assign+="${assign+; }$var=${!var}"
+        export+="${export+ }$var"
+      fi
+    done
+
+    cmd="$assign; export $export; $cmd"
+  fi
+
+  newwin --title "$title" ssh -t "$host" "$cmd"
+}
+
+alias  a='_ssh Athena'
+alias aa='_ssh Athena.remote'
+alias  m='_ssh Minerva'
+alias er='_ssh Erato'
+alias wf='_ssh WebFaction'
+alias hiroko='_ssh Hiroko'
