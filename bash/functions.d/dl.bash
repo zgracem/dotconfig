32fac0d9a5cbd24b737bd6a23ad9a69172614c4c
Grace Moreau <zgm@inescapable.org>
2016-12-01 08:33:19 -0700

    move logic inside functions

--- bash/functions.d/dl.bash
+++ bash/functions.d/dl.bash
@@ -1,10 +1,19 @@
-if _inPath wget; then
-    # download a file
-    dl()      { wget "$@"; }
+# download a file
+dl()
+{
+  if _inPath wget; then
+    wget "$@"
+  elif inPath curl; then
+    curl -OJ "$@"
+  fi
+}
 
-    # get HTTP headers
-    headers() { wget --spider -Snv "$@"; }
-else
-    dl()      { curl -OJ "$@"; }
-    headers() { curl -Is "$@"; }
-fi
+# get HTTP headers
+headers()
+{
+  if _inPath wget; then
+    wget --spider -Snv "$@"
+  elif inPath curl; then
+    curl -Is "$@"
+  fi
+}
