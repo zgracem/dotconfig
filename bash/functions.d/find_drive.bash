cf98e96d82414b1eb81da2f9db8f9fa9b6eb56ff
Grace Moreau <zgm@inescapable.org>
2016-12-01 08:38:57 -0700

    `g2 drive` obsoletes drive()

--- bash/functions.d/find_drive.bash
+++ bash/functions.d/find_drive.bash
@@ -1,6 +1,5 @@
 # `find_drive` echoes the path to the root of the volume labeled $1.
 # It returns 0 on success and 1 on any kind of failure.
-
 find_drive()
 {
   if [[ -n $1 ]]; then
@@ -34,21 +33,4 @@ find_drive()
   
   scold "volume not found: $label"
   return 1
-}
-
-# `drive` changes PWD to the volume named $myDrive, if present, and sets the
-# environment variable `dir_drive` accordingly before returning 0 on success;
-# otherwise, it returns 1.
-
-drive()
-{
-  if [[ -z $myDrive ]]; then
-    scold "\$myDrive not set"
-    return 1
-  elif [[ ! -d $dir_drive ]]; then
-    export dir_drive
-    dir_drive=$(find_drive "$myDrive") || return
-  fi
-
-  cd "$dir_drive"
 }
