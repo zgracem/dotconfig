21072fcf39647dddf633529e2544249057a0c734
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-12-01 08:30:45 -0700

    sizes() needs its own du(1) call b/c of globbing

--- bash/functions.d/dirsize.bash
+++ bash/functions.d/dirsize.bash
@@ -1,14 +1,16 @@
+# display total size of a directory
 dirsize()
-{ # display total size of a directory
-  command du -sh "${1-$PWD}"
-  #           │└── human-readable sizes (e.g. 640K 1.44M 4G)
-  #           └─── summarize (display total only)
+{ 
+  du -sh "${1-.}"
+  #   │└─ human-readable sizes (e.g. 640K 1.44M 4G)
+  #   └── summarize (display total only)
 }
 
+# sort files and directories by size
 sizes()
-{ # sort files and directories by size
-  dirsize "${1-$PWD}"/* \
+{ 
+  command du -sh "${1-.}"/* \
   | sort -rh
-  #       │└────── compare human-readable numbers
-  #       └─────── reverse sort (largest sizes first)
+  #       │└─ compare human-readable numbers
+  #       └── reverse sort (largest sizes first)
 }
