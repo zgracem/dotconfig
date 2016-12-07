141b757f7d0d7b28567f77ea4935b30767cd1649
Grace Moreau <zgm@inescapable.org>
2016-12-07 07:11:01 -0700

    improve local git config management

--- bash/bashrc.d/git.bash
+++ bash/bashrc.d/git.bash
@@ -1,6 +1,7 @@
-# symlink config file into HOME
-if [[ -f ~/.local/config/gitconfig ]]; then
-  _z_config_symlink ".local/config/gitconfig"
+# symlink host-specific config file into HOME
+if [[ -f ~/.local/config/git/config ]]; then
+  _z_config_symlink ".local/config/git/config" .gitconfig
 elif [[ -L ~/.gitconfig ]]; then
+  # use general config file in ~/.config/git/config
   rm -fv ~/.gitconfig
 fi
