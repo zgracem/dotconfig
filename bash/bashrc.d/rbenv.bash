50c4eebdbd5525bb653bec7abb2f4cb429064706
Grace Moreau <zgm@inescapable.org>
2016-12-07 08:51:14 -0700

    don't add non-existent dir to PATH

--- bash/bashrc.d/rbenv.bash
+++ bash/bashrc.d/rbenv.bash
@@ -1,6 +1,6 @@
 if [ -d "$HOME/.rbenv" ]; then
   # In case it isn't installed to /usr/local/bin
-  PATH=$HOME/.rbenv/bin:$PATH
+  [ -d "$HOME/.rbenv/bin" ] && PATH=$HOME/.rbenv/bin:$PATH
 
   if command -v rbenv >/dev/null; then
     eval "$(rbenv init -)"
