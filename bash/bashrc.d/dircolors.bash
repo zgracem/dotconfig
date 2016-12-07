d29d2348bbaa9a40f5b42b68fbb558fdb9325ea8
Grace Moreau <zgm@inescapable.org>
2016-12-07 10:24:33 -0700

    dircolors config tweaks

--- bash/bashrc.d/dircolors.bash
+++ bash/bashrc.d/dircolors.bash
@@ -8,13 +8,13 @@ if [[ -z $LS_COLORS || -n $Z_RELOADING ]]; then
   dc_stub="default"
 
   if [[ -n $HV_LOADED ]]; then
-    dc_stub="500kv.dircolors"
+    dc_stub="thirty2k"
   elif [[ -n $Z_SOLARIZED ]]; then
-    dc_stub="solarized.$Z_SOLARIZED"
+    dc_stub="solarized_$Z_SOLARIZED"
   fi
 
-  dc_src="$XDG_CONFIG_HOME/dircolors/$dc_stub"
-  dc_cache="$XDG_CACHE_HOME/dircolors/$dc_stub"
+  dc_src="$XDG_CONFIG_HOME/dircolors/$dc_stub.dircolors"
+  dc_cache="$XDG_CACHE_HOME/dircolors/$dc_stub.dircolors"
 
   if [[ -n $Z_RELOADING || ! -f $dc_cache || $dc_src -nt $dc_cache ]] \
     && [[ -f $dc_src ]]
