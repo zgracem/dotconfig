3e48300018546371ec9b970206ee322349c41f76
Z. Grace Moreau <zgracem@users.noreply.github.com>
2016-12-08 15:02:10 -0700

    move rootme() to functions.d

--- bash/bashrc.d/sudo.bash
+++ bash/bashrc.d/sudo.bash
@@ -1,35 +1,8 @@
 if _inPath sudo; then
-    # allow aliases to be sudo'ed
-    alias sudo='sudo '
+  # allow aliases to be sudo'ed
+  alias sudo='sudo '
 else
-    # just pass through commands
-    sudo() { $*; }
-    return
+  # just pass through commands
+  sudo() { $*; }
+  return
 fi
-
-rootme()
-{   # temporarily become root for $1 minutes (default is 3)
-
-    local def=5
-    local tmout=$(( ${1:-$def} * 60 ))
-
-    # rename window, if applicable
-    if _inScreen; then
-        printf '%b' "\eksudo${ST}"
-    elif _inTmux; then
-        tmux rename-window sudo 2>/dev/null
-    fi
-
-    sudo \
-        ${Z_SOLARIZED:+Z_SOLARIZED=$Z_SOLARIZED} \
-        TMOUT=$tmout \
-        -s
-        # -p "%u "$'\xe2\x86\x92'" %U@%H: "
-
-    # restore window name
-    if _inScreen; then
-        printf '%b' "\ekbash${ST}"
-    elif _inTmux; then
-        tmux set-window-option automatic-rename on 2>/dev/null
-    fi
-}
---
+++ bash/functions.d/rootme.bash
@@ -0,0 +1,25 @@
+rootme()
+{ # temporarily become root for $1 minutes (default is 3)
+
+  local def=5
+  local tmout=$(( ${1:-$def} * 60 ))
+
+  # rename window, if applicable
+  if _inScreen; then
+    printf '%b' "\eksudo${ST}"
+  elif _inTmux; then
+    tmux rename-window sudo 2>/dev/null
+  fi
+
+  sudo \
+    ${Z_SOLARIZED:+Z_SOLARIZED=$Z_SOLARIZED} \
+    TMOUT=$tmout \
+    -s
+
+  # restore window name
+  if _inScreen; then
+    printf '%b' "\ekbash${ST}"
+  elif _inTmux; then
+    tmux set-window-option automatic-rename on 2>/dev/null
+  fi
+}
