b168d723c87399a07a46a903464ecae1f8b19191
Grace Moreau <zgm@inescapable.org>
2017-01-05 08:35:17 -0700

    whitespace/style

--- bash/functions.d/console.bash
+++ bash/functions.d/console.bash
@@ -1,28 +1,28 @@
 console()
 {   # scan system messages from Terminal
-    # >> http://brettterpstra.com/a-simple-but-handy-bash-function-console/
+  # >> http://brettterpstra.com/a-simple-but-handy-bash-function-console/
 
-    # Usage: console [-w]
+  # Usage: console [-w]
 
-    local -a locations=(/var/log/system.log /var/log/messages)
-    local log_file
+  local -a locations=(/var/log/system.log /var/log/messages)
+  local log_file
 
-    for log_file in "${locations[@]}"; do
-    	if [[ -r $log_file ]]; then
-    		break
-    	else
-    		unset -v log_file
-    	fi
-    done
-
-    if [[ -z $log_file ]]; then
-    	scold "${FUNCNAME[0]}: no log file found"
-    	return 72
-    fi
-
-    if [[ $1 == -w ]] && _inTmux; then
-    	command tmux split-window "tail -n $(( LINES * 2 )) -f '${log_file}'"
+  for log_file in "${locations[@]}"; do
+    if [[ -r $log_file ]]; then
+      break
     else
-        newwin -t console tail -n $(( LINES * 2 )) -f "${log_file}"
+      unset -v log_file
     fi
+  done
+
+  if [[ -z $log_file ]]; then
+    scold "${FUNCNAME[0]}: no log file found"
+    return 72
+  fi
+
+  if [[ $1 == -w ]] && _inTmux; then
+    command tmux split-window "tail -n $(( LINES * 2 )) -f '${log_file}'"
+  else
+    newwin -t console tail -n $(( LINES * 2 )) -f "${log_file}"
+  fi
 }
