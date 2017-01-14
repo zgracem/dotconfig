a89d07b880ad5aa33cb2fcddff35968139eac1be
Grace Moreau <zgm@inescapable.org>
2017-01-14 09:25:48 -0700

    improve vgrep() appearance

--- bash/functions.d/vgrep.bash
+++ bash/functions.d/vgrep.bash
@@ -1,12 +1,18 @@
 vgrep()
 {
-  declare -p \
-  | grep -i "$*" \
-  | LESS= LESS_TERMCAP_so=$'\e[95m' LESS_TERMCAP_se=$'\e[39m' \
-    less -EQUXi # +"/$*"
-  #       ││││└─ case-insensitive searching (--ignore-case)
-  #       │││└── don't clear the screen on exit (--no-init)
-  #       ││└─── treat BS, TAB, and CR as special (--UNDERLINE-SPECIAL)
-  #       │└──── never ring the terminal bell (--QUIET)
-  #       └───── automatically quit at end of file (--QUIT-AT-EOF)
+  local var="$1"
+
+  # Use the same colour for grep matches as specified in GREP_COLORS
+  local colour=${GREP_COLORS#*ms=}; colour=${colour%%:*}
+
+  declare -p | grep -i "$var" \
+  | LESS= LESS_TERMCAP_so=$'\e'"[${colour}m" LESS_TERMCAP_se=$'\e[39m' \
+    less -EQUXi +/$'\v'"$var"
+  #       │││││ │ └─ highlight matches w/out moving to first one
+  #       │││││ └─── initial command: search
+  #       ││││└───── case-insensitive searching         (--ignore-case)
+  #       │││└────── don't clear the screen on exit     (--no-init)
+  #       ││└─────── treat BS, TAB, and CR as special   (--UNDERLINE-SPECIAL)
+  #       │└──────── never ring the terminal bell       (--QUIET)
+  #       └───────── automatically quit at end of file  (--QUIT-AT-EOF)
 }
