7e847b1673b8762c8752df4be46705772e284c78
Grace Moreau <zgm@inescapable.org>
2017-01-14 09:10:30 -0700

    use `pushover` wrapper if available

--- bash/functions.d/handoff.bash
+++ bash/functions.d/handoff.bash
@@ -1,14 +1,23 @@
 handoff()
 {
   local url="$1"
+  local title="URL from ${HOSTNAME%%.*}"
+  local device="zPhone"
+  local message="$url"
 
-  curl -sS \
-    --form-string "user=$PUSHOVER_USER_KEY" \
-    --form-string "token=$PUSHOVER_APP_TOKEN" \
-    --form-string "device=zPhone" \
-    --form-string "title=URL from ${HOSTNAME%%.*}" \
-    --form-string "url=$url" \
-    --form-string "message=$url" \
-    "https://api.pushover.net/1/messages.json" \
-    >/dev/null &
+  if _inPath pushover; then
+    pushover -k "$PUSHOVER_APP_TOKEN" -u "$PUSHOVER_USER_KEY" \
+      -d "$device" -t "$title" -U "$url" \
+      "$message"
+  else
+    curl -sS \
+        --form-string "user=$PUSHOVER_USER_KEY" \
+        --form-string "token=$PUSHOVER_APP_TOKEN" \
+        --form-string "device=$device" \
+        --form-string "title=$title" \
+        --form-string "url=$url" \
+        --form-string "message=$message" \
+        "https://api.pushover.net/1/messages.json" \
+        >/dev/null
+  fi  
 }
--- bash/functions.d/notify.bash
+++ bash/functions.d/notify.bash
@@ -1,10 +1,19 @@
 notify()
 {
-  curl -sS \
-    --form-string "user=$PUSHOVER_USER_KEY" \
-    --form-string "token=$PUSHOVER_APP_TOKEN" \
-    --form-string "title=${HOSTNAME%%.*}" \
-    --form-string "message=$*" \
-    "https://api.pushover.net/1/messages.json" \
-    >/dev/null &
+  local title=${HOSTNAME%%.*}
+  local message="$*"
+
+  if _inPath pushover; then
+    pushover -k "$PUSHOVER_APP_TOKEN" -u "$PUSHOVER_USER_KEY" \
+      -t "$title" \
+      "$message"
+  else
+    curl -sS \
+      --form-string "token=$PUSHOVER_APP_TOKEN" \
+      --form-string "user=$PUSHOVER_USER_KEY" \
+      --form-string "title=$title" \
+      --form-string "message=$message" \
+      "https://api.pushover.net/1/messages.json" \
+      >/dev/null
+  fi
 }
