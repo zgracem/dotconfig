3a37d1ade21da31229c0243fb58ef6c9cec7dbb6
Grace Moreau <zgm@inescapable.org>
2017-01-14 09:05:38 -0700

    use output format from youtube-dl config

--- bash/bashrc.d/youtube-dl.bash
+++ bash/bashrc.d/youtube-dl.bash
@@ -5,6 +5,7 @@ _inPath youtube-dl || return
 
 ydl()
 {
-  local output_fmt="%(title)s.%(ext)s"
+  local output_fmt
+  output_fmt=$(sed -nE 's/^--output "([^"]+)".*/\1/p' ~/.config/youtube-dl/config)
   youtube-dl -o "$dir_downloads/$output_fmt" "$@"
 }
