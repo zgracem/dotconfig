815a516043886baf2fa4e86eaf9cab67faa6a047
Z. Grace Moreau <zgracem@users.noreply.github.com>
2017-01-31 08:42:26 -0700

    2017-01-09: disable CURL_CA_BUNDLE

--- bash/bashrc.d/openssl.bash
+++ bash/bashrc.d/openssl.bash
@@ -1,6 +1,13 @@
 # OpenSSL
 
-# wget http://curl.haxx.se/ca/cacert.pem -o "$HOME/etc/ssl/cacert.pem"
+### ZGM disabled 2017-01-09
+#   DO NOT RESTORE without documenting why it's needed, what breaks w/out it,
+#   and how to make it work w/out fucking up e.g. Homebrew downloads and
+#   Pushover API calls. Make sure you test on Erato, Hiroko and WebFaction and
+#   not just Athena.
+return
+
+# wget http://curl.haxx.se/ca/cacert.pem -O "$HOME/etc/ssl/cacert.pem"
 declare -a certificates=(
   "/usr/local/etc/openssl/cert.pem"
   "/etc/ssl/certs/ca-bundle.crt"
--- local/Erato/config/bashrc.d/curl.bash
+++
@@ -1,2 +0,0 @@
-# cURL throws certificate errors if this is set on Erato
-unset -v CURL_CA_BUNDLE
