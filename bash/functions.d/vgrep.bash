be2bcdea6d25d7720e77b7db8515eaacca822bd6
Grace Moreau <zgm@inescapable.org>
2017-02-06 08:01:49 -0700

    simplify vgrep()

--- bash/functions.d/vgrep.bash
+++ bash/functions.d/vgrep.bash
@@ -1,16 +1,4 @@
 vgrep()
 {
-  # Use the same colour for grep matches as specified in GREP_COLORS
-  local colour=${GREP_COLORS#*ms=}; colour=${colour%%:*}
-
-  declare -p | grep -i "$1" \
-  | LESS= LESS_TERMCAP_so=$'\e'"[${colour}m" LESS_TERMCAP_se=$'\e[39m' \
-    less -EQUXi +/$'\v'"$1"
-  #       │││││ │ └─ highlight matches w/out moving to first one
-  #       │││││ └─── initial command: search
-  #       ││││└───── case-insensitive searching         (--ignore-case)
-  #       │││└────── don't clear the screen on exit     (--no-init)
-  #       ││└─────── treat BS, TAB, and CR as special   (--UNDERLINE-SPECIAL)
-  #       │└──────── never ring the terminal bell       (--QUIET)
-  #       └───────── automatically quit at end of file  (--QUIT-AT-EOF)
+  declare -p | strip_ansi | grep -i "$@" 
 }
