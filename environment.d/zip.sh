dfa3a1f448e5b0c0d58b4cae402ac7f83fd640b0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2017-02-09 08:48:33 -0700

    exclude .DS_Store files from .zip archives

--- environment.d/zip.sh
+++ environment.d/zip.sh
@@ -1,3 +1,4 @@
-export ZIPOPTS="-9 --symlinks"
-#                │   └─ store symlinks as symlinks
-#                └───── maximum compression
+export ZIPOPTS="-9 --symlinks -x \*.DS_Store"
+#                │   │         └─ exclude .DS_Store files (in subdirs too)
+#                │   └─────────── store symlinks as symlinks
+#                └─────────────── maximum compression
