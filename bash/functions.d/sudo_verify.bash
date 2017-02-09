c6e3521bd6ba73d7890ed40a6076fe59f1683bee
Z. Grace Moreau <zgracem@users.noreply.github.com>
2017-02-09 08:36:55 -0700

    new function: sudo_verify()

---
+++ bash/functions.d/sudo_verify.bash
@@ -0,0 +1,13 @@
+sudo_verify()
+{ #: - verify sudo privileges w/out escalating
+  #: = true/false
+  command -v sudo >/dev/null || return 1
+  # TODO: check admin status on Cygwin
+
+  if sudo -v; then  # verified...
+    sudo -k         # ...revoke sudo privileges...
+    return 0        # ...success!
+  else              # user could not verify...
+    return 77       # ...failure :(
+  fi
+}
