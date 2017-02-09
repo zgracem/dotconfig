47a3e1d10541cd1dde553333820231c30c852d85
Grace Moreau <zgm@inescapable.org>
2017-02-09 08:46:14 -0700

    remove BUNDLE_BIN from environment
    (causes problems with rbenv)

--- environment.d/rubygems.sh
+++ environment.d/rubygems.sh
@@ -3,9 +3,4 @@ if command -v gem >/dev/null; then
   export GEM_SPEC_CACHE="${XDG_CACHE_HOME:-$HOME/var/cache}/gem/specs"
 fi
 
-if command -v bundle >/dev/null; then
-  # Bundler should install binstubs to ~/opt/bin, not ~/bin
-  export BUNDLE_BIN="$HOME/opt/bin"
-fi
-
 return 0
