7536e176fd31ea10d1f1ac2e3f0da79847870c76
Grace Moreau <zgm@inescapable.org>
2017-02-09 08:19:31 -0700

    rename field() to chop()

--- bash/functions.d/field.bash
+++
@@ -1,7 +0,0 @@
-# Usage:
-#   echo "foo bar baz" | field " "2 #=> bar
-#   echo "foo:bar:baz" | field :2-3 #=> bar baz
-field()
-{ # >> http://2048.fi/shellnotes.txt
-  cut -d"${1:0:1}" -f"${1:1}"
-}
