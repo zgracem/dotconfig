e69dd971e9c036b7e1a3417cf1ac0d2ffb9760d7
Grace Moreau <zgm@inescapable.org>
2017-02-09 18:12:13 -0700

    map(): upgraded to take stdin

--- bash/functions.d/map.bash
+++ bash/functions.d/map.bash
@@ -1,10 +1,18 @@
 map()
-{ #: - applies a command to each item in a list
+{ #: - applies a command to each item in a list (or stdin)
   #: $ map <command>: <item> [<item> ...]
+  #: $ map <command> <<< <item>[\n<item>[\n<item>...]]
   #: > http://redd.it/aks3u
 
+  if [[ ! -t 0 ]]; then
+    cmd="$@"
+    local i; while read -r i || [[ -n $i ]]; do
+      "$cmd" "$i"
+    done
+    return
+  fi
+
   if [[ $# -lt 2 ]] || [[ ! $@ =~ :[[:space:]] ]]; then
-    scold "${FUNCNAME[0]}: invalid syntax"
     fdoc_usage >&2
     return 64
   fi
