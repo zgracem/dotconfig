7ef64b5ad1dd274ea98ff3524d7644755472e7b3
Grace Moreau <zgm@inescapable.org>
2017-05-17 10:17:26 -0600

    [2017-03-17] improve cd()

--- bash/bashrc.d/cd.bash
+++ bash/bashrc.d/cd.bash
@@ -1,7 +1,12 @@
 # do not create function in Midnight Commander
-if [[ -z $MC_SID ]]; then
-  cd() { builtin pushd "${@:-$HOME}" 1>/dev/null; }
+[[ -n $MC_SID ]] && return
 
-  alias --  -='pushd +1 1>/dev/null'  # -  = go back 1 dir
-  alias -- --='pushd -0 1>/dev/null'  # -- = go forward 1 dir
-fi
+cd()
+{
+  local NEWPWD="${@:-$HOME}"
+  [[ $PWD == $NEWPWD ]] && return
+  builtin pushd "$NEWPWD" 1>/dev/null
+}
+
+alias --  -='pushd +1 1>/dev/null'  # -  = go back 1 dir
+alias -- --='pushd -0 1>/dev/null'  # -- = go forward 1 dir
