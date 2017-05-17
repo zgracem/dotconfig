c996cee9f708e6d1d93e9330d66e970fbaa30522
Grace Moreau <zgm@inescapable.org>
2017-05-17 10:15:38 -0600

    [2017-05-09] upgrade to VNC 6

--- bash/bashrc.d/vnc.bash
+++ bash/bashrc.d/vnc.bash
@@ -2,10 +2,29 @@
 
 vnc()
 {
-  local vnc_conf="$XDG_CONFIG_HOME/vnc/vnc.conf"
+  local vnc_dir="$dir_apps/RealVNC"
+  local vnc_exe="$vnc_dir/VNC-Viewer-6.1.0-Windows-32bit.exe"
+  local vnc_host="10.0.1.10:1"
+
+  if [[ -x $vnc_exe ]]; then
+    if run "$vnc_exe"; then
+      _dtterm "6t" # lower window
+      rollback
+    else
+      return
+    fi
+  else
+    scold "not found: $vnc_exe"
+    return 69
+  fi
+}
+
+vnc5()
+{
   local vnc_dir="$dir_apps/RealVNC"
   local vnc_exe="$vnc_dir/vncviewer.exe"
   local vnc_host="10.0.1.10:1"
+  local vnc_conf="$XDG_CONFIG_HOME/vnc/vnc.conf"
 
   if [[ -x $vnc_exe ]]; then
     vnc_conf=$(cygpath -aw "$vnc_conf")
