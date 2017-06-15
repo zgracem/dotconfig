493469475ce4d0714fc924e347b6802774e9b9b6
Grace Moreau <zgm@inescapable.org>
2017-06-15 07:36:21 -0600

    return of some old image-manipulating functions

---
+++ bash/functions.d/max_width.bash
@@ -0,0 +1,23 @@
+max_width()
+{ #: - resize an image to a maximum number of pixels wide
+  #: $ max_width <pixels> <file>
+
+  (( $# != 2 )) || return 64
+
+  local new_width="$1"
+  local file="$2"
+  local old_width="" old_height="" ratio="" new_height=""
+
+  # get current width and height
+  old_width=$(get_property pixelWidth "$file")
+  old_height=$(get_property pixelHeight "$file")
+
+  # calculate aspect ratio
+  ratio=$(bc -q 2>/dev/null <<< "scale=3;$old_width/$old_height")
+
+  # calculate new height
+  new_height=$(bc -q 2>/dev/null <<< "scale=0;$new_width/$ratio")
+
+  # resize image
+  sips -z "$new_height" "$new_width" "$file" &>/dev/null
+}
