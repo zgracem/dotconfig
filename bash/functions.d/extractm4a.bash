e587f92a50fa9077220c917c02b219808b375ced
Z. Grace Moreau <zgracem@users.noreply.github.com>
2017-06-30 08:37:14 -0600

    new/restored ffmpeg conversion functions

---
+++ bash/functions.d/extractm4a.bash
@@ -0,0 +1,13 @@
+extractm4a()
+{ #: - extracts an M4A audio track from a video container
+  #: $ extractm4a <file> [<file> ...]
+  #: < ffmpeg
+  _require ffmpeg || return
+
+  local input; for input in "$@"; do
+    ffmpeg -i "$in_file" -c:a copy -vn -sn "${input%.*}_extract.m4a"
+    #                     │         │   └─ no subtitles
+    #                     │         └───── no video
+    #                     └─────────────── copy audio as-is
+  done
+}
---
+++ bash/functions.d/makemp3.bash
@@ -0,0 +1,13 @@
+makemp3()
+{ #: - converts an audio file to MP3 (libmp3lame, max quality VBR)
+  #: $ makemp3 <file> [<file> ...]
+  #: < ffmpeg
+  _require ffmpeg || return
+
+  local input; for input in "$@"; do
+    ffmpeg -i "$input" \
+      -codec:a libmp3lame -qscale:a 0 \
+      -map_metadata 0 -id3v2_version 3 -write_id3v1 1 \
+      "${input%.*}.mp3"
+  done
+}
