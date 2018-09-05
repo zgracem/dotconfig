00ee46e5266fa7a8c2b908a5e32f2e51899b7990
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-05 10:20:20 -0600

    new function: itunes()

---
+++ bash/functions.d/itunes.bash
@@ -0,0 +1,15 @@
+itunes()
+( #: - retrieves high-res album art from the iTunes Store
+  #: $ itunes <album_id> [album2_id ...]
+  #: | album_id = the number at the end of the album's iTunes Store URL
+  #: < itunes-album-art.sh
+
+  local script="$dir_scripts/util/itunes-album-art.sh"
+  local dir="$HOME/Desktop"
+
+  [[ -x $script ]] || return 69
+
+  cd "$dir"
+
+  map "$script": "$@"
+)
