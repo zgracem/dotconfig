c24031e2a60954ac4922b2e74efd47f0e9187997
Z. Grace Moreau <zgm@inescapable.org>
2018-09-17 15:02:43 -0600

    new function: favicon()

---
+++ bash/functions.d/favicon.bash
@@ -0,0 +1,26 @@
+_inPath sips && _inPath convert || return
+
+favicon()
+{ #: - create a multi-size favicon.ico from a single PNG file
+  #: $ favicon [img [ico]]
+  #: | img = a 256 × 256 PNG file (default: ./favicon.png)
+  #: | ico = the output icon file (default: ./favicon.ico)
+  local img="${1:-favicon.png}"
+  local out_file="${2:-favicon.ico}"
+
+  if [[ $img != *.png ]]; then
+    scold "$img: not a .png file!"
+    return 65
+  fi
+
+  local -a imgs=()
+  local size; for size in 16 32 48 128; do
+    local size_img="favicon_${size}.png"
+    sips -Z "$size" "$img" --out "$size_img" >/dev/null || return
+    imgs+=("$size_img")
+  done
+
+  convert "${imgs[@]}" "$img" "$out_file" || return
+
+  rm -f "${imgs[@]}" >/dev/null
+}
