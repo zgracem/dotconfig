d8610455f462293278efaa740b3015403ebd751d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-21 09:13:19 -0600

    remove unused functions

--- bash/functions.d/def.bash
+++
@@ -1,4 +0,0 @@
-def()
-{
-  _require dict && dict -d wn "$@"
-}
--- environment.d/inkscape.sh
+++
@@ -1,7 +0,0 @@
-INKSCAPE_PROFILE_DIR="$XDG_DATA_HOME/inkscape" # defaults to ~/.config/inkscape
-
-if command -v inkscape >/dev/null; then
-  export INKSCAPE_PROFILE_DIR
-else
-  unset -v INKSCAPE_PROFILE_DIR
-fi
