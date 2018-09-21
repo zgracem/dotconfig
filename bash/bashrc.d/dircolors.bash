859741cfc9a248fa4df7e219bc6d574bab940e6c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-21 09:47:31 -0600

    replace dircolors logic with Makefile

--- bash/bashrc.d/dircolors.bash
+++ bash/bashrc.d/dircolors.bash
@@ -11,23 +11,13 @@ if [[ -z $LS_COLORS || -n $Z_RELOADING ]]; then
     dc_stub="default"
   fi
 
-  dc_src="$XDG_CONFIG_HOME/dircolors/$dc_stub.dir_colors"
-  dc_cache="$XDG_CACHE_HOME/dircolors/$dc_stub.dir_colors"
+  dc_cache="$XDG_CACHE_HOME/dircolors/$dc_stub.ls_colors"
 
-  if [[ -n $Z_RELOADING || ! -f $dc_cache || $dc_src -nt $dc_cache ]] \
-    && [[ -f $dc_src ]]
-  then
-    verbose 2 ">> dircolors is recreating $dc_cache from $dc_src"
-
-    # create cache dir if it doesn't exist
-    mkdir -p "${dc_cache%/*}" >/dev/null
-
-    # create cache file
-    dircolors -b "$dc_src" >| "$dc_cache"
-  fi
+  # build new .ls_colors files if needed
+  (cd "$XDG_CONFIG_HOME/dircolors" && make --quiet all)
 
   # set and export LS_COLORS
   [[ -f $dc_cache ]] && eval "$(<"$dc_cache")"
 
-  unset -v dc_cache dc_src dc_stub
+  unset -v dc_cache dc_stub
 fi
