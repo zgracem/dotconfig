b0718e9cc03b60cc125420493e1eb7a1d0b67ab7
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-23 12:48:33 -0600

    actually set the variable we're relying on

--- bash/bash_completion.d/brew.bash
+++ bash/bash_completion.d/brew.bash
@@ -1,6 +1,12 @@
+# shellcheck disable=SC1090
 # Homebrew
 # >> http://brew.sh/
+
+HOMEBREW_COMPLETION="$HOMEBREW_PREFIX/etc/bash_completion.d"
+
 if [[ -f $HOMEBREW_COMPLETION/brew ]]; then
   # shellcheck disable=SC1090
   . "$HOMEBREW_COMPLETION/brew"
 fi
+
+unset -v HOMEBREW_COMPLETION
