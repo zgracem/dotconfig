8b4f36d0c3bd2191f3d7dd9fb56cb15501f9468d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-26 21:45:54 -0600

    tweak ag settings

--- sh/profile.d/ag.sh
+++ sh/profile.d/ag.sh
@@ -3,7 +3,8 @@ _inPath ag || return
 ag()
 { #: - search files and directories in the current directory
   command ag \
+      --nobreak --noheading \
       --color-path="34" --color-line-number="36" --color-match="1;4;35" \
-      --hidden --skip-vcs-ignores --smart-case \
+      --hidden --ignore .git --skip-vcs-ignores \
       "$@"
 }
