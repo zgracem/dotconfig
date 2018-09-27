a3bc3bc7dbe13bd60e5305e1f1da9677bd4c81dc
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-27 09:46:54 -0600

    minor fixes

--- bash/functions.d/dbox.bash
+++ bash/functions.d/dbox.bash
@@ -1,5 +1,5 @@
 dbox()
-{ # open the current (or specified) folder at dropbox.com
+{ #: - open the current folder or $1 at dropbox.com
   
   local target="${1-$PWD}"
   local thing; thing=$(readlink -e "$target") || return
@@ -7,16 +7,17 @@ dbox()
   if [[ $thing == $dir_dropbox/* ]]; then
     local db_path=${BASH_REMATCH[1]// /%20}
   else
-    scold "Error: $target does not appear to be in Dropbox"
+    scold "$target: does not appear to be in Dropbox"
     return 1
   fi
 
   if [[ -d $thing ]]; then
     local url="https://www.dropbox.com/home/$db_path?d=1"
-    # get parent directory instead
-    # thing=${thing%/*}
-  else
+  elif [[ -e $thing ]]; then
     local url="https://www.dropbox.com/revisions/$db_path?_subject_uid=$DROPBOX_UID"
+  else
+    scold "$thing: not found"
+    return 1
   fi
 
   "$BROWSER" "$url" 
