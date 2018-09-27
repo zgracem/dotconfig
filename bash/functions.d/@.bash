86aae886d759cedcd0033384ddfc7ff999ce00e0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-27 08:18:33 -0600

    fix fxdoc comments

--- bash/functions.d/@.bash
+++ bash/functions.d/@.bash
@@ -1,6 +1,5 @@
-#: - converts a Unix timestamp to an RFC 2822–formatted string
-#: $ @ <timestamp>
 function @ 
-{ 
+{ #: - converts a Unix timestamp to an RFC 2822–formatted string
+  #: $ @ <timestamp>
   date -d "@$1" --rfc-2822; 
 }
