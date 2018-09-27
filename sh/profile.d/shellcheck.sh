4ec5426fe1dc728f06e5d64d17c4fca8c81019cf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-26 21:47:41 -0600

    remove redundant SHELLCHECK_OPTS

--- sh/profile.d/shellcheck.sh
+++
@@ -1,6 +0,0 @@
-if _inPath shellcheck; then
-  export SHELLCHECK_OPTS="-x -e SC2155 -e SC2034"
-  # -x = source external files
-  # -e SC2155 = don't require shebang
-  # -e SC2034 = don't flag "unused" variables
-fi
