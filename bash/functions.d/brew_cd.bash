2fa4540c6f98d157b6d2cdcbca226275d771b791
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-29 12:06:48 -0600

    improve documentation of brew_cd

--- bash/functions.d/brew_cd.bash
+++ bash/functions.d/brew_cd.bash
@@ -2,6 +2,7 @@ _inPath brew || return
 
 brew_cd()
 { #: - navigate to important Homebrew directories
+  #: $ brew_cd [cache|cellar|prefix|repo[sitory]]
   local destination="$1"
   case $destination in
     cache|cellar|prefix|repo|repository)
