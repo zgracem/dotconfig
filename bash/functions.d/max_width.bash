d96ef981ae4196f0bfe46bbf931f2202fe7aba15
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-09-30 15:40:35 -0600

    add IM as 1st preference to max_{dim,width}

--- bash/functions.d/max_width.bash
+++ bash/functions.d/max_width.bash
@@ -1,3 +1,5 @@
+_inPath convert || _inPath sips || return
+
 max_width()
 { #: - resize an image to a maximum number of pixels wide
   #: $ max_width <pixels> <file>
@@ -7,23 +9,25 @@ max_width()
     return 64
   fi
 
-  _require sips || return
-
   local new_width="$1"
   local file="$2"
   local new_file="${file%.*}_${new_width}px.${file##*.}"
   local old_width="" old_height="" ratio="" new_height=""
 
-  # get current width and height
-  old_width=$(get_property pixelWidth "$file")
-  old_height=$(get_property pixelHeight "$file")
+  if _inPath convert; then
+    convert "$file" -resize "$new_width" "$new_file"
+  elif _inPath sips; then
+    # get current width and height
+    old_width=$(get_property pixelWidth "$file")
+    old_height=$(get_property pixelHeight "$file")
 
-  # calculate aspect ratio
-  ratio=$(bc -q 2>/dev/null <<< "scale=3;$old_width/$old_height")
+    # calculate aspect ratio
+    ratio=$(bc -q 2>/dev/null <<< "scale=3;$old_width/$old_height")
 
-  # calculate new height
-  new_height=$(bc -q 2>/dev/null <<< "scale=0;$new_width/$ratio")
+    # calculate new height
+    new_height=$(bc -q 2>/dev/null <<< "scale=0;$new_width/$ratio")
 
-  # resize image
-  sips -z "$new_height" "$new_width" "$file" --out "$new_file" &>/dev/null
+    # resize image
+    sips -z "$new_height" "$new_width" "$file" --out "$new_file" &>/dev/null
+  fi
 }
