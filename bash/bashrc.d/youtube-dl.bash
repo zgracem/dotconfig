dd6dcaa26c3479e63e4353b18a993841abf6f41f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-10-06 12:36:56 -0600

    simplify ydl()

--- bash/bashrc.d/youtube-dl.bash
+++ bash/bashrc.d/youtube-dl.bash
@@ -1,17 +1,11 @@
 # youtube-dl
 # >> http://rg3.github.io/youtube-dl/
+_inPath youtube-dl || return
 
 ydl()
-{ #: - download video(s) to ~/Downloads
-  _require youtube-dl || return
-
-  # get output format from configuration file
-  local output_fmt
-  local config_file="$XDG_CONFIG_HOME/youtube-dl/config"
-  output_fmt=$(sed -nE 's/^--output "([^"]+)".*/\1/p' "$config_file")
-
-  youtube-dl -o "$dir_downloads/$output_fmt" "$@"
-}
+( #: - download video(s) to ~/Downloads
+  cd "$dir_downloads" && youtube-dl "$@"
+)
 
 ydlq()
 { #: - download video(s) quietly in the background
