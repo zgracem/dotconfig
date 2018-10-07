8eb579750a2b2b2fe7a19f547cc85a7f221aeb7e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-10-07 15:39:00 -0600

    clean up completions

--- bash/bash_completion.d/brew.bash
+++ bash/bash_completion.d/brew.bash
@@ -1,10 +1,6 @@
 # Homebrew
 # >> http://brew.sh/
 
-HOMEBREW_COMPLETION="$HOMEBREW_PREFIX/etc/bash_completion.d"
-
 if [[ -f $HOMEBREW_COMPLETION/brew ]]; then
   . "$HOMEBREW_COMPLETION/brew"
 fi
-
-unset -v HOMEBREW_COMPLETION
--- bash/bash_completion.d/youtube-dl.bash
+++
@@ -1,5 +0,0 @@
-# youtube-dl
-# >> https://rg3.github.io/youtube-dl/
-if [[ -f $HOMEBREW_COMPLETION/youtube-dl.bash-completion ]]; then
-  . "$HOMEBREW_COMPLETION/youtube-dl.bash-completion"
-fi
