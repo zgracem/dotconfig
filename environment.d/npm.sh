23e8cebc5809c4d7aa238c61850dfdb55364f29a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-10-07 15:51:46 -0600

    prevent creation of ~/.config/configstore

--- environment.d/npm.sh
+++ environment.d/npm.sh
@@ -1,4 +1,6 @@
 if command -v npm >/dev/null; then
   export npm_config_userconfig="$XDG_CONFIG_HOME/npmrc"
   export npm_config_cache="$XDG_CACHE_HOME/npm"
+  # prevent creation of ~/.config/configstore
+  export NO_UPDATE_NOTIFIER=true
 fi
