89a53b127612a571b4e4366b0a51622cec85c4e2
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-10-07 11:42:35 -0600

    update npm settings

--- environment.d/npm.sh
+++ environment.d/npm.sh
@@ -1,7 +1,4 @@
-npm_config_userconfig="$XDG_CONFIG_HOME/npmrc"
-
 if command -v npm >/dev/null; then
-  export npm_config_userconfig
-else
-  unset -v npm_config_userconfig
+  export npm_config_userconfig="$XDG_CONFIG_HOME/npmrc"
+  export npm_config_cache="$XDG_CACHE_HOME/npm"
 fi
