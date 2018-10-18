a1d42e49cae2589b825a5ddca8f42ac07f9fd4f9
Z. Grace Moreau <zgracem@users.noreply.github.com>
2018-10-18 17:01:56 -0600

    improve docstrings

--- bash/bashrc.d/subl.bash
+++ bash/bashrc.d/subl.bash
@@ -1,8 +1,8 @@
 # Sublime Text
 s()
-{ #: - launches Sublime Text
-  #: $ s <file> [<file> ...]
-  #: $ echo "$list_of_files" | s
+{ #: - launches Sublime Text and adds files or dirs to topmost window
+  #: $ s <file_or_dir> [file_or_dir ...]
+  #: $ ls -1 *.md | s
   if [[ ! -t 0 ]]; then
     # accept a list of files to open on standard input
     local file; while read -r file || [[ -n $file ]]; do
