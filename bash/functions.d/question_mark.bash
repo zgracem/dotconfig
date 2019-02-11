7d627f602c681e06542de181f6661a63b686af68
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-11 11:10:44 -0700

    print signal name if applicable

--- bash/functions.d/question_mark.bash
+++ bash/functions.d/question_mark.bash
@@ -5,11 +5,22 @@ function ?
   local text="" colour="" reset=$'\e[0m'
 
   if (( last_exit == 0 )); then
-    text="true"
     colour=$'\e[92m'
+    text="true"
   else
-    text="false [$last_exit]"
     colour=$'\e[91m'
+    if (( last_exit > 128 )) && sigspec=$(builtin kill -l "$last_exit" 2>/dev/null); then
+      last_exit="$last_exit=${sigspec#SIG}"
+    elif (( last_exit >= 64 && last_exit <= 78 )); then
+      # Someone's been reading sysexits(3)... ;)
+      local -ra exits=( [64]="USAGE"    [65]="DATAERR"     [66]="NOINPUT"
+        [67]="NOUSER"   [68]="NOHOST"   [69]="UNAVAILABLE" [70]="SOFTWARE"
+        [71]="OSERR"    [72]="OSFILE"   [73]="CANTCREAT"   [74]="IOERR"
+        [75]="TEMPFAIL" [76]="PROTOCOL" [77]="NOPERM"      [78]="CONFIG" )
+      last_exit="$last_exit=${exits[$last_exit]}"
+    fi
+
+    text="false [$last_exit]"
   fi
 
   printf "%b\\n" "${colour}${text}${reset}"
