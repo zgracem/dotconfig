0fb5602f4328fdf5c6ee67712eb70850b54d589a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-11 11:09:17 -0700

    remove some unused functions

--- bash/functions.d/emoji_support.bash
+++
@@ -1,5 +0,0 @@
-emoji_support()
-{ #: - checks TERM_PROGRAM for emoji support
-  #: = true/false
-  [[ $TERM_PROGRAM =~ Apple_Terminal|iTerm\.app|Prompt_2|Coda ]]
-}
--- bash/functions.d/html.bash
+++
@@ -1,4 +0,0 @@
-html()
-{ #: - opens HTML string (from arg or stdin) in BROWSER
-  ~/scripts/util/md.sh <<< "${@:-$(</dev/stdin)}"
-}
