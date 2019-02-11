1dfb4522641a7c21e2861a71e4981b4b2955acec
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-11 07:45:40 -0700

    run astro.rb only once per day

--- bash/init.bash
+++ bash/init.bash
@@ -7,6 +7,10 @@ if [[ -d $dir_scripts ]]; then
   # cute banner
   "$dir_scripts/login/loginbanner.sh"
 
-  # astronomical info -- requires suncalc and weather_ca gems
-  [[ $HOSTNAME == Athena* ]] && "$dir_scripts/login/astro.rb"
+  # astronomical info 
+  # -- requires suncalc and weather_ca gems
+  # -- runs only on first login of the day
+  if [[ $HOSTNAME == Athena* ]] && "$dir_scripts/login/matins.sh"; then
+    "$dir_scripts/login/astro.rb"
+  fi
 fi
