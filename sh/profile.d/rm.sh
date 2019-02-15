afcb5a45fee99cf4b4950cd04c2453ee86faa5fc
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-15 08:06:42 -0700

    give GNU rm "flexible" interactivity

--- sh/profile.d/rm.sh
+++ sh/profile.d/rm.sh
@@ -1,3 +1,7 @@
-rm() { command rm -iv "$@"; }
-#                  │└─ verbose
-#                  └── interactive
+if _isGNU rm; then
+  rm() { command rm -Iv "$@"; }
+else
+  rm() { command rm -iv "$@"; }
+  #                  │└─ verbose
+  #                  └── interactive
+fi
