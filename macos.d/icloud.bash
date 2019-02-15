e9514c702170424d0854cdae1d8d142d672c7afa
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-15 15:29:49 -0700

    remove macOS config files from repo

--- macos.d/icloud.bash
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------------------------------
-# iCloud
-# -----------------------------------------------------------------------------
-
-# Save to disk (not to iCloud) by default
-defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
--- macos.d/screenshots.bash
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------------------------------
-# Screenshots
-# -----------------------------------------------------------------------------
-
-# Disable shadow in screenshots
-defaults write com.apple.screencapture disable-shadow -bool true
