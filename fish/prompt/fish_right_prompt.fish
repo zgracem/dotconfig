abf55f8412569207549c24bcd04b94133286eb7d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-18 08:26:39 -0700

    fix spacing

--- fish/prompt/fish_right_prompt.fish
+++ fish/prompt/fish_right_prompt.fish
@@ -1,13 +1,15 @@
 function fish_right_prompt --description 'Display the right side of the interactive prompt'
-	set -l exit $status
+  set -l exit $status
 
-	if test $exit -ne 0
-		set_color $__fish_prompt_color_exit
-		echo -s (__fish_right_prompt_signal $exit) " "
+  if test $exit -ne 0
+    set_color $__fish_prompt_color_exit
+    echo -s (__fish_right_prompt_signal $exit) " "
     set_color normal
-	end
+  end
 
-  # __fish_right_prompt_mode
+  # if test $fish_key_bindings = "fish_vi_key_bindings"
+  #   __fish_right_prompt_mode
+  # end
 
   # if test $COLUMNS -gt 132
   #   set_color $__fish_prompt_color_clock
