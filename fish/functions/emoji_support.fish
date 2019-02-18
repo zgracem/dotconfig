a4460a88eed5d3b6f029ecc17840a73c4921f972
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-18 08:26:47 -0700

    add `emoji_support` function

---
+++ fish/functions/emoji_support.fish
@@ -0,0 +1,8 @@
+function emoji_support --description 'Returns true if TERM_PROGRAM supports emoji'
+	switch "$TERM_PROGRAM"
+  case Apple_Terminal 'iTerm*' Prompt_2 Coda
+    true
+  case '*'
+    false
+  end
+end
