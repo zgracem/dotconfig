e1f45497010468aa417575cf30886695715f1723
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-18 07:46:15 -0700

    reverse direction of ascii fish in fish_greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,10 +1,11 @@
 function fish_greeting --description 'Prints a greeting message on startup'
   echo -s \
-    (set_color cyan)   '>' \
-    (set_color green)  '<' \
-    (set_color blue)   '(((' \
+    (set_color cyan)  '<' \
     (set_color brcyan) '°' \
-    (set_color green)  '>' \
+    (set_color brblue)   ')))' \
+    (set_color cyan)  '>' \
+    (set_color green)   '<' \
     (set_color normal) ' Welcome to ' \
     (set_color white)  "fish $FISH_VERSION"
+  set_color normal
 end
