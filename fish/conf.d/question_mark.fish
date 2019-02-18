54ee4cd01412842e725cdfb1649022ab910f93e6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-18 09:37:34 -0700

    reformat code and output of `?`

--- fish/conf.d/question_mark.fish
+++ fish/conf.d/question_mark.fish
@@ -8,18 +8,25 @@ function '?' --description 'Prints the exit status of the last command'
     return 0
   end
 
-  set -l signals HUP INT QUIT ILL TRAP ABRT EMT FPE KILL BUS SEGV SYS PIPE ALRM TERM URG STOP TSTP CONT CHLD TTIN TTOU IO XCPU XFSZ VTALRM PROF WINCH PWR USR1 USR2 RTMIN RTMIN+1 RTMIN+2 RTMIN+3 RTMIN+4 RTMIN+5 RTMIN+6 RTMIN+7 RTMIN+8 RTMIN+9 RTMIN+10 RTMIN+11 RTMIN+12 RTMIN+13 RTMIN+14 RTMIN+15 RTMIN+16 RTMAX-15 RTMAX-14 RTMAX-13 RTMAX-12 RTMAX-11 RTMAX-10 RTMAX-9 RTMAX-8 RTMAX-7 RTMAX-6 RTMAX-5 RTMAX-4 RTMAX-3 RTMAX-2 RTMAX-1 RTMAX
-  set -l sysexits USAGE DATAERR NOINPUT NOUSER NOHOST UNAVAILABLE SOFTWARE OSERR OSFILE CANTCREAT IOERR TEMPFAIL PROTOCOL NOPERM CONFIG
+  # Source: `kill -l`
+  set -l signals  HUP INT QUIT ILL TRAP ABRT EMT FPE KILL BUS SEGV SYS PIPE \
+                  ALRM TERM URG STOP TSTP CONT CHLD TTIN TTOU IO XCPU XFSZ \
+                  VTALRM PROF WINCH INFO USR1 USR2
+  # Source: `man 3 sysexits`
+  set -l sysexits USAGE DATAERR NOINPUT NOUSER NOHOST UNAVAILABLE SOFTWARE \
+                  OSERR OSFILE CANTCREAT IOERR TEMPFAIL PROTOCOL NOPERM CONFIG
 
   set_color brred
-  echo -ns "false ($last_exit"
 
   if test $last_exit -gt 128 -a $last_exit -le 165
-    echo -ns "=" $signals[(math "$last_exit - 128")]
+    echo -ns "SIG" $signals[(math "$last_exit - 128")]
   else if test $last_exit -ge 64 -a $last_exit -le 78
-    echo -ns "=" $sysexits[(math "$last_exit - 63")]
+    echo -ns "EX_" $sysexits[(math "$last_exit - 63")]
+  else
+    echo -ns "false"
   end
 
-  echo ")"
+  set_color red
+  echo " ($last_exit)"
   set_color normal
 end
