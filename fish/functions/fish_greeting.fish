43a359fc7736830bb869d8dbf47d404e26289a55
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-19 07:39:33 -0700

    fix alignment in fish_greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,11 +1,13 @@
 function fish_greeting --description 'Prints a greeting message on startup'
   echo -s \
-    (set_color cyan)  '<' \
+    (set_color cyan)   '<' \
     (set_color brcyan) '°' \
-    (set_color brblue)   ')))' \
-    (set_color cyan)  '>' \
-    (set_color green)   '<' \
+    (set_color brblue) ')))' \
+    (set_color cyan)   '>' \
+    (set_color green)  '<' \
     (set_color normal) ' Welcome to ' \
-    (set_color white)  "fish $FISH_VERSION"
+    (set_color white)  'fish' \
+    (set_color normal) ', version ' \
+    (set_color white)  "$FISH_VERSION"
   set_color normal
 end
