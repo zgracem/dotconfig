f6c015bbfc277cfe66d07cfee2d8ea552039704e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-19 07:39:13 -0700

    new `ef` function to wrap `funced`

---
+++ fish/functions/ef.fish
@@ -0,0 +1,5 @@
+function ef --wraps funced --description 'Edit a function interactively'
+	set -lx TMPDIR "$XDG_RUNTIME_DIR/fish"
+  test -d "$TMPDIR"; or mkdir -pv "$TMPDIR"
+  funced --save $argv[1]
+end
