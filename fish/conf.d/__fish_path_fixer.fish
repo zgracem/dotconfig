a1056e2256d5c8567b0ba9c476941c9360af3877
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-19 08:58:42 -0700

    make path_fixer function more fish-y

--- fish/conf.d/__fish_path_fixer.fish
+++ fish/conf.d/__fish_path_fixer.fish
@@ -1,15 +1,15 @@
 function __fish_path_fixer --description 'Removes duplicate and missing dirs from a PATH variable'
   set -l dir
-  set -l out_PATH
+  set -l ORIGINAL_PATH $argv
+  set -l FIXED_PATH
 
-  for dir in $argv
-    switch "$out_PATH"
-    case "*$dir*"
-      :
-    case '*'
-      test -d "$dir"; and set -a out_PATH $dir
+  for dir in $ORIGINAL_PATH
+    if contains $dir $FIXED_PATH
+      continue
+    else
+      test -d "$dir"; and set -a FIXED_PATH $dir
     end
   end
 
-  echo -n "$out_PATH"
+  echo -n "$FIXED_PATH"
 end
