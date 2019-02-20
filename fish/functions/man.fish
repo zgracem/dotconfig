22bc98119613c517c5a3a8ffa7f9b8c2029447ac
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-20 11:51:42 -0700

    port clever `man` wrapper to fish

---
+++ fish/functions/man.fish
@@ -0,0 +1,40 @@
+function man --description 'Display manual pages in a new window with a nice title'
+  if not isatty stdout
+    command man $argv
+    return
+  end
+
+  # TODO: replace with fish_opt/argparse
+  if test (string sub -l 1 -- $argv[1]) = "-"
+    switch (string sub -s 2 -- $argv[1])
+    case '*d*' '*f*' '*h*' '*k*' '*V*' '*w*' '*W*' '*?*'
+      command man $argv
+      return $status
+    case '*'
+      true
+    end
+  end
+
+  set -l title (__man_title $argv)
+  or return $status
+
+  if in-tmux
+    tmux new-window -n $title "env MANLESS= man $argv"
+  else
+    __fish_prompt_set_window_title $title
+    command man $argv
+  end
+end
+
+function __man_title
+  set -l manfile (command man -w $argv)
+  or return $status
+
+  set -l match (string match -r '.*/(.+?)\.(.+?)(?:\.gz)?' "$manfile")
+  or return $status
+
+  set -l title $match[2]
+  set -l section $match[3]
+
+  echo "$title($section)"
+end
