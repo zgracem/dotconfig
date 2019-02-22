0f76d89ed429ebda874be83921147955d4efb355
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-22 09:24:13 -0700

    Use sh(1) to populate the environment
    Now that I'm using fish as my login shell (!), I need to bootstrap it via sh(1)
    to allow it access to the contents of ~/.config/environment.d.

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,12 @@ if test $FISH_VERSINFO[1] -lt 3
   exit 1
 end
 
+if test -z "$ENV"
+  # Use sh(1) to populate the environment from ~/.config/environment.d.
+  # Both the -l and -i flags are needed to prevent an infinite loop.
+  exec sh -l -i -c "exec "(status fish-path)
+end
+
 source "$__fish_config_dir/paths.fish"
 source "$__fish_config_dir/aliases.fish"
 
