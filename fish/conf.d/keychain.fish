065c5b55225a6578227bdb287abff7ab627bc70b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-22 19:13:04 -0700

    port `keychain` ssh-agent init to fish

---
+++ fish/conf.d/keychain.fish
@@ -0,0 +1,11 @@
+if [ -z "$SSH_AGENT_PID" ]
+  set -l keys id_ed25519 id_rsa
+  set -l keychain_env \
+    (keychain --eval --quick --quiet \
+              --absolute --dir "$XDG_RUNTIME_DIR/keychain" \
+              --inherit any \
+              --ignore-missing $keys)
+  or exit
+
+  eval $keychain_env
+end
