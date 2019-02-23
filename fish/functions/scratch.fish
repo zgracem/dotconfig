29b440e8c6573cbc6ff34e88d0dca716b4bdd3b2
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-23 12:17:27 -0700

    port `scratch` function to fish

---
+++ fish/functions/scratch.fish
@@ -0,0 +1,9 @@
+function scratch -a template --description 'Create a temporary directory and change into it'
+	# Inspired by <https://sanctum.geek.nz/cgit/dotfiles.git/tree/sh/shrc.d/scr.sh>
+  set -q template; or set -l template (status current-function)
+
+  # be BSD/GNU-agnostic vis-á-vis mktemp(1)
+  is-gnu mktemp; and set template "$template.XXXXXX"
+
+  set -l dir (mktemp -d -t "$template"); and cd "$dir"
+end
