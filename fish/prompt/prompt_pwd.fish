1adfc3245e29a92809d517cf08a15434174c6551
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-23 10:59:23 -0700

    don't choke and die on short paths (oops)

--- fish/prompt/prompt_pwd.fish
+++ fish/prompt/prompt_pwd.fish
@@ -8,11 +8,17 @@ function prompt_pwd --description 'Print the current working directory, shortene
   set -q fish_prompt_pwd_dir_glyph
   or set -l fish_prompt_pwd_dir_glyph "…"
 
-  set -l cwd_parts (echo $argv[1] | string replace -r "^$HOME(?=\$|/)" "~" | string split "/")
+  set -l cwd (string replace -r "^$HOME(?=\$|/)" "~" $argv[1])
+  set -l cwd_parts (string split "/" "$cwd")
 
-  for i in (seq 1 (math (count $cwd_parts) - 1))
-    if [ (string length $cwd_parts[$i]) -gt $fish_prompt_pwd_dir_length ]
-      set cwd_parts[$i] (string sub -l $fish_prompt_pwd_dir_length "$cwd_parts[$i]")"$fish_prompt_pwd_dir_glyph"
+  if [ (count $cwd_parts) -le 1 ]
+    echo $cwd
+    return 0
+  else
+    for i in (seq 1 (math (count $cwd_parts) - 1))
+      if [ (string length $cwd_parts[$i]) -gt $fish_prompt_pwd_dir_length ]
+        set cwd_parts[$i] (string sub -l $fish_prompt_pwd_dir_length "$cwd_parts[$i]")"$fish_prompt_pwd_dir_glyph"
+      end
     end
   end
 
