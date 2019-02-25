81de4653e9ac18c3d73bff90c6392657bdc1a9c0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-25 09:12:02 -0700

    obtain long hostname (once) for fish_title

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,3 +1,7 @@
 function fish_title --description 'Update the window title'
-  echo "$USER@$hostname:" (pwd | string replace -r "^$HOME" "~")
+  set -q long_hostname
+  or set -g long_hostname (/bin/hostname -f 2>/dev/null)
+  or set -g long_hostname $hostname
+
+  printf "%s@%s: %s" $USER $long_hostname (pwd | string replace -r "^$HOME" "~")
 end
