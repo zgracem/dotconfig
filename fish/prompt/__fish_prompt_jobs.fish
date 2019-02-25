921883840b28dfe2a191b0172b01cd9d64c6c741
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-24 20:24:26 -0700

    simplify jobs status in prompt

--- fish/prompt/__fish_prompt_jobs.fish
+++ fish/prompt/__fish_prompt_jobs.fish
@@ -1,7 +1,5 @@
 function __fish_prompt_jobs --description 'Helper function for fish_prompt'
-  jobs >/dev/null; or return 0
-
-  set job_count (jobs | wc -l)
-
-  echo -n (set_color $__fish_prompt_color_jobs) "$job_count"
+  set job_count (count (jobs)); or return 0
+  set_color $__fish_prompt_color_jobs
+  echo -n " $job_count"
 end
