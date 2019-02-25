e9bfa13f355aa27bcea55eceeacb845e21269041
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-24 19:45:26 -0700

    add `cd` subcommand to `brew`

--- fish/functions/brew.fish
+++ fish/functions/brew.fish
@@ -1,8 +1,19 @@
 # Addresses https://github.com/Homebrew/brew/issues/5791
 function brew
-  if [ "$argv[1]" = "update" ]
-  	env SHELL=(type -P bash) brew $argv
-  else
+  switch $argv[1]
+  case 'cd'
+    __brew_cd $argv[2..-1]
+  case '*'
     command brew $argv
+  end
+end
+
+function __brew_cd
+  switch $argv[1]
+  case 'cache' 'cellar' 'prefix' 'repo' 'repository'
+    cd (command brew --$argv[1] $argv[2..-1])
+  case '*'
+    printf >&2 "%s: destination unknown\\n" "$destination"
+    return 1
   end
 end
