8e412deff3fc7e8e75db6e478316bdf5cda1f102
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-26 09:31:14 -0700

    fix majorly bone-headed error

--- fish/conf.d/___env.fish
+++ fish/conf.d/___env.fish
@@ -3,7 +3,7 @@ set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME "$HOME/var/cache"
 set -q XDG_RUNTIME_DIR; or set -gx XDG_RUNTIME_DIR "$HOME/var/run"
 
-if set -q ENV; and string match -eq 'fish' "$SHELL"
+if not set -q ENV; and string match -eq 'fish' "$SHELL"
   # Use sh(1) to populate the environment from ~/.config/environment.d.
   # Both the -l and -i flags are needed to prevent an infinite loop.
   exec sh -l -i -c "exec $SHELL"
