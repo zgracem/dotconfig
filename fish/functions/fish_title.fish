5a9641b9f370f36430007b3bc013ad403daada1a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-02-26 08:13:11 -0700

    add current command to fish_title
    Also allow for setting of tab titles in terminals like iTerm.

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,7 +1,11 @@
+# See also ~/.config/fish/conf.d/__fish_prompt_update_title.fish
 function fish_title --description 'Update the window title'
   set -q long_hostname
   or set -g long_hostname (/bin/hostname -f 2>/dev/null)
   or set -g long_hostname $hostname
 
   printf "%s@%s: %s" $USER $long_hostname (pwd | string replace -r "^$HOME" "~")
+
+  set -q argv[1]
+  and printf " (%s)" (status current-command)
 end
