08c147ce4b9644314d5be40aed3cf192b5b16e4c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-02 10:36:10 -0700

    new function `rl` for reloading config files

---
+++ fish/functions/rl.fish
@@ -0,0 +1,28 @@
+function rl
+	if test (count $argv) -eq 0
+    source "$__fish_config_dir/config.fish"
+    return
+  end
+
+  set -l errors 0
+  for arg in $argv
+    if functions -q $arg
+      set -l function_file (functions -D $arg)
+      if test -f "$function_file"
+        source "$function_file"
+        or set errors (math $errors + 1)
+      else
+        echo >&2 "couldn't find file for '$arg': $function_file"
+        set errors (math $errors + 1)
+      end
+    else if test -f "$__fish_config_dir/$arg.fish"
+      source "$__fish_config_dir/$arg.fish"
+      or set errors (math $errors + 1)
+    else
+      echo >&2 "couldn't reload '$arg'"
+      set errors (math $errors + 1)
+    end
+  end
+
+  return $errors
+end
