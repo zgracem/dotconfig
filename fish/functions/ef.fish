0d111964e29d66309cd154322a919aae73f28434
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-03 13:34:52 -0700

    rewrite `ef` to replace `funced`, not wrap it
    The way `funced` currently works results in data loss: specifically, because of
    its reliance on `fish_indent`, `funced` will strip comments & any code outside
    the main function definition from any file it touches. I'm reluctant to call
    this a bug in fish but it's definitely not behaviour I want, so here's a
    minimal reimplementation that behaves the way I prefer.

--- fish/functions/ef.fish
+++ fish/functions/ef.fish
@@ -1,5 +1,26 @@
-function ef --wraps funced --description 'Edit a function interactively'
-  set -lx TMPDIR "$XDG_RUNTIME_DIR/fish"
-  test -d "$TMPDIR"; or mkdir -pv "$TMPDIR"
-  funced --save $argv[1]
+function ef -a function --wraps funcsave --description 'Edit a function interactively'
+  set -l function_info (functions --details --verbose -- $function)
+  set -l function_source $function_info[1]
+  set -l function_line $function_info[3]
+
+  switch $function_source
+  case 'n/a' '-'
+    set function_source "$__fish_config_dir/functions/$function.fish"
+  end
+
+  if functions -q $function; and not test -f $function_source
+    functions -- $function >$function_source
+  end
+
+  if test $function_line -gt 1
+    set function_source "$function_source:$function_line"
+  end
+
+  if $VISUAL $function_source
+    and test -f $function_source
+    source (string split : "$function_source")[1]
+  else
+    echo >&2 "editor failed or was cancelled"
+    return $status
+  end
 end
