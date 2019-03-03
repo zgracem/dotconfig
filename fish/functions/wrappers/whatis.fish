899bdd7a4ba17eca56fcb5ed0110c79bdb8243e9
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-03 13:02:23 -0700

    wrap man-db `whatis` for actually useful output

---
+++ fish/functions/wrappers/whatis.fish
@@ -0,0 +1,14 @@
+# If `gwhatis` is present, that's the one whose completions we want to use.
+# No need to worry about executing it; there's a shadow `whatis` link in PATH.
+set cmd 'whatis'; in-path gwhatis; and set cmd 'gwhatis'
+
+function whatis -a term --wraps $cmd --description 'Search the whatis database for exact matches'
+  set -l args $argv[2..-1]
+  set -l output (whatis --regex '\b'"$term"'\b' $args 2>/dev/null | sort -u)
+  test -n "$output"; or return 16
+
+  # Collapse needless whitespace in $output
+  string replace -r '\s+' ' ' "$output"
+end
+
+set --erase cmd
