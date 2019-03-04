59226396cfc950289ecba6fefb4f0ef0ef3a26f0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-04 10:13:24 -0700

    port `max_dim` function to fish

---
+++ fish/functions/max_dim.fish
@@ -0,0 +1,11 @@
+function max_dim -a pixels file --description 'Resize an image to fit within a specified pixel size'
+  set -l new_file (string replace -r '(.*)\.(.*)' '$1_'$pixels'px.$2' $file)
+
+  if in-path sips
+    sips -Z $pixels $file --out $new_file >/dev/null 2>&1
+  else if in-path convert
+    convert $file -resize "$pixels"x"$pixels" $new_file
+  else
+    return 127
+  end
+end
