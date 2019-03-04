62b01ae2472d4bac4b9d2e2e29d292d2d6fa80ec
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-04 09:00:53 -0700

    no need to prefix these with `command`

--- fish/functions/newwin.fish
+++ fish/functions/newwin.fish
@@ -5,12 +5,12 @@ function newwin --description 'Open a command in a new tmux window'
     eval $argv
     return
   else if test (count $argv) -eq 0
-    command tmux new-window
+    tmux new-window
     return
   end
 
   set -q _flag_title
   or set _flag_title (string split -r -m1 / $argv[1])[-1]
 
-  command tmux new-window -n $_flag_title (string escape -- $argv)
+  tmux new-window -n $_flag_title (string escape -- $argv)
 end
