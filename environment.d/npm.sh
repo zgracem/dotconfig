ddf7eee9362a27b592b9f1b8b8f732990faa52c4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-04 08:58:15 -0700

    move npm_config vars into npmrc

--- environment.d/npm.sh
+++ environment.d/npm.sh
@@ -1,7 +1,5 @@
 if command -v npm >/dev/null; then
   export npm_config_userconfig="$XDG_CONFIG_HOME/npm/npmrc"
-  export npm_config_cache="$XDG_CACHE_HOME/npm"
-  export npm_config_tmp="$XDG_RUNTIME_HOME/npm"
   # prevent creation of ~/.config/configstore
   export NO_UPDATE_NOTIFIER=true
 fi
