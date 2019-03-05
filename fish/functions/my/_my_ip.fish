a6e4df7ce8a2081612655dcd0ee27760fb9e4625
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-04 17:34:11 -0700

    change order
    `ip` is more reliable than `dig`

--- fish/functions/my/_my_ip.fish
+++ fish/functions/my/_my_ip.fish
@@ -1,9 +1,9 @@
 function _my_ip
-  if in-path dig
+  if in-path ip
+    ip -o route get to 1.1.1.1 | string replace -rf '.*src ([\d.]+).*' '$1'
+  else if in-path dig
     dig +short @resolver1.opendns.com myip.opendns.com
   else if in-path nslookup
-    echo (nslookup myip.opendns.com resolver1.opendns.com 2>/dev/null | string replace -fr '^Address: +(.*)' '$1')[-1]
-  else if in-path ip
-    ip -o route get to 1.1.1.1 | string replace -rf '.*src ([\d.]+).*' '$1'
+    echo (nslookup myip.opendns.com resolver1.opendns.com 2>/dev/null \| string replace -fr '^Address: +(.*)' '$1')[-1]
   end
 end
