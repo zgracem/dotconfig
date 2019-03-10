ce8a5be72506c829c4f89098d4fc319b25d9df5c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-10 10:26:57 -0600

    remove unused function

--- fish/functions/emoji_support.fish
+++
@@ -1,8 +0,0 @@
-function emoji_support --description 'Returns true if TERM_PROGRAM supports emoji'
-  switch "$TERM_PROGRAM"
-  case Apple_Terminal 'iTerm*' Prompt_2 Coda
-    true
-  case '*'
-    false
-  end
-end
