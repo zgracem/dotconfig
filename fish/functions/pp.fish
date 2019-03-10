f70a8b466654c08a508b0871e08676e89e69cc02
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-10 14:02:29 -0600

    cleaner syntax

--- fish/functions/pp.fish
+++ fish/functions/pp.fish
@@ -1,5 +1,8 @@
 function pp -a file --description 'Pretty-print data and source code'
-  switch $file
+  switch "$file"
+  case ''
+    echo >&2 'Usage: pp <file>'
+    return 1
   case '*.json'
     jq . < $file
   case '*.plist'
@@ -10,8 +13,8 @@ function pp -a file --description 'Pretty-print data and source code'
     if in-path src-hilite-lesspipe.sh
       src-hilite-lesspipe.sh $file
     else
-      echo >&2 -s "error: don't know how to print ." \
-        (string split -r -m1 . $file)[-1]" files"
+      set -l ext (string split -r -m1 . $file)[-1]
+      echo >&2 "error: don't know how to print .$ext files"
       return 1
     end
   end
