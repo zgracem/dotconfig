a62295f45f24647bb1c6dec7f04c485d82a149bf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-10 07:54:23 -0600

    set fish_greeting colours explicitly

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -2,14 +2,14 @@ function fish_greeting --description 'Prints a greeting message on startup'
   status is-interactive; or return
   test $FISH_VERSINFO[1] -ge 3; or return
   echo -s \
-    (set_color cyan)   '<' \
-    (set_color brcyan) '°' \
-    (set_color brblue) ')))' \
-    (set_color cyan)   '>' \
-    (set_color green)  '<' \
-    (set_color normal) ' Welcome to ' \
-    (set_color white)  'fish' \
-    (set_color normal) ', version ' \
-    (set_color white)  "$FISH_VERSION"
+    (set_color 1f998f cyan)   '<' \
+    (set_color 15e6df brcyan) '°' \
+    (set_color 1895ff brblue) ')))' \
+    (set_color 1f998f cyan)   '>' \
+    (set_color 29994f green)  '<' \
+    (set_color 7c8a8b normal) ' Welcome to ' \
+    (set_color 9fb3b3 white)  'fish' \
+    (set_color 7c8a8b normal) ', version ' \
+    (set_color 9fb3b3 white)  "$FISH_VERSION"
   set_color normal
 end
