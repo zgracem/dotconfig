0f1cd8503cd588a805ad3327e0cb4776a59eab2f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-10 15:42:46 -0600

    change to dir list

--- fish/functions/g2.fish
+++ fish/functions/g2.fish
@@ -1,12 +1,13 @@
 function g2 --description 'Go somewhere' --argument destination
   switch $destination
-    case 'defunct'; cd "$HOME/src/_defunct"
-    case 'stow';    cd "$HOME/opt/stow"
     case 'inbox';   cd "$HOME/Dropbox/inbox"
-    case 'scratch'; cd "$HOME/tmp/_scratch"
-    case 'vs9';     cd "$HOME/Dropbox/www/vs2017"
-    case 'imprint'; cd "$HOME/www/2018/imprint"
     case 'proj';    cd "$HOME/Dropbox/Projects"
+    case 'stow';    cd "$HOME/opt/stow"
+    case 'defunct'; cd "$HOME/src/_defunct"
+    case 'scratch'; cd "$HOME/tmp/_scratch"
+    case 'vs9';     cd (realpath "$HOME/www/2017/vs9")
+    case 'imprint'; cd "$HOME/www/2018/imprint"
+    case '2a';      cd "$HOME/www/2018/2a18"
     case '*';       return 1
   end
 end
