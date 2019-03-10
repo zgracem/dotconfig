2689cc8a14ae619afc1f50cdfced26d14da2d6bf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-10 15:44:08 -0600

    add more event hooks
    This still needs testing on Cygwin.

--- fish/conf.d/__fish_prompt_update_title.fish
+++ fish/conf.d/__fish_prompt_update_title.fish
@@ -1,4 +1,4 @@
-function __fish_prompt_update_title --on-event fish_prompt
+function __fish_prompt_update_title --on-event fish_prompt --on-event fish_preexec --on-event fish_postexec
   if functions -q fish_window_title fish_tab_title
     set_terminal_title --window (fish_window_title)
     set_terminal_title --tab (fish_tab_title)
