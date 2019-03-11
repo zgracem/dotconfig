969f449cdb69756c1dc75c29c39daf4165f5f0be
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-11 14:15:53 -0600

    clarity and comments

--- fish/conf.d/rbenv-init.fish
+++ fish/conf.d/rbenv-init.fish
@@ -1,4 +1,4 @@
 if in-path rbenv
-  source (rbenv init - | psub)
-  __fish_fix_path PATH
+  source (rbenv init - | psub)  # adds duplicate shims dir to PATH
+  __fish_fix_path PATH          # removes duplicate shims dir
 end
--- fish/prompt/fish_prompt.fish
+++ fish/prompt/fish_prompt.fish
@@ -8,24 +8,23 @@ function fish_prompt --description 'Display the interactive prompt'
     echo -ns (prompt_hostname) (set_color normal) ":"
   end
 
-  echo -ns (set_color $fish_color_cwd) (prompt_pwd)
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
 
   __fish_prompt_git
   __fish_prompt_jobs
 
   if set -q fish_private_mode
-    set color_user $fish_color_dimmed
+    set_color $fish_color_dimmed
     set glyph "?"
   else if test (id -u) -ne 0
-    set color_user $fish_color_user
+    set_color $fish_color_user
     set glyph "¶"
   else
-    set color_user $fish_color_user_root
+    set_color $fish_color_user_root
     set glyph "#"
   end
-
-  set_color $color_user
-  echo -n " $glyph "
+  echo -ns " $glyph "
 
   set_color normal
 end
