b9ee6b239741fc8332f02055c20a34ec80f03e55
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-18 15:24:32 -0600

    add some dirs

--- fish/functions/g2.fish
+++ fish/functions/g2.fish
@@ -3,11 +3,13 @@ function g2 --description 'Go somewhere' --argument destination
     case 'inbox';   cd "$HOME/Dropbox/inbox"
     case 'proj';    cd "$HOME/Dropbox/Projects"
     case 'stow';    cd "$HOME/opt/stow"
-    case 'defunct'; cd "$HOME/src/_defunct"
+    case 'defunct'; cd "$HOME/Dropbox/src/_defunct"
     case 'scratch'; cd "$HOME/tmp/_scratch"
     case 'vs9';     cd (realpath "$HOME/www/2017/vs9")
     case 'imprint'; cd "$HOME/www/2018/imprint"
     case '2a';      cd "$HOME/www/2018/2a18"
+    case 'desktop'; cd "$XDG_DESKTOP_DIR"
+    case 'dl';      cd "$XDG_DOWNLOAD_DIR"
     case '*';       return 1
   end
 end
