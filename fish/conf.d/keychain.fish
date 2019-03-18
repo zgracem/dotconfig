8514ce1809fe9f0d45fb8d9b17e92d66e0498609
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-18 15:24:48 -0600

    only init keychain if there's a dir to use

--- fish/conf.d/keychain.fish
+++ fish/conf.d/keychain.fish
@@ -10,8 +10,10 @@ if status is-interactive; and in-path keychain; and not set -gq SSH_AGENT_PID
   set params $params --absolute --dir "$keychain_dir"
   set params $params --inherit any --ignore-missing
 
-  set -lx SHELL (status fish-path 2>/dev/null; or type -P fish)
-  keychain $params $keys > $ssh_env
-  command chmod 600 $ssh_env
-  source $ssh_env
+  if test -d "$keychain_dir"
+    set -lx SHELL (status fish-path 2>/dev/null; or type -P fish)
+    keychain $params $keys > $ssh_env
+    command chmod 600 $ssh_env
+    source $ssh_env
+  end
 end
