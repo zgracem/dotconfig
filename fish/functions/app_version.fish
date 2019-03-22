add81e52da3f947c35a01e51717a0b25b035bdb3
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-22 08:20:25 -0600

    add cygwin support to `app_version`

--- fish/functions/app_version.fish
+++ fish/functions/app_version.fish
@@ -1,10 +1,16 @@
-function app_version -a app --description 'Get the version number from an .app bundle'
-  set -l info "$app/Contents/Info.plist"
+function app_version -a app --description 'Get version number from an .app bundle or .exe file'
+  if test -x /usr/libexec/PlistBuddy
+    set -l info "$app/Contents/Info.plist"
 
-  if [ -f $info ]
-    /usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' "$info"
-  else
-    echo >&2 "not found:" $info
-    return 1
+    if test -f $info
+      /usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' "$info"
+    else
+      echo >&2 "not found:" $info
+      return 1
+    end
+  else if in-path wmic
+    set -l win_path (cygpath -aw $app | string replace -a '\\' '\\\\')
+    wmic datafile where "name=\"$win_path\"" get Version /value \
+    | string replace -rf 'Version=([\d.]+)' '$1'
   end
 end
