60c872f98782cf24d2a5326aaa27d2679c2e08d6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-22 07:55:51 -0600

    don't use math trickery here
    If `git status` doesn't output a `branch.ab` line, these variables won't be
    populated to do math with. Doing it this way correctly returns zero in such
    a case.

--- fish/prompt/__fish_prompt_git.fish
+++ fish/prompt/__fish_prompt_git.fish
@@ -50,13 +50,16 @@ function __fish_prompt_git --description 'Display git info in the fish prompt'
   set -l ahead $ahead_behind[2]
   set -l behind $ahead_behind[3]
 
-  if test (math "$ahead + $behind") -gt 0
+  test -n "$ahead"; or set -l ahead 0
+  test -n "$behind"; or set -l behind 0
+
+  if test "$ahead" -gt 0 -o "$behind" -gt 0
     set_color $fish_prompt_color_git_needs_push
-    if test $ahead -gt 0 -a $behind -eq 0
+    if test $behind -eq 0
       echo -n $icon_ahead
-    else if test $ahead -eq 0 -a $behind -gt 0
+    else if test $ahead -eq 0
       echo -n $icon_behind
-    else if test $ahead -gt 0 -a $behind -gt 0
+    else
       echo -n $icon_ahead_and_behind
     end
   end
