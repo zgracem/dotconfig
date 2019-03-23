ad039ffdff035bed285560df87cdee28a9246d14
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-23 08:06:34 -0600

    improve comments and error output

--- fish/conf.d/___env.fish
+++ fish/conf.d/___env.fish
@@ -1,8 +1,8 @@
 if not set -q ENV; and string match -q '*fish' "$SHELL"
   # Use sh(1) to populate the environment from ~/.config/environment.d.
-  # Both the -l and -i flags are needed to prevent an infinite loop.
   set -l params -c "exec $SHELL"
 
+  # Both the -l and -i flags are needed to prevent an infinite loop.
   status is-login; and set params -l $params
   status is-interactive; and set params -i $params
 
--- fish/config.fish
+++ fish/config.fish
@@ -54,6 +54,7 @@ else
   if cygwin?
     # fish_help_browser overrides the browser that may be defined by $BROWSER.
     # The variable may be an array containing a browser name plus options.
+    # N.B. This must be a GUI app; fish will use cygstart(1) to launch it.
     set -g fish_help_browser "$LOCALAPPDATA\\Mozilla Firefox\\firefox.exe"
   end
 end
