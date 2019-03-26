6e03731b94000344abe00e6762388977c83f0a28
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-26 08:20:05 -0600

    don't truncate output of `ff` and `fd`

--- fish/functions/fd.fish
+++ fish/functions/fd.fish
@@ -1,5 +1,4 @@
 function fd --description 'Find directories in PWD whose name contains a given string'
-  find -H "$PWD" -xtype d -iname "*$argv*" -print 2>/dev/null | \
-  string replace -r "^$HOME(?=\$|/)" "~" | \
-  command grep -i --color=auto "$argv"
+  find -H "$PWD" -xtype d -iname "*$argv[1]*" -print 2>/dev/null | \
+  command grep -i --color=auto $argv[1]
 end
--- fish/functions/ff.fish
+++ fish/functions/ff.fish
@@ -1,5 +1,4 @@
 function ff --description 'Finds files in PWD whose name contains a given string'
   find -H "$PWD" -xtype f -iname "*$argv*" -print 2>/dev/null | \
-  string replace -r "^$HOME(?=\$|/)" "~" | \
-  command grep -i --color=auto "$argv"
+  command grep -i --color=auto $argv[1]
 end
