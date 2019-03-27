6c0f8288321a2cb9eb7bfff67f5308fd181df32c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-27 10:26:55 -0600

    fix silly variable scope error

--- fish/functions/wrappers/gem.fish
+++ fish/functions/wrappers/gem.fish
@@ -1,11 +1,15 @@
 function gem --description 'RubyGems program'
   in-path gem; or return 127
 
+  set -lx http_proxy
+  set -lx https_proxy
+  set -lx HTTPS_PROXY
+
   # See ~/.private/fish/conf.d/proxy.fish
   if string match -eq 'WS*' $hostname
-    set -lx http_proxy "http://"$USER":"(cat ~/.p)"@proxy:8080/"
-    set -lx https_proxy $http_proxy
-    set -lx HTTPS_PROXY $http_proxy
+    set http_proxy "http://"$USER":"(cat ~/.p)"@proxy:8080/"
+    set https_proxy $http_proxy
+    set HTTPS_PROXY $http_proxy
   end
 
   command gem $argv
