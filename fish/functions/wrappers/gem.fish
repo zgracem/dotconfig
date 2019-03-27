e58edc66306b96cac765c29af8f0559d1e79bca1
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-27 08:33:55 -0600

    `gem` should always use proxy on WS* workstations

--- fish/functions/wrappers/gem.fish
+++ fish/functions/wrappers/gem.fish
@@ -2,7 +2,7 @@ function gem --description 'RubyGems program'
   in-path gem; or return 127
 
   # See ~/.private/fish/conf.d/proxy.fish
-  if test -n "$ALL_PROXY"
+  if string match -eq 'WS*' $hostname
     set -lx http_proxy "http://"$USER":"(cat ~/.p)"@proxy:8080/"
     set -lx https_proxy $http_proxy
     set -lx HTTPS_PROXY $http_proxy
