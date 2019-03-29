f07d7db2efc106c45858e678c239fd559a9d55a6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-03-29 07:33:24 -0600

    use simpler `fgrep` to highlight search matches

--- fish/functions/fd.fish
+++ fish/functions/fd.fish
@@ -1,4 +1,4 @@
 function fd --description 'Find directories in PWD whose name contains a given string'
   find -H "$PWD" -xtype d -iname "*$argv[1]*" -print 2>/dev/null | \
-  command grep -i --color=auto $argv[1]
+  command fgrep -i --color=auto $argv[1]
 end
--- fish/functions/ff.fish
+++ fish/functions/ff.fish
@@ -1,4 +1,4 @@
 function ff --description 'Finds files in PWD whose name contains a given string'
   find -H "$PWD" -xtype f -iname "*$argv*" -print 2>/dev/null | \
-  command grep -i --color=auto $argv[1]
+  command fgrep -i --color=auto $argv[1]
 end
