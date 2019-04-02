e60dc28bad2984f0bd1814c39db30a6831474f81
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-02 13:00:44 -0600

    quotes + colouring is overkill

--- fish/functions/bind_eof.fish
+++ fish/functions/bind_eof.fish
@@ -1,5 +1,5 @@
 function bind_eof
   set -l cmd (set_color $fish_color_command)"exit"(set_color normal)
-  echo >&2 "Use “$cmd” to leave the shell."
+  echo >&2 "Use $cmd to leave the shell."
   commandline --function repaint
 end
