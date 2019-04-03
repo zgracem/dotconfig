deac7cd2772be28ed1dc3746a7dbd4b52dadbed5
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-03 15:32:15 +0100

    add more custom completions

---
+++ fish/completions/regfont.fish
@@ -0,0 +1,5 @@
+complete -c regfont -s a -l add     -r    -d 'Add specified fonts'
+complete -c regfont -s r -l remove  -r    -d 'Remove specified fonts'
+complete -c regfont -s h -l help       -x -d 'This help message'
+complete -c regfont -s v -l version    -x -d 'Print version information'
+complete -c regfont -s d -l debug         -d 'Turn on debugging information'
