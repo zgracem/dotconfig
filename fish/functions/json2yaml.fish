b06a370b5e9282a64538229fb90efcf06ac42ab9
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-04 07:37:38 -0600

    adjust json↔yaml functions to take stdin or $1

--- fish/functions/json2yaml.fish
+++ fish/functions/json2yaml.fish
@@ -1,4 +1,13 @@
 function json2yaml --description "Convert a JSON file to YAML"
-  set -l cmd "puts JSON.load(Pathname.new(ARGV[0])).to_yaml"
-  ruby -rjson -rpathname -ryaml -e $cmd $argv[1]
+  set -l src
+  if set -q argv[1]
+    set src "Pathname.new(ARGV[0])"
+  else if not isatty stdin
+    set src "ARGF"
+  else
+    echo >&2 "nothing to convert!"
+    return 1
+  end
+
+  ruby -rjson -rpathname -ryaml -e "puts JSON.load($src).to_yaml" $argv[1]
 end
--- fish/functions/yaml2json.fish
+++ fish/functions/yaml2json.fish
@@ -1,4 +1,13 @@
 function yaml2json --description "Convert a YAML file to JSON"
-  set -l cmd "puts YAML.load(Pathname.new(ARGV[0]).read).to_json"
-  ruby -rjson -rpathname -ryaml -e $cmd $argv[1]
+  set -l src
+  if set -q argv[1]
+    set src "Pathname.new(ARGV[0])"
+  else if not isatty stdin
+    set src "ARGF"
+  else
+    echo >&2 "nothing to convert!"
+    return 1
+  end
+
+  ruby -rjson -rpathname -ryaml -e "puts YAML.load($src.read).to_json" $argv[1]
 end
