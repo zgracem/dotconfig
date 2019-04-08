0900375f5b1aeb31be56dfb133f1041e23a5db4e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-08 10:35:08 -0600

    use Ruby instead of sed

--- fish/functions/md2html.fish
+++ fish/functions/md2html.fish
@@ -1,7 +1,7 @@
-function md2html --description 'Convert Markdown to HTML with simple smart quotes' -a file
-  multimarkdown -s $file | sed \
-    -e 's/&#8211;/–/g' -e 's/&#8212;/—/g' \
-    -e 's/&#8216;/‘/g' -e 's/&#8217;/’/g' \
-    -e 's/&#8220;/“/g' -e 's/&#8221;/”/g'
-    -e 's/&#8230;/…/g'
+function md2html --description 'Convert Markdown to HTML' -a file
+  # This function uses MultiMarkdown to convert a Markdown file to an HTML
+  # snippet, then uses a Ruby gem <github.com/threedaymonk/htmlentities> to
+  # decode HTML entities like "&#8230;" to Unicode chars like "…".
+  multimarkdown -s $file \
+  | ruby -rhtmlentities -e 'puts HTMLEntities.new.decode(ARGF.read)'
 end
