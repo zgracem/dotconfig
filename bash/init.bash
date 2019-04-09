9d4b29cf805ab5db9cf9e0bfadf22141a3afaafd
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-09 11:23:13 -0600

    disable login items for faster startup

--- bash/init.bash
+++ bash/init.bash
@@ -3,14 +3,15 @@
 # sourced at the end of ~/.bashrc
 # -----------------------------------------------------------------------------
 
-if [[ -d $HOME/scripts ]]; then
-  # cute login banner
-  "$HOME/scripts/login/loginbanner.sh"
+### ZGM disabled 2019-04-09 -- for quicker bash startup
+# if [[ -d $HOME/scripts ]]; then
+#   # cute login banner
+#   "$HOME/scripts/login/loginbanner.sh"
 
-  # astronomical info 
-  # -- requires suncalc and weather_ca gems
-  # -- runs only on first login of the day
-  if [[ $HOSTNAME == Athena* ]] && "$HOME/scripts/login/matins.sh"; then
-    "$HOME/scripts/login/astro.rb"
-  fi
-fi
+#   # astronomical info
+#   # -- requires suncalc and weather_ca gems
+#   # -- runs only on first login of the day
+#   if [[ $HOSTNAME == Athena* ]] && "$HOME/scripts/login/matins.sh"; then
+#     "$HOME/scripts/login/astro.rb"
+#   fi
+# fi
