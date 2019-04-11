48d40beef6ebb7e4cc8629761b3ee5535d7009ae
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-11 08:30:20 -0600

    fix scope issues(?) in `scratch`

--- fish/functions/debug/scratch.fish
+++ fish/functions/debug/scratch.fish
@@ -1,6 +1,7 @@
-function scratch -a template --description 'Create a temporary directory and change into it'
+function scratch --description 'Create a temporary directory and change into it'
   # Inspired by <https://sanctum.geek.nz/cgit/dotfiles.git/tree/sh/shrc.d/scr.sh>
-  set -q template; or set -l template (status current-function)
+  set -l template (status current-function)
+  set -q argv[1]; and set template $argv[1]
 
   # be BSD/GNU-agnostic vis-á-vis mktemp(1)
   is-gnu mktemp; and set template "$template.XXXXXX"
