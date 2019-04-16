e8538beeb6f80e33ce43a803e6f184b80f608d5a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-16 17:16:54 +0100

    add `bat` wrapper for cygwin

---
+++ fish/functions/wrappers/bat.fish
@@ -0,0 +1,17 @@
+function bat --wraps bat --description 'A cat clone with wings'
+  set -l args
+  for arg in $argv
+    switch "$arg"
+    case "cache" "-*"
+      set -a args $arg
+    case "*"
+      if cygwin?
+        set -a args (cygpath --windows $arg)
+      else
+        set -a args $arg
+      end
+    end
+  end
+
+  command bat $args
+end
