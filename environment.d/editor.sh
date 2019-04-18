22b0640b78838e59a7232ca3e7c470eeb1931777
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-18 17:23:13 +0100

    change default GUI editor to Code

--- environment.d/editor.sh
+++ environment.d/editor.sh
@@ -6,12 +6,12 @@ export VISUAL="$EDITOR"
 
 # Use GUI app if not logged in remotely
 if [ -z "$SSH_CONNECTION" ]; then
-  if [ -x "$HOME/bin/subl-wait" ]; then
-    VISUAL="$HOME/bin/subl-wait"
-  elif [ -x "$HOME/bin/subl" ]; then
+  if [ -x "$HOME/bin/code-wait" ]; then
+    VISUAL="$HOME/bin/code-wait"
+  elif [ -x "$HOME/bin/code" ]; then
     # The above solution is preferred because some things that use $VISUAL
-    # check to see whether it exists. Since there is no file "subl --wait" in
+    # check to see whether it exists. Since there is no file "code --wait" in
     # ~/bin, those things will fail. :(
-    VISUAL="$HOME/bin/subl --wait"
+    VISUAL="$HOME/bin/code --wait"
   fi
 fi
