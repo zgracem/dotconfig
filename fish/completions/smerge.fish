2e4566067e8d930935a267367c3d4f65ba3f3919
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-04-25 08:23:38 -0600

    completion improvements
    I'm starting to get the hang of these.

--- fish/completions/smerge.fish
+++ fish/completions/smerge.fish
@@ -6,3 +6,4 @@ complete -c smerge -a 'search' -x -d 'Search for commits in the current reposito
 complete -c smerge -a 'blame' -r -d 'Blame the given file in the current repo'
 complete -c smerge -a 'log' -r -d 'Show the file history in the current repo'
 complete -c smerge -a 'mergetool' -r -d 'Open the merge tool for the given files'
+complete -c smerge -l no-wait -d "Don't wait for the application to close" -n '__fish_seen_subcommand_from mergetool'
