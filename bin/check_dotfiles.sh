eb7b38d6738355589f17dae693aa952b21c357fe
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-05 12:56:12 -0600

    add VS Code tasks (alpha)

--- bin/check_dotfiles.sh
+++ bin/check_dotfiles.sh
@@ -9,15 +9,19 @@ if ! type -P shellcheck >/dev/null; then
   exit 1
 fi
 
+# SC1090:   ignore non-constant sourcing
+# ├─SC1091: don't follow sourced files
+# ├─SC2034: don't flag "unused" variables
+# └─SC2154: variable is referenced but not assigned
+# SC2148:   don't require shebang
+export SHELLCHECK_OPTS="$SHELLCHECK_OPTS -e SC1090,SC1091,SC2034,SC2154,SC2148"
+if [[ $TERM_PROGRAM == "vscode" ]]; then
+  export SHELLCHECK_OPTS="$SHELLCHECK_OPTS --color=never --format=gcc"
+fi
+
 main()
 (
-  # SC1090:   ignore non-constant sourcing (OK)
-  # ├─SC1091: don't follow sourced files (OK)
-  # ├─SC2034: don't flag "unused" variables (OK)
-  # └─SC2154: variable is referenced but not assigned (OK)
-  # SC2148:   don't require shebang (OK)
   local err=0
-  export SHELLCHECK_OPTS="$SHELLCHECK_OPTS -e SC1090,SC1091,SC2034,SC2154,SC2148"
   _check_dotfiles_env "$@"  || ((err++))
   _check_dotfiles_sh "$@"   || ((err++))
   _check_dotfiles_bash "$@" || ((err++))
