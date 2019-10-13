e1bcb039c45bba0701bbc17bdc402233bba06e76
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-13 16:29:09 -0600

    vscode-init also creates empty extensions file

--- fish/functions/vscode-init.fish
+++ fish/functions/vscode-init.fish
@@ -1,10 +1,14 @@
 function vscode-init --description 'Initialize directory for use with VS Code'
-  set -l dir $PWD
+  set -l dir $PWD/.vscode
   set -q argv[1]; and set dir $argv[1]
 
-  mkdir -p "$dir/.vscode"
+  mkdir -p "$dir"
 
-  if not test -f "$dir/.vscode/settings.json"
-    echo '{}' > "$dir/.vscode/settings.json"
+  if not test -f "$dir/settings.json"
+    echo '{}' > "$dir/settings.json"
+  end
+
+  if not test -f "$dir/extensions.json"
+    echo '{}' | jq '.recommendations=[]' > "$dir/extensions.json"
   end
 end
