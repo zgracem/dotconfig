0495c8dd0bdb339e14faeb09632c983e17d83d7f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-13 11:33:10 -0600

    refactor `whatis` wrapper
    This is so `fish -n` won't complain.

--- fish/functions/wrappers/whatis.fish
+++ fish/functions/wrappers/whatis.fish
@@ -1,8 +1,4 @@
-# If `gwhatis` is present, that's the one whose completions we want to use.
-# No need to worry about executing it; there's a shadow `whatis` link in PATH.
-set cmd 'whatis'; in-path gwhatis; and set cmd 'gwhatis'
-
-function whatis -a term -w $cmd -d 'Search the whatis database for exact matches'
+function _whatis --argument-names term
   set -l args $argv[2..-1]
   set -l output (command whatis --regex '\b'"$term"'\b[^-]' $args 2>/dev/null | sort -u)
   test -n "$output"; or return 16
@@ -11,4 +7,8 @@ function whatis -a term -w $cmd -d 'Search the whatis database for exact matches
   string replace -ar '\s+' ' ' $output
 end
 
-set --erase cmd
+if in-path gwhatis
+  function whatis --wraps gwhatis; _whatis $argv; end
+else
+  function whatis --wraps whatis; _whatis $argv; end
+end
