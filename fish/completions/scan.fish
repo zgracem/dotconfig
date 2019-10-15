c4e695d7931d58356107c81ba8a27660b9a1b869
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-15 14:06:31 -0600

    simplify completion logic

--- fish/completions/scan.fish
+++ fish/completions/scan.fish
@@ -1,13 +1,7 @@
-function __fish_seen_scan_subcmd
-  set -l scan_commands wifi ssh fs port pid file
-
-  __fish_seen_subcommand_from $scan_commands
-end
-
-complete -c scan -n "not __fish_seen_scan_subcmd" -a wifi -d (__fish_describe_function _scan_wifi)
-complete -c scan -n "not __fish_seen_scan_subcmd" -a ssh -d (__fish_describe_function _scan_ssh)
-complete -c scan -n "not __fish_seen_scan_subcmd" -a fs -d (__fish_describe_function _scan_fs)
-complete -c scan -n "not __fish_seen_scan_subcmd" -a port -d (__fish_describe_function _scan_port)
-complete -c scan -n "not __fish_seen_scan_subcmd" -a pid -d (__fish_describe_function _scan_pid)
+complete -c scan -n "__fish_is_first_token" -a wifi -f -d (__fish_describe_function _scan_wifi)
+complete -c scan -n "__fish_is_first_token" -a ssh -f -d (__fish_describe_function _scan_ssh)
+complete -c scan -n "__fish_is_first_token" -a fs -f -d (__fish_describe_function _scan_fs)
+complete -c scan -n "__fish_is_first_token" -a port -r -d (__fish_describe_function _scan_port)
+complete -c scan -n "__fish_is_first_token" -a pid -r -d (__fish_describe_function _scan_pid)
 complete -c scan -n "__fish_seen_subcommand_from pid" -x -a "(__fish_complete_pids)"
-complete -c scan -n "not __fish_seen_scan_subcmd" -a file -d (__fish_describe_function _scan_file)
+complete -c scan -n "__fish_is_first_token" -a file -d (__fish_describe_function _scan_file)
