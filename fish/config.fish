5deb8bc033f6f3e38ca99466bd7537dd0d19d6d6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-15 14:39:52 -0600

    remove unnecessary `else` block

--- fish/config.fish
+++ fish/config.fish
@@ -11,50 +11,50 @@ if test $FISH_VERSINFO[1] -lt 3
       (set_color black) "<<<" (set_color normal)
   end >&2
   exit 1
-else
-  source "$__fish_config_dir/paths.fish"
-  source "$__fish_config_dir/aliases.fish"
+end
 
-  # function subdirectories
-  for dir in $__fish_config_dir/functions $__fish_config_dir/functions/**/
-    set -p fish_function_path (string trim -rc/ $dir)
-  end
+source "$__fish_config_dir/paths.fish"
+source "$__fish_config_dir/aliases.fish"
 
-  # load private and per-machine configuration if available
-  for dir in ~/.local/config/fish ~/.private/fish
-    if test -d $dir
-      if test -d $dir/conf.d
-        for file in $dir/conf.d/*.fish; source "$file"; end
-      end
-      set -p fish_function_path $dir/functions
-      set -p fish_complete_path $dir/completions
+# function subdirectories
+for dir in $__fish_config_dir/functions $__fish_config_dir/functions/**/
+  set -p fish_function_path (string trim -rc/ $dir)
+end
+
+# load private and per-machine configuration if available
+for dir in ~/.local/config/fish ~/.private/fish
+  if test -d $dir
+    if test -d $dir/conf.d
+      for file in $dir/conf.d/*.fish; source "$file"; end
     end
+    set -p fish_function_path $dir/functions
+    set -p fish_complete_path $dir/completions
   end
+end
 
-  if status is-interactive
-    # setup colours
-    source "$__fish_config_dir/colours.fish"
+if status is-interactive
+  # setup colours
+  source "$__fish_config_dir/colours.fish"
 
-    # activate custom prompt
-    set -p fish_function_path "$__fish_config_dir/prompt"
+  # activate custom prompt
+  set -p fish_function_path "$__fish_config_dir/prompt"
 
-    # source vendor completions
-    set -p fish_complete_path "$HOME/opt/etc/fish/completions"
-  end
+  # source vendor completions
+  set -p fish_complete_path "$HOME/opt/etc/fish/completions"
+end
 
-  # remove duplicate & nonexistent directories
-  __fish_fix_path fish_function_path
-  __fish_fix_path fish_complete_path
+# remove duplicate & nonexistent directories
+__fish_fix_path fish_function_path
+__fish_fix_path fish_complete_path
 
-  if is-cygwin
-    # fish_help_browser overrides the browser that may be defined by $BROWSER.
-    # The variable may be an array containing a browser name plus options.
-    # N.B. This must be a GUI app; fish will use cygstart(1) to launch it.
-    set -g fish_help_browser "$LOCALAPPDATA\\Mozilla Firefox\\firefox.exe"
-  end
+if is-cygwin
+  # fish_help_browser overrides the browser that may be defined by $BROWSER.
+  # The variable may be an array containing a browser name plus options.
+  # N.B. This must be a GUI app; fish will use cygstart(1) to launch it.
+  set -g fish_help_browser "$LOCALAPPDATA\\Mozilla Firefox\\firefox.exe"
+end
 
-  # See functions/update_lastpwd.fish
-  if test -f ~/.lastpwd
-    cd (cat ~/.lastpwd)
-  end
+# See functions/update_lastpwd.fish
+if test -f ~/.lastpwd
+  cd (cat ~/.lastpwd)
 end
