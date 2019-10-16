ba61f5e4fbf449bfbf82a1b7e75d5783f123e170
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-16 17:54:37 -0600

    shorter variable names

--- fish/prompt/prompt_pwd.fish
+++ fish/prompt/prompt_pwd.fish
@@ -1,11 +1,12 @@
 function prompt_pwd --description 'Print the current working directory, shortened to fit the prompt'
-  set -q argv[1]; or set -l argv[1] (pwd)
+  set -q argv[1]
+  or set -l argv[1] (pwd)
 
-  set -q fish_prompt_pwd_dir_length
-  or set -l fish_prompt_pwd_dir_length 3
+  set -q fish_prompt_pwd_length
+  or set -l fish_prompt_pwd_length 3
 
-  set -q fish_prompt_pwd_dir_glyph
-  or set -l fish_prompt_pwd_dir_glyph "…"
+  set -q fish_prompt_pwd_glyph
+  or set -l fish_prompt_pwd_glyph "…"
 
   set -l cwd (short_home $argv[1])
   set -l cwd_parts (string split "/" "$cwd")
@@ -15,8 +16,8 @@ function prompt_pwd --description 'Print the current working directory, shortene
     return 0
   else
     for i in (seq 1 (math (count $cwd_parts) - 1))
-      if test (string length $cwd_parts[$i]) -gt (math $fish_prompt_pwd_dir_length + 1)
-        set cwd_parts[$i] (string sub -l $fish_prompt_pwd_dir_length "$cwd_parts[$i]")"$fish_prompt_pwd_dir_glyph"
+      if test (string length $cwd_parts[$i]) -gt (math $fish_prompt_pwd_length + 1)
+        set cwd_parts[$i] (string sub -l $fish_prompt_pwd_length "$cwd_parts[$i]")"$fish_prompt_pwd_glyph"
       end
     end
   end
