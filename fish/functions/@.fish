5d784ddbfdf8d11e7ce06904c37ef2b8eafc4b05
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-17 10:09:56 -0600

    cleanup unused functions

--- fish/functions/@.fish
+++
@@ -1,3 +0,0 @@
-function @ --description 'Convert a UNIX timestamp to an RFC 2822-formatted string'
-  date -d "@$argv[1]" --rfc-2822
-end
--- fish/functions/md2html.fish
+++
@@ -1,7 +0,0 @@
-function md2html --description 'Convert Markdown to HTML' -a file
-  # This function uses MultiMarkdown to convert a Markdown file to an HTML
-  # snippet, then uses a Ruby gem <github.com/threedaymonk/htmlentities> to
-  # decode HTML entities like "&#8230;" to Unicode chars like "…".
-  multimarkdown -s $file \
-  | ruby -rhtmlentities -e 'puts HTMLEntities.new.decode(ARGF.read)'
-end
