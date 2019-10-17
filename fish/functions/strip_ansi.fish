54ceb510c2eeacd53598238ed887daac6c48ea11
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-17 10:13:03 -0600

    rewrite `strip_ansi` in pure fish
    The spurious use of `cat` is because otherwise `string` won't read from
    stdin. I believe this is an open enhancement issue in fish but I don't
    feel like looking it up.

--- fish/functions/strip_ansi.fish
+++ fish/functions/strip_ansi.fish
@@ -1,3 +1,3 @@
 function strip_ansi --description 'Strips ANSI formatting escape codes from stdin'
-  sed -E "s/\x1b\\[[0-9]+(;[0-9]+)*m//g" -
+  cat | string replace -ar "\x1b\[\d+(;\d+)*m" ""
 end
