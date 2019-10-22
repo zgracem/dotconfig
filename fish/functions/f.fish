zgracem/dotconfig

commit 9fb231f8428a4e59c8ea79b911b436c9d6087a96

syntax improvements

fish/functions/f.fish
@@ -1,12 +1,12 @@
-function f --description 'Open a Finder/Explorer window for $PWD/$1'
-  set -q argv[1]; or set argv[1] (pwd)
+function f --description 'Open a Finder/Explorer window for $PWD/$1' -a target
+  set -q target; or set target (pwd)
 
   switch (uname -s)
     case 'Darwin'
-      open -a Finder $argv[1]
+      open -a Finder $target
     case 'CYGWIN*'
       set -l windir (cygpath --windir)
-      "$windir/explorer" (cygpath -w $argv[1])
+      "$windir/explorer" (cygpath -w $target)
     case '*'
       echo >&2 "not available on this system"
       return 1

fish/functions/reveal.fish
@@ -1,12 +1,12 @@
-function reveal --description 'Reveal $1 in Finder/Explorer'
-  set -q argv[1]; or set argv[1] (pwd)
+function reveal --description 'Reveal $1 in Finder/Explorer' -a target
+  set -q target; or set target (pwd)
 
   switch (uname -s)
     case 'Darwin'
-      open -R $argv[1]
+      open -R $target
     case 'CYGWIN*'
       set -l windir (cygpath --windir)
-      "$windir/explorer" /select, (cygpath -w $argv[1])
+      "$windir/explorer" /select, (cygpath -w $target)
     case '*'
       echo >&2 "not available on this system"
       return 1

fish/config.fish
@@ -17,7 +17,7 @@ source "$__fish_config_dir/paths.fish"
 
 # function subdirectories
 for dir in $__fish_config_dir/functions $__fish_config_dir/functions/**/
-  set -p fish_function_path (string trim -rc/ $dir)
+  set -p fish_function_path (string trim --right --chars=/ $dir)
 end
 
 # load private and per-machine configuration if available

fish/functions/_wrappers/ag.fish
@@ -1,7 +1,7 @@
 function ag --description 'The Silver Searcher. Like ack, but faster'
-  command ag \
-      --nobreak --noheading \
-      --color-path="34" --color-line-number="36" --color-match="1;4;35" \
-      --hidden --ignore .git --skip-vcs-ignores \
-      $argv
+  set -l opts --nobreak --noheading
+  set -a opts --color-path="34" --color-line-number="36" --color-match="1;4;35"
+  set -a opts --hidden --ignore=.git --skip-vcs-ignores
+
+  command ag $opts $argv
 end

fish/functions/fd.fish
@@ -1,4 +1,4 @@
-function fd --description 'Find directories in PWD whose name contains a given string'
-  find -H "$PWD" -xtype d -iname "*$argv[1]*" -print 2>/dev/null | \
-  command fgrep -i --color=auto $argv[1]
+function fd --description 'Find directories in PWD whose name contains a given string' -a string
+  find -H "$PWD" -xtype d -iname "*$string*" -print 2>/dev/null | \
+  command fgrep -i --color=auto $string
 end

fish/functions/ff.fish
@@ -1,4 +1,4 @@
-function ff --description 'Finds files in PWD whose name contains a given string'
-  find -H "$PWD" -xtype f -iname "*$argv*" -print 2>/dev/null | \
-  command fgrep -i --color=auto $argv[1]
+function ff --description 'Finds files in PWD whose name contains a given string' -a string
+  find -H "$PWD" -xtype f -iname "*$string*" -print 2>/dev/null | \
+  command fgrep -i --color=auto $string
 end

fish/functions/short_home.fish
@@ -1,3 +1,3 @@
 function short_home --description 'Replace $HOME with ~ in a path'
-  string replace -r "^$HOME(?=\$|/)" "~" $argv
+  string replace --regex "^$HOME(?=\$|/)" "~" $argv
 end

fish/functions/shove.fish
@@ -1,5 +1,5 @@
 function shove --description 'Touch a file, creating all necessary directories'
-  for arg in $argv
-    mkdir -p (dirname $arg); and touch $arg
+  for path in $argv
+    mkdir -p (dirname $path); and touch $path
   end
 end

commit c776c74b1d62eace1d6e106361ae2ed38985e60d

fix for bug introduced in 9fb231f

fish/functions/f.fish
@@ -1,5 +1,5 @@
 function f --description 'Open a Finder/Explorer window for $PWD/$1' -a target
-  set -q target; or set target (pwd)
+  set -q target[1]; or set target (pwd)
 
   switch (uname -s)
     case 'Darwin'

fish/functions/reveal.fish
@@ -1,5 +1,5 @@
 function reveal --description 'Reveal $1 in Finder/Explorer' -a target
-  set -q target; or set target (pwd)
+  set -q target[1]; or set target (pwd)
 
   switch (uname -s)
     case 'Darwin'