a051fd5da58e50529a2c7ba0d77158651c384743
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-25 07:06:26 -0600

    remove inaccurate comments

--- fish/functions/fish_tab_title.fish
+++ fish/functions/fish_tab_title.fish
@@ -1,12 +1,8 @@
-# Called by ~/.config/fish/conf.d/__fish_prompt_update_title.fish
 function fish_tab_title --description 'Update the tab title'
-  set -q short_hostname
-  or set -g short_hostname (string split "." $hostname)[1]
-
   set -l cwd (short_home "$PWD")
   set -l cwd_parts (string split "/" "$cwd")
 
-  echo -n "$short_hostname:"
+  echo -s (prompt_hostname) ":"
 
   if test (count $cwd_parts) -le 2
     echo -n $cwd
--- fish/functions/fish_window_title.fish
+++ fish/functions/fish_window_title.fish
@@ -1,4 +1,3 @@
-# Called by ~/.config/fish/conf.d/__fish_prompt_update_title.fish
 function fish_window_title --description 'Update the window title'
   set -q long_hostname
   or set -g long_hostname (/bin/hostname -f 2>/dev/null)
