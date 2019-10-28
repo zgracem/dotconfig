ebd943a527fa92bd5d1a4b2def95cdb238fc181e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-28 11:12:20 -0600

    reformat fish_greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -5,7 +5,7 @@ function fish_greeting --description 'Prints a greeting message on startup'
   echo -ns (set_color f3a) '<' (set_color f28) '°' (set_color f66) ')))' \
     (set_color f80) '>' (set_color fa0) '<' (set_color normal)
 
-  printf ' Welcome to %bfish%b, version %b%s%b\\n' \
-    (set_color brwhite) (set_color normal) \
-    (set_color brwhite) $FISH_VERSION (set_color normal)
+  printf " Welcome to %bfish%b, version %b$FISH_VERSION%b\\n" \
+    (set_color brwhite --italics) (set_color normal) \
+    (set_color white) (set_color normal)
 end
