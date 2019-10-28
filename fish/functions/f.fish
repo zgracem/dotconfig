39ba3508178e0ef194bec9fedccca35c03c4929c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-28 09:44:21 -0600

    use if/else block instead of switch/case

--- fish/functions/f.fish
+++ fish/functions/f.fish
@@ -1,14 +1,13 @@
 function f --description 'Open a Finder/Explorer window for $PWD/$1' -a target
   set -q target[1]; or set target (pwd)
 
-  switch (uname -s)
-    case 'Darwin'
-      open -a Finder $target
-    case 'CYGWIN*'
-      set -l windir (cygpath --windir)
-      "$windir/explorer" (cygpath -w $target)
-    case '*'
-      echo >&2 "not available on this system"
-      return 1
+  if is-macos
+    open -a Finder $target
+  else if is-cygwin
+    set -l windir (cygpath --windir)
+    "$windir/explorer" (cygpath -w $target)
+  else
+    echo >&2 "not available on this system"
+    return 1
   end
 end
--- fish/functions/reveal.fish
+++ fish/functions/reveal.fish
@@ -1,14 +1,13 @@
 function reveal --description 'Reveal $1 in Finder/Explorer' -a target
   set -q target[1]; or set target (pwd)
 
-  switch (uname -s)
-    case 'Darwin'
-      open -R $target
-    case 'CYGWIN*'
-      set -l windir (cygpath --windir)
-      "$windir/explorer" /select, (cygpath -w $target)
-    case '*'
-      echo >&2 "not available on this system"
-      return 1
+  if is-macos
+    open -R $target
+  else if is-cygwin
+    set -l windir (cygpath --windir)
+    "$windir/explorer" /select, (cygpath -w $target)
+  else
+    echo >&2 "not available on this system"
+    return 1
   end
 end
