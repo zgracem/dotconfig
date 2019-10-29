d408d3c3e8d4ee5fb07477a5ac94b7dc1b49927c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-29 09:50:55 -0600

    use bright, not bold, for ag matches

--- fish/functions/_wrappers/ag.fish
+++ fish/functions/_wrappers/ag.fish
@@ -1,6 +1,6 @@
 function ag --description 'The Silver Searcher. Like ack, but faster'
   set -l opts --nobreak --noheading
-  set -a opts --color-path="34" --color-line-number="36" --color-match="1;4;35"
+  set -a opts --color-path="34" --color-line-number="36" --color-match="4;95"
   set -a opts --hidden --ignore=.git --skip-vcs-ignores
 
   command ag $opts $argv
