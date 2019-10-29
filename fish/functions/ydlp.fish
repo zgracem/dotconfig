a1e8468d1b072ed482c81904f7c07b1f22709438
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-29 09:30:44 -0600

    check for presence of parallel

--- fish/functions/ydlp.fish
+++ fish/functions/ydlp.fish
@@ -1,4 +1,5 @@
 function ydlp --wraps youtube-dl --description 'Download video(s in parallel)'
   in-path youtube-dl; or return 127
+  in-path parallel; or return 127
   parallel -k youtube-dl {} ::: $argv
 end
