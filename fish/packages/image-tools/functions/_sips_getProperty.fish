6724c9129d1eacdbcbd85d88e1254d8895ace6c3
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-30 07:16:59 -0600

    use a more precise regexp

--- fish/packages/image-tools/functions/_sips_getProperty.fish
+++ fish/packages/image-tools/functions/_sips_getProperty.fish
@@ -1,4 +1,4 @@
 function _sips_getProperty -a property image
-  set -l regex '.*: (\d+)$'
+  set -l regex ".*$property: (.+)\$"
   sips --getProperty $property $image | string replace -rf $regex '$1'
 end
