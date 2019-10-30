ad2850668fb27ee0af21eb5a30ec1cc52085bbf7
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-30 07:16:44 -0600

    remove custom command-not-found handler
    This was mostly a proof of concept in bash that stuck around through the
    Great Fishening. Mostly redundant with fish's live syntax highlighting.

--- fish/conf.d/__fish_command_not_found_handler.fish
+++
@@ -1,5 +0,0 @@
-function __fish_command_not_found_handler --on-event fish_command_not_found
-  set cmd (string escape -- $argv[1])
-  echo -s (set_color $fish_color_error) $cmd (set_color normal) ": command not found" >&2
-  return 127
-end
