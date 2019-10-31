ec9a86e63a6d1c41e7fdd8157420be45b57712b6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-31 07:25:31 -0600

    make vspresszip more versatile

--- fish/packages/vsdotcom/functions/vspresszip.fish
+++ fish/packages/vsdotcom/functions/vspresszip.fish
@@ -1,7 +1,14 @@
 if string match -q 'Athena.*' $hostname
   function vspresszip -d 'Create/update a .zip file' -a slug
+    set -l zip_file
+    if string match -q '*_*' $slug
+      set zip_file "zip/$slug.zip"
+    else
+      set zip_file "zip/vivekshraya_"$slug"_press.zip"
+    end
+
     pushd $HOME/Dropbox/www/vs2017/src/media/images/press
-    rollup "zip/vivekshraya_"$slug"_press.zip" *"$slug"_*.jpg
+    rollup "$zip_file" *"$slug"_*.jpg
     popd
   end
 end
