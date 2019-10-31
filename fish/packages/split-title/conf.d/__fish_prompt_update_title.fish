3ee91c134564f848e5422fe0c48234a61c3d2769
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-31 08:35:33 -0600

    check separately

--- fish/packages/split-title/conf.d/__fish_prompt_update_title.fish
+++ fish/packages/split-title/conf.d/__fish_prompt_update_title.fish
@@ -1,6 +1,4 @@
 function __fish_prompt_update_title --on-event fish_prompt
-  if functions -q fish_window_title fish_tab_title
-    set_terminal_title --window (fish_window_title)
-    set_terminal_title --tab (fish_tab_title)
-  end
+  functions -q fish_window_title; and set_terminal_title --window (fish_window_title)
+  functions -q fish_tab_title; and set_terminal_title --tab (fish_tab_title)
 end
