b0ce673fdb944ddbe613df3731f0697966c2ec54
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-31 07:25:40 -0600

    improve fish_version completions

--- fish/completions/fish_version.fish
+++ fish/completions/fish_version.fish
@@ -1,1 +1,4 @@
-complete -c fish_version -xa 'major minor patch revision commit state'
+set -l fish_version_parts major minor patch revision commit state
+
+complete -c fish_version -x
+complete -c fish_version -n "not __fish_seen_subcommand_from $fish_version_parts" -a "$fish_version_parts"
