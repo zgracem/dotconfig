c225159ddabda7d824f4dd42d787b10c50e388e6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-31 07:22:07 -0600

    install hexyl

---
+++ fish/completions/hexyl.fish
@@ -0,0 +1,8 @@
+complete -c hexyl -s n -l length -x -d 'Read only N bytes from the input'
+complete -c hexyl -s c -l bytes -x -d 'Read only N bytes from the input'
+complete -c hexyl -s v -l no-squeezing -d 'Display all input data'
+complete -c hexyl -l color -xa 'always auto never' -d 'When to use colors'
+complete -c hexyl -l border -xa 'unicode ascii none' -d 'Draw a border'
+complete -c hexyl -s o -l display-offset -x -d 'Add OFFSET to displayed file position'
+complete -c hexyl -s h -l help -d 'Print help information'
+complete -c hexyl -s V -l version -d 'Print version information'
