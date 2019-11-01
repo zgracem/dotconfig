c2624e5aadb5541c6df1859e5b3a304575c42b5c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-01 11:29:02 -0600

    new function: `funcsrc` (like `compsrc`)

---
+++ fish/functions/_debug/funcsrc.fish
@@ -0,0 +1,6 @@
+function funcsrc -d 'Display function source file(s)' -a query
+  for file in $fish_function_path/$query.fish;
+    test -f "$file"; and short_home "$file"
+  end
+  or functions -D $query
+end
