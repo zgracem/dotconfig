3758b14c3892ebffdb0234221368a66fab619a25
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-01 11:28:27 -0600

    bikeshed needed a new coat of paint

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,10 @@
 # ~/.config/fish/config.fish
 # -----------------------------------------------------------------------------
 
-if test (fish_version major) -lt 3
+if test (fish_version major) -le 2
   begin
-    set_color red --reverse
-    echo " This configuration file cannot run on fish $version "
+    set_color brred --reverse
+    echo "*** This configuration file cannot run on fish $version ***"
     set_color normal
   end >&2
   exit 1
