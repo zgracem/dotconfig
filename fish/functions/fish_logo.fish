40e8d4248208bcbbada4c2fd9ad7727378a2762b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-02 12:23:43 -0600

    refactor old-school fish_logo

--- fish/functions/fish_logo.fish
+++ fish/functions/fish_logo.fish
@@ -1,18 +1,22 @@
 function fish_logo --description 'Display the fish logo'
-  echo '                 '(set_color F00)'___
-  ___======____='(set_color F80)'-'(set_color FF0)'-'(set_color F80)'-='(set_color F00)')
-/T            \_'(set_color FF0)'--='(set_color F80)'=='(set_color F00)')
-[ \ '(set_color F80)'('(set_color FF0)'0'(set_color F80)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color F80)'='(set_color F00)')
- \      / )J'(set_color F80)'~~    \\'(set_color FF0)'-='(set_color F00)')
-  \\\\___/  )JJ'(set_color F80)'~'(set_color FF0)'~~   '(set_color F00)'\)
-   \_____/JJJ'(set_color F80)'~~'(set_color FF0)'~~    '(set_color F00)'\\
-   '(set_color F80)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color F80)'~~~'(set_color FF0)'~~     '(set_color F80)'\\
-  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color F80)'|'(set_color FF0)'\\\\\\'(set_color F80)'~~'(set_color FF0)'~~       '(set_color F80)'L_'(set_color FF0)'_
-  '(set_color F80)'('(set_color F00)'\\'(set_color F80)'\\)  ('(set_color FF0)'\\'(set_color F80)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color F80)'__
-   '(set_color F00)'\V    '(set_color F80)'\\\\'(set_color F00)'\) =='(set_color F80)'=_____   '(set_color FF0)'\\\\\\\\'(set_color F80)'\\\\
-          '(set_color F00)'\V)     \_) '(set_color F80)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color F80)'J\)
-                      '(set_color F00)'/'(set_color F80)'J'(set_color FF0)'\\'(set_color F80)'J'(set_color F00)'T\\'(set_color F80)'JJJ'(set_color F00)'J)
-                      (J'(set_color F80)'JJ'(set_color F00)'| \UUU)
+  set -l red (set_color f00)
+  set -l ora (set_color f80)
+  set -l yel (set_color ff0)
+
+  echo '                 '$red'___
+  ___======____='$ora'-'$yel'-'$ora'-='$red')
+/T            \_'$yel'--='$ora'=='$red')
+[ \ '$ora'('$yel'0'$ora')   '$red'\~    \_'$yel'-='$ora'='$red')
+ \      / )J'$ora'~~    \\'$yel'-='$red')
+  \\\\___/  )JJ'$ora'~'$yel'~~   '$red'\)
+   \_____/JJJ'$ora'~~'$yel'~~    '$red'\\
+   '$ora'/ '$yel'\  '$yel', \\'$red'J'$ora'~~~'$yel'~~     '$ora'\\
+  (-'$yel'\)'$red'\='$ora'|'$yel'\\\\\\'$ora'~~'$yel'~~       '$ora'L_'$yel'_
+  '$ora'('$red'\\'$ora'\\)  ('$yel'\\'$ora'\\\)'$red'_           '$yel'\=='$ora'__
+   '$red'\V    '$ora'\\\\'$red'\) =='$ora'=_____   '$yel'\\\\\\\\'$ora'\\\\
+          '$red'\V)     \_) '$ora'\\\\'$yel'\\\\JJ\\'$ora'J\)
+                      '$red'/'$ora'J'$yel'\\'$ora'J'$red'T\\'$ora'JJJ'$red'J)
+                      (J'$ora'JJ'$red'| \UUU)
                        (UU)
 '
 end
