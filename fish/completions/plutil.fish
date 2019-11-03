a02a74d57d33c498edba751b0db42aec0c21b049
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-03 07:54:23 -0700

    refine `plutil` completions
    These might be ready to submit upstream.

--- fish/completions/plutil.fish
+++ fish/completions/plutil.fish
@@ -1,14 +1,16 @@
+# overwrite $XDG_DATA_HOME/fish/generated_completions/plutil.fish
 complete -c plutil --erase
+
 complete -c plutil -o help -d "Show usage information and exit"
 complete -c plutil -o lint -d "Check plist files for syntax errors"
 complete -c plutil -o convert -xa "xml1 binary1 json swift objc" -d "Rewrite file in FORMAT"
 complete -c plutil -s r -n "__fish_seen_argument -s convert; and __fish_seen_subcommand_from json" -d "Pretty-print JSON"
 complete -c plutil -s o -r -n "__fish_seen_argument -s convert" -d "Alternate path for conversion result"
-complete -c plutil -s e -r -n "__fish_seen_argument -s convert" -d "Alternate extension for converted files"
-complete -c plutil -o header -n "__fish_seen_argument -s convert; and __fish_seen_subcommand_from objc" -d "Convert the named file to Obj-C literal syntax and create a .h file"
-complete -c plutil -o insert -x -d "Insert a value into the property list"
-complete -c plutil -o replace -x -d "Overwrite an existing value in the property list"
-complete -c plutil -o type -n "__fish_seen_argument -o insert; or __fish_seen_argument -o replace" -xa "-"{bool,integer,float,string,date,data,xml,json}
+complete -c plutil -s e -x -n "__fish_seen_argument -s convert" -d "Alternate extension for converted files"
+complete -c plutil -o header -n "__fish_seen_argument -s convert; and __fish_seen_subcommand_from objc" -d "Convert FILE to Obj-C literal and create a .h file"
+complete -c plutil -o insert -x -d "Insert VALUE into the plist"
+complete -c plutil -o replace -x -d "Overwrite existing VALUE in the plist"
+complete -c plutil -o type -n "__fish_seen_argument -o insert; or __fish_seen_argument -o replace" -xa "-bool -integer -float -string -date -data -xml -json"
 complete -c plutil -o remove -x -d "Removes the value at KEYPATH"
 complete -c plutil -o extract -x -d "Outputs the value at KEYPATH as a new plist"
 complete -c plutil -s p -d "Print the plist in a human-readable fashion"
