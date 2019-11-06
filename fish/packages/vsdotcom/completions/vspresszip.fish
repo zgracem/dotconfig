646dd8f49f70ed1ca2e213f58eac38d7795027d1
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-06 09:34:25 -0700

    change completion source
    By definition, we will only ever be completing slugs from this file,
    which is smaller than parsing the entire project database.

--- fish/packages/vsdotcom/completions/vspresszip.fish
+++ fish/packages/vsdotcom/completions/vspresszip.fish
@@ -1,5 +1,5 @@
 function __fish_complete_vsdotcom_slugs
-  set -l file $HOME/Dropbox/www/vs2017/data/projects.yaml
+  set -l file $HOME/Dropbox/www/vs2017/data/press_photos.yaml
   string match -r '(?<=").+(?=":)' < "$file" | sort -u
 end
 
