4404c31d63f6e371e26f1b7868c4b9c06834368b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-08 08:13:47 -0700

    new function to launch VS Code

---
+++ fish/functions/_wrappers/code.fish
@@ -0,0 +1,12 @@
+if is-macos
+  function code --description "Open Visual Studio Code"
+    # Circumvent https://github.com/microsoft/vscode/issues/60579 if we're just
+    # opening a file or directory and don't need command-line options.
+    set -l cli_rx "--?[a-z-]+"
+    if string match -rq -- $cli_rx $argv
+      command code $argv
+    else
+      open -a "Visual Studio Code" -- $argv
+    end
+  end
+end
