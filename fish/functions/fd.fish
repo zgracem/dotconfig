99a28fa46076fe31b720d4e1b9999a62fb67fdef
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-11-10 05:20:31 -0700

    brew install fd

--- fish/functions/fd.fish
+++
@@ -1,4 +0,0 @@
-function fd --description 'Find directories in PWD whose name contains a given string' -a string
-  find -H "$PWD" -xtype d -iname "*$string*" -print 2>/dev/null | \
-  command fgrep -i --color=auto $string
-end
--- fish/functions/ff.fish
+++
@@ -1,4 +0,0 @@
-function ff --description 'Finds files in PWD whose name contains a given string' -a string
-  find -H "$PWD" -xtype f -iname "*$string*" -print 2>/dev/null | \
-  command fgrep -i --color=auto $string
-end
