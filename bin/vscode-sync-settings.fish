70e728b38fc14a1dc85cf8383e4a4d15fd856f5f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-07-07 08:47:58 -0600

    revert 4e11de2, kind of
    Now that I'm back at work, I need this functionality again.

--- bin/vscode-sync-settings.fish
+++ bin/vscode-sync-settings.fish
@@ -17,7 +17,15 @@ case 'CYGWIN*'
 
   ### sync settings
 
-  command cp -af $config_dir/settings.json $dir/settings.json
+  set base_settings (cygpath -aw $config_dir/settings.json)
+  set more_settings (cygpath -aw $config_dir/settings.windows.json)
+
+  if not test -d $dir
+    echo >&2 "error: not found:" $dir
+    exit 1
+  end
+
+  jq '. + '(jq -cj '.' $more_settings) $base_settings > $dir/settings.json
 
   ### sync keybindings
 
