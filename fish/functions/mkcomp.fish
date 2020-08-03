cf9d8337aae94c8a149b08d26f9e3592d6a23e31
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-08-03 12:23:50 -0600

    new-completion helper function

---
+++ fish/functions/mkcomp.fish
@@ -0,0 +1,29 @@
+function mkcomp
+  set -l src_dir $HOME/Dropbox/text/man
+  set -l comp_dir $HOME/.config/fish/completions
+  set -l man_files
+
+  for cmd in $argv
+    set -l man_file (command man -w $cmd)
+    or return
+
+    set -l match (string match -r '.*/(.+?)\.(.+?)(?:\.gz)?' "$man_file")
+    or return
+
+    set -l section $match[3]
+    set -l man_dir $src_dir/man$section
+
+    command mkdir -p $man_dir
+    or return
+
+    help2man --no-info --no-discard-stderr --output=$man_dir/$cmd.$section $cmd
+    or return
+
+    set man_files $man_files $man_dir/$cmd.$section
+  end
+
+  set -lx MANPATH $src_dir
+  fish_update_completions --manpath --progress --directory=$comp_dir
+
+  command rm -f $man_files
+end
