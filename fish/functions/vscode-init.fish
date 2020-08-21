dea9a15ff99d1586a1d1eff12653888c6b5fb547
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-08-21 10:10:15 -0600

    use .vscode skel directory

--- fish/functions/vscode-init.fish
+++ fish/functions/vscode-init.fish
@@ -1,5 +1,6 @@
 function vscode-init --description 'Initialize directory for use with VS Code'
   set -l dir $PWD/.vscode
+  set -l skel $HOME/Dropbox/etc/vscode-workspace-skel
   set -q argv[1]; and set dir $argv[1]/.vscode
 
   mkdir -p "$dir"
@@ -10,5 +11,9 @@ function vscode-init --description 'Initialize directory for use with VS Code'
 
   if not test -f "$dir/extensions.json"
     jq --null-input '.recommendations=[]' > "$dir/extensions.json"
+  end
+
+  if not test -f "$dir/tasks.json"; and test -d "$skel"
+    cp -a "$skel/tasks.json" "$dir/tasks.json"
   end
 end
