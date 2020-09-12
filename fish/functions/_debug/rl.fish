9ed5df66f53bd12857a6b9e6cd08503a39cbb177
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-09-12 13:03:52 -0600

    refactor `rl` function

--- fish/functions/_debug/rl.fish
+++ fish/functions/_debug/rl.fish
@@ -1,39 +1,101 @@
-function rl --description 'Reload configuration files'
-  set -g rl_exit 0
+function rl --description "Reload configuration files"
+  set --global _rl_errors 0
+  set --erase --local files_to_reload
 
+  # Prints arguments to standard error
   function _yikes
-    echo >&2 "$argv"
-    set -g rl_exit (math $rl_exit + 1)
-    return $rl_exit
+    echo -e $argv >&2
   end
 
-  function __source_v
-    for file in $argv
-      if source $file
-        short_home $file
-      else
-        _yikes "error sourcing file: $file"
-        return 1
-      end
-    end
+  # Prints each argument, with any leading $HOME replaced by "~"
+  function _confirm
+    short_home $argv
   end
 
-  function _source_f
-    if test -f "$argv[1]"
-      __source_v "$argv[1]"
+  # Returns true if $file exists
+  function _exist -a file
+    test -f "$file"
+  end
+
+  # Checks whether $file exists:
+  #   if it does, returns true;
+  #   if not, updates the error counter and returns false.
+  function _must_exist -a file
+    if _exist "$file"
+      return 0
     else
+      _increment_errors
       return 1
     end
   end
 
+  # Checks whether $func is a defined, reloadable function:
+  #   if $func was defined in an existing file, prints the path to stdout and returns true;
+  #   if $func can't be reloaded, prints the reason to stderr and returns false;
+  #   if $func is not defined, silently returns false.
+  function _reloadable_function -a func
+    set --local src (functions -D $func)
+    set --local reason "file not found: $src"
+
+    switch "$src"
+    case "/*"
+      _must_exist "$src"; and echo "$src"; and return 0
+    case "stdin"
+      set reason "defined interactively"
+    case "-"
+      set reason "defined via `source`"
+    case "n/a"
+      return 1
+      # set reason "not defined"
+    end
+
+    _yikes "can’t reload function ‘$func’: $reason"
+    return 1
+  end
+
+  # Attempts to source file argument:
+  #   on success, prints the filename to stdout;
+  #   on failure, prints a message to stderr and returns false.
+  function __source_v -a file
+    if source $file
+      _confirm $file
+    else
+      set -l code $status
+      _yikes "error sourcing file: $file"
+      _increment_errors
+      return $code
+    end
+  end
+
+  # Checks if each argument exists as a file:
+  #   if it does, tries to source it;
+  #     on success, prints the filename to stdout;
+  #     on failure, prints a message to stderr and continues;
+  #   if it doesn't exist, silently continues;
+  #   returns false if anything failed.
+  function _source_all
+    set --local code 0
+    for file in $argv
+      if _must_exist $file
+        __source_v "$file"; or set code $status
+      end
+    end
+    return (math $status + $code)
+  end
+
+  # Increments the error counter
+  function _increment_errors -a inc
+    set -q inc[1]; or set inc 1
+    set --global _rl_errors (math "$_rl_errors + $inc")
+  end
+
+  # With no arguments, reload the main config file
   if test (count $argv) -eq 0
-    __source_v "$__fish_config_dir/config.fish"
-    return
+    set --append files_to_reload "$__fish_config_dir/config.fish"
   end
 
   for arg in $argv
-    set --local error_message "couldn't find file for ‘$arg’"
-
+    # Some reloads require other actions first
     switch "$arg"
     case colours
       set --erase -U __zgm_init_colours
@@ -42,16 +104,37 @@ function rl --description 'Reload configuration files'
       set --erase -g SSH_AGENT_PID
     end
 
-    if functions -q $arg
-      set --local function_file (functions -D $arg)
-      _source_f "$function_file"
-        or _yikes "$error_message: $function_file"
+    set --erase --local checked_files
+
+    # Search config dirs
+    set --local dirs_to_check "$__fish_config_dir" "$__fish_config_dir"/conf.d
+    set --local files_to_check $dirs_to_check/"$arg.fish"
+
+    for config_file in $files_to_check
+      _exist "$config_file"; and set --append checked_files "$config_file"
+    end
+
+    # Search functions
+    if set --local function_file (_reloadable_function $arg)
+      set --append checked_files $function_file
+    end
+
+    # Search completion dirs and take only the first result
+    for completion_file in $fish_complete_path/"$arg.fish"
+      if _exist "$completion_file"
+        set --append checked_files "$completion_file"
+        break
+      end
+    end
+
+    if test (count $checked_files) -gt 0
+      set --append files_to_reload $checked_files
     else
-      _source_f "$__fish_config_dir/$arg.fish"
-        or _source_f "$__fish_config_dir/conf.d/$arg.fish"
-        or _yikes $error_message
+      _increment_errors
+      _yikes "nothing found for “$arg”"
     end
   end
 
-  return $rl_exit
+  _source_all $files_to_reload
+  return $_rl_errors
 end
