20e487a4c013cb2e6ebc63f9919f1898202115cc
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-10-26 10:33:44 -0600

    finally installed Cygwin on Alcazar

--- local/Alcazar/fish/functions/cygsetup.fish
+++ local/Alcazar/fish/functions/cygsetup.fish
@@ -1,28 +1,34 @@
 function cygsetup --description 'Launch the Cygwin installer'
-  set -l installer_dir "$HOME/Downloads/_Installers"
-  set -l cyg_bin "$installer_dir/setup-x86_64.exe"
-  set -l pkg_dir (cygpath -aw "$USERPROFILE/Cygwin")
+  set -l installer_dir (cygpath -au "$USERPROFILE")"/Applications/cygwin64"
+  set -l pkg_dir (cygpath -aw "$installer_dir")
   set -l log_dir "$HOME/var/log/cygwin"
+
+  set -l cyg_bin "$installer_dir/setup-x86_64.exe"
 
   set -l cyg_root (cygpath -aw "/" | string escape)
 
   set -l params
+
   # Root installation directory (-R)
   set -a params --root $cyg_root
+
   # Directory to save downloaded packages in (-l)
   set -a params --local-package-dir $pkg_dir
+
   # Architecture to install: x86_64 or x86
   set -a params --arch x86_64
+
   # Semi-attended mode
   set -a params --package-manager
+
   # Also upgrade installed packages
   set -a params --upgrade-also
+
   # Don't verify setup.ini signature
   set -a params --no-verify
+
   # Don't create Desktop/Start menu shortcuts (-n)
   set -a params --no-shortcuts
-  # Don't require admin privileges
-  set -a params --no-admin
 
   if not test -x "$cyg_bin"
     echo >&2 "can't find $cyg_bin"
