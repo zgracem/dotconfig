51768048411cc331a3697c3d42adcbc99b3ea311
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-08 13:45:51 -0700

    circumvent keychain(1) exiting OK w/ status code 4

--- fish/conf.d/keychain.fish
+++ fish/conf.d/keychain.fish
@@ -8,14 +8,20 @@ if status is-interactive; and in-path keychain; and not set -gq SSH_AGENT_PID
   set -l ssh_env $keychain_dir/.env
 
   set -l params      --eval --quick --quiet
-  set params $params --absolute --dir "$keychain_dir"
+  set params $params --dir "$keychain_dir" --absolute
   set params $params --inherit any --ignore-missing
 
   if test -d "$keychain_dir"
     set -lx SHELL (status fish-path 2>/dev/null; or command -s fish)
-    if keychain $params $keys > $ssh_env
+    keychain $params $keys > $ssh_env
+
+    if test -s $ssh_env # ssh-agent loaded, or existing agent found
       command chmod 600 $ssh_env
       source $ssh_env
+      exit 0
+    else # `keychain` command failed and (hopefully) printed to stderr
+      command rm $ssh_env
+      exit 1
     end
   end
 end
