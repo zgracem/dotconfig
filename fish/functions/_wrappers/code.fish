8874867518ffa2f857b2f7eaebb2610e6ae1ccda
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-08 13:45:27 -0700

    disable `code` wrapper function

--- fish/functions/_wrappers/code.fish
+++ fish/functions/_wrappers/code.fish
@@ -1,12 +1,13 @@
-if is-macos
-  function code --description "Open Visual Studio Code"
-    # Circumvent https://github.com/microsoft/vscode/issues/60579 if we're just
-    # opening a file or directory and don't need command-line options.
-    set -l cli_rx "--?[a-z-]+"
-    if string match -rq -- $cli_rx $argv
-      command code $argv
-    else
-      open -b com.microsoft.VSCode -- $argv
-    end
-  end
-end
+### ZGM disabled 2020-09-17 -- replaced by ~/Dropbox/scripts/code.fish
+# if is-macos
+#   function code --description "Open Visual Studio Code"
+#     # Circumvent https://github.com/microsoft/vscode/issues/60579 if we're just
+#     # opening a file or directory and don't need command-line options.
+#     set -l cli_rx "--?[a-z-]+"
+#     if string match -rq -- $cli_rx $argv
+#       command code $argv
+#     else
+#       open -b com.microsoft.VSCode -- $argv
+#     end
+#   end
+# end
