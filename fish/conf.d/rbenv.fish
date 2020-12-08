0e3284791c2d514085601d180c9955b930d4398b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-08 13:46:33 -0700

    setup rbenv intelligently if required

--- fish/conf.d/rbenv.fish
+++ fish/conf.d/rbenv.fish
@@ -1,3 +1,5 @@
 if in-path rbenv
+  test -d $HOME/.rbenv; or mkdir $HOME/.rbenv
+  test -f $HOME/.rbenv/version; or rbenv global system
   source (rbenv init - | psub)  # adds duplicate shims dir to PATH
 end
