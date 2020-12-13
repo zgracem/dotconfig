98d1f412b5bc13ba6e3d0ed8f60fb64e379e6fd3
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-13 12:29:31 -0700

    new function: install-github-fish

---
+++ fish/functions/install-github-fish.fish
@@ -0,0 +1,22 @@
+function install-github-fish
+    set src_dir "$HOME/.local/src/github.com/fish-shell"
+    set build_dir "$src_dir/build"
+    set install_dir "$HOME/opt/stow/fish-HEAD"
+
+    if test -d "$src_dir/.git"
+        rm -rf "$build_dir"
+        cd "$src_dir"; or return
+        git pull --force; or return
+    else
+        mkdir -p "$src_dir"; or return
+        cd "$src_dir"; or return
+        git clone git@github.com:fish-shell/fish-shell.git; or return
+    end
+
+    mkdir -p "$build_dir"; and cd "$build_dir"; or return
+
+    cmake .. -DCMAKE_INSTALL_PREFIX=$install_dir; or return
+
+    make
+    and make install
+end
