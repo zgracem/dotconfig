31ce8fb24263c94ef2482ec99188e0a516346f06
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-21 15:46:51 -0700

    follow-up to 53fbad6

--- local/Citadel/config/bashrc.d/bitbar.bash
+++
@@ -1,4 +0,0 @@
-# If BitBar is set to open at login, it will launch too early to read the value
-# of RUBYLIB from ~/.config/environment.d, and my menu items will throw
-# LoadErrors trying to find my personal stdlib.
-killall -s BitBar &>/dev/null || open -a BitBar
