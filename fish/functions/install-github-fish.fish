96df42ab50011521d1d67f1b758499b5106c11cb
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-30 09:53:39 -0700

    refactor install-github-fish into sub-functions

--- fish/functions/install-github-fish.fish
+++ fish/functions/install-github-fish.fish
@@ -3,8 +3,19 @@ function install-github-fish
     set -l src_dir "$HOME/.local/src/github.com/fish-shell"
     set -l build_dir "$src_dir/build"
     set -l fish_prefix "$HOME/opt/stow/fish-HEAD"
-    set -l install__fish_data_dir "$fish_prefix/share/fish"
 
+    __install-github-fish_git-acquire
+    or return
+
+   __install-github-fish_make-fish
+    or return
+
+    __install-github-fish_link-completions
+
+   cd "$prev_wd"
+end
+
+function __install-github-fish_git-acquire
     if test -d "$src_dir/.git"
         rm -rf "$build_dir" >/dev/null
         cd "$src_dir"; or return
@@ -14,15 +25,16 @@ function install-github-fish
         cd "$src_dir"; or return
         git clone git@github.com:fish-shell/fish-shell.git; or return
     end
+end
 
+function __install-github-fish_make-fish
     mkdir -p "$build_dir"; and cd "$build_dir"; or return
-
     cmake .. -DCMAKE_INSTALL_PREFIX=$fish_prefix; or return
-
     make
     and make install
-    or return
+end
 
+function __install-github-fish_link-completions
     set -l fish_completions_dir (pkg-config --variable completionsdir fish); or return
     set -l vendor_completions_dir $fish_prefix/share/fish/vendor_completions.d
     if test (count $vendor_completions_dir/*.fish) -eq 0
@@ -30,6 +42,4 @@ function install-github-fish
         command rm -rf $vendor_completions_dir
         command ln -s $fish_completions_dir $vendor_completions_dir; or return
     end
-
-    cd "$prev_wd"
 end
