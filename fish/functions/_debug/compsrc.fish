0845d6e6fd8ce62fd6cd0d8901b5b73be684cea4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2020-12-30 14:15:21 -0700

    initialize completions before searching them
    Not sure if this makes a difference tbh

--- fish/functions/_debug/compsrc.fish
+++ fish/functions/_debug/compsrc.fish
@@ -1,5 +1,7 @@
 function compsrc -d 'Display completion source file(s)' -a query
+    complete -C$query >/dev/null
     for file in $fish_complete_path/$query.fish
         test -f "$file"; and short_home "$file"
     end
+    return 0
 end
