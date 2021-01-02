d42bb35f0e7052b415caa2921f1b20e447e15972
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-02 13:44:47 -0700

    initialize (and use) rbenv-default-gems

--- fish/conf.d/rbenv.fish
+++ fish/conf.d/rbenv.fish
@@ -1,5 +1,16 @@
 if in-path rbenv
     test -d $HOME/.rbenv; or mkdir $HOME/.rbenv
     test -f $HOME/.rbenv/version; or rbenv global system
+
+    set -l plugin_dir $HOME/.rbenv/plugins/rbenv-default-gems
+    if test ! -d $plugin_dir
+        set -l plugin_git https://github.com/rbenv/rbenv-default-gems.git
+        git clone $plugin_git $plugin_dir
+    end
+
+    if test ! -L $HOME/.rbenv/default-gems
+        ln -sf $XDG_CONFIG_HOME/rbenv/default-gems $HOME/.rbenv/default-gems
+    end
+
     source (rbenv init - | psub) # adds duplicate shims dir to PATH
 end
