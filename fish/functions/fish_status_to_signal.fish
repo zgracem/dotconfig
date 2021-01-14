978e2120a01b32cdef31778190732125a586f4e1
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-14 15:34:04 -0700

    simplify `fish_status_to_signal`

--- fish/functions/fish_status_to_signal.fish
+++ fish/functions/fish_status_to_signal.fish
@@ -1,30 +1,22 @@
 # Overrides $__fish_data_dir/functions/fish_status_to_signal.fish
 # because different systems have different names for signals above 134.
-function fish_status_to_signal --description "Convert an exit code into a named signal, if appropriate" -a code
-    if test (count $argv) -ne 1
-        echo "expected single integer argument" >&2
-        return 1
-    end
+function fish_status_to_signal -d "Convert an exit code into a named signal, if appropriate"
+    argparse -X1 'i/int' -- $argv; or return
 
-    if test $code -eq 0
-        return 0
-    end
+    set -l code $argv[1]
+    test -z "$code"; or test $code -eq 0; and return 0
 
-    set -l output $code
-
-    if test $code -gt 128
+    if test $code -gt 128; and not set -q _flag_int
         set -l signals HUP INT QUIT ILL TRAP ABRT
 
         if test $code -gt 134
-            for ext_sig in (command kill -l | string upper | string split " ")
-                contains $ext_sig $signals; or set -a signals $ext_sig
-            end
+            set -a signals (command kill -l | string upper | string split " ")
         end
 
-        if test $code -le (math (count $signals) + 128)
-            set output $signals[(math $code - 128)]
+        if set -q signals[(math $code % 128)]
+            set code $signals[(math $code % 128)]
         end
     end
 
-    echo $output
+    echo $code
 end
