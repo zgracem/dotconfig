9cc93d242f65bc2b89de7f5578ecf5d755d5ebaf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-15 12:39:18 -0700

    not how -x works, lol

--- fish/packages/pyjamas/functions/pyjamas.fish
+++ fish/packages/pyjamas/functions/pyjamas.fish
@@ -1,5 +1,5 @@
 function pyjamas --description "Convert configuration files between formats"
-    argparse -xh,{i,o,m} -x{i,o},m 'i/in=' 'o/out=' 'm/mode=' -- $argv
+    argparse -x{i,o},m 'i/in=' 'o/out=' 'm/mode=' -- $argv
     or return
 
     if set -q _flag_mode
