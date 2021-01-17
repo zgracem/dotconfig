fdf329006ab848c158fdb94a27c8fe3bd1945efa
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-17 15:01:18 -0700

    update config settings
    Don't mind if I `man brew | grep -ow 'HOMEBREW_\w+' | sort -u`

--- environment.d/homebrew.sh
+++ environment.d/homebrew.sh
@@ -7,7 +7,6 @@ if [ -n "$HOMEBREW_BREW_FILE" ]; then
 else
   return
 fi
-
 unset -v HOMEBREW_BREW_FILE
 
 if [ "${HOMEBREW_PREFIX#*linuxbrew}" != "$HOMEBREW_PREFIX" ]; then
@@ -17,19 +16,34 @@ if [ "${HOMEBREW_PREFIX#*linuxbrew}" != "$HOMEBREW_PREFIX" ]; then
   export HOMEBREW_TEMP="$XDG_RUNTIME_DIR/homebrew"
 fi
 
-# Only auto-update every 10 minutes
+# Only auto-update every 10 minutes (default is 5 minutes)
 export HOMEBREW_AUTO_UPDATE_SECS=600
 
 # Use `bat` for `brew cat`
 export HOMEBREW_BAT=1
 export HOMEBREW_BAT_CONFIG_PATH="$XDG_CONFIG_HOME/bat/config_homebrew"
 
+# `brew bundle`
+export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/brew/Brewfile"
+
+# Delete cached files after 30 days (default is 120 days)
+export HOMEBREW_CLEANUP_MAX_AGE_DAYS=30
+
+# Print install times for each formula
+export HOMEBREW_DISPLAY_INSTALL_TIMES=1
+
+# File listing default formulae for `brew livecheck` to check
+export HOMEBREW_LIVECHECK_WATCHLIST="$XDG_CONFIG_HOME/brew/livecheck_watchlist"
+
+# `brew update` will only mention installed formulae
+export HOMEBREW_UPDATE_REPORT_ONLY_INSTALLED=1
+
 # Always use the latest stable tag (even if developer commands have been run)
 export HOMEBREW_UPDATE_TO_TAG=1
 
 # Check for macOS/iOS terminal clients w/ emoji support
 case $TERM_PROGRAM in
-  Apple_Terminal|iTerm*|Prompt_2|Coda|vscode)
+  Apple_Terminal|iTerm*|Prompt_2|vscode)
     case "$(date +%B)" in
       "October")
         # Pumpkin Spice Homebrew!
