0141dfd0507db22d0d950112147da9036104198c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-17 15:02:16 -0700

    copyedit some completions

--- fish/completions/regfont.fish
+++ fish/completions/regfont.fish
@@ -2,6 +2,6 @@
 
 complete -c regfont -s a -l add -r -d 'Add specified fonts'
 complete -c regfont -s r -l remove -r -d 'Remove specified fonts'
-complete -c regfont -s h -l help -d 'This help message'
+complete -c regfont -s h -l help -d 'Print help message'
 complete -c regfont -s v -l version -d 'Print version information'
 complete -c regfont -s d -l debug -d 'Turn on debugging information'
