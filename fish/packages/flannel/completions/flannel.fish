7740a0551396f94e26acb0471d70c976fc142b5d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-17 15:02:59 -0700

    fix completions

--- fish/packages/flannel/completions/flannel.fish
+++ fish/packages/flannel/completions/flannel.fish
@@ -1,4 +1,4 @@
-complete -c flannel -n __fish_is_first_token -a "export import dump touch"
-complete -c flannel -n "__fish_seen_subcommand export" -s b -l byhost -rF
-complete -c flannel -n "__fish_seen_subcommand export" -s o -l old -rF
-complete -c flannel -n "__fish_seen_subcommand import" -s n -l dryrun
+complete -c flannel -n __fish_is_first_token -x -a "export import dump touch"
+complete -c flannel -n "__fish_seen_subcommand_from export" -s b -l byhost -rF
+complete -c flannel -n "__fish_seen_subcommand_from export" -s o -l old -rF
+complete -c flannel -n "__fish_seen_subcommand_from import" -s n -l dryrun
