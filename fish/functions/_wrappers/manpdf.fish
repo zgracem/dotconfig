zgracem/dotconfig

commit 5cdf77d667fb29e54e6e51bf3cb79e676687a88a

use `set -q` more often

fish/functions/_wrappers/manpdf.fish
@@ -11,7 +11,7 @@ function manpdf --description 'View a manual page as PDF'
     if string match -q "/*" "$pdf"
         echo "$pdf"
 
-        if test -z "$SSH_CONNECTION"
+        if set -q SSH_CONNECTION
             open "$pdf"
         end
     end

fish/functions/_debug/rl.fish
@@ -90,7 +90,7 @@ function rl --description "Reload configuration files"
     end
 
     # With no arguments, reload the main config file
-    if test (count $argv) -eq 0
+    if not set -q argv[1]
         set --append files_to_reload "$__fish_config_dir/config.fish"
     end
 
@@ -127,7 +127,7 @@ function rl --description "Reload configuration files"
             end
         end
 
-        if test (count $checked_files) -gt 0
+        if set -q checked_files[1]
             set --append files_to_reload $checked_files
         else
             _increment_errors

fish/functions/newwin.fish
@@ -4,7 +4,7 @@ function newwin --description 'Open a command in a new tmux window'
     if not in-tmux
         eval $argv
         return
-    else if test (count $argv) -eq 0
+    else if not set -q argv[1]
         tmux new-window
         return
     end

fish/functions/rolldice.fish
@@ -41,7 +41,7 @@ function rolldice --description 'Roll some dice'
         set -a totals "($total)"
     end
 
-    if test (count $totals) -gt 1
+    if set -q totals[2]
         set -l grand_total (string join " + " $totals)
         sum $grand_total
     end

fish/functions/wtf.fish
@@ -1,5 +1,5 @@
 function wtf -d "Display information about commands"
-    test -n "$argv"; or return 1
+    set -q argv[1]; or return 1
 
     for subject in $argv
         for type in (type -at $subject)

fish/prompt/__fish_prompt_git.fish
@@ -42,7 +42,7 @@ function __fish_prompt_git --description 'Display git info in the fish prompt'
     else if test $staged -gt 0
         set_color $fish_prompt_color_git_needs_commit
         echo -n $icon_dirty
-    else if test -n "$__fish_prompt_show_git_clean"
+    else if set -q __fish_prompt_show_git_clean
         set_color $fish_prompt_color_git_clean
         echo -n $icon_clean
     end

commit d2f3e667fb0c924ef8c375d5720ce7cf77e0074b

oops
Fix for issue introduced in 5cdf77d

fish/functions/_wrappers/manpdf.fish
@@ -11,7 +11,7 @@ function manpdf --description 'View a manual page as PDF'
     if string match -q "/*" "$pdf"
         echo "$pdf"
 
-        if set -q SSH_CONNECTION
+        if not set -q SSH_CONNECTION
             open "$pdf"
         end
     end