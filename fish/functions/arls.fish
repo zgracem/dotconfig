294b99bbe7afbe1b6c5d3b610c669270be904c9b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-23 11:18:28 -0700

    rename function

--- fish/functions/arls.fish
+++ fish/functions/arls.fish
@@ -1,4 +1,4 @@
-function exls --description 'List the contents of archives'
+function arls --description 'List the contents of archives'
     for archive in $argv
         switch $archive
             case '*.7z'
@@ -10,15 +10,15 @@ function exls --description 'List the contents of archives'
             case '*.rar'
                 unrar vb $archive
                 echo
-            case '*.tar*' '*.tbz2' '*.tgz'
+            case '*.tar' '*.tar.*' '*.tbz2' '*.tgz'
                 tar tf $archive
             case '*.zip'
                 zip -sf $archive
             case Payload
                 cpio -itv -F $archive
             case '*'
-                echo >&2 -s "error: don't know how to list ." \
-                    (string split -r -m1 . $archive)[-1]" files"
+                set -l type (string split -r -m1 . $archive)[-1]
+                echo >&2 -s "error: don't know how to list .$type files"
                 return 1
         end
     end
