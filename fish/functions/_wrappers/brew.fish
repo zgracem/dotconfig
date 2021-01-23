293fd29b52e5b6d3b884aa59aa4d44722d563caf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-01-23 11:20:48 -0700

    remove no-longer-needed workaround

--- fish/functions/_wrappers/brew.fish
+++ fish/functions/_wrappers/brew.fish
@@ -2,11 +2,15 @@ function brew --description 'The missing package manager for macOS'
     in-path brew; or return 127
 
     switch $argv[1]
-        case update
-            # Addresses https://github.com/Homebrew/brew/issues/5791
-            env SHELL=(command -s bash) brew $argv
         case cd
-            _brew_cd $argv[2..-1]
+            set -e argv[1]
+            switch $argv[1]
+                case cache cellar prefix repo repository
+                    cd (command brew --$argv[1] $argv[2..-1])
+                case '*'
+                    printf >&2 "%s: destination unknown\\n" "$destination"
+                    return 1
+            end
         case '*'
             command brew $argv
     end
