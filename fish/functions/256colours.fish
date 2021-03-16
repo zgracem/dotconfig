b79e5d396fc31591cd27a04ca5d9bb7b669a4a43
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-03-16 11:57:20 -0600

    add function to print 256-colour terminal palette 🎨️

---
+++ fish/functions/256colours.fish
@@ -0,0 +1,16 @@
+function 256colours
+    argparse 'g/grey' -- $argv
+    or return
+
+    set -l colours (seq 16 231)
+    set -l width -c 192
+
+    if set -q _flag_grey
+        set colours (seq 0 15) (seq 232 255)
+    end
+
+    for n in $colours
+        set -l nnn (printf "%03d" $n)
+        echo -e "\e[7;38;5;$n;48;5;16m" $nnn "\e[0m"
+    end | column $width
+end
