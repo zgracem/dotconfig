40b9709950c94d2d3fd31c024ab6594a5f897b8c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-03-19 15:51:14 -0600

    fix bugs from rewrite in 5fcaae6

--- youtube-dl/postexec.fish
+++ youtube-dl/postexec.fish
@@ -19,14 +19,19 @@ end
 #  in: /path/to/video.mp4
 # out: /path/to/video.info.json
 function metadata_filename -a video_filename
-    set -l dirname (dirname $video_filename)
-    set -l basename (basename $video_filename | string split -r -m1 -f2 .)
-    echo -n $dirname/$basename.info.json
+    set -l basename (string split -r -m1 -f1 . $video_filename)
+    set -l metadata_file $basename.info.json
+
+    if test -f $metadata_file
+        echo -n $basename.info.json
+    else
+        bail "metadata not found: $metadata_file"
+    end
 end
 
 # `jq` extracts the value of "webpage_url" from metadata in $json_file;
 # `plutil` converts that string to a binary property list;
-# `xxd` prints that binary plist as plain (-p) uppercase (-u) hex bytes;
+# `xxd` prints that binary plist as `-p`lain `-u`ppercase hex bytes;
 # `string join` removes newlines.
 function binary_plist_url -a json_file
     plutil -convert binary1 -o - (jq .webpage_url $json_file | psub) \
@@ -35,27 +40,30 @@ function binary_plist_url -a json_file
 end
 
 # Extracts the source URL from the metadata associated with $file;
-# converts it to an ASCII hex representation of a binary representation of
-# an entire XML property list representing that single string, because
-# what do you mean NextStep isn't still 100% good decisions 20 years later...;
-# then writes that string to $file's Spotlight "WhereFrom" attribute.
+# converts it to an ASCII hex representation of a binary representation
+# of an entire XML property list representing that single string
+# (because NextStep is still 100% good decisions 20 years later...);
+# then writes that string to $file's "WhereFrom" Spotlight attribute.
 function set_wherefrom -a file
     set -l metadata_file (metadata_filename $file)
-    set -l hex (binary_plist_url $metadata_file)
+    set -l hex_url (binary_plist_url $metadata_file)
     or bail "failed to process: $metadata_file"
 
     set -l attr com.apple.metadata:kMDItemWhereFroms
-    xattr -wx $attr $hex $file
-    or bail "xattr failed to set $attr to `$hex`"
+
+    xattr -wx $attr $hex_url $file
+    or bail "xattr failed to set $attr to `$hex_url`"
 end
 
 # Creates a string from the current UNIX epoch, the name of the application,
 # and a new UUID, and applies it to $file to set its quarantine attribute.
 function set_quarantine_timestamp -a file
-    set -l metadata (printf "0081;%x;youtube-dl;%s" (date +%s) (uuidgen))
+    set -l timestamp_parts 0081 (printf "%x" (date +%s)) youtube-dl (uuidgen)
 
     set -l attr com.apple.quarantine
-    xattr -w $attr $metadata $video_file
+    set -l metadata (string join \; $timestamp_parts)
+
+    xattr -w $attr $metadata $file
     or bail "xattr failed to set $attr to `$metadata`"
 end
 
@@ -63,5 +71,5 @@ end
 # main()
 # ----------------------------------------------------------------------------
 
-set_quarantine_timestamp $video_file
-set_wherefrom $video_file
+set_quarantine_timestamp $argv
+set_wherefrom $argv
