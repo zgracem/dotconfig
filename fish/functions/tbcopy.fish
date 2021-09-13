cbd3b8194c1e1574094fc18561af046925cb9aca
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-09-13 14:46:42 -0600

    add a description to `tbcopy`

--- fish/functions/tbcopy.fish
+++ fish/functions/tbcopy.fish
@@ -1,12 +1,14 @@
+set -l tbcopy_desc "Read to clipboard and write to standard output"
+
 if in-path pbcopy
-    function tbcopy
+    function tbcopy -d $tbcopy_desc
         set -l tmpfile (gmktemp -t tbcopy.XXXXXXXX); or return
         cat >$tmpfile
         pbcopy <$tmpfile
         cat $tmpfile
     end
 else if test -w /dev/clipboard
-    function tbcopy
+    function tbcopy -d $tbcopy_desc
         tee /dev/clipboard
     end
 end
