d90f82f0e63a33ac98a57221649c5f7d89a83227
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-11-29 07:12:56 -0700

    use Windows-1252 encoding, or taskbar will have "â€¦" instead of dots

--- fish/functions/fish_tab_title.fish
+++ fish/functions/fish_tab_title.fish
@@ -1,12 +1,16 @@
 function fish_tab_title --description 'Update the tab title'
     set -l cwd (short_home "$PWD")
     set -l cwd_parts (string split "/" "$cwd")
+    set -l ellipsis "…"
+
+    # use Windows-1252 encoding, or taskbar will have "â€¦" instead of dots
+    is-cygwin; and set ellipsis (echo -ne "\x85")
 
     echo -ns (prompt_hostname) ":"
 
     if test (count $cwd_parts) -le 2
         echo -n $cwd
     else
-        string join / -- $cwd_parts[1] "…" $cwd_parts[-1]
+        string join / -- $cwd_parts[1] $ellipsis $cwd_parts[-1]
     end
 end
