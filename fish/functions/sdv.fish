13cd0d19092c6d7fb2849a4cf4876bce7c51a920
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-12-21 08:21:38 -0700

    no longer needed

--- fish/functions/sdv.fish
+++
@@ -1,4 +0,0 @@
-function sdv -d 'Stardew Valley'
-    set -l dir "$HOME/Library/Application Support/Steam/SteamApps/common/Stardew Valley"
-    env TERM=xterm $dir/Contents/MacOS/StardewModdingAPI.bin.osx
-end
