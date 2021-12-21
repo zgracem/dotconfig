0b3a7fe226640a79f2fdb4ba52f3e47f8ff7382f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2021-12-21 08:21:25 -0700

    new aliases in `g2`

--- fish/functions/g2.fish
+++ fish/functions/g2.fish
@@ -1,23 +1,27 @@
 function g2 --description 'Go somewhere' -a alias
     set -l destination (string split / --max=1 -- $alias)
+
+    set -l steam_dir "$HOME/Library/Application Support/Steam"
+    set -l dropbox "$HOME/Dropbox"
+
     set -l dir
     switch $destination[1]
         case inbox
-            set dir "$HOME/Dropbox/inbox"
+            set dir "$dropbox/inbox"
         case proj
-            set dir "$HOME/Dropbox/Projects"
+            set dir "$dropbox/Projects"
         case stow
             set dir "$HOME/opt/stow"
         case defunct
-            set dir "$HOME/Dropbox/src.old"
+            set dir "$dropbox/src.old"
         case scratch
             set dir "$HOME/tmp/_scratch"
         case vs9
-            set dir "$HOME/Dropbox/VS/www/vsdotcom"
+            set dir "$dropbox/VS/www/vsdotcom"
         case imprint
-            set dir "$HOME/Dropbox/VS/www/vsbooks"
+            set dir "$dropbox/VS/www/vsbooks"
         case 2a
-            set dir "$HOME/Dropbox/VS/www/2adotcom"
+            set dir "$dropbox/VS/www/2adotcom"
         case desktop
             set dir "$XDG_DESKTOP_DIR"
         case dl
@@ -25,9 +29,15 @@ function g2 --description 'Go somewhere' -a alias
         case github
             set dir "$HOME/.local/src/github.com"
         case ruby
-            set dir "$HOME/Dropbox/src"
+            set dir "$dropbox/src"
         case scripts
-            set dir "$HOME/Dropbox/src/shell"
+            set dir "$dropbox/src/shell"
+        case steam
+            set dir "$steam_dir"
+        case steamapps
+            set dir "$steam_dir/steamapps"
+        case steamuser
+            set dir "$steam_dir/userdata"
         case '*'
             echo >&2 "don't know how to go to $destination!"
             return 1
