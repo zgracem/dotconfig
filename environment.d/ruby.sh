a47ace90588f9977902bad4b7a5f75b4d079c869
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-02-10 10:47:57 -0700

    set `RUBY_CONFIGURE_OPTS` to use Homebrew's up-to-date OpenSSL

--- environment.d/ruby.sh
+++ environment.d/ruby.sh
@@ -3,3 +3,10 @@ export RI="--format=ansi"
 export IRBRC="$XDG_CONFIG_HOME/ruby/irbrc"
 export PRYRC="$XDG_CONFIG_HOME/ruby/pryrc"
 export RUBOCOP_CACHE_ROOT="${XDG_CACHE_HOME:-$HOME/var/cache}"
+
+# ruby-build installs a non-Homebrew OpenSSL for each Ruby version installed
+# and these are never upgraded. So link Rubies to Homebrew's OpenSSL 1.1 (which
+# is upgraded):
+if command -v brew >/dev/null; then
+  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt/openssl@1.1"
+fi
