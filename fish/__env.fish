84788efc0a89147b6f06a6fc8a1bfb824f82ccea
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-03-23 11:05:50 -0600

    add `~/.private/tokens/README.md`

--- fish/__env.fish
+++ fish/__env.fish
@@ -23,6 +23,7 @@ end
 
 # API tokens
 for token_file in ~/.private/tokens/*
+    string match -rq '^[A-Z_]+$' (basename $token_file); or continue
     read -gx (basename $token_file) <$token_file
 end
 
