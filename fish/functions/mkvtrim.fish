f1aaa6d7852ed28a4e5c509b3a32e665c2ccf3d0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-03-23 08:17:55 -0600

    add function `mkvtrim`

---
+++ fish/functions/mkvtrim.fish
@@ -0,0 +1,7 @@
+function mkvtrim -d "Trim a segment from an MKV file"
+    set -l start $argv[1] # e.g. 00:00:25.375
+    set -l end $argv[2]   # e.g. 00:00:34.458
+    set -l input $argv[3]
+    set -l output $argv[4]
+    mkvmerge --output $output --split parts:$start-$end $input
+end
