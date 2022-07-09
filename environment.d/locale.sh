bed7717aab2a85e67a060aaa111aa08b221a7164
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-07-09 06:35:00 -0600

    locale detection
    - on fish, actually cache the locale as originally intended
    - on bash, which no longer caches the locale, remove inaccurate comment

--- environment.d/locale.sh
+++ environment.d/locale.sh
@@ -7,8 +7,6 @@ if [ -z "$LANGUAGE" ]; then
 fi
 
 # LANG: used as a substitute for any unset LC_* variable.
-# Some systems have 'en_CA.UTF-8', some have 'en_CA.utf8' -- search locales
-# to find whichever is available, and cache the result.
 lang_file="$XDG_DATA_HOME/locale/LANG"
 
 if [ -f "$lang_file" ] && [ -z "$LANG" ]; then
