26d9a128640381fa066cd071360ed717002983ec
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-07-09 06:35:33 -0600

    change name of ImageMagick binary

--- fish/functions/average.fish
+++ fish/functions/average.fish
@@ -1,3 +1,3 @@
 function average --description 'Get the average colour of an image'
-    convert $argv[1] -resize 1x1 txt:- | string match -rg "(#[[:xdigit:]]{6})"
+    magick $argv[1] -resize 1x1 txt:- | string match -rg "(#[[:xdigit:]]{6})"
 end
