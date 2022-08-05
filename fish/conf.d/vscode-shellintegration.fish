79e37a0b003b82159c58ad849d7fbf8c5d972be4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-04 19:18:01 -0600

    commenting and formatting

--- fish/conf.d/vscode-shellintegration.fish
+++ fish/conf.d/vscode-shellintegration.fish
@@ -6,8 +6,9 @@
 #
 # 1) Place this file at `~/.config/fish/conf.d/vscode-shellintegration.fish`.
 #
-# 2) Add lines to the beginning and the end of the `fish_prompt` function in
-#    `~/.config/fish/functions/fish_prompt.fish`, as follows:
+# 2) Add `__vsc_fish_prompt_before` and `__vsc_fish_prompt_after` to the
+#    beginning and end, respectively, of the function defined in
+#    `~/.config/fish/functions/fish_prompt.fish`:
 #
 #     function fish_prompt
 #         __vsc_fish_prompt_before
@@ -19,32 +20,27 @@
 # ----------------------------------------------------------------------------
 
 function __vsc_esc
-    if string match -q "$TERM_PROGRAM" "vscode"
-        builtin printf "\e]633;%s\a" (string join ';' $argv)
-    end
+    string match -q "$TERM_PROGRAM" "vscode"
+    and builtin printf "\e]633;%s\a" (string join ";" $argv)
 end
 
 function __vsc_preexec --on-event fish_preexec
-    __vsc_esc "C"
-    __vsc_esc "E" "$argv"
+    __vsc_esc C
+    __vsc_esc E "$argv"
 end
 
 function __vsc_postexec --on-event fish_postexec
-    __vsc_esc "D" $status
+    __vsc_esc D $status
 end
 
 function __vsc_update_cwd --on-event fish_prompt
-    __vsc_esc "P" "Cwd=$PWD"
-end
-
-function __vsc_clear --on-event fish_cancel
-    __vsc_esc "D"
+    __vsc_esc P "Cwd=$PWD"
 end
 
 function __vsc_fish_prompt_before
-    __vsc_esc "A"
+    __vsc_esc A
 end
 
 function __vsc_fish_prompt_after
-    __vsc_esc "B"
+    __vsc_esc B
 end
