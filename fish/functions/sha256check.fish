d2854d5b5aff2821b1fbbf923c479a0cc2559813
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-05 20:14:44 -0600

    cleanup and clarify

--- fish/functions/sha256check.fish
+++ fish/functions/sha256check.fish
@@ -1,7 +1,7 @@
-function sha256check -a sum file
+function sha256check -a checksum file
     if not set -q argv[2]
         echo >&2 Usage: (status function) CHECKSUM FILE
         return 1
     end
-    sha256sum -c (printf "%s  %s" $sum $file | psub)
+    sha256sum -c (printf "%s  %s" $checksum $file | psub)
 end
