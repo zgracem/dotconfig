5d20a4376ca5de61245f1eca3d9df157eecf9b41
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-08 13:45:20 -0600

    increase minimum required fish version

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 # ~/.config/fish/config.fish
 # -----------------------------------------------------------------------------
 
-if fish-is-older-than 3.1
+if fish-is-older-than 3.4
     begin
         set_color brred --reverse
         echo "*** These configuration files should not run on fish $version ***"
