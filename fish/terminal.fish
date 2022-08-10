4a56c676ab8dbbd33834d26b77f97d322039cf91
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-10 15:55:14 -0600

    report status consistently to VS Code

--- fish/terminal.fish
+++ fish/terminal.fish
@@ -83,7 +83,12 @@ end
 # Marks the end of command output.
 # Sent right after an interactive command has finished executing.
 function __si_cmd_finished --on-event fish_postexec
-    __si_esc D $status
+    switch $TERM_PROGRAM
+        case vscode
+            __vsc_esc D $status
+        case '*'
+            __si_esc D $status
+    end
 end
 
 # Updates the current working directory, etc.
