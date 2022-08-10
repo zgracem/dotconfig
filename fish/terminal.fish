2788322f9955c9440665b8dd4e028255ff563290
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-10 14:37:03 -0600

    tidy up and improve naming

--- fish/terminal.fish
+++ fish/terminal.fish
@@ -11,22 +11,24 @@ or exit
 set --global SHELL_INTEGRATION $TERM_PROGRAM
 
 # Setup variables
-set --global __term_Ps 133
+set --global __si_Ps 133
+set --global __iterm_Ps 1337
+set --global __vsc_Ps 633
 
-function __esc_seq -d "Emit escape sequences for shell integration"
+function __si_esc -d "Emit escape sequences for shell integration"
     set --local OSC "\e]"
-    set --local BEL "\a"
-    builtin printf "$OSC$__term_Ps;%s$BEL" (string join ";" $argv)
+    set --local ST "\a"
+    builtin printf "$OSC$__si_Ps;%s$ST" (string join ";" $argv)
 end
 
 function __iterm_esc -d "Emit escape sequences for iTerm.app shell integration"
-    set --local --export __term_Ps 1337
-    __esc_seq $argv
+    set --local --export __si_Ps $__iterm_Ps
+    __si_esc $argv
 end
 
 function __vsc_esc -d "Emit escape sequences for VS Code shell integration"
-    set --local --export __term_Ps 633
-    __esc_seq $argv
+    set --local --export __si_Ps $__vsc_Ps
+    __si_esc $argv
 end
 
 # Escapes backslashes, newlines, and semicolons.
@@ -40,7 +42,7 @@ function __vsc_escape_cmd -d "Serialize the command line"
 end
 
 # Report (fake) shell integration version to iTerm.
-function __iterm_shin_version
+function __iterm_si_version
     string match -q $TERM_PROGRAM "iTerm.app"
     and __iterm_esc ShellIntegrationVersion=69 shell=fish
 end
@@ -48,18 +50,18 @@ end
 # ----------------------------------------------------------------------------
 
 # Mark the beginning of the prompt (and, implicitly, a new line).
-function __shin_prompt_start
-    __esc_seq A
+function __si_prompt_start
+    __si_esc A
 end
 
 # Marks the end of the prompt and the beginning of the user's command input.
-function __shin_prompt_end
-    __esc_seq B
+function __si_cmd_start
+    __si_esc B
 end
 
 # Marks the beginning of command output.
-# Sent right before executing an interactive command.
-function __shin_output_start --on-event fish_preexec
+# Sent after accepting, but before executing, an interactive command.
+function __si_cmd_executed --on-event fish_preexec
     # Ignore commands with leading spaces or in private mode
     if string match --quiet -- " *" "$argv"; or set --query fish_private_mode
         set argv ""
@@ -70,59 +72,60 @@ function __shin_output_start --on-event fish_preexec
             __vsc_esc C
             __vsc_esc E (__vsc_escape_cmd "$argv")
         case iTerm.app
-            __esc_seq C \r
+            __si_esc C \r
         case '*'
-            __esc_seq C
+            __si_esc C
     end
 
-    set --global __shin_cmd_active
+    set --global __si_cmd_active
 end
 
 # Marks the end of command output.
 # Sent right after an interactive command has finished executing.
-function __shin_report_status --on-event fish_postexec
-    __esc_seq D $status
+function __si_cmd_finished --on-event fish_postexec
+    __si_esc D $status
 end
 
 # Updates the current working directory, etc.
 # Sent whenever a new fish prompt is about to be displayed.
-function __shin_update_cwd --on-event fish_prompt
+function __si_update_cwd --on-event fish_prompt
+    set -q __si_hostname; or set -g __si_hostname (hostname -f 2>/dev/null)
     switch $TERM_PROGRAM
         case vscode
             __vsc_esc P "Cwd=$PWD"
         case iTerm.app
-            __iterm_esc "RemoteHost=$USER@"(hostname -f 2>/dev/null)
+            __iterm_esc "RemoteHost=$USER@$__si_hostname"
             __iterm_esc "CurrentDir=$PWD"
     end
 end
 
 # Marks the cleared line with neither success nor failure.
 # Sent when a command line is cleared or reset, but no command was run.
-function __shin_cmd_cancelled --on-event fish_cancel
+function __si_cmd_cancelled --on-event fish_cancel
     switch $TERM_PROGRAM
         case vscode
             __vsc_esc E
             __vsc_esc D
         case '*'
-            __esc_seq D
+            __si_esc D
     end
 end
 
 # Marks the line as cancelled if no command was actually run.
 # Runs whenever a new fish prompt is about to be displayed.
-function __shin_cmd_check --on-event fish_prompt
-    if set --query __shin_cmd_active
-        set --erase __shin_cmd_active
+function __si_cmd_check --on-event fish_prompt
+    if set --query __si_cmd_active
+        set --erase __si_cmd_active
     else
-        __shin_cmd_cancelled
+        __si_cmd_cancelled
     end
 end
 
 # Ctrl+X adds a mark to the line it was triggered on.
-function __shin_mark_line -d "Mark the current line in the terminal"
+function __si_mark_line -d "Mark the current line in the terminal"
     __iterm_esc SetMark
 end
-bind \cx __shin_mark_line
+bind \cx __si_mark_line
 
 # ----------------------------------------------------------------------------
 
@@ -130,9 +133,9 @@ bind \cx __shin_mark_line
 functions --copy fish_prompt __original_fish_prompt
 
 function fish_prompt
-    __shin_prompt_start
+    __si_prompt_start
     __original_fish_prompt
-    __shin_prompt_end
+    __si_cmd_start
 end
 
-__iterm_shin_version
+__iterm_si_version
