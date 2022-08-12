29c2330bd162e175a1412d7d31fb8171546ea771
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-12 12:29:59 -0600

    because I cannot stop typing `brew --cask` to save my life 😩️

--- fish/functions/_wrappers/brew.fish
+++ fish/functions/_wrappers/brew.fish
@@ -9,11 +9,16 @@ function brew --description 'The missing package manager for macOS'
             switch $argv[1]
                 case $homebrew_dirs
                     cd (command brew --$argv[1] $argv[2..-1])
+                    return
                 case '*'
                     printf >&2 "%s: destination unknown\\n" "$destination"
                     return 1
             end
-        case '*'
-            command brew $argv
+        case --cask
+            # Auto-fix `brew --cask <cmd> <args>` to `brew <cmd> --cask <args>`
+            set argv $argv[2] $argv[1] $argv[3..-1]
+            echo >&2 "Running `brew $argv` instead..."
     end
+
+    command brew $argv
 end
