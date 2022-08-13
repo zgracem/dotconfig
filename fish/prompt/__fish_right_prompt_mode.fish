dffadfc60f6030b0025d968936a32c03d751ab87
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-13 09:48:58 -0600

    clean up `$fish_key_bindings` check

--- fish/prompt/__fish_right_prompt_mode.fish
+++ fish/prompt/__fish_right_prompt_mode.fish
@@ -1,6 +1,5 @@
 function __fish_right_prompt_mode --description 'Display the current mode for the prompt'
-    if string match -q fish_vi_key_bindings "$fish_key_bindings"
-        or string match -q fish_hybrid_key_bindings "$fish_key_bindings"
+    if string match -rq "fish_(vi|hybrid)_key_bindings" "$fish_key_bindings"
         switch $fish_bind_mode
             case default
                 set_color blue
