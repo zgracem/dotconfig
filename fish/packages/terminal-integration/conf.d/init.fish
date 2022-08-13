cdfed27966333952251e56d64163d04a08ba85e6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-13 14:10:47 -0600

    Partially revert f6ac9f9
    The `--on-event` functions don't get registered if they're lazy-loaded.

--- fish/packages/terminal-integration/conf.d/init.fish
+++ fish/packages/terminal-integration/conf.d/init.fish
@@ -26,6 +26,73 @@ function __si_initialize --on-event fish_prompt
 
     set --global __si_Ps 133
 
+    # Marks the beginning of command output.
+    # Sent after accepting, but before executing, an interactive command.
+    function __si_cmd_executed --on-event fish_preexec
+        # Ignore commands with leading spaces or in private mode
+        if string match --quiet -- " *" "$argv"; or set --query fish_private_mode
+            set argv ""
+        end
+
+        switch $TERM_PROGRAM
+            case vscode
+                __vsc_esc C
+                __vsc_esc E (__vsc_escape_cmd "$argv")
+            case iTerm.app
+                __si_esc C \r
+            case '*'
+                __si_esc C
+        end
+
+        set --global __si_cmd_active
+    end
+
+    # Marks the end of command output.
+    # Sent right after an interactive command has finished executing.
+    function __si_cmd_finished --on-event fish_postexec
+        switch $TERM_PROGRAM
+            case vscode
+                __vsc_esc D $status
+            case '*'
+                __si_esc D $status
+        end
+    end
+
+    # Updates the current working directory, etc.
+    # Sent whenever a new fish prompt is about to be displayed.
+    function __si_update_cwd --on-event fish_prompt
+        set -q __si_hostname; or set -g __si_hostname (hostname -f 2>/dev/null)
+        switch $TERM_PROGRAM
+            case vscode
+                __vsc_esc P "Cwd=$PWD"
+            case iTerm.app
+                __iterm_esc "RemoteHost=$USER@$__si_hostname"
+                __iterm_esc "CurrentDir=$PWD"
+        end
+    end
+
+    # Marks the cleared line with neither success nor failure.
+    # Sent when a command line is cleared or reset, but no command was run.
+    function __si_cmd_cancelled --on-event fish_cancel
+        switch $TERM_PROGRAM
+            case vscode
+                __vsc_esc E
+                __vsc_esc D
+            case '*'
+                __si_esc D
+        end
+    end
+
+    # Marks the line as cancelled if no command was actually run.
+    # Runs whenever a new fish prompt is about to be displayed.
+    function __si_cmd_check --on-event fish_prompt
+        if set --query __si_cmd_active
+            set --erase __si_cmd_active
+        else
+            __si_cmd_cancelled
+        end
+    end
+
     # Preserve the user's existing prompt to wrap in escape sequences.
     functions --copy fish_prompt __original_fish_prompt
 
--- fish/packages/terminal-integration/functions/__si_cmd_check.fish
+++
@@ -1,9 +0,0 @@
-# Marks the line as cancelled if no command was actually run.
-# Runs whenever a new fish prompt is about to be displayed.
-function __si_cmd_check --on-event fish_prompt
-    if set --query __si_cmd_active
-        set --erase __si_cmd_active
-    else
-        __si_cmd_cancelled
-    end
-end
