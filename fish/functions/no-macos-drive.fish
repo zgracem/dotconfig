703ce8194fd56c8d5947126508a970dbcef8bfb0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-14 17:39:35 -0600

    only run where appropriate

--- fish/functions/no-macos-drive.fish
+++ fish/functions/no-macos-drive.fish
@@ -1,7 +1,14 @@
 function no-macos-drive -d "Prevent macOS from writing metadata"
+    if string match -vq /Volumes (path resolve $PWD/..)
+        echo >&2 "This is not the root directory of a removeable drive!"
+        return 1
+    end
+
     set -l url "http://blog.hostilefork.com/trashes-fseventsd-and-spotlight-v100/"
-    echo "This file disables Spotlight indexing for this drive."\n$url > .metadata_never_index
-    realpath .metadata_never_index
-    echo "This file disables macOS's Trash for this drive."\n$url > .Trashes
-    realpath .Trashes
+
+    echo "This file disables Spotlight indexing for this drive."\n$url >.metadata_never_index
+    and realpath .metadata_never_index
+
+    echo "This file disables macOS's Trash for this drive."\n$url >.Trashes
+    and realpath .Trashes
 end
