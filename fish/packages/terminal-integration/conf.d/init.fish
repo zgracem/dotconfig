21b32c235a0be976f5276cd2ddbc454d112f3364
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-14 17:05:24 -0600

    `hostname -f` outputs all lowercase for some reason

--- fish/packages/terminal-integration/conf.d/init.fish
+++ fish/packages/terminal-integration/conf.d/init.fish
@@ -61,7 +61,7 @@ function __si_initialize --on-event fish_prompt
     # Updates the current working directory, etc.
     # Sent whenever a new fish prompt is about to be displayed.
     function __si_update_cwd --on-event fish_prompt
-        set -q __si_hostname; or set -g __si_hostname (hostname -f 2>/dev/null)
+        set -q __si_hostname; or set -g __si_hostname (hostname 2>/dev/null)
         switch $TERM_PROGRAM
             case vscode
                 __vsc_esc P "Cwd=$PWD"
