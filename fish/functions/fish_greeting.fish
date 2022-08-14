0c59edcd573a6b62d5ef8bc7a01faa845a7ce295
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-14 17:06:13 -0600

    different colour when connecting remotely

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -3,8 +3,13 @@ function fish_greeting --description 'Prints a greeting message on startup'
     status is-interactive; or return
     fish-is-newer-than 3.0; or return
 
-    echo -ns (set_color f3a) '<' (set_color f28) '°' (set_color f66) ')))' \
-        (set_color f80) '>' (set_color fa0) '<' (set_color normal)
+    if set -q SSH_CONNECTION
+        echo -ns (set_color 08b) '<' (set_color 09a) '°' (set_color 58d) ')))' \
+            (set_color a8f) '>' (set_color d9f) '<' (set_color normal)
+    else
+        echo -ns (set_color f3a) '<' (set_color f28) '°' (set_color f66) ')))' \
+            (set_color f80) '>' (set_color fa0) '<' (set_color normal)
+    end
 
     printf " Welcome to %bfish%b, version %b$version%b\\n" \
         (set_color brwhite --italics) (set_color normal) \
