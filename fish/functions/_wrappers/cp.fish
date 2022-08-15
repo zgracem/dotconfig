7ff58152d10768d28138905e8fdb781f65d7052e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-15 13:50:49 -0600

    detect interactive sessions (properly)

--- fish/functions/_wrappers/cp.fish
+++ fish/functions/_wrappers/cp.fish
@@ -1,6 +1,6 @@
 function cp --description 'Copy files and directories'
     set -p argv -a # preserve attributes
-    set -p argv -i # confirm before overwriting
+    status is-interactive; and set -p argv -i # confirm before overwriting
     set -p argv -v # verbose
     command cp $argv
 end
--- fish/functions/_wrappers/rm.fish
+++ fish/functions/_wrappers/rm.fish
@@ -1,8 +1,10 @@
 function rm --description 'Remove directory entries'
-    if status is-interactive; and is-gnu rm
-        set -p argv -I # prompt before large operations
-    else
-        set -p argv -i # request confirmation before each file
+    if status is-interactive
+        if is-gnu rm
+            set -p argv -I # prompt before large operations
+        else
+            set -p argv -i # request confirmation before each file
+        end
     end
     set -p argv -v # verbose
     command rm $argv
