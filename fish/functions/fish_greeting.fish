4fa296e0d0f33a79ca5c8b6fa46ef520dd2fc245
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-15 12:51:28 -0600

    refactor

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -4,14 +4,19 @@ function fish_greeting --description 'Prints a greeting message on startup'
     fish-is-newer-than 3.0; or return
 
     if set -q SSH_CONNECTION
-        echo -ns (set_color 08b) '<' (set_color 09a) '°' (set_color 58d) ')))' \
-            (set_color a8f) '>' (set_color d9f) '<' (set_color normal)
+        set -f clr 08b 09a 58d a8f d9f
     else
-        echo -ns (set_color f3a) '<' (set_color f28) '°' (set_color f66) ')))' \
-            (set_color f80) '>' (set_color fa0) '<' (set_color normal)
+        set -f clr f3a f28 f66 f80 fa0
     end
 
-    printf " Welcome to %bfish%b, version %b$version%b\\n" \
-        (set_color brwhite --italics) (set_color normal) \
+    set -l fish_parts "<" "°" ")))" ">" "<"
+
+    for n in (seq 1 5)
+        set_color $clr[$n]
+        echo -ns $fish_parts[$n]
+    end
+
+    printf "%b Welcome to %bfish%b, version %b$version%b\\n" \
+        (set_color normal) (set_color brwhite --italics) (set_color normal) \
         (set_color white) (set_color normal)
 end
