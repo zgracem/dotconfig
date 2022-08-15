e5013166a6f7d9adbf2068bc37db424059cb62df
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-15 10:42:54 -0600

    eliminate duplicate results

--- fish/functions/_debug/funcsrc.fish
+++ fish/functions/_debug/funcsrc.fish
@@ -1,6 +1,7 @@
 function funcsrc -d 'Display function source file(s)' -a query
+    set --function found nothing
     for file in $fish_function_path/$query.fish
-        path is -f $file; and short_home $file
+        path is -f $file; and set found something; and short_home $file
     end
-    or functions -D $query
+    string match -q nothing $found; and functions -D $query
 end
