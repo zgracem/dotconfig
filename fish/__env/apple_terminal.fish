d23e9d670498dce1948cdebaffaf96984b6102ad
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-16 10:06:27 -0600

    invert syntax
    Technically correct: the best kind of correct!

--- fish/__env/apple_terminal.fish
+++ fish/__env/apple_terminal.fish
@@ -1,4 +1,4 @@
 # Explicitly disable Apple Terminal's session-saving in El Capitan or later.
-if string match -q "$TERM_PROGRAM" Apple_Terminal
+if string match -q Apple_Terminal "$TERM_PROGRAM"
     set -gx SHELL_SESSION_HISTORY 0
 end
