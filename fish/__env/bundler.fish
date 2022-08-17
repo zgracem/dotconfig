f82b11b8ac20f945d0821089261b6cf9272d23db
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-17 12:19:18 -0600

    move to less confusing location
    1) The name of the software is "Bundler".
    2) The executable is `bundle`, no `r`.
    3) The default config path is `~/.bundle/config`.
    
    That's 2 for, 1 against.

--- fish/__env/bundler.fish
+++ fish/__env/bundler.fish
@@ -1,5 +1,5 @@
 in-path bundle; or exit
 
-set -gx BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundler/config
+set -gx BUNDLE_USER_CONFIG $XDG_CONFIG_HOME/bundle/config
 set -gx BUNDLE_USER_CACHE $XDG_CACHE_HOME/bundler
 set -gx BUNDLE_USER_PLUGIN $XDG_DATA_HOME/bundler
