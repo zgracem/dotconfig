abbb5d52cfc1d50006dc12a38d4c73cb1ca9c5e4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-17 16:07:00 -0600

    remove redundant checks

--- fish/__env/perllib.fish
+++ fish/__env/perllib.fish
@@ -1,9 +1,9 @@
 set -gx --path PERLLIB $PERLLIB
-path is -d $XDG_DATA_HOME/perl; and set -p PERLLIB $XDG_DATA_HOME/perl
-path is -d $HOME/opt/share/perl; and set -p PERLLIB $HOME/opt/share/perl
+set -p PERLLIB $XDG_DATA_HOME/perl
+set -p PERLLIB $HOME/opt/share/perl
 fix-path PERLLIB
 
 set -gx --path PERLLIB5 $PERLLIB5
-path is -d $XDG_DATA_HOME/perl5; and set -p PERL5LIB $XDG_DATA_HOME/perl5
-path is -d $HOME/opt/share/perl5; and set -p PERL5LIB $HOME/opt/share/perl5
+set -p PERL5LIB $XDG_DATA_HOME/perl5
+set -p PERL5LIB $HOME/opt/share/perl5
 fix-path PERLLIB5
