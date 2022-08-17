9d362bbbde706f08df181ab19e705b7c4a6c93ae
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-17 17:09:59 -0600

    simplify

--- bin/user-agent-update.fish
+++ bin/user-agent-update.fish
@@ -3,7 +3,4 @@
 # Execute this script to rebuild configuration files for utilities that spoof
 # their user-agent string, based on the current version of Google Chrome.
 
-if set -q XDG_CONFIG_HOME
-    cd $XDG_CONFIG_HOME
-    make -B user-agent
-end
+make -C $XDG_CONFIG_HOME -B user-agent
