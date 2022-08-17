a9aa523f3ad61a64bcde5557fcd9e904d9388649
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-17 16:52:49 -0600

    add another long command

--- fish/packages/terminal-integration/conf.d/alert_after_long_commands.fish
+++ fish/packages/terminal-integration/conf.d/alert_after_long_commands.fish
@@ -4,7 +4,7 @@ contains -- $TERM_PROGRAM $SUPPORTED_TERMS; or exit
 # to another tab from boredom.
 set -q LONG_CMD_DURATION[1]; or set --global LONG_CMD_DURATION 15000
 # Set LONG_COMMANDS to an exclusive list of commands to monitor for long runs.
-set -q LONG_COMMANDS[1]; or set --global LONG_COMMANDS brew
+set -q LONG_COMMANDS[1]; or set --global LONG_COMMANDS brew backup_discord
 function __iterm_alert_after_long_commands -a commandline --on-event fish_postexec
     if test $CMD_DURATION -ge $LONG_CMD_DURATION
         string match -rq "(?:^|(?:and|or|;)\s*)("(string join "|" $LONG_COMMANDS)")" $commandline
