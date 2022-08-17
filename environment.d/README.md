ed80aa368ae16967088cd170e810e4eb5a67e09d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-17 17:03:40 -0600

    remove "GUI environment" hack
    This was never very reliable, and I stopped relying on it over time.

--- environment.d/README.md
+++ environment.d/README.md
@@ -2,6 +2,3 @@
 
 `*.sh` files in this directory are sourced during the startup of any POSIX shell
 to produce a consistent environment.
-
-In addition, `~/.config/launchd/Makefile` makes that environment available to
-macOS GUI apps via `launchd(8)`.
--- fish/functions/rlcode.fish
+++
@@ -1,9 +0,0 @@
-function rlcode -d "Reload Visual Studio Code with the current environment"
-    osascript -e 'tell application "Visual Studio Code" to quit'
-    sleep 1
-
-    killall Dock
-    and sleep 1
-
-    and open -b com.microsoft.VSCode
-end
