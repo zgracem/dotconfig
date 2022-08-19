c1e060c5697b9ff138f9a800d43c8ba01b9f3735
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-19 11:24:14 -0600

    support multiple arguments

--- fish/conf.d/in-path.fish
+++ fish/conf.d/in-path.fish
@@ -1,9 +1,17 @@
 if fish-is-older-than 2.4
     function in-path
-        command -v $argv 2>&1 >/dev/null
+        for exe in $argv
+            command -v $exe 2>&1 >/dev/null
+            or return
+        end
+        return 0
     end
 else
     function in-path --description 'Exits 0 if all arguments are available in PATH'
-        command -v -q $argv
+        for exe in $argv
+            command -v -q $exe
+            or return
+        end
+        return 0
     end
 end
