e5cea9bfbbcaba66352ba7e92fc725a41ac3989d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-21 08:32:36 -0600

    support multiple "inbox" directories to archive from

--- fish/packages/image-tools/functions/archive-acorn-layers.fish
+++ fish/packages/image-tools/functions/archive-acorn-layers.fish
@@ -2,7 +2,8 @@ function archive-acorn-layers
     argparse 'n/dry-run' -- $argv
     or return
 
-    set -l inbox ~/Library/"Mobile Documents"/com~apple~CloudDocs/Images/_inbox
+    set -l inboxes ~/Library/"Mobile Documents"/com~apple~CloudDocs/Images/_inbox
+    set -a inboxes /Volumes/Hub/Art/inbox.old
     set -l used_dir /Volumes/Hub/Art/used
 
     function list-acorn-layers
@@ -18,18 +19,24 @@ function archive-acorn-layers
         end
 
         for layer in $layers
-            set -l layer_file $inbox/$layer.JPG
-            if path is -f $layer_file
-                if set -lq _flag_dry_run
-                    echo "~~~ Would archive:" $layer_file
-                else
-                    /bin/mv $layer_file $used_dir
-                    and echo ">>> Archived:" (basename $layer_file)
+            set -l layer_files $inboxes/$layer.JPG
+            set -f --erase found
+            for layer_file in $layer_files
+                if path is -f $layer_file
+                    if set -lq _flag_dry_run
+                        echo "~~~ Would archive:" $layer_file
+                    else
+                        /bin/mv $layer_file $used_dir
+                        and echo ">>> Archived:" (basename $layer_file)
+                    end
+                    set -f found $layer_file
+                else if path is -f $used_dir/$layer.JPG
+                    echo "*** Already archived:" (basename $layer_file)
                 end
-            else if path is -f $used_dir/$layer.JPG
-                echo "*** Already archived:" (basename $layer_file)
-            else
-                echo >&2 "*** Layer file not found: $layer"
+            end
+
+            if not set -fq found[1]
+                echo >&2 "*** File not found for layer: $layer"
             end
         end
     end
