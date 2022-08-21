724c0e5d4160599d424e03797b29e6d921c36c18
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-21 09:16:30 -0600

    archive other material too

--- fish/packages/image-tools/functions/archive-acorn-layers.fish
+++ fish/packages/image-tools/functions/archive-acorn-layers.fish
@@ -4,25 +4,42 @@ function archive-acorn-layers
 
     set -l inboxes ~/Library/"Mobile Documents"/com~apple~CloudDocs/Images/_inbox
     set -a inboxes /Volumes/Hub/Art/inbox.old
-    set -l used_dir /Volumes/Hub/Art/used
+    set -l used_art_dir /Volumes/Hub/Art/used
+
+    set -l stock_dirs $HOME/{Downloads,Pictures}/unsplash
+    set -l used_stock_dir /Volumes/Hub/Art/used/unsplash
 
     function list-acorn-layers
-        $XDG_CONFIG_HOME/bin/acorn-layers.applescript $argv
+        $XDG_CONFIG_HOME/bin/acorn-layers.applescript $argv \
+            | string match -rg '(IMG[_ ]\d{4}|[\w-]+-[a-zA-Z0-9_-]+-unsplash)'
     end
 
     for file in $argv
-        set -l layers (list-acorn-layers $file | ag -o 'IMG[_ ]\d{4}')
+        set -l layers (list-acorn-layers $file)
 
         if test -z "$layers"
-            echo >&2 "*** No `IMG_####` layers:" (basename $file)
+            echo >&2 "*** No archivable layers:" (basename $file)
             continue
         end
 
         for layer in $layers
-            set -l layer_files $inboxes/$layer.JPG
+            switch $layer
+                case "IMG*"
+                    set -f used_dir $used_art_dir
+                    set -f layer_files $inboxes/$layer.JPG
+                case "*-unsplash"
+                    set -f used_dir $used_stock_dir
+                    set -l layer_dirs $stock_dirs/**/
+                    set -f layer_files (path normalize $layer_dirs/$layer.jpg)
+                case "*"
+                    continue
+            end
+
             set -f --erase found
             for layer_file in $layer_files
-                if path is -f $layer_file
+                if set -fq found[1]
+                    continue
+                else if path is -f $layer_file
                     if set -lq _flag_dry_run
                         echo "~~~ Would archive:" $layer_file
                     else
@@ -30,8 +47,11 @@ function archive-acorn-layers
                         and echo ">>> Archived:" (basename $layer_file)
                     end
                     set -f found $layer_file
-                else if path is -f $used_dir/$layer.JPG
+                else if path is -f $used_dir/$layer.jpg
                     echo "*** Already archived:" (basename $layer_file)
+                    set -f found $used_dir/$layer.jpg
+                else
+                    echo >&2 "*** File not found:" $layer.jpg
                 end
             end
 
