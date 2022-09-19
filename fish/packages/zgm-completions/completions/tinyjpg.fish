dd4695e22ba73d61bc0d658c580c0b68fb28dde8
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-09-19 10:45:32 -0600

    add completions for tiny_dancer
    Eventually I'll make these auto-generate and move them over to
    https://github.com/zgracem/tiny_dancer

---
+++ fish/packages/zgm-completions/completions/tinyjpg.fish
@@ -0,0 +1,1 @@
+complete -c tinyjpg -w tinypng
---
+++ fish/packages/zgm-completions/completions/tinypng.fish
@@ -0,0 +1,19 @@
+function __tiny_metadata_opts
+    set -l opts copyright creation location
+    __fish_complete_list , (printf "%s\t\n" $opts)
+end
+
+complete -c tinypng -s o -l overwrite -d "Replace original file"
+complete -c tinypng      -l no-overwrite
+complete -c tinypng -s c -l clobber -d "Overwrite original file"
+complete -c tinypng      -l no-clobber
+complete -c tinypng -s t -l mtime -d "Copy last-modified time"
+complete -c tinypng      -l no-mtime
+complete -c tinypng -s m -l metadata -x -a "(__tiny_metadata_opts)" -d "Preserve metadata"
+complete -c tinypng -s a all-metadata -d "Copy all metadata"
+complete -c tinypng -s n no-metadata -d "Discard all metadata"
+complete -c tinypng -s W -l width -x -d "Resize to WIDTH"
+complete -c tinypng -s H -l height -x -d "Resize to HEIGHT"
+complete -c tinypng -s M -l method -x -a "fit scale cover thumb" -d "Use METHOD when resizing"
+complete -c tinypng -s V version -d "Print version"
+complete -c tinypng -s h help -d "Print help"
