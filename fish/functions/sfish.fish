6c90b09126202fe7016bcee70b55c58c8f4fb45b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-04 15:53:19 -0600

    remove needless indirection

--- fish/functions/sfish.fish
+++ fish/functions/sfish.fish
@@ -1,5 +1,5 @@
 # For ssh'ing directly to a `~/opt/bin/fish --login` session on shared web
 # servers where `/usr/bin/fish` hasn't been updated since 2016.
 function sfish -d "Remote login to fish" -a remote_host
-    _ssh $remote_host TERM_PROGRAM=$TERM_PROGRAM fish -l
+    ssh $remote_host TERM_PROGRAM=$TERM_PROGRAM fish -l
 end
