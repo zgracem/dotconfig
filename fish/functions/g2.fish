c78a30c9138e95a87bf74a7d2c79210667096583
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-04 14:22:54 -0600

    update `g2`

--- fish/functions/g2.fish
+++ fish/functions/g2.fish
@@ -6,50 +6,25 @@ function g2 --description 'Go somewhere' -a alias
 
     set -l dir
     switch $destination[1]
-        case config
-            set dir "$XDG_CONFIG_HOME"
-        case desktop
-            set dir "$XDG_DESKTOP_DIR"
-        case docs
-            set dir "$XDG_DOCUMENTS_DIR"
-        case dl
-            set dir "$XDG_DOWNLOAD_DIR"
-        case local
-            set dir "$HOME/.local"
-        case hub
-            set dir "$HOME/.local/src/github.com"
-        case private
-            set dir "$HOME/.private"
-        case scratch
-            set dir "$HOME/tmp/_scratch"
-        case defunct
-            set dir "$HOME/old/src"
-        case git
-            set dir "$HOME/GitHub"
-        case proj
-            set dir "$HOME/Projects"
-        case ruby
-            set dir "$dropbox/dev/ruby"
-        case scripts
-            set dir "$dropbox/dev/shell"
-        case sdmods
-            set dir "$steam_dir/steamapps/common/Stardew Valley/Contents/MacOS/Mods"
-        case sdsaves
-            set dir "$XDG_CONFIG_HOME/StardewValley/Saves"
-        case steam
-            set dir "$steam_dir"
-        case steamapps
-            set dir "$steam_dir/steamapps"
-        case steamuser
-            set dir "$steam_dir/userdata"
-        case stow
-            set dir "$HOME/opt/stow"
-        case vs9
-            set dir "$HOME/VS/www/vsdotcom"
-        case 2a
-            set dir "$HOME/VS/www/2adotcom"
-        case imprint
-            set dir "$HOME/VS/www/vsbooks"
+        case config;    set dir "$XDG_CONFIG_HOME"
+        case data;      set dir "$XDG_DATA_HOME"
+        case desktop;   set dir "$XDG_DESKTOP_DIR"
+        case docs;      set dir "$XDG_DOCUMENTS_DIR"
+        case dl;        set dir "$XDG_DOWNLOAD_DIR"
+        case tmp;       set dir "$HOME/var/tmp"
+        case local;     set dir "$HOME/.local"
+        case private;   set dir "$HOME/.private"
+        case defunct;   set dir "$HOME/old/src"
+        case hub;       set dir "$HOME/.local/src/github.com"
+        case git;       set dir "$HOME/GitHub"
+        case proj;      set dir "$dropbox/Projects"
+        case scripts;   set dir "$dropbox/dev/shell"
+        case steam;     set dir "$steam_dir"
+        case steamapps; set dir "$steam_dir/steamapps"
+        case steamuser; set dir "$steam_dir/userdata"
+        case stow;      set dir "$HOME/opt/stow"
+        case vs9;       set dir "$HOME/VS/www/vsdotcom"
+        case imprint;   set dir "$HOME/VS/www/vsbooks"
         case '*'
             echo >&2 "don't know how to go to $destination!"
             return 1
--- fish/packages/zgm-completions/completions/g2.fish
+++ fish/packages/zgm-completions/completions/g2.fish
@@ -1,18 +1,8 @@
 # for my custom "goto" function
 
-function __fish_complete_g2_parsefile -a rx
-    set -l func_file $__fish_config_dir/functions/g2.fish
-    string match -rg "^\s+$rx\$" <$func_file
-end
-
 function __fish_complete_g2
-    set -l aliases (__fish_complete_g2_parsefile 'case (\w+)')
-    set -l dirs (__fish_complete_g2_parsefile 'set dir (\S+)' \
-        | tr -d '"' | string replace -r '^\$HOME' '~' )
-
-    for n in (seq 1 (count $aliases))
-        printf '%s\t%s\n' "$aliases[$n]" "$dirs[$n]"
-    end
+    string replace -fr '^\s+case ([^;]+);\s+set dir "?([^"]+)"?' '$1\t$2' \
+        <$__fish_config_dir/functions/g2.fish
 end
 
 complete -c g2 -xa "(__fish_complete_g2)"
