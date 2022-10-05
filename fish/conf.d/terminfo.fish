6626ca53bf311fc5a256c6b07ff42bedfe162c47
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-04 18:18:39 -0600

    wrap long line

--- fish/conf.d/terminfo.fish
+++ fish/conf.d/terminfo.fish
@@ -1,10 +1,13 @@
-string match -q dumb $TERM; and exit
+string match -q dumb "$TERM"; and exit
 
 # TERMCAP will override TERMINFO if set (e.g. by GNU screen)
 set --erase TERMCAP
 
 if fish-is-newer-than 3.0
-    set -gx --path TERMINFO_DIRS $XDG_DATA_HOME/terminfo /usr/local/opt/ncurses/share/terminfo /usr/share/terminfo
+    set -gx --path TERMINFO_DIRS
+    set -a TERMINFO_DIRS $XDG_DATA_HOME/terminfo
+    set -a TERMINFO_DIRS /usr/local/opt/ncurses/share/terminfo
+    set -a TERMINFO_DIRS /usr/share/terminfo
     fix-path TERMINFO_DIRS
     set -gx TERMINFO $TERMINFO_DIRS[1]
 else
