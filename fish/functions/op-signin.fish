76642161acdbaef750701bb160d1c4c806e22e5f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-06 12:58:31 -0600

    simplify and clean up

--- fish/functions/op-signin.fish
+++ fish/functions/op-signin.fish
@@ -1,11 +1,7 @@
 function op-signin --description "Sign into 1Password's CLI tool"
     argparse f/force -- $argv
 
-    function _1p_
-        jq -r ".accounts[0].$argv[1]" $XDG_CONFIG_HOME/.op/config; or return
-    end
-
-    set -l me (_1p_ userUUID)
+    set -l me (jq -r ".accounts[0].userUUID" $XDG_CONFIG_HOME/.op/config)
     set -l op_env_var "OP_SESSION_$me"
 
     if set -q $op_env_var; and not set -q _flag_force
