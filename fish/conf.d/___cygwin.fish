a9db8ffeb5ad641dd6a8fd4d017d03dd7a2c9dc0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-11 13:56:37 -0600

    fish: fixes for Cygwin

---
+++ fish/conf.d/___cygwin.fish
@@ -0,0 +1,7 @@
+string match -q "CYGWIN*" (/usr/bin/uname -s); or exit
+
+# When launched in e.g. VSCode's integrated terminal, only Windows' %PATH% will
+# be present, and all other config files will error out because they can't find
+# any executables. This allows things to progress as far as ../paths.fish.
+set -p PATH /bin
+set -p PATH /usr/bin
