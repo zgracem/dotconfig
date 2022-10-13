8397d848023ae07cd3b04b49ffba688d71e553b4
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-13 16:02:41 -0600

    git: add pre-commit hook

---
+++ libexec/git-hook-pre-commit.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+set -g staged_files (git diff --name-only --staged)
+
+set -g user_agent_files
+set -a user_agent_files aria2/aria2.conf.m4
+set -a user_agent_files curl/.curlrc.m4
+set -a user_agent_files wget/wgetrc.m4
+set -a user_agent_files yt-dlp/config.m4
+
+for file in $user_agent_files
+    if contains -- $file $staged_files
+        make -C $XDG_CONFIG_HOME user-agent; or exit
+        git add (path change-extension "" $user_agent_files); or exit
+        break
+    end
+end
+
+true
