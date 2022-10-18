cdd265fe2ffc0135cd4bf526a6959ee0b9922646
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-10-18 10:50:54 -0600

    fish: remove redundant code from `?`

--- fish/conf.d/question-mark.fish
+++ fish/conf.d/question-mark.fish
@@ -19,18 +19,14 @@ function '?' --description 'Prints the exit status of the last command'
         return
     end
 
-    # Source: `kill -l`
-    set -l signals HUP INT QUIT ILL TRAP ABRT EMT FPE KILL BUS SEGV SYS PIPE \
-        ALRM TERM URG STOP TSTP CONT CHLD TTIN TTOU IO XCPU XFSZ \
-        VTALRM PROF WINCH INFO USR1 USR2
     # Source: `man 3 sysexits`
     set -l sysexits USAGE DATAERR NOINPUT NOUSER NOHOST UNAVAILABLE SOFTWARE \
         OSERR OSFILE CANTCREAT IOERR TEMPFAIL PROTOCOL NOPERM CONFIG
 
     set_color brred
 
-    if test $last_exit -gt 128 -a $last_exit -le 165
-        echo -ns SIG $signals[(math "$last_exit - 128")]
+    if test $last_exit -gt 128 -a $last_exit -le 159
+        echo -ns (fish_status_to_signal $last_exit)
     else if test $last_exit -ge 64 -a $last_exit -le 78
         echo -ns EX_ $sysexits[(math "$last_exit - 63")]
     else
