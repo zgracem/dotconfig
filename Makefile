zgracem/dotconfig

commit f0c1441fdb2b538da4b1698cd0b73bb370fa0785

update vimrc

vim/.vimrc
@@ -9,13 +9,10 @@ set nocompatible
 " $VIM        = /usr/share/vim
 " $VIMRUNTIME = /usr/share/vim/vim74
 
-set runtimepath =$XDG_CONFIG_HOME/vim,$XDG_DATA_HOME/vim
+set runtimepath =$XDG_DATA_HOME/vim
 set runtimepath+=$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after
 
 set viminfo+=n$XDG_DATA_HOME/vim/.viminfo
-
-silent! execute pathogen#infect()
-" silent! execute pathogen#helptags()
 
 " clear all vimrc-related autocmds 
 " >> http://ruderich.org/simon/config/vimrc
@@ -47,9 +44,9 @@ set backup                      " do make backups
 set backupcopy=yes              " copy/overwrite instead of renaming
 
 if has("unix")
-  set backupdir=$HOME/var/lib/vim,$TMPDIR
+  set backupdir=$XDG_CACHE_HOME/vim,$TMPDIR
 elseif has("win32") || has("win64")
-  set backupdir=$HOME/var/lib/vim,$TEMP
+  set backupdir=$XDG_CACHE_HOME/vim,$TEMP
 endif
 
 " swap files
@@ -59,13 +56,13 @@ set updatecount=80              " update swapfile after x characters
 " swap files will be created in the first possible directory of:
 " (double slash = include %-separated path in name of swap file)
 if has("unix")
-  set directory=.,$HOME/var/lib/vim//,$TMPDIR//
+  set directory=.,$XDG_CACHE_HOME/vim//,$TMPDIR//
 elseif has("win32") || has("win64")
-  set directory=.,$HOME/var/lib/vim//,$TEMP//
+  set directory=.,$XDG_CACHE_HOME/vim//,$TEMP//
 endif
 
 " location of .netrwhist file
-let g:netrw_home=$XDG_CACHE_HOME.'/vim'
+let g:netrw_home=$XDG_DATA_HOME.'/vim'
 
 "-----------------------------------------------------------------------------
 " Behaviour

commit 32336be73a7f04a057ab5d20c9704e22e3eebbea

xdg: add & use `XDG_STATE_HOME`
Also fix .vimrc issue introduced in f0c1441

Makefile
@@ -122,8 +122,8 @@ all: ~/.vimrc
 ~/.vimrc: vim/.vimrc
 	$(link-home)
 # -- create cache & data dirs
-all: | $(datadir)/vim $(XDG_CACHE_HOME)/vim
-$(datadir)/vim $(XDG_CACHE_HOME)/vim:
+all: | $(datadir)/vim $(XDG_CACHE_HOME)/vim $(XDG_STATE_HOME)/vim
+$(datadir)/vim $(XDG_CACHE_HOME)/vim $(XDG_STATE_HOME)/vim:
 	mkdir -pv $@
 # --  install packages
 all: $(datadir)/vim/pack/.installed

bash/_dirs.bash
@@ -5,6 +5,7 @@
 [ -z "$XDG_DATA_HOME" ] && export XDG_DATA_HOME="$HOME/.local/share"
 [ -z "$XDG_CACHE_HOME" ] && export XDG_CACHE_HOME="$HOME/var/cache"
 [ -z "$XDG_RUNTIME_DIR" ] && export XDG_RUNTIME_DIR="$HOME/var/run"
+[ -z "$XDG_STATE_HOME" ] && export XDG_STATE_HOME="$HOME/var/lib"
 
 mkdir -p "$XDG_RUNTIME_DIR" \
 && chown "$USER" "$XDG_RUNTIME_DIR" \

common.mk
@@ -17,6 +17,7 @@ GNUMAKEFLAGS = --output-sync
 XDG_CONFIG_HOME ?= $(wildcard ~/.config)
 XDG_DATA_HOME ?= $(wildcard ~/.local/share)
 XDG_CACHE_HOME ?= $(wildcard ~/var/cache)
+XDG_STATE_HOME ?= $(wildcard ~/var/lib)
 
 # Installation directories
 prefix ?= $(HOME)

fish/conf.d/___dirs.fish
@@ -6,6 +6,7 @@ set -gx XDG_CONFIG_HOME ~/.config
 set -gx XDG_DATA_HOME ~/.local/share
 set -gx XDG_CACHE_HOME ~/var/cache
 set -gx XDG_RUNTIME_DIR ~/var/run
+set -gx XDG_STATE_HOME ~/var/lib
 
 mkdir -p -v $XDG_RUNTIME_DIR
 and chown $USER $XDG_RUNTIME_DIR

fish/packages/goto-lastpwd/conf.d/goto-lastpwd.fish
@@ -1,4 +1,4 @@
-set -gx LAST_PWD_CACHE $XDG_CACHE_HOME/fish/last_pwd
+set -gx LAST_PWD_CACHE $XDG_STATE_HOME/fish/last_pwd
 mkdir -p (path dirname $LAST_PWD_CACHE)
 
 function update-lastpwd --on-variable PWD

launchd/setenv.fish
@@ -5,11 +5,13 @@ set -g XDG_CONFIG_HOME ~/.config
 set -g XDG_DATA_HOME ~/.local/share
 set -g XDG_CACHE_HOME ~/var/cache
 set -g XDG_RUNTIME_DIR ~/var/run
+set -g XDG_STATE_HOME ~/var/lib
 
 launchctl setenv XDG_CONFIG_HOME $XDG_CONFIG_HOME
 launchctl setenv XDG_DATA_HOME $XDG_DATA_HOME
 launchctl setenv XDG_CACHE_HOME $XDG_CACHE_HOME
 launchctl setenv XDG_RUNTIME_DIR $XDG_RUNTIME_DIR
+launchctl setenv XDG_STATE_HOME $XDG_STATE_HOME
 
 set -g SOLARGRAPH_CACHE $XDG_CACHE_HOME/solargraph/cache
 launchctl setenv SOLARGRAPH_CACHE $SOLARGRAPH_CACHE

vim/.vimrc
@@ -44,9 +44,9 @@ set backup                      " do make backups
 set backupcopy=yes              " copy/overwrite instead of renaming
 
 if has("unix")
-  set backupdir=$XDG_CACHE_HOME/vim,$TMPDIR
+  set backupdir=$XDG_STATE_HOME/vim,$TMPDIR
 elseif has("win32") || has("win64")
-  set backupdir=$XDG_CACHE_HOME/vim,$TEMP
+  set backupdir=$XDG_STATE_HOME/vim,$TEMP
 endif
 
 " swap files
@@ -56,9 +56,9 @@ set updatecount=80              " update swapfile after x characters
 " swap files will be created in the first possible directory of:
 " (double slash = include %-separated path in name of swap file)
 if has("unix")
-  set directory=.,$XDG_CACHE_HOME/vim//,$TMPDIR//
+  set directory=.,$XDG_STATE_HOME/vim//,$TMPDIR//
 elseif has("win32") || has("win64")
-  set directory=.,$XDG_CACHE_HOME/vim//,$TEMP//
+  set directory=.,$XDG_STATE_HOME/vim//,$TEMP//
 endif
 
 " location of .netrwhist file