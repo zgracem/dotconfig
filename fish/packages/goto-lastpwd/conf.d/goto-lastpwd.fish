579143c9b12d96c6a4b9dccb19862e6ee6e2171a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-12-01 12:21:21 -0700

    goto-lastpwd: don't run in iTerm's "visor" mode; silence debug messages

--- fish/packages/goto-lastpwd/conf.d/goto-lastpwd.fish
+++ fish/packages/goto-lastpwd/conf.d/goto-lastpwd.fish
@@ -2,7 +2,9 @@ set -gx LAST_PWD_CACHE $XDG_STATE_HOME/fish/last_pwd
 mkdir -p (path dirname $LAST_PWD_CACHE)
 
 function update-lastpwd --on-variable PWD
-    set -q fish_private_mode; and return
+    string match -q vscode "$TERM_PROGRAM"
+    or string match -q Visor "$ITERM_PROFILE"
+    or set -q fish_private_mode; and return
     echo "$PWD" >$LAST_PWD_CACHE
 end
 
@@ -25,10 +27,11 @@ function goto-lastpwd --on-event fish_prompt
         return 0
     end
 
-    # Abort if running in VSCode's integrated terminal
+    # Abort if running in VSCode's integrated terminal or iTerm in dropdown mode
     string match -q vscode "$TERM_PROGRAM"
+    or string match -q Visor "$ITERM_PROFILE"
     and begin
-        echo -s (set_color --dim --italic) "skipping lastpwd..." (set_color normal) >&2
+        # echo -s (set_color --dim --italic) "skipping lastpwd..." (set_color normal) >&2
         return 0
     end
 
@@ -37,13 +40,16 @@ function goto-lastpwd --on-event fish_prompt
     # Abort if we'd be moving to the same directory
     string match -q $PWD "$LWD"
     and begin
-        echo -s (set_color --dim --italic) "lastpwd was pwd..." (set_color normal) >&2
+        #echo -s (set_color --dim --italic) "lastpwd was pwd..." (set_color normal) >&2
         return 0
     end
 
     # Abort if the directory doesn't exist anymore
     path is --type=dir "$LWD"
-    or return 0
+    or begin
+        #echo -s (set_color --dim --italic) "lastpwd not found: $LWD" (set_color normal) >&2
+        return 0
+    end
 
     # Should be OK now
     cd $LWD
