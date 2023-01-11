b5b037834c78fbf00385feb7e1989b6472a0ac74
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-01-11 08:10:39 -0700

    fish: better man page title parsing
    still not perfect, though...

--- fish/functions/__man_title.fish
+++ fish/functions/__man_title.fish
@@ -2,11 +2,5 @@ function __man_title -d "Print a manpage title in name(section) format"
     set -l manfile (command man -w $argv)
     or return
 
-    set -l match (string match -r '.*/(.+?)\.(.+?)(?:\.gz)?' "$manfile")
-    or return
-
-    set -l title $match[2]
-    set -l section $match[3]
-
-    echo "$title($section)"
+    path basename $manfile | string replace -rf '\.([^.]+)(?:\.gz)?$' '($1)'
 end
