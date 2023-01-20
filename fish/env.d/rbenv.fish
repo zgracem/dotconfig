af3512f09184088eaad28c682b0eec3321898eb6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-01-20 13:56:38 -0700

    fish_prompt: speed up `rbenv` check considerably

--- fish/env.d/rbenv.fish
+++ fish/env.d/rbenv.fish
@@ -1,3 +1,11 @@
-if path is -d $RBENV_ROOT
-    in-path rbenv; and source (rbenv init - | psub) # adds duplicate shims dir to PATH
+# RBENV_ROOT set in paths.fish
+if path is -d $RBENV_ROOT; and in-path rbenv
+    source (rbenv init - | psub) # adds duplicate shims dir to PATH
+
+    set -l rbenv_version_file $RBENV_ROOT/version
+
+    path is -f $rbenv_version_file
+    or rbenv global >$rbenv_version_file
+
+    read -gx GLOBAL_RBENV_VERSION <$rbenv_version_file
 end
--- fish/prompt/functions/__fish_prompt_rbenv.fish
+++ fish/prompt/functions/__fish_prompt_rbenv.fish
@@ -2,18 +2,25 @@ function __fish_prompt_rbenv
     in-path rbenv
     or return 0
 
-    if not set -q GLOBAL_RBENV_VERSION
-        set -l vfile $RBENV_ROOT/version
-        if not path is -f $vfile
-            mkdir -pv (dirname $vfile)
-            and rbenv global >$vfile
+    set -l vfile (
+        set -l pwd_ (string split / $PWD)
+        for n in (seq (count $pwd_) -1 1)
+            set -l i -$n
+            string join / $pwd_[1..$n] .ruby-version \
+                | path filter
+            and break
         end
-        read -g GLOBAL_RBENV_VERSION <$vfile
+    )
+    if path is -f $vfile
+        read -gx LOCAL_RBENV_VERSION <$vfile
+    else
+        return
+        ## this is very slow!
+        # set -gx LOCAL_RBENV_VERSION (rbenv version | string split -f1 " ")
     end
-    set -g LOCAL_RBENV_VERSION (rbenv version | string split -f1 " ")
 
-    string match -q "$GLOBAL_RBENV_VERSION" "$LOCAL_RBENV_VERSION"
-    and return
+    # string match -q "$GLOBAL_RBENV_VERSION" "$LOCAL_RBENV_VERSION"
+    # and return
 
     set_color $fish_prompt_color_rbenv
     echo -n $LOCAL_RBENV_VERSION
