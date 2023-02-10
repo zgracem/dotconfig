6848a9990542a41b5636862fb5f7ad2903a1303e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-02-10 11:02:17 -0700

    fish: fix `cleanup-chrome` function

--- fish/functions/cleanup-chrome.fish
+++ fish/functions/cleanup-chrome.fish
@@ -2,7 +2,11 @@ function cleanup-chrome -d "Cleanup Google Chrome files"
     # N.B. The asterisk needs to stay outside the braces so $detritus
     # will only include "Google" subdirs if they actually exist.
     set -l detritus {,$HOME}/Library/{Launch{Agents,Daemons},Application Support,Caches,Preferences}/{Google,com.google.}*
-    test (count $detritus) -gt 0; or return
 
-    rm -rfv $detritus
+    for dir in $detritus
+        if path is -d $dir
+            command rm -rfv $dir
+            or sudo command rm -rfv $dir
+        end
+    end
 end
