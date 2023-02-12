5d1797171800137ffc53a3447d018243c38a9455
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-02-12 12:59:05 -0700

    fish: add `--stop` option to `pgd`, which now detaches on load

--- fish/functions/pgd.fish
+++ fish/functions/pgd.fish
@@ -1,4 +1,23 @@
 function pgd
+    argparse s/stop -- $argv; or return
+
+    if set -q _flag_stop[1]
+        __pygopherd_stop
+        return
+    end
+
     cd ~/opt/etc/pygopherd; or return
     PYTHONPATH=. bin/pygopherd conf/local.conf
+end
+
+function __pygopherd_stop
+    set -l pidfile ~/var/run/pygopherd.pid
+
+    if test -f $pidfile
+        kill -INT (cat $pidfile)
+        and rm $pidfile
+    else
+        echo >&2 "pygopherd pidfile not found!"
+        return 1
+    end
 end
