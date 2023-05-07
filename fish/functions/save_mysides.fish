0fa91746e6be42ad860ef51c285b7822ffc562b0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-05-07 10:28:20 -0600

    save-mysides: write to temp file first

--- fish/functions/save_mysides.fish
+++ fish/functions/save_mysides.fish
@@ -1,9 +1,15 @@
 function save_mysides
+    set -l json_file $XDG_CONFIG_HOME/macos/mysides.json
+    set -l temp_file (mktemp -t mysides.XXXXXX); or return
+
     begin
         echo -n '['
         mysides list | while read line
             printf '{"name":"%s","path":"%s"},' (string split -- " -> " $line)
         end | head -c-1
         echo -n ']'
-    end | tee $XDG_CONFIG_HOME/macos/mysides.json
+    end > $temp_file
+    or return
+
+    command mv -f $temp_file $json_file
 end
