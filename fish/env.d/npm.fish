c66cf18eb7efa496f5dff0fd08073881076bd201
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-06-08 14:39:46 -0600

    npm: add NODE_PATH to environment

--- fish/env.d/npm.fish
+++ fish/env.d/npm.fish
@@ -4,5 +4,7 @@ set -gx OPEN_SOURCE_CONTRIBUTOR true
 
 in-path npm; or exit
 
+set -gx NODE_PATH $XDG_DATA_HOME/npm/lib/node_modules /usr/local/lib/node_modules
+
 # prevent creation of ~/.config/configstore by update-notifier module
 set -gx NO_UPDATE_NOTIFIER true
--- sh/env.d/npm.sh
+++ sh/env.d/npm.sh
@@ -3,6 +3,7 @@ export ADBLOCK=true
 export OPEN_SOURCE_CONTRIBUTOR=true
 
 if command -v npm >/dev/null; then
+  export NODE_PATH="$XDG_DATA_HOME/npm/lib/node_modules:/usr/local/lib/node_modules"
   # prevent creation of ~/.config/configstore by update-notifier module
   export NO_UPDATE_NOTIFIER=true
 fi
