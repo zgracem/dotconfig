zgracem/dotconfig

commit a425833ecd5b8f0cfd7ee57314c7d74731540e27

env.d: actually source `brew.env`
Also refactor `sh/env.d/homebrew.sh` into portable code/out of existence.

env.d/homebrew.env
@@ -1,0 +1,11 @@
+# As of commit e3c0faf, `HOMEBREW_*` environment variables can be set in files:
+#
+# - /etc/homebrew/brew.env                  (system-wide)
+# - $HOMEBREW_PREFIX/etc/homebrew/brew.env  (prefix-specific)
+# → $XDG_CONFIG_HOME/homebrew/brew.env      (if $XDG_CONFIG_HOME is set)
+# - $HOME/.homebrew/brew.env                (otherwise)
+#
+[ -r "$XDG_CONFIG_HOME/homebrew/brew.env" ] && source "$XDG_CONFIG_HOME/homebrew/brew.env"
+
+# Make `brew home` et al. print the URL instead of launching a browser
+[ -n "$SSH_CONNECTION" ] && export HOMEBREW_BROWSER=/bin/echo

fish/env.d/homebrew.fish
@@ -1,4 +1,4 @@
-# See also: $XDG_CONFIG_HOME/homebrew/brew.env
+# See also: $XDG_CONFIG_HOME/env.d/homebrew.env
 in-path brew; or exit
 
 # Check for macOS/iOS terminal clients w/ emoji support
@@ -13,9 +13,4 @@ switch $TERM_PROGRAM
         end
     case "*"
         set -gx HOMEBREW_NO_EMOJI 1
-end
-
-if set -q SSH_CONNECTION
-    # make `brew home` et al. print the URL instead of launching a browser
-    set -gx HOMEBREW_BROWSER (command -v echo)
 end

sh/env.d/homebrew.sh
@@ -1,6 +1,0 @@
-# See also: $XDG_CONFIG_HOME/homebrew/brew.env
-
-if [ -n "$SSH_CONNECTION" ]; then
-  # make `brew home` et al. print the URL instead of launching a browser
-  export HOMEBREW_BROWSER=/bin/echo
-fi

commit 0faf6c3a4749177ea3775437e2d0e54f8b05847b

env.d: actually source `brew.env` *properly*
Fixes issues introduced by a425833.

env.d/homebrew.env
@@ -1,11 +1,7 @@
-# As of commit e3c0faf, `HOMEBREW_*` environment variables can be set in files:
-#
-# - /etc/homebrew/brew.env                  (system-wide)
-# - $HOMEBREW_PREFIX/etc/homebrew/brew.env  (prefix-specific)
-# → $XDG_CONFIG_HOME/homebrew/brew.env      (if $XDG_CONFIG_HOME is set)
-# - $HOME/.homebrew/brew.env                (otherwise)
-#
-[ -r "$XDG_CONFIG_HOME/homebrew/brew.env" ] && source "$XDG_CONFIG_HOME/homebrew/brew.env"
+# See also:
+# - $XDG_CONFIG_HOME/fish/env.d/homebrew.fish
+# - $XDG_CONFIG_HOME/sh/env.d/homebrew.sh
+# - $XDG_CONFIG_HOME/homebrew/brew.env
 
 # Make `brew home` et al. print the URL instead of launching a browser
 [ -n "$SSH_CONNECTION" ] && export HOMEBREW_BROWSER=/bin/echo

fish/env.d/homebrew.fish
@@ -1,5 +1,9 @@
-# See also: $XDG_CONFIG_HOME/env.d/homebrew.env
 in-path brew; or exit
+
+# Read brew.env
+while read line
+    eval (string replace -rf '(HOMEBREW_[^=]+)=(.*)' 'set -gx $1 $2' $line)
+end <"$XDG_CONFIG_HOME/homebrew/brew.env"
 
 # Check for macOS/iOS terminal clients w/ emoji support
 switch $TERM_PROGRAM

sh/env.d/homebrew.sh
@@ -1,0 +1,2 @@
+grep -E '^HOMEBREW_[A-Z_]+=.*$' "$XDG_CONFIG_HOME/homebrew/brew.env" \
+| while read -r line; do eval "export $line"; done