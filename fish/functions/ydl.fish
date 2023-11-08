fe1c4087e98b2a27ab4bb0eb57f62ccf46c9880e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-11-08 10:03:25 -0700

    ydl: don't hardcode path

--- fish/functions/ydl.fish
+++ fish/functions/ydl.fish
@@ -1,4 +1,4 @@
 function ydl -d "Download from YouTube"
-    set -l config ~/.config/yt-dlp/config-youtube
+    set -l config $XDG_CONFIG_HOME/yt-dlp/config-youtube
     yt-dlp --config-location=$config $argv
 end
