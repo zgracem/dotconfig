f672e88d617cc56d892ecf12fa082ce75d4e9711
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-11-15 16:50:25 -0700

    libexec: enable working script and delete broken one

--- libexec/tx-postexec-2023.fish
+++ libexec/tx-postexec-2023.fish
@@ -1,9 +1,17 @@
 #!/usr/local/bin/fish
 
+# This script is called by Transmission.app when a torrent finishes downloading,
+# and writes a simple log file to ~/var/log/transmission about it.
+# See: <https://github.com/transmission/transmission/blob/main/docs/Scripts.md>
+#
+# To "install":
+#   defaults write -app Transmission DoneScriptPath "$XDG_CONFIG_HOME/libexec/tx-postexec-2023.fish"
+
 set -gx LOG_DIR "$HOME/var/log/transmission"
 set -gx TIMESTAMP (gdate +%Y%m%d_%H%M%S) # for errors
-set -gx LOG_FILE "$LOG_DIR/$TIMESTAMP.txt"
+set -gx LOG_FILE "$LOG_DIR/$TIMESTAMP.txt" # fallback path
 
+## For testing:
 # set -gx TR_TORRENT_HASH 7367c6abd6a0da19a7da08f5178f1bf46b1a2bcf
 # set -gx TR_TORRENT_ID 10
 
@@ -29,7 +37,4 @@ function main
     transmission-remote --torrent $TR_TORRENT_ID --info-files
 end
 
-main #| tee $LOG_FILE
-
-# # also generate JSON (script is broken)
-# eval (status dirname)/tx-postexec.fish
+main | tee $LOG_FILE
