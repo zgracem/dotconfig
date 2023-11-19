e1d32956948a013e78070654bfa98cd0e1d8ef72
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-11-19 10:17:58 -0700

    rbenv: fix mangled PATH
    This executes after paths.fish and adds a duplicate of rbenv's shims dir
    to the top of the PATH (meaning it can't be overridden by ~/bin et al.)
    so let's just discard whatever changes are made here.

--- fish/env.d/rbenv.fish
+++ fish/env.d/rbenv.fish
@@ -1,6 +1,8 @@
-# RBENV_ROOT set in paths.fish
+# RBENV_ROOT set in conf.d/xdg-overrides.fish
 if path is -d $RBENV_ROOT; and in-path rbenv
+    set -l OLD_PATH $PATH
     source (rbenv init - | psub) # adds duplicate shims dir to PATH
+    set -gx PATH $OLD_PATH
 
     set -l rbenv_version_file $RBENV_ROOT/version
 
