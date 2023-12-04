a732cf3cb5a2714d24da0e6b3734c83bda70e08d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-04 16:24:20 -0700

    fish: clean up completions

--- fish/completions/brew.fish
+++ fish/completions/brew.fish
@@ -26,15 +26,3 @@ complete -c brew -l max-downloads -x -d "Maximum formulae to download and update
 
 # `brew env` (~/bin/brew-env)
 complete -c brew -n __fish_use_subcommand -a env -d "Print all HOMEBREW_* vars"
-
-# `brew cd` (../functions/_wrappers/brew.fish)
-set -gq __fish_complete_brew_cd; or set -g __fish_complete_brew_cd '
-    cache\t'(command brew --cache)'
-    caskroom\t'(command brew --caskroom)'
-    cellar\t'(command brew --cellar)'
-    prefix\t'(command brew --prefix)'
-    repository\t'(command brew --repository)'
-'
-
-complete -c brew -a cd -d "Change to a Homebrew directory" -n __fish_use_subcommand
-complete -c brew -x -a "$__fish_complete_brew_cd" -n '__fish_seen_subcommand_from cd'
--- fish/completions/op.fish
+++
@@ -1,4 +0,0 @@
-# fish completion for 1Password CLI
-command -sq op; or exit
-
-op completion fish | source
