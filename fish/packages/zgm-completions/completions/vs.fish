8ab720dcbbc672219a2be2e01e1e2630c0f6024f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-09 15:47:03 -0700

    fish: update `vs` completions

--- fish/packages/zgm-completions/completions/vs.fish
+++ fish/packages/zgm-completions/completions/vs.fish
@@ -1,9 +1,9 @@
 complete -c vs -f # do not operate on files
 
-set -l vs_cmds help complete bundle test cleanup make media fashion build sync deploy server commit readme
+set -l vs_cmds help complete bundle test cleanup make media build sync deploy server commit readme
 complete -c vs -n '__fish_is_first_token' -xa "$vs_cmds"
 complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from help' -xa "$vs_cmds"
-complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from complete' -xa 'print write'
+complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from complete' -xa 'write print'
 complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from test' -l build
 complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from test' -l cleanup
 complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from test' -l quiet
@@ -11,8 +11,8 @@ complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from test' 
 complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from make' -xa 'all icon presszip thumbs'
 complete -c vs -n '__fish_is_nth_token 3; and __fish_seen_subcommand_from make; and __fish_seen_subcommand_from thumbs' -xa 'all covers photos posters press'
 complete -c vs -n '__fish_is_nth_token 4; and __fish_seen_subcommand_from help; and __fish_seen_subcommand_from make; and __fish_seen_subcommand_from thumbs' -xa 'all covers photos posters press'
-complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from media' -xa 'build source all'
-complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from build' -xa 'site fast docs json all'
-complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from sync' -xa 'all build fast docs json'
-complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from deploy' -xa 'all site fast docs json'
+complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from media' -xa 'all source build'
+complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from build' -xa 'all site docs json fast'
+complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from sync' -xa 'all fast build docs json'
+complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from deploy' -xa 'all site docs json fast'
 complete -c vs -n '__fish_is_nth_token 2; and __fish_seen_subcommand_from server' -xa 'site console docs'
