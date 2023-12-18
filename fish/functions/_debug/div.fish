b56432a987f030f25552d98d1e91b9f7c91d747e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-18 13:06:44 -0700

    fish: add `-f/--force` flag to divider function

--- fish/functions/_debug/div.fish
+++ fish/functions/_debug/div.fish
@@ -1,4 +1,7 @@
 function div --description 'Print a divider across the terminal'
+    argparse f/force -- $argv
+    or return
+
     set -l symbol \u2500
     if set -q argv[1]
         set symbol (string sub -l1 $argv[1])
@@ -6,7 +9,7 @@ function div --description 'Print a divider across the terminal'
 
     set -l line (string repeat -N -n$COLUMNS $symbol)
     if in-path lolcat
-        echo $line | lolcat --spread 8
+        echo $line | lolcat --spread 8 $_flag_force
     else
         set_color brwhite
         echo $line
