09b928f6ce5a5e81ccad06178508568ea609a978
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-19 10:58:44 -0700

    homebrew: add default behaviour to `brew uses`

--- fish/functions/_wrappers/brew.fish
+++ fish/functions/_wrappers/brew.fish
@@ -1,13 +1,12 @@
 in-path brew; or exit
 
 function brew --description 'The missing package manager for macOS'
-    # Implement `brew cd <location>` to quickly move to Homebrew dirs.
-    set -l homebrew_dirs cache caskroom cellar prefix repo repository
     switch $argv[1]
         case cd
+            # Implement `brew cd <location>` to quickly move to Homebrew dirs.
             set --erase argv[1] # shift `cd` out of the arguments
             switch $argv[1]
-                case $homebrew_dirs
+                case cache caskroom cellar prefix repo repository
                     cd (command brew --$argv[1] $argv[2..-1])
                     return
                 case '*'
@@ -18,6 +17,12 @@ function brew --description 'The missing package manager for macOS'
             # Auto-fix `brew --cask <cmd> <args>` to `brew <cmd> --cask <args>`
             set argv $argv[2] $argv[1] $argv[3..-1]
             echo >&2 "Running `brew $argv` instead..."
+        case uses
+            # Add default behaviour to `brew uses`
+            if string match -vq -- '-*' $argv[2]
+                set argv $argv[1] --installed $argv[2..-1]
+                echo >&2 "Running `brew $argv` instead..."
+            end
     end
 
     command brew $argv
