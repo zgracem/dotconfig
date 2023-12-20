cbf3e7fa1913ee2bd2b90b9753b84074c95fbf0a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-20 11:14:03 -0700

    fish: clarify logic

--- fish/functions/ff.fish
+++ fish/functions/ff.fish
@@ -1,4 +1,5 @@
-in-path fd; and fish-is-older-than 3.6; or return
+in-path fd; and return
+fish-is-older-than 3.6; or return
 
 function ff -d "Find files"
     find -H . -xtype f -iname "*$argv*" -print 2>/dev/null
