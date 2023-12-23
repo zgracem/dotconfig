e372b20116c4672ec7b611708ec13d19bc659bfe
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-23 10:48:38 -0700

    fish: always count jobs correctly
    This will be unnecessary once 3.7 ships; see fish's #9875

--- fish/prompt/functions/__fish_prompt_jobs.fish
+++ fish/prompt/functions/__fish_prompt_jobs.fish
@@ -1,5 +1,5 @@
 function __fish_prompt_jobs --description 'Helper function for fish_prompt'
-    set job_count (jobs | count)
+    set -l job_count (builtin jobs -g | count)
     or return 0
 
     set_color $fish_prompt_color_jobs
