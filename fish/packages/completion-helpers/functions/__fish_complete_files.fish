43de6c44fd8f48efe26c7253ef542a379ae91666
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-23 11:08:17 -0700

    fish: update completion helper function

--- fish/packages/completion-helpers/functions/__fish_complete_files.fish
+++ fish/packages/completion-helpers/functions/__fish_complete_files.fish
@@ -1,6 +1,18 @@
-function __fish_complete_files -a token
-    # "Cheat" to complete files by calling `complete -C` on a fake command name,
-    # like `__fish_complete_directories` does.
-    set -l fake_command aaabccccdeeeeefffffffffgghhhhhhiiiii
-    complete -C"$fake_command $token"
+# Based on $__fish_data_dir/functions/__fish_complete_directories.fish
+function __fish_complete_files -a token desc
+    if not set -q desc[1]
+        set -f desc File
+    end
+
+    if not set -q token[1]
+        set -f token (commandline --current-token --cut-at-cursor)
+    end
+
+    # "Cheat" to complete files by calling `complete -C` on an empty command
+    # and filtering out directories.
+    set -f files (complete -C"'' $token" | string match -rv '.*/$')
+
+    if set -q files[1]
+        printf "%s\n" $files\t"$desc"
+    end
 end
