d7164ae1c45097a52d05dcc83f627ad337634197
Z. Grace Moreau <zgracem@users.noreply.github.com>
2023-12-23 09:20:55 -0700

    fish: always download YouTube videos to `XDG_DOWNLOAD_DIR`

--- fish/functions/ydl.fish
+++ fish/functions/ydl.fish
@@ -1,4 +1,4 @@
 function ydl -d "Download from YouTube"
     set -l config $XDG_CONFIG_HOME/yt-dlp/config-youtube
-    yt-dlp --config-location=$config $argv
+    yt-dlp --config-location=$config --paths=$XDG_DOWNLOAD_DIR $argv
 end
