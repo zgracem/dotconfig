9d975703d8ccf43d269a6d47c05421eb8f696067
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-02-11 12:50:53 -0700

    fish: fix `wtf` completions

--- fish/packages/zgm-completions/completions/wtf.fish
+++ fish/packages/zgm-completions/completions/wtf.fish
@@ -1,4 +1,5 @@
+complete -c wtf --require-parameter --force-files
 complete -c wtf -a "(functions --names --all)" -d function
 complete -c wtf -a "(builtin --names)" -d builtin
 complete -c wtf -a "(abbr --list)" -d abbrev.
-complete -c wtf --force-files
+complete -c wtf -a "(complete -C (printf %s\n (commandline --tokenize --current-token)))"
