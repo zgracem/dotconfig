49b3cff36d7ff872573ddf0ccbea7ee2fe217509
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-03-07 10:29:53 -0700

    fish: add function to quickly update Decky Loader

---
+++ local/Alyx/fish/functions/update-decky.fish
@@ -0,0 +1,4 @@
+function update-decky
+    # Sometimes needed after SteamOS updates
+    curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
+end
