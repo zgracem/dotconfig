423c7d4b0b1faff83d6436e4a6ee8a5d0de05d75
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-03-12 15:33:36 -0600

    pwsh: more messing around

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,6 @@
 #   Windows:   %HOME%/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
 #   Mac/Linux: ~/.config/powershell/Microsoft.PowerShell_profile.ps1
 # ----------------------------------------------------------------------------
+
+# $ActualProfile = "D:\cygwin\home\uxhj\.config\powershell\Microsoft.PowerShell_profile.ps1"
+# if (Test-Path($ActualProfile)) { . "$ActualProfile" }
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -5,11 +5,12 @@
 # ----------------------------------------------------------------------------
 
 using namespace System.Security
+using namespace System.Text
 
 # Locale
 [CultureInfo]::CurrentCulture = "en-CA"
 
-# Setup readline
+# Setup PSReadline
 if ($host.Name -eq 'ConsoleHost') {
     Import-Module PSReadLine
     # $PSReadLineOptions = @{
@@ -31,27 +32,36 @@ $env:Path = $env:Path, "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin" -join 
 
 # ----------------------------------------------------------------------------
 
+$PSDefaultParameterValues += @{
+    'Get-Help:ShowWindow' = $true
+    'Out-File:Encoding' = 'utf8'
+    'Remove-Item:Confirm' = $true
+}
+
 Set-Alias clear Clear-Host
 Set-Alias man Get-Help
 Set-Alias open Start-Process
 Set-Alias pbpaste Get-Clipboard
-Set-Alias rm Remove-ItemSafely
+Set-Alias rm Remove-Item
 Set-Alias sudo Invoke-Elevated
 Set-Alias wget Invoke-WebRequest
 
-function ListFilesWide { Get-ChildItem -Force $args | Format-Wide -AutoSize }
+function ListFilesWide { Get-ChildItem -Force @Args | Format-Wide -AutoSize }
 Set-Alias ls ListFilesWide
-function ListFilesLong { Get-ChildItem -Force $args }
+function ListFilesLong { Get-ChildItem -Force @Args }
 Set-Alias ll ListFilesLong
 
-function GetMyIP { Write-Host (Invoke-WebRequest ifconfig.me/ip).Content.Trim() }
-Set-Alias myip GetMyIP
+function Get-MyIP { Write-Host (Invoke-WebRequest ifconfig.me/ip).Content.Trim() }
+Set-Alias myip Get-MyIP
 
-function OpenExplorer { explorer $args }
-Set-Alias f OpenExplorer
+function Open-Explorer { explorer.exe $args[0] }
+Set-Alias f Open-Explorer
 
-function SelectAllObjects { Select-Object * }
-Set-Alias unpack SelectAllObjects
+function Show-InExplorer { explorer.exe /reveal $args[0] }
+Set-Alias reveal Show-InExplorer
+
+function Select-AllObjects { Select-Object * }
+Set-Alias unpack Select-AllObjects
 
 # ----------------------------------------------------------------------------
 
@@ -61,29 +71,41 @@ $env:POWERSHELL_TELEMETRY_OPTOUT = 1
 $global:IsAdmin = if ([OperatingSystem]::IsWindows()) {
     $local:CurrentUser = [Principal.WindowsPrincipal][Principal.WindowsIdentity]::GetCurrent()
     $local:CurrentUser.IsInRole([Principal.WindowsBuiltInRole]::Administrator)
-} elseif ([OperatingSystem]::IsMacOS() -or [OperatingSystem]::IsLinux()) {
-    $(id -u) -eq 0
-}
-
-# ----------------------------------------------------------------------------
-
-$PSDefaultParameterValues += @{
-    'Get-Help:ShowWindow' = $true
-    'Out-File:Encoding' = 'utf8'
+} elseif ([OperatingSystem]::IsMacOS()) {
+    $local:uid = $(id -u)
+    $local:uid -eq 0 -or $local:uid -eq 501
+} elseif ([OperatingSystem]::IsLinux()) {
+    $local:uid = $(id -u)
+    $local:uid -eq 0
 }
 
 # ----------------------------------------------------------------------------
 
 function Prompt {
-    $local:colour = if ($global:IsAdmin -eq $true) {
-        $PSStyle.Foreground.Red
+    $local:colour = [string] ""
+    $local:prefix = [string] ""
+    $local:path = [string] ""
+    $local:suffix = [string] ""
+
+    $colour = if ($global:IsAdmin -eq $true) {
+        $PSStyle.Foreground.BrightRed
     } else {
-        $PSStyle.Foreground.Blue
+        $PSStyle.Foreground.BrightBlue
     }
-    $local:prefix = ($local:colour + "PS" + $PSStyle.Reset + " ")
-    # $local:body = $(Get-Location)
-    $local:suffix = ('>' * ($nestedPromptLevel + 1)) + " "
-    Return ($local:prefix + $PWD + $local:suffix)
+    $prefix = ($colour + "PS" + $PSStyle.Reset + " ")
+
+    # Use ProviderPath if there's no drive defined for the location provider.
+    $path = if ($executionContext.SessionState.Path.CurrentLocation.Drive) {
+        $executionContext.SessionState.Path.CurrentLocation.Path
+    }
+    else {
+        $executionContext.SessionState.Path.CurrentLocation.ProviderPath
+    }
+    if ($path -like "$Home*") { $path = $path.Replace($Home, '~') }
+
+    $suffix = ('>' * ($nestedPromptLevel + 1)) + " "
+
+    Return ($prefix + $path + $suffix)
 }
 
 function CustomizeConsole {
