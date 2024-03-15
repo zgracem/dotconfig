91121976b30121a65c6b5982a5ef16eff1869f19
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-03-15 11:21:00 -0600

    pwsh: even more messing around

---
+++ powershell/Microsoft.VSCode_profile.ps1
@@ -0,0 +1,8 @@
+# ----------------------------------------------------------------------------
+# $PROFILE.CurrentUserCurrentHost (when running in VS Code)
+#   Windows:   %HOME%/Documents/PowerShell/Microsoft.VSCode_profile.ps1
+#   Mac/Linux: ~/.config/powershell/Microsoft.VSCode_profile.ps1
+# ----------------------------------------------------------------------------
+
+# $ActualProfile = "D:\cygwin\home\$env:USERNAME\.config\powershell\Microsoft.VSCode_profile.ps1"
+# if (Test-Path($ActualProfile)) { . "$ActualProfile" }
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -40,41 +40,42 @@ $PSDefaultParameterValues += @{
 
 Set-Alias clear Clear-Host
 Set-Alias man Get-Help
-Set-Alias open Start-Process
+Set-Alias open Invoke-Item
 Set-Alias pbpaste Get-Clipboard
 Set-Alias rm Remove-Item
 Set-Alias sudo Invoke-Elevated
 Set-Alias wget Invoke-WebRequest
 
-function ListFilesWide { Get-ChildItem -Force @Args | Format-Wide -AutoSize }
-Set-Alias ls ListFilesWide
-function ListFilesLong { Get-ChildItem -Force @Args }
-Set-Alias ll ListFilesLong
+function Get-AllItemsWide { Get-ChildItem -Force @Args | Format-Wide -AutoSize }
+Set-Alias ls Get-AllItemsWide
+function Get-AllItemsLong { Get-ChildItem -Force @Args }
+Set-Alias ll Get-AllItemsLong
 
-function Get-MyIP { Write-Host (Invoke-WebRequest ifconfig.me/ip).Content.Trim() }
-Set-Alias myip Get-MyIP
+function myip { Write-Host (Invoke-WebRequest ifconfig.me/ip).Content.Trim() }
 
-function Open-Explorer { explorer.exe $args[0] }
-Set-Alias f Open-Explorer
+function Open-ExplorerHere { explorer.exe $args[0] }
+Set-Alias f Open-ExplorerHere
 
-function Show-InExplorer { explorer.exe /reveal $args[0] }
-Set-Alias reveal Show-InExplorer
+function reveal { explorer.exe /reveal $args[0] } # doesn't work
 
-function Select-AllObjects { Select-Object * }
-Set-Alias unpack Select-AllObjects
+function unpack { process { $_ | Select-Object * } }
+
+function about { process { $_ | Get-Member } }
+
+function .. { Set-Location .. }
 
 # ----------------------------------------------------------------------------
 
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 $env:POWERSHELL_TELEMETRY_OPTOUT = 1
 
-$global:IsAdmin = if ([OperatingSystem]::IsWindows()) {
+$global:IsAdmin = if ($IsWindows) {
     $local:CurrentUser = [Principal.WindowsPrincipal][Principal.WindowsIdentity]::GetCurrent()
     $local:CurrentUser.IsInRole([Principal.WindowsBuiltInRole]::Administrator)
-} elseif ([OperatingSystem]::IsMacOS()) {
+} elseif ($IsMacOS) {
     $local:uid = $(id -u)
     $local:uid -eq 0 -or $local:uid -eq 501
-} elseif ([OperatingSystem]::IsLinux()) {
+} elseif ($IsLinux) {
     $local:uid = $(id -u)
     $local:uid -eq 0
 }
@@ -82,17 +83,11 @@ $global:IsAdmin = if ([OperatingSystem]::IsWindows()) {
 # ----------------------------------------------------------------------------
 
 function Prompt {
-    $local:colour = [string] ""
-    $local:prefix = [string] ""
     $local:path = [string] ""
-    $local:suffix = [string] ""
-
-    $colour = if ($global:IsAdmin -eq $true) {
-        $PSStyle.Foreground.BrightRed
-    } else {
-        $PSStyle.Foreground.BrightBlue
-    }
-    $prefix = ($colour + "PS" + $PSStyle.Reset + " ")
+    $local:pathColour = $PSStyle.Foreground.BrightBlack
+    $local:pathParentColour = $PSStyle.Foreground.BrightBlack
+    $local:sigil = [string] ">"
+    $local:sigilColour = $PSStyle.Foreground.Blue
 
     # Use ProviderPath if there's no drive defined for the location provider.
     $path = if ($executionContext.SessionState.Path.CurrentLocation.Drive) {
@@ -101,11 +96,23 @@ function Prompt {
     else {
         $executionContext.SessionState.Path.CurrentLocation.ProviderPath
     }
-    if ($path -like "$Home*") { $path = $path.Replace($Home, '~') }
 
-    $suffix = ('>' * ($nestedPromptLevel + 1)) + " "
+    # Replace path to home directory with `~`
+    if ("$path" -eq "$Home") {
+        $path = $path.Replace($Home, '~')
+    } else {
+        $local:parentDir = Get-Item $path | Split-Path -Parent
+        if ($parentDir -like "$Home*") { $parentDir = $parentDir.Replace($Home, '~') }
+        $local:baseDir = Get-Item $path | Split-Path -Leaf
+        $local:pathSep = [System.IO.Path]::DirectorySeparatorChar
+        $path = $PSStyle.Foreground.BrightBlack + $parentDir + $pathSep + $PSStyle.Reset + $baseDir
+    }
 
-    Return ($prefix + $path + $suffix)
+    if ($global:IsAdmin -eq $true) { $sigilColour = $PSStyle.Foreground.Blue }
+
+    $sigil = $sigilColour + ($sigil * ($nestedPromptLevel + 1)) + $PSStyle.Reset
+
+    Return ($PSStyle.Reset + $path + " " + $sigil + " ")
 }
 
 function CustomizeConsole {
