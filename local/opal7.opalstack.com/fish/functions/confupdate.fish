9763e45d49a25647b5585c8866acd7a0fa394047
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-04-06 11:13:03 -0600

    local: fix function to work w/ very old version of git

--- local/opal7.opalstack.com/fish/functions/confupdate.fish
+++ local/opal7.opalstack.com/fish/functions/confupdate.fish
@@ -1,9 +1,10 @@
 function confupdate -d "Update configuration directories from GitHub"
+    set -l oldpwd $PWD
     for repo in dotconfig dotprivate
-        git -C ~/src/github.com/zgracem/$repo pull
+        cd ~/src/github.com/zgracem/$repo; and git pull
     end
+    cd $oldpwd
 
     # This server doesn't like `~/.ssh` (or any of its contents) being a symlink
-    cd ~/.ssh; or return
-    cp -afv ~/src/github.com/zgracem/dotprivate/ssh/* .; or return
+    cp -afv ~/src/github.com/zgracem/dotprivate/ssh/* ~/.ssh; or return
 end
