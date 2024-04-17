623d499607b26a988b1336ddc3b47f531b5925d3
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-04-17 10:30:52 -0600

    fish: improve unified `confupdate` function
    See cf4b0aa

--- fish/functions/confupdate.fish
+++ fish/functions/confupdate.fish
@@ -2,18 +2,19 @@ function confupdate -d "Update configuration directories from GitHub"
     set -f dirs
 
     switch $hostname
-    case Alyx phosphor.pink "*.opalstack.com"
-        set -f dirs ~/src/github.com/zgracem/dot{config,private}
+    case Alyx
+        set -f dirs ~/src/**/dot{config,private}
     case "*"
         set -f dirs ~/.{config,private}
     end
 
     for dir in $dirs
-        git -C $dir pull
+        git -C (path resolve $dir) pull
     end
 
-    if string match -q "*.opalstack.com" $hostname
-        # This server doesn't like `~/.ssh` (or any of its contents) being a symlink
+    switch $hostname
+    case "*.opalstack.com" "WS-*"
+        # These servers doesn't like `~/.ssh` (or any of its contents) being a symlink
         cp -af $dirs[2]/ssh/* $HOME/.ssh
         and chmod -R -c go-rwx $HOME/.ssh
     end
