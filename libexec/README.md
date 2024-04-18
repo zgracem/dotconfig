bfdb2c3b3bc0ca593813b7f2a614458e7d61d4c3
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-04-18 12:25:21 -0600

    misc: remove some redundant files, simplify some others

--- libexec/README.md
+++ libexec/README.md
@@ -1,7 +1,4 @@
 # ~/.config/libexec
 
 This directory contains various scripts I've written to be indirectly executed
-by other software, e.g. Visual Studio Code's [tasks] (see [`tasks.json`]).
-
-[tasks]: https://code.visualstudio.com/docs/editor/tasks
-[`tasks.json`]: https://github.com/zgracem/dotconfig/blob/main/.vscode/tasks.json
+by other software or scripts.
--- vim/install.fish
+++ vim/install.fish
@@ -1,28 +1,17 @@
 #!/usr/bin/env fish
 
-if not set -q XDG_CONFIG_HOME XDG_DATA_HOME
+if not set -q XDG_{CONFIG,DATA,CACHE,STATE}_HOME
     echo >&2 "fatal error: XDG basedirs not found in environment!"
     exit 1
 end
 
-set VIM $XDG_DATA_HOME/vim
-
-mkdir -pv $VIM/pack/zgm; and cd $VIM/pack/zgm; or exit
-ln -sfv $XDG_CONFIG_HOME/vim/data/pack/zgm/start ./start; or exit
-
-mkdir -pv $VIM/pack/tpope/start; and cd $VIM/pack/tpope/start; or exit
-set tpope_pkgs characterize commentary repeat speeddating unimpaired
-for pkg in $tpope_pkgs
-    path is -d $pkg; and continue
-    git submodule add "git@github.com:tpope/vim-$pkg.git" "./$pkg"; or exit
+for dir in $XDG_DATA_HOME $XDG_CACHE_HOME $XDG_STATE_HOME
+    mkdir -pv $dir/vim
+    or exit
 end
 
-mkdir -pv $VIM/pack/easymotion/start; and cd $VIM/pack/easymotion/start; or exit
-if not path is -d easymotion
-    git submodule add git@github.com:easymotion/vim-easymotion.git ./easymotion; or exit
-end
+set -g VIM $XDG_DATA_HOME/vim
 
-mkdir -pv $VIM/pack/elzr/start; and cd $VIM/pack/elzr/start; or exit
-if not path is -d json
-    git submodule add git@github.com:elzr/vim-json.git ./json; or exit
-end
+mkdir -pv $VIM/pack/zgm
+and ln -sfv $XDG_CONFIG_HOME/vim/data/pack/zgm/start $VIM/pack/zgm/start
+or exit
