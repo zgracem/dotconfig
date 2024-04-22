952f833233d47e370e3c576143b4c913664bf0a6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-04-22 13:37:27 -0600

    fish: improve clarity

--- fish/functions/256colours.fish
+++ fish/functions/256colours.fish
@@ -2,20 +2,20 @@ function 256colours -d "Print supported terminal colours"
     argparse -xb,g b/basic g/grey -- $argv
     or return
 
-    set -f colours (seq 16 231)
-    set -f width -c 192
-
     if set -q _flag_basic
-        set colours (seq 0 15)
-        set width[2] 64
+        set -f colours (seq 0 15)
+        set -f width 64
     else if set -q _flag_grey
-        set colours (seq 232 255)
-        set width[2] 128
+        set -f colours (seq 232 255)
+        set -f width 128
+    else
+        set -f colours (seq 16 231)
+        set -f width 192
     end
 
     for n in $colours
         set -l nnn (printf "%03d" $n)
         # set -l nnn "█▓▒░ ░▒▓█"
         echo -e "\e[7;38;5;$n;48;5;16m" $nnn "\e[0m"
-    end | column $width
+    end | column -c $width
 end
