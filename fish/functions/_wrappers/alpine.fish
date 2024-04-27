471166c3baae957766bfb8bb5929693142878705
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-04-27 17:03:00 -0600

    alpine: add generic config

---
+++ fish/functions/_wrappers/alpine.fish
@@ -0,0 +1,11 @@
+function alpine
+    argparse 'w/with=' -- $argv
+    or return
+
+    set -q _flag_with[1]; or set -f _flag_with pink
+
+    set -fx PINERC $XDG_CONFIG_HOME/alpine/.pinerc
+    set -lx PINERCEX ~/.private/alpine/.pinerc-$_flag_with
+
+    command alpine -i -p$PINERC -x$PINERCEX $argv
+end
