1dad1768d693522723564354e6b60d35534a0f29
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-05-10 08:19:40 -0600

    ssh: remove key files from private repo
    See also 7aa758a

--- local/WS-01JGX23293FB/fish/functions/confupdate.fish
+++ local/WS-01JGX23293FB/fish/functions/confupdate.fish
@@ -5,7 +5,4 @@ function confupdate -d "Update configuration from GitHub"
     # This server doesn't like `~/.ssh` (or any of its contents) being a symlink
     cp -af $USER_CONFIG_DIRS[2]/ssh/* ~/.ssh
     and chmod -R -c go-rwx ~/.ssh
-
-    # Ironically, Cygwin's OpenSSH/SSL doesn't like CRLF in key files
-    dos2unix -q -k ~/.ssh/id_*
 end
