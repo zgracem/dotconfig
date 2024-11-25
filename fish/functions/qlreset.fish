e3be6e4f04f7e11545309cf0ac80dab6e3cbfa4c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-11-25 08:24:37 -0700

    fish: `qlreset` should kill all relevant processes

--- fish/functions/qlreset.fish
+++ fish/functions/qlreset.fish
@@ -2,5 +2,5 @@ function qlreset -d "Reset QuickLook"
     command -q qlmanage; or return 127
     qlmanage -r cache   # qlmanage: call reset on cache
     and qlmanage -r     # qlmanage: resetting quicklookd
-    and killall Finder  # kill -term ####
+    and killall quicklookd Dock Finder
 end
