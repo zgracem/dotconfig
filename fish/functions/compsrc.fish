f2415d30b29596e2d2ec6e5b8c91b0b5f43a996b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-11-28 15:01:58 -0700

    fish: let `compsrc` take multiple arguments

--- fish/functions/compsrc.fish
+++ fish/functions/compsrc.fish
@@ -1,5 +1,5 @@
-function compsrc -d 'Display completion source file(s)' -a query
-    for file in $fish_complete_path/$query.fish
+function compsrc -d 'Display completion source file(s)'
+    for file in $fish_complete_path/$argv.fish
         path is -f $file; and set -f found something; and short_home $file
     end
     string match -q something "$found"
