31de91a7ca3ac5b73880878b209c02f0af1cc8d6
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-12-08 21:31:27 -0700

    fish: plain `{comp,func}src` prints `fish_{complete,function}_path`

--- fish/functions/compsrc.fish
+++ fish/functions/compsrc.fish
@@ -1,4 +1,8 @@
 function compsrc -d 'Display completion source file(s)'
+    if test -z "$argv"
+        short_home $fish_complete_path
+        return
+    end
     for file in $fish_complete_path/$argv.fish
         path is -f $file; and set -f found something; and short_home $file
     end
--- fish/functions/funcsrc.fish
+++ fish/functions/funcsrc.fish
@@ -1,4 +1,8 @@
 function funcsrc -d 'Display function source file(s)' -a query
+    if test -z "$query"
+        short_home $fish_function_path
+        return
+    end
     set --function found nothing
     for file in $fish_function_path/$query.fish
         path is -f $file; and set found something; and short_home $file
