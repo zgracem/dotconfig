0e49adb9e9a4246f145166fd1cf9ea939f9d9aac
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-12-16 11:00:48 -0700

    dosbox: update settings

--- fish/functions/_wrappers/dosbox.fish
+++ fish/functions/_wrappers/dosbox.fish
@@ -1,3 +1,6 @@
 function dosbox
-    dosbox-x -conf $XDG_CONFIG_HOME/dosbox-x/dosbox-x.conf $argv
+    set -l opts
+    set -a opts -conf $XDG_CONFIG_HOME/dosbox-x/dosbox-x.conf
+    set -a opts -display2 amber
+    dosbox-x $opts $argv
 end
