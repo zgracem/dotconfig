363402e2b8fda231fb7b2300b45164dc874e9c70
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-12-18 14:28:49 -0700

    fish: update `commandline` switches for 4.0

--- fish/packages/completion-helpers/functions/__fish_is_nth_arg.fish
+++ fish/packages/completion-helpers/functions/__fish_is_nth_arg.fish
@@ -1,4 +1,6 @@
 function __fish_is_nth_arg -a n -d 'Test if current arg is the Nth (regardless if switch or not)'
-    set -l tokens (commandline -poc)
+    set -l token_switch --tokenize
+    fish-is-newer-than 4.0b1; and set token_switch --tokens-expanded
+    set -l tokens (commandline --current-process $token_switch --cut-at-cursor)
     test (count $tokens) -eq "$n"
 end
--- fish/packages/zgm-completions/completions/wtf.fish
+++ fish/packages/zgm-completions/completions/wtf.fish
@@ -2,4 +2,7 @@ complete -c wtf --require-parameter --force-files
 complete -c wtf -a "(functions --names --all)" -d function
 complete -c wtf -a "(builtin --names)" -d builtin
 complete -c wtf -a "(abbr --list)" -d abbrev.
-complete -c wtf -a "(complete -C (printf %s\n (commandline --tokenize --current-token)))"
+
+set -l token_switch --tokenize
+fish-is-newer-than 4.0b1; and set token_switch --tokens-expanded
+complete -c wtf -a "(complete -C (printf %s\n (commandline $token_switch --current-token)))"
