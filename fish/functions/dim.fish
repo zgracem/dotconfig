78a315e6f775b5ba44cc83326cd171d6025abb37
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-01-05 14:16:23 -0700

    fish: simplify `dim` function

--- fish/functions/dim.fish
+++ fish/functions/dim.fish
@@ -1,18 +1,10 @@
 function dim --description 'Get the pixel dimensions of an image' -a image
-    if command -q sips
-        set -l sips_rx "^\s+\w+: (.+)"
-        set -f width (sips --getProperty pixelWidth $image | string match -rg $sips_rx)
-        or return
-        set -f height (sips --getProperty pixelHeight $image | string match -rg $sips_rx)
-        or return
-    else
-        set -l regex '.*, (\d+) ?x ?(\d+),.*'
-        set -l dims (file -bp $image | string replace -rf $regex '$1\n$2')
-        or return
+    set -l regex '.*, (\d+) ?x ?(\d+),.*'
+    set -l dims (file -bp $image | string replace -rf $regex '$1\n$2')
+    or return
 
-        set -f width $dims[1]
-        set -f height $dims[2]
-    end
+    set -f width $dims[1]
+    set -f height $dims[2]
 
     printf "%s: %d × %d\\n" $image $width $height
 end
