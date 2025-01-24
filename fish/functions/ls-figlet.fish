b7c07f4c7e190725e0d93e53ade190a1d61d9492
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-01-24 12:55:11 -0700

    fish: adjust alternate path for `figlet` fonts
    Follow up to c168563

--- fish/functions/ls-figlet.fish
+++ fish/functions/ls-figlet.fish
@@ -5,7 +5,7 @@ function ls-figlet -d "Print a phrase in all available figlet(1) fonts"
     set -q _flag_width[1]; or set -f _flag_width $COLUMNS
     set -q _flag_width[1]; or set -f _flag_width 80
 
-    set -lx FIGLET_FONTDIR (path filter /usr/share/figlet $HOMEBREW_PREFIX/share/figlet/fonts)
+    set -lx FIGLET_FONTDIR (path filter $HOMEBREW_PREFIX/share/figlet/fonts $XDG_DATA_HOME/figlet)
     set -l figfonts $FIGLET_FONTDIR[1]/*.flf
 
     set -q argv[1]; or set -f argv hello world
