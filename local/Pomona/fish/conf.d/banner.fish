93acf6d83652ae93de9e7d073b73c1ac7dd2c2af
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-01-24 13:03:59 -0700

    fish: make `banner` override `motd`

---
+++ local/Pomona/fish/conf.d/banner.fish
@@ -0,0 +1,4 @@
+status is-interactive; or return
+figlet -f cosmic $hostname | lolcat -S 333
+
+set -U last_motd (date +%F)
--- local/phosphor.pink/fish/conf.d/banner.fish
+++ local/phosphor.pink/fish/conf.d/banner.fish
@@ -3,3 +3,5 @@ set_color "#cc33cc"; echo -n '  _  /_ _   _ _  /_ _  _   _  ._  /_'\r\n
 set_color "#fc0fc0"; echo -n ' /_// //_/_\ /_// //_// . /_/// //\ '\r\n
 set_color "#ff00ff"; echo -n '/           /            /          '\r\n
 set_color normal
+
+set -U last_motd (date +%F)
