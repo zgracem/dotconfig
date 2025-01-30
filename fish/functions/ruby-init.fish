e392cf81106463f187e418e86b2c507a2ee3628e
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-01-30 10:58:18 -0700

    ruby: update config and default gems

---
+++ fish/functions/ruby-init.fish
@@ -0,0 +1,15 @@
+function ruby-init -d "Initialize directory for use with ruby-lsp"
+    set -l skel ~/Developer/skel/ruby
+
+    path is -f $PWD/.rubocop.yml
+    or cp -aiv $skel/.rubocop.yml $PWD/.rubocop.yml
+
+    path is -f $PWD/.ruby-version
+    or rbenv global >$PWD/.ruby-version
+
+    path is -f $PWD/Gemfile
+    or cp -aiv $skel/Gemfile $PWD/Gemfile
+
+    path is -f $PWD/Gemfile.lock
+    or bundle install
+end
