8d4b6f1c47efd1d98981d555522e8e4272552b0f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-01-31 13:50:25 -0700

    fish: update `ls-vscode` to v1.9
    - handle `vscode-remote://` URLs
    - consistently scope variables
    - store directory hashes in variable for clarity
    - remove needless flow control
    - improved output makes it clearer which JSON data is/isn't present

--- fish/functions/ls-vscode.fish
+++ fish/functions/ls-vscode.fish
@@ -3,7 +3,11 @@ function ls-vscode -d "List all Visual Studio Code workspaces"
     path is -d $dir; or return 1
 
     for folder in $dir/*/
-        set json_file (path resolve $folder/workspace.json)
+        set -f json_file (path resolve $folder/workspace.json)
+        path is -f $json_file; or continue
+
+        set -f hash (path basename $folder)
+
         set -f url (jq -r '.folder' $json_file)
         if string match -q null $url
             set -f path (jq -r '.configuration.path' $json_file)
@@ -11,43 +15,40 @@ function ls-vscode -d "List all Visual Studio Code workspaces"
                 set -f ws (jq -r '.workspace' $json_file)
                 if string match -q null $ws
                     echo -s (set_color brred) "No .folder, .path, or .workspace: " \
-                        (set_color red) (basename $folder) (set_color normal)
+                        (set_color red) $hash (set_color normal)
                 else
                     set -f wsfile (string match -rg '^file://(/.+)' $ws | string unescape --style=url)
                     if path is $wsfile
-                        echo -s (set_color brgreen) "Found .workspace: " (set_color green) $wsfile \
-                            ": " (set_color normal) (basename $folder)
-                        continue
+                        echo -s (set_color brgreen) ".workspace: " (set_color green) $wsfile \
+                            ": " (set_color normal) $hash
                     else
-                        echo -s (set_color brred) ".workspace not found: " \
+                        echo -s (set_color brred) "no .workspace: " \
                             (set_color red) $wsfile ": " \
-                            (set_color normal) (basename $folder)
-                        # break
+                            (set_color normal) $hash
                     end
                 end
             else
                 if path is $path
-                    echo -s (set_color brgreen) "Found .path: " (set_color green) $path \
-                        ": " (set_color normal) (basename $folder)
-                    continue
+                    echo -s (set_color brgreen) ".path: " (set_color green) $path \
+                        ": " (set_color normal) $hash
                 else
-                    echo -s (set_color brred) ".path not found: " \
+                    echo -s (set_color brred) "no .path: " \
                         (set_color red) $path ": " \
-                        (set_color normal) (basename $folder)
-                    # break
+                        (set_color normal) $hash
                 end
             end
         else
-            set -f file (string match -rg '^file://(/.+)' $url | string unescape --style=url)
-            if path is $file
-                echo -s (set_color brgreen) "Found .file: " (set_color green) $file \
-                    ": " (set_color normal) (basename $folder)
-                continue
+            set -f file (string match -rg '^(?:\w+)://(/.+)' $url | string unescape --style=url)
+            if string match -q "*vscode-remote://*" $url
+                echo -s (set_color bryellow) ".folder: " (set_color yellow) (string unescape --style=url $url) \
+                    ": " (set_color normal) $hash
+            else if path is $file
+                echo -s (set_color brgreen) ".folder: " (set_color green) $file \
+                    ": " (set_color normal) $hash
             else
-                echo -s (set_color brred) ".folder not found: " \
+                echo -s (set_color brred) "no .folder: " \
                     (set_color red) $file ": " \
-                    (set_color normal) (basename $folder)
-                # break
+                    (set_color normal) $hash
             end
         end
     end
