a724701e1c1a9ea06d6172f76713161a7041cf24
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-01-31 14:35:45 -0700

    fish: update `ls-vscode` to v2.0
    - code is much easier to read
    - output is much easier to parse
    - new `--src` option reveals source in JSON data

--- fish/functions/ls-vscode.fish
+++ fish/functions/ls-vscode.fish
@@ -1,55 +1,43 @@
 function ls-vscode -d "List all Visual Studio Code workspaces"
+    argparse s/src -- $argv
+    or return
+
     set -f dir ~/Library/"Application Support"/Code/User/workspaceStorage
     path is -d $dir; or return 1
 
-    for folder in $dir/*/
-        set -f json_file (path resolve $folder/workspace.json)
-        path is -f $json_file; or continue
+    if set -q argv[1]
+        set -f files $dir/$argv/workspace.json
+    else
+        set -f files $dir/*/workspace.json
+    end
 
-        set -f hash (path basename $folder)
+    for file in $files
+        set -l hash (path basename (path dirname $file))
+        echo -ns $hash \t
 
-        set -f url (jq -r '.folder' $json_file)
-        if string match -q null $url
-            set -f path (jq -r '.configuration.path' $json_file)
-            if string match -q null $path
-                set -f ws (jq -r '.workspace' $json_file)
-                if string match -q null $ws
-                    echo -s (set_color brred) "No .folder, .path, or .workspace: " \
-                        (set_color red) $hash (set_color normal)
-                else
-                    set -f wsfile (string match -rg '^file://(/.+)' $ws | string unescape --style=url)
-                    if path is $wsfile
-                        echo -s (set_color brgreen) ".workspace: " (set_color green) $wsfile \
-                            ": " (set_color normal) $hash
-                    else
-                        echo -s (set_color brred) "no .workspace: " \
-                            (set_color red) $wsfile ": " \
-                            (set_color normal) $hash
-                    end
-                end
-            else
-                if path is $path
-                    echo -s (set_color brgreen) ".path: " (set_color green) $path \
-                        ": " (set_color normal) $hash
-                else
-                    echo -s (set_color brred) "no .path: " \
-                        (set_color red) $path ": " \
-                        (set_color normal) $hash
-                end
-            end
+        if set -q _flag_src
+            echo -ns (jq -r 'if has("configuration") then ".path" elif has("folder") then ".folder" elif has("workspace") then ".ws" else ".ERROR" end' $file) \t
+        end
+
+        set -l data (jq -r '.configuration.path // .folder // .workspace // "¡ERROR!"' $file)
+
+        switch $data
+            case "/*"
+                set -f path $data
+            case "file://*"
+                set -f path (string unescape --style=url $data | string replace -r "^file://" "")
+            case "vscode-remote://*"
+                set -f path (string unescape --style=url $data)
+                echo -s (set_color bryellow) 302 (set_color yellow) \t $path (set_color normal)
+                continue
+            case "*"
+                echo -s (set_color brred) 500 (set_color red) \t $data (set_color normal)
+                continue
+        end
+        if path is $path
+            echo -s (set_color brgreen) 200 (set_color green) \t (short_home $path) (set_color normal)
         else
-            set -f file (string match -rg '^(?:\w+)://(/.+)' $url | string unescape --style=url)
-            if string match -q "*vscode-remote://*" $url
-                echo -s (set_color bryellow) ".folder: " (set_color yellow) (string unescape --style=url $url) \
-                    ": " (set_color normal) $hash
-            else if path is $file
-                echo -s (set_color brgreen) ".folder: " (set_color green) $file \
-                    ": " (set_color normal) $hash
-            else
-                echo -s (set_color brred) "no .folder: " \
-                    (set_color red) $file ": " \
-                    (set_color normal) $hash
-            end
+            echo -s (set_color brred) 404 (set_color red) \t \e"[9m" (short_home $path) (set_color normal)
         end
     end
 end
