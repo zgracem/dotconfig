07caa8f4c5ef679ea5da35681ffdf008263a22d5
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-02-27 11:37:17 -0700

    fish: updates and adjustments for 4.0.0

--- fish/conf.d/__fish-is-newer-than.fish
+++ fish/conf.d/__fish-is-newer-than.fish
@@ -1,28 +1,35 @@
 function fish-is-newer-than -a test_version
     # __fish_versinfo splits fish's $version into an array like $BASH_VERSINFO.
+    #
     # Released fish versions are in the format `X.Y.Z` (even X.0.0 and X.Y.0).
-    # Dev build versions are `X.Y.Z-A-gH`, where `X.Y.Z` is the last release,
+    #
+    # Dev builds look like `X.Y.Z-A-gH`, where `X.Y.Z` is the last release,
     # `A` is the count of commits since x.y.z, `g` is literally the letter g,
     # and `H` is the shortened hash of the latest git commit (which we discard).
-    # Beta releases look like "X.YbB" where uppercase B is the beta number, and
-    # post-beta dev versions look like "X.YbB-A-gH".
+    #
+    # Beta releases look like `X.YbB`, where uppercase B is the beta number,
+    # and post-beta dev versions look like `X.YbB-A-gH`.
     function __fish_versinfo -a fish_ver
-        set -l version_info (string split "." "$fish_ver" | string split "-")
+        set -f version_info (string split "." $fish_ver | string split "-")
+
+        # Discard git commit hash (which can't be easily compared)
+        string match -q "g*" $version_info[5]
+        and set --erase version_info[5]
 
         # The following allows for usage like `fish-is-newer-than 3.1`
         # (i.e. without the trailing `.0`) and for comparing releases to
         # beta and development builds.
-        set -q version_info[2]; or set version_info[2] 0  # Y
-        set -q version_info[3]; or set version_info[3] 0  # Z
-        set -q version_info[4]; or set version_info[4] 0  # A
-        set -q version_info[5]; or set version_info[5] 99 # B
+        set -q version_info[2]; or set version_info[2] 0   # Y
+        set -q version_info[3]; or set version_info[3] 0   # Z
+        set -q version_info[4]; or set version_info[4] 0   # A
+        set -q version_info[5]; or set version_info[5] 1.0 # B
 
-        # Special handling for beta releases
+        # Special handling for beta releases (`X.YbB` or `X.YbB-A-gH`)
         if string match -rq '^(?<minor>\d+)b(?<beta>\d+)$' $version_info[2]
-            set version_info[5] $beta
-            set version_info[4] $version_info[3]
-            set version_info[3] 0
-            set version_info[2] $minor
+            set version_info[5] "0.$beta"        # B
+            set version_info[4] $version_info[3] # A
+            set version_info[3] 0                # Z
+            set version_info[2] $minor           # Y
         end
 
         string join \n $version_info
--- fish/packages/completion-helpers/functions/__fish_is_nth_arg.fish
+++ fish/packages/completion-helpers/functions/__fish_is_nth_arg.fish
@@ -1,6 +1,6 @@
 function __fish_is_nth_arg -a n -d 'Test if current arg is the Nth (regardless if switch or not)'
     set -l token_switch --tokenize
-    fish-is-newer-than 4.0b1 # released Dec 2024
+    fish-is-newer-than 4.0 # released Feb 2025
     and set token_switch --tokens-expanded
     set -l tokens (commandline --current-process $token_switch --cut-at-cursor)
     test (count $tokens) -eq "$n"
