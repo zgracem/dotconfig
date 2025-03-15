8c9cab64f62a65b13c9d62504d1100b1a54bedaf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-03-15 17:54:09 -0600

    fish: move `fish_add_manpath` into functions dir

---
+++ fish/functions/fish_add_manpath.fish
@@ -0,0 +1,23 @@
+function fish_add_manpath --description "Add paths to the MANPATH"
+    # See $__fish_data_dir/functions/fish_add_path.fish
+    argparse -x g,U -x a,p g/global U/universal p/prepend a/append -- $argv
+    or return
+
+    set -l scope $_flag_global $_flag_universal
+    set -q scope[1]; or set scope -g
+
+    set -l var MANPATH
+
+    set -l mode $_flag_prepend $_flag_append
+    set -q mode[1]; or set mode -p
+
+    for path in $argv
+        set -l path (builtin realpath -s -- $path 2>/dev/null)
+        # Skip non-existing paths
+        path is -d $path; or continue
+        # Skip pre-set paths
+        contains -- $path $$var; and continue
+
+        set $scope $mode $var $path
+    end
+end
