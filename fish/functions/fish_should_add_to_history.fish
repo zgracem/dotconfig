120cf65028f9c6fe48833a02d1611edd49ca82dd
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-04-05 11:58:24 -0600

    fish: add more query commands to ignore & update doc link

--- fish/functions/fish_should_add_to_history.fish
+++ fish/functions/fish_should_add_to_history.fish
@@ -1,11 +1,11 @@
-# https://fishshell.com/docs/4.0b1/cmds/fish_should_add_to_history.html
+# https://fishshell.com/docs/4.0/cmds/fish_should_add_to_history.html
 function fish_should_add_to_history
     # don't save commands w/ leading space or comment character
     string match -rq '^[\s#]' $argv
     and return 1
 
     # don't save common query commands
-    string match -rq "^(art|wtf)\b" $argv
+    string match -rq "^(art|(comp|func|man)src|halp|wtf)\b" $argv
     and return 1
 
     return 0
