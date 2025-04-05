d8de3aeed352391c6a8d7d97c090977d4737b3fa
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-04-05 11:59:11 -0600

    fish: add `commandline_parser` and `Ctrl-O` shortcut

---
+++ fish/conf.d/commandline_parser.fish
@@ -0,0 +1,12 @@
+function commandline_parser
+    set -l joint " ‡ "
+    echo
+    echo "current-buffer:  "(commandline -b)
+    echo "current-job:     "(commandline -j)
+    echo "current-process: "(commandline -p)
+    echo "current-token:   "(commandline -t)
+    echo "cut-at-cursor:   "(commandline -c)
+    echo "tokens-expanded: "(commandline -x | string join $joint)
+    echo "tokens-raw:      "(commandline --tokens-raw | string join $joint)
+end
+bind ctrl-o commandline_parser repaint
