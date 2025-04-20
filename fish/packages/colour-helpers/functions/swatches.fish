beee9ee367daaff6bc455ee1df1ec62ec411ccd1
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-04-20 15:14:53 -0600

    fish: validate `--tile` options in `swatches`

--- fish/packages/colour-helpers/functions/swatches.fish
+++ fish/packages/colour-helpers/functions/swatches.fish
@@ -5,7 +5,14 @@ function swatches -d "Create an image with multiple colour swatches"
     or return
 
     set -q _flag_size[1]; or set -f _flag_size 128
-    set -q _flag_tile; and set -f tile_opt -tile $_flag_tile
+    if set -q _flag_tile[1]
+        if string match -rq '\d+x\d+' $_flag_tile
+            set -f tile_opt -tile $_flag_tile
+        else
+            echo >&2 "invalid tile setting: $_flag_tile"
+            return 1
+        end
+    end
     set -q _flag_output[1]; or set -f _flag_output "swatches_"(date +%Y%m%d%H%M%S)".png"
 
     set -f tmpfiles
