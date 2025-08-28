1fc74686f8130bd87508ed082e610d343be09eaf
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-08-28 12:22:25 -0600

    fish: small tweak to new `hfind`
    See 3ee5a18

--- fish/functions/hfind.fish
+++ fish/functions/hfind.fish
@@ -1,5 +1,5 @@
 # Adds timestamps and syntax highlighting
 function hfind -d "Search the command history"
     command -q bat; and set -fx PAGER bat -pp -lfish
-    history search --show-time="# %F %T%n" $argv
+    history search --reverse --show-time="# %F %T%n" $argv
 end
