fda22a15780208d8a3e2011d1cc9aa75b6c5cf37
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-08-28 12:21:37 -0600

    fish: `@` with no args gives the epoch time

--- fish/functions/@.fish
+++ fish/functions/@.fish
@@ -4,7 +4,10 @@ function @ --description 'Format UNIX epoch as human-readable date'
 
     set -l epoch $argv[1]
 
-    if set -q _flag_long
+    if not set -q epoch[1]
+        gdate +%s
+        return
+    else if set -q _flag_long
         set -f format +"%A, %B %-e, %Y @ %H:%M:%S %z"
     else if set -q _flag_medium
         set -f format --rfc-2822 # +"%a, %d %b %Y %T %z"
