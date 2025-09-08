2f2717e410109495a22e4315e36063437c48e34d
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-09-08 15:53:17 -0600

    fish: move `rewind` to Developer dir

--- fish/packages/zgm-completions/completions/rewind.fish
+++
@@ -1,6 +0,0 @@
-complete -c rewind -F
-complete -c rewind -s e -l exif -d "Use EXIF creation date"
-complete -c rewind -s f -l filename -d "Use filename"
-complete -c rewind -s h -l help -d "Print help"
-complete -c rewind -s n -l dry-run -d "Only print what would be done"
-complete -c rewind -s v -l verbose -d "List files as they are touched"
