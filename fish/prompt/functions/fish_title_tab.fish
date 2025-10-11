b1e1c3cb6de21419714ebed985b5d5b81080a62b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-11 16:38:06 -0600

    fish: new function: `shortest_home`

--- fish/prompt/functions/fish_title_tab.fish
+++ fish/prompt/functions/fish_title_tab.fish
@@ -1,16 +1,3 @@
 function fish_title_tab --description 'Update the tab title'
-    set -l cwd (short_home $PWD)
-    set -l cwd_parts (string split "/" "$cwd")
-    set -l ellipsis "…"
-
-    # use Windows-1252 encoding, or taskbar will have "â€¦" instead of dots
-    is-cygwin; and set ellipsis (echo -ne "\x85")
-
-    echo -ns (prompt_hostname) ":"
-
-    if test (count $cwd_parts) -le 2
-        echo -n $cwd
-    else
-        string join / -- $cwd_parts[1] $ellipsis $cwd_parts[-1]
-    end
+    echo -ns (prompt_hostname) ":" (shortest_home $PWD)
 end
