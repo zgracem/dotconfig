735f82ffe93f3dd6bdb57f91579534f67d8c529c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-14 10:51:38 -0600

    fish: remove needless version check
    Versions of fish prior to 4.1 were never built with embedded files.

--- fish/functions/fish_source_data_file.fish
+++ fish/functions/fish_source_data_file.fish
@@ -1,8 +1,5 @@
 function fish_source_data_file -d "Evaluate contents of actual or embedded file"
-    if fish-is-older-than 4.1
-        source "$__fish_data_dir/$argv[1]"
-        return
-    else if set -q __fish_data_dir[1]
+    if set -q __fish_data_dir[1]
         source "$__fish_data_dir/$argv[1]"
     else
         status get-file $argv[1] | source
