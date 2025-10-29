dfa8b096b960bdf843e610b6c7f4ab01de10015c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-29 10:11:24 -0600

    fish: update `fish_logo` for v4

--- fish/functions/fish_logo.fish
+++ fish/functions/fish_logo.fish
@@ -1,4 +1,9 @@
 function fish_logo --description 'Display the fish logo'
+    __fish_logo_v4
+    set_color normal
+end
+
+function __fish_logo_v3
     set -l red (set_color f00)
     set -l ora (set_color f80)
     set -l yel (set_color ff0)
@@ -18,5 +23,34 @@ function fish_logo --description 'Display the fish logo'
                       '$red'/'$ora'J'$yel'\\'$ora'J'$red'T\\'$ora'JJJ'$red'J)
                       (J'$ora'JJ'$red'| \UUU)
                        (UU)
+'
+end
+
+function __fish_logo_v4
+    # set -l palette "#810793" "#9d0287" "#dd4f8f" "#fff0a8"
+    # set -l palette "#0055aa" "#0088ff" "#33ddbb" "#55dd33"
+    # set -l palette "#0088bb" "#5588dd" "#aa88ff" "#dd99ff"
+    set -l palette "#ff33aa" "#ff2288" "#ff8800" "#ffaa00"
+
+    set -l shadow (set_color $palette[1])
+    set -l lolite (set_color $palette[2])
+    set -l hilite (set_color $palette[3])
+    set -l accent (set_color $palette[4])
+
+    echo '
+                                 '$lolite'__~'$hilite'’
+                               '$lolite'_/,'$hilite'~\
+                             '$lolite'_-'$hilite' _~}
+        '$lolite',/'$hilite'/}-__             '$lolite'/'$hilite'  __~}
+       '$lolite'//'$hilite'‘,/ /,}-_         '$lolite'/ '$hilite' __~/
+   '$lolite'__-/ ('$hilite'-,/'$lolite'_'$hilite',/ /,}      '$hilite'_'$lolite'/  '$hilite'_~‘}
+  '$lolite'/           '$lolite'‘—'$hilite'/'$lolite'_'$hilite'/}'$lolite'__-_/   '$lolite'..'$hilite'=<
+'$accent'O'$lolite')    ('$accent'O'$lolite')                    '$hilite'~_}
+'$lolite'|   o       )   '$hilite'.....'$lolite'_===--==_ '$hilite'~_\
+'$lolite'\        '$hilite'..'$lolite') '$hilite'....'$lolite'=='$shadow'=/        '$lolite'\-_~>
+  '$lolite'\-_ '$hilite'....'$lolite'//'$hilite'....'$lolite'=='$shadow'='$hilite'\\'$shadow'’
+     '$lolite'|\\'$shadow'====`'$lolite'|\_'$shadow'=‘’’'$hilite'\}
+     '$lolite'| '$hilite'}    '$lolite'\ '$hilite'~}
+             '$lolite'\\'$hilite'_}
 '
 end
