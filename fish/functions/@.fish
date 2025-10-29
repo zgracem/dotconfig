11510b01c8d2fc5bfb6f40ff93b8aae04862daef
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-29 10:21:46 -0600

    fish: explicitly require GNU `date(1)` for `@`

--- fish/functions/@.fish
+++ fish/functions/@.fish
@@ -2,10 +2,15 @@ function @ --description 'Format UNIX epoch as human-readable date'
     argparse -xs,m,l 's/short' 'm/medium' 'l/long' -- $argv
     or return
 
+    if not is-gnu date
+        echo >&2 "error: GNU date required"
+        return 1
+    end
+
     set -l epoch $argv[1]
 
     if not set -q epoch[1]
-        gdate +%s
+        date +%s
         return
     else if set -q _flag_long
         set -f format +"%A, %B %-e, %Y @ %H:%M:%S %z"
@@ -15,5 +20,5 @@ function @ --description 'Format UNIX epoch as human-readable date'
         set -f format --iso-8601=seconds # +"%Y-%m-%dT%H:%M:%S%z"
     end
 
-    gdate --date="@$epoch" $format
+    date --date="@$epoch" $format
 end
