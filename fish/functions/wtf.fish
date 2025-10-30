2a7f19b315cf2a172d14f089ac409b284fa6cde8
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-30 15:57:17 -0600

    fish: update `wtf`
    - also query variables
    - handle abbreviations properly (previously the query would fail unless
      the abbr. was also another valid type)
    - minor refactoring for improved clarity

--- fish/functions/wtf.fish
+++ fish/functions/wtf.fish
@@ -1,27 +1,26 @@
 function wtf -d "Display information about commands"
     set -q argv[1]; or return 1
-    set -f all_abbr (abbr --list)
 
     for subject in $argv
         set -l types (type -at $subject 2>/dev/null)
+        contains -- $subject (abbr --list); and set -p types abbreviation
+        contains -- $subject (set --names); and set -p types variable
+
         if test (count $types) -eq 0
             echo >&2 "not found: $subject"
             return 1
         end
 
-        if contains -- $subject $all_abbr
-            set --prepend types abbreviation
-        end
         for type in $types
             switch $type
                 case function
                     functions $subject
                 case builtin
-                    set_color --underline --italic $fish_color_keyword
-                    echo -ns $subject
-                    set_color normal
+                    echo -ns (set_color -iu brmagenta) $subject (set_color normal)
                     echo " is a builtin"
                 case file
+                    echo -ns (set_color -iu brblue) $subject (set_color normal)
+                    echo " is a file"
                     set -l paths (type -aP $subject)
                     if command -q eza
                         eza --long --sort none $paths
@@ -29,11 +28,13 @@ function wtf -d "Display information about commands"
                         ls -lh $paths
                     end
                 case abbreviation
-                    set_color --underline --italic brcyan
-                    echo -n $subject
-                    set_color normal
+                    echo -ns (set_color -iu brcyan) $subject (set_color normal)
                     echo " is an abbreviation"
-                    abbr -s | string match -er -- "-- $subject\b" | fish_indent --ansi
+                    abbr --show | string match -er -- "-- $subject\b" | fish_indent --ansi
+                case variable
+                    echo -ns (set_color -i bryellow) $subject (set_color normal)
+                    echo " is a variable"
+                    set --show $subject
             end
         end
     end
