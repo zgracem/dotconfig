4100522c2e5f8ceff60a664bc111ca412db352ad
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-30 13:05:25 -0600

    fish: use `my-prompt-pwd` in `short_home` if available

--- fish/functions/short_home.fish
+++ fish/functions/short_home.fish
@@ -1,3 +1,7 @@
 function short_home --description 'Replace $HOME with ~ in a path'
-    string replace --regex "^$HOME(?=\$|/)" "~" $argv
+    if functions -q my-prompt-pwd
+        my-prompt-pwd -Z $argv
+    else
+        string replace --regex "^$HOME(?=\$|/)" "~" $argv
+    end
 end
