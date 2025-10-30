87d095e04ce7812e20dc08086177c8f9bce63b2a
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-30 09:19:00 -0600

    fish: `qlreset` → `ql --reset`

--- fish/functions/ql.fish
+++ fish/functions/ql.fish
@@ -1,4 +1,14 @@
 function ql -d "Preview with QuickLook"
     command -q qlmanage; or return 127
-    qlmanage -p >/dev/null 2>&1 $argv
+    argparse r/reset -- $argv; or return
+
+    if set -q _flag_reset[1]
+        qlmanage -r cache   # qlmanage: call reset on cache
+        and qlmanage -r     # qlmanage: resetting quicklookd
+        and killall quicklookd Dock Finder
+    else if set -q argv[1]
+        qlmanage -p >/dev/null 2>&1 $argv[1]
+    else
+        return 2
+    end
 end
--- fish/functions/qlreset.fish
+++
@@ -1,6 +0,0 @@
-function qlreset -d "Reset QuickLook"
-    command -q qlmanage; or return 127
-    qlmanage -r cache   # qlmanage: call reset on cache
-    and qlmanage -r     # qlmanage: resetting quicklookd
-    and killall quicklookd Dock Finder
-end
