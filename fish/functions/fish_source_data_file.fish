2126201733805a08abffbe2bcb9949d799a34a53
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-10-30 15:57:27 -0600

    fish: remove unnecessary function

--- fish/functions/fish_source_data_file.fish
+++
@@ -1,7 +0,0 @@
-function fish_source_data_file -d "Evaluate contents of actual or embedded file"
-    if set -q __fish_data_dir[1]
-        source "$__fish_data_dir/$argv[1]"
-    else
-        status get-file $argv[1] | source
-    end
-end
