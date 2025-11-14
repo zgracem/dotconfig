1982ae0401e9fb9e9d17a34e002e4a5ef9aae128
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-11-14 16:01:33 -0700

    fish: add `-X` to history search pager
    This stops `less` from clearing the screen on exit.

--- fish/functions/hfind.fish
+++ fish/functions/hfind.fish
@@ -1,5 +1,6 @@
 # Adds timestamps and syntax highlighting
 function hfind -d "Search the command history"
     command -q bat; and set -fx PAGER bat -p -lfish
+    set -fx BAT_PAGER "less -RFX"
     history search --reverse --show-time="# %F %T%n" $argv
 end
