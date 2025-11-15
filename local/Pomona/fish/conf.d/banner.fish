0ed4cf62ec697a61b7100e391f4dc330b311f6d0
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-11-15 12:51:12 -0700

    fish: small changes to remote login banners

--- local/Pomona/fish/conf.d/banner.fish
+++ local/Pomona/fish/conf.d/banner.fish
@@ -1,6 +1,15 @@
 status is-interactive; or return
 
-echo
-figlet -f cosmic $hostname | lolcat -S 333
-echo
-motd --done
+set c0 (set_color normal)
+set g1 (set_color green)
+set g2 (set_color brgreen)
+set p1 (set_color magenta)
+set p2 (set_color brmagenta)
+set p3 (echo -n $p2; set_color --bold --italics)
+
+echo -ns ' ' $g1 '\\\\' $g2 '\ /' $g1 '//' \r \n
+echo -ns $p1 'o88' $p2 'OOO' $p1 '88o' \r \n
+echo -ns ' 8' $p2 'OOOOO' $p1 '8   ' $g1 'Welcome to' $c0 \r \n
+echo -ns $p1 ' °O' $p2 'OOO' $p1 'O°     ' $p3 'Pomona' $c0 \r \n
+echo -ns $p1 '  8O' $p2 'O' $p1 'O8  ' \r \n
+echo -ns '   °°°' $c0 \r \n
--- local/phosphor.pink/fish/conf.d/banner.fish
+++ local/phosphor.pink/fish/conf.d/banner.fish
@@ -3,5 +3,3 @@ set_color "#cc33cc"; echo -n '  _  /_ _   _ _  /_ _  _   _  ._  /_'\r\n
 set_color "#fc0fc0"; echo -n ' /_// //_/_\ /_// //_// . /_/// //\ '\r\n
 set_color "#ff00ff"; echo -n '/           /            /          '\r\n
 set_color normal
-
-motd --done
