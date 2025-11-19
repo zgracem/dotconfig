9d4589f6fb96dabfbc135b789612b5138d3d001f
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-11-19 11:54:09 -0700

    fish: completely rewrite `rl` function

--- fish/colours.fish
+++ fish/colours.fish
@@ -1,67 +1,68 @@
-set -q __zgm_init_colours
-and return
+if not set -Uq __zgm_init_colours
+    echo "Sourcing colours…" | if command -q lolcat
+        lolcat --animate
+    else
+        cat
+    end
 
-echo "Sourcing colours…" | if command -q lolcat
-    lolcat --animate
-else
-    cat
+    # https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
+    set -U fish_color_normal normal
+    set -U fish_color_command white --bold
+    set -U fish_color_keyword brmagenta
+    set -U fish_color_quote cyan
+    set -U fish_color_redirection yellow # IO redirections like `>/dev/null`
+    set -U fish_color_end yellow # process separators like `;` and `&`
+    set -U fish_color_error brred # syntax errors
+    set -U fish_color_param normal # ordinary command parameters
+    set -U fish_color_valid_path green --underline # filename parameters (if the file exists)
+    set -U fish_color_option white # options starting with `-`
+    set -U fish_color_comment brblack --italics # comments like `# TODO`
+    set -U fish_color_selection white --background=brblack # selected text (in vi visual mode)
+    set -U fish_color_operator bryellow # parameter expansion operators like '*' and '~'
+    set -U fish_color_escape brred # character escapes like '\n' and '\x70'
+    set -U fish_color_autosuggestion brblack
+    set -U fish_color_cwd brwhite # the current directory in the default prompt
+    set -U fish_color_cwd_root red # ditto, but for the root user
+    set -U fish_color_user brblack # the username in some default prompts
+    set -U fish_color_at normal # the '@' in 'user@host'
+    set -U fish_color_host brblack # the hostname in some default prompts
+    set -U fish_color_host_remote brblack # ditto for remote sessions (like ssh)
+    set -U fish_color_status $fish_color_error # the last command's nonzero exit code in the default prompt
+    set -U fish_color_cancel red # the '^C' indicator on a canceled command
+    set -U fish_color_search_match --background=black # history search matches & pager selections (background colour)
+    set -U fish_color_history_current blue # used by `dirh`
+
+    # completion highlighting
+    set -U fish_pager_color_progress blue # "…and 6 more rows" in the bottom left corner
+    # unselected lines
+    set -U fish_pager_color_background --background=normal # the background color of a line
+    set -U fish_pager_color_prefix brblack # the un-completed (incomplete) string
+    set -U fish_pager_color_completion normal # the proposed completion suffix
+    set -U fish_pager_color_description $fish_color_comment # the completion description
+    # selected line
+    set -U fish_pager_color_selected_background --background=brblack
+    set -U fish_pager_color_selected_prefix --dim
+    set -U fish_pager_color_selected_completion brwhite --bold
+    set -U fish_pager_color_selected_description --italics
+
+    # ---------------------------------------------------------------------------
+    # $__fish_config_dir/prompt
+    # ---------------------------------------------------------------------------
+
+    set -U fish_prompt_color_ps blue
+    set -U fish_prompt_color_ps_root $fish_color_cwd_root
+    set -U fish_prompt_color_duration --dim
+    set -U fish_prompt_color_jobs yellow
+    set -U fish_prompt_color_rbenv brmagenta
+    set -U __fish_git_prompt_color_branch --dim --italics
+    set -U __fish_git_prompt_color_stashstate --dim
+    set -U __fish_git_prompt_color_dirtystate red
+    set -U __fish_git_prompt_color_stagedstate yellow
+    set -U __fish_git_prompt_color_upstream cyan
+    set -U __fish_git_prompt_color_cleanstate green
+
+    set -U __zgm_init_colours ✓
 end
-
-# https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
-set -U fish_color_normal normal
-set -U fish_color_command white --bold
-set -U fish_color_keyword brmagenta
-set -U fish_color_quote cyan
-set -U fish_color_redirection yellow # IO redirections like `>/dev/null`
-set -U fish_color_end yellow # process separators like `;` and `&`
-set -U fish_color_error brred # syntax errors
-set -U fish_color_param normal # ordinary command parameters
-set -U fish_color_valid_path green --underline # filename parameters (if the file exists)
-set -U fish_color_option white # options starting with `-`
-set -U fish_color_comment brblack --italics # comments like `# TODO`
-set -U fish_color_selection white --background=brblack # selected text (in vi visual mode)
-set -U fish_color_operator bryellow # parameter expansion operators like '*' and '~'
-set -U fish_color_escape brred # character escapes like '\n' and '\x70'
-set -U fish_color_autosuggestion brblack
-set -U fish_color_cwd brwhite # the current directory in the default prompt
-set -U fish_color_cwd_root red # ditto, but for the root user
-set -U fish_color_user brblack # the username in some default prompts
-set -U fish_color_at normal # the '@' in 'user@host'
-set -U fish_color_host brblack # the hostname in some default prompts
-set -U fish_color_host_remote brblack # ditto for remote sessions (like ssh)
-set -U fish_color_status $fish_color_error # the last command's nonzero exit code in the default prompt
-set -U fish_color_cancel red # the '^C' indicator on a canceled command
-set -U fish_color_search_match --background=black # history search matches & pager selections (background colour)
-set -U fish_color_history_current blue # used by `dirh`
-
-# completion highlighting
-set -U fish_pager_color_progress blue # "…and 6 more rows" in the bottom left corner
-# unselected lines
-set -U fish_pager_color_background --background=normal # the background color of a line
-set -U fish_pager_color_prefix brblack # the un-completed (incomplete) string
-set -U fish_pager_color_completion normal # the proposed completion suffix
-set -U fish_pager_color_description $fish_color_comment # the completion description
-# selected line
-set -U fish_pager_color_selected_background --background=brblack
-set -U fish_pager_color_selected_prefix --dim
-set -U fish_pager_color_selected_completion brwhite --bold
-set -U fish_pager_color_selected_description --italics
-
-# ---------------------------------------------------------------------------
-# $__fish_config_dir/prompt
-# ---------------------------------------------------------------------------
-
-set -U fish_prompt_color_ps blue
-set -U fish_prompt_color_ps_root $fish_color_cwd_root
-set -U fish_prompt_color_duration --dim
-set -U fish_prompt_color_jobs yellow
-set -U fish_prompt_color_rbenv brmagenta
-set -U __fish_git_prompt_color_branch --dim --italics
-set -U __fish_git_prompt_color_stashstate --dim
-set -U __fish_git_prompt_color_dirtystate red
-set -U __fish_git_prompt_color_stagedstate yellow
-set -U __fish_git_prompt_color_upstream cyan
-set -U __fish_git_prompt_color_cleanstate green
 
 # ----------------------------------------------------------------------------
 # other
@@ -108,5 +109,3 @@ begin
     set -l jq_obj (get_color normal)
     set -Ua JQ_COLORS $jq_nil $jq_non $jq_oui $jq_int $jq_str $jq_arr $jq_obj
 end
-
-set -U __zgm_init_colours ✓
--- fish/functions/rl.fish
+++ fish/functions/rl.fish
@@ -1,123 +1,118 @@
 function rl --description "Reload configuration files"
-    argparse 'v/verbose' -- $argv; or return
+    argparse v/verbose -- $argv
+    or return
 
-    set -q _flag_verbose; and set -fx VERBOSE 1
-    set -f _rl_errors 0
-    set --erase -f files_to_reload
-    set -f files_to_reload
+    set -f found 0
 
-    # Checks whether $func is a defined, reloadable function:
-    #   if $func was defined in an existing file, prints the path to stdout and returns true;
-    #   if $func can't be reloaded, prints the reason to stderr and returns false;
-    #   if $func is not defined, silently returns false.
-    function _rl_can_reload -a func
-        set -f src (functions -D $func)
-        set -f reason "file not found: $src"
+    if not set -q argv[1]
+        _rl_reset --all
+        exec fish
+    end
 
-        switch "$src"
+    for item in $argv
+        _rl_reset $_flag_verbose $item
+
+        # Is it a file that can be sourced?
+        set -f confdirs $__fish_config_dir{,/**/conf.d}
+        for file in (path filter $confdirs/$item.fish)
+            set -f found (math "$found+1")
+            _rl_source $_flag_verbose $file
+            or return
+        end
+
+        set -f envdirs $XDG_CONFIG_HOME $XDG_CONFIG_HOME/../.private $__fish_config_dir
+        for envfile in (path filter $envdirs/env.d/$item.{env,fish})
+            set -f found (math "$found+1")
+            _rl_source $_flag_verbose $envfile
+            or return
+        end
+
+        # Is it a function that can be reloaded?
+        set -f funcfile (functions -D $item)
+        switch $funcfile
             case "/*"
-                if path is -f "$src"
-                    echo "$src"
-                    and return 0
+                if path is -f $funcfile
+                    set -f found (math "$found+1")
+                    _rl_source $_flag_verbose $funcfile
+                    or return
                 else
-                    _rl_increment_errors
+                    echo >&2 "can't reload $item(): file not found: $funcfile"
                     return 1
                 end
-            case stdin
-                set reason "defined interactively"
-            case -
-                set reason "defined via `source`"
-            case n/a
+            case - stdin
+                echo >&2 "can't reload $item(): no source file"
                 return 1
-                # set reason "not defined"
         end
 
-        echo >&2 "can’t reload function ‘$func’: $reason"
+        # Are there completions that can be reloaded?
+        for compfile in (path filter $fish_complete_path/$item.fish)
+            set -f found (math "$found+1")
+            _rl_source $_flag_verbose $compfile
+            or return
+        end
+
+        # Is it a special case?
+        switch $item
+            case cygwin dirs
+                set -f found (math "$found+1")
+                _rl_source $_flag_verbose $__fish_config_dir/conf.d/___$item.fish
+        end
+    end
+
+    if test $found -gt 0
+        set -q _flag_verbose[1]; and echo "$found file(s) reloaded"
+        return
+    else
+        echo >&2 "nothing found for '$item'"
         return 1
     end
+end
 
-    # Checks if each argument exists as a file:
-    #   if it does, tries to source it;
-    #     on success, prints the filename to stdout;
-    #     on failure, prints a message to stderr and continues;
-    #   if it doesn't exist, silently continues;
-    #   returns false if anything failed.
-    function _rl_source_all
-        set -f errcode 0
-        for file in $argv
-            if path is -f $file
-                if source "$file"
-                    set -q VERBOSE; and my-prompt-pwd -Z $file
-                    continue
-                else
-                    set errcode $status
-                    echo >&2 "error sourcing file: $file"
-                    _rl_increment_errors
-                end
-            else
-                _rl_increment_errors
-            end
+function _rl_source
+    argparse v/verbose -- $argv
+    or return
+
+    for file in $argv
+        if source $file
+            set -q _flag_verbose[1]
+            and short_home (path resolve $file)
+        else
+            set -l source_exit $status
+            echo >&2 "failed to source file: $file"
+            return $source_exit
         end
-        return (math $status + $errcode)
+    end
+end
+
+function _rl_reset
+    argparse a/all v/verbose -- $argv
+    or return
+
+    if set -q _flag_all[1]
+        _rl_reset env colours keychain
+        return
     end
 
-    # Increments the error counter
-    function _rl_increment_errors -a inc
-        set -q inc[1]; or set inc 1
-        set _rl_errors (math "$_rl_errors + $inc")
-    end
-
-    # With no arguments, reload the main config file
-    if not set -q argv[1]
-        set --append files_to_reload "$__fish_config_dir/config.fish"
-    end
-
-    set -f dirs_to_check "$__fish_config_dir" "$__fish_config_dir"/conf.d
-    for arg in $argv
-        set --erase -f checked_files
-
-        # Some reloads require other actions first
-        switch "$arg"
+    for item in $argv
+        switch $item
+            case env
+                set -q _flag_verbose[1]; and echo '[RESET] $__z_env_loaded'
+                set -g --erase __z_env_loaded
             case colours
                 set -l colour_vars (set -n \
                     | string match -rg '(fish_(?:[a-z]+_)*color_\w+)|([[:upper:]]+_COLORS)')
-                set --erase -U $colour_vars __zgm_init_colours
+                set -q _flag_verbose[1]; and echo '[RESET] $__zgm_init_colours' '$'$colour_vars
+                set -U --erase __zgm_init_colours $colour_vars
             case keychain
-                killall -v ssh-agent
-                set --erase -g SSH_AGENT_PID
-            case prompt
-                set --prepend checked_files $__fish_config_dir/prompt/{conf.d,functions}/*.fish
-        end
-
-        # Search config dirs
-        set -f files_to_check $dirs_to_check/"$arg.fish"
-
-        for config_file in $files_to_check
-            path is -r $config_file; and set --append checked_files $config_file
-        end
-
-        # Search functions
-        if set -f function_file (_rl_can_reload $arg)
-            set --append checked_files $function_file
-        end
-
-        # Search completion dirs and take only the first result
-        for completion_file in $fish_complete_path/"$arg.fish"
-            if path is -r $completion_file
-                set --append checked_files $completion_file
-                break
-            end
-        end
-
-        if set -q checked_files[1]
-            set --append files_to_reload $checked_files
-        else
-            _rl_increment_errors
-            echo >&2 "nothing found for “$arg”"
+                if set -q _flag_verbose[1]
+                    killall -v ssh-agent
+                else
+                    killall ssh-agent
+                end 2>/dev/null
+                set -q _flag_verbose[1]; and echo '[RESET] $SSH_AGENT_PID'
+                set -g --erase SSH_AGENT_PID
+            case '*'
+                continue
         end
     end
-
-    _rl_source_all $files_to_reload
-    functions --erase (functions --names --all | string match -e "_rl_*")
-    return $_rl_errors
 end
