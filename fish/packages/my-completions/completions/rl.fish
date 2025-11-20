d465f647c6780babe30fc4f97c47c2fa82603c27
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-11-20 15:26:49 -0700

    fish: update completions for new `rl`

--- fish/packages/my-completions/completions/rl.fish
+++ fish/packages/my-completions/completions/rl.fish
@@ -1,15 +1,24 @@
 function __fish_complete_rl
-    begin
-        functions -n \
-            | string split ", "
+    set -f output
 
-        printf "%s\\n" $__fish_config_dir/*.fish \
-            | string match -rg "^$__fish_config_dir/(.+)\.fish"
+    # Add sourceable files
+    set -f envdirs $XDG_CONFIG_HOME ~/.private $__fish_config_dir
+    set -f envfiles (path basename -E $envdirs/env.d/*.{env,fish})
+    set -a output $envfiles\t.env\n
 
-        printf "%s\\n" $__fish_config_dir/conf.d/*.fish \
-            | string match -rg "^$__fish_config_dir/conf.d/(.+)\.fish"
-    end \
-        | path sort -u
+    set -f conffiles (path basename -E $__fish_config_dir{,/**/conf.d}/*.fish)
+    set -a output $conffiles\tfile\n
+
+    # Add function names
+    set -f funcs (functions -n)
+    set -a output $funcs\tfunction\n
+
+    # Add completion definitions
+    set -f comps (path basename -E $fish_complete_path/*.fish)
+    set -a output $comps\tcompletion\n
+
+    echo -ns $output | path sort -u
 end
 
-complete -c rl -xa "(__fish_complete_rl)"
+complete -c rl -n "__fish_is_nth_token 1" -xa "(__fish_complete_rl)"
+complete -c rl -s v -l verbose -d "Verbose output"
