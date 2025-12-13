29ba99fe80ec84f5fd8fc6491d846a06e0ee3048
Z. Grace Moreau <zgracem@users.noreply.github.com>
2025-12-13 11:06:15 -0700

    findalias: it lives in `XDG_BIN_HOME` now

--- fish/functions/alias2symlink.fish
+++ fish/functions/alias2symlink.fish
@@ -1,12 +1,12 @@
 function alias2symlink
-    functions -q find-alias; or return 127
+    command -q findalias; or return 127
     # Requires `brew install coreutils`
     command -q gmktemp; and command -q gln; or return 127
 
     for file in $argv
         if file -p $file | string match -q '*: MacOS Alias file'
             set -l path (path resolve $file)
-            and set -l dest (find-alias $path | string trim -rc/)
+            and set -l dest (findalias $path | string trim -rc/)
             and set -l tmpfile (gmktemp --suffix .alias)
             and gln -s -b -v $dest $path
             or return
