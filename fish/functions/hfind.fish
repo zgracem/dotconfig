75f5d7884c68353cbc74cde9c24e7ff26946790c
Z. Grace Moreau <zgracem@users.noreply.github.com>
2026-03-20 17:25:52 -0600

    hfind: simplify
    `bat` doesn't render fish script correctly and I don't care enough to
    figure out why.

--- fish/functions/hfind.fish
+++ fish/functions/hfind.fish
@@ -1,6 +1,4 @@
 # Adds timestamps and syntax highlighting
 function hfind -d "Search the command history"
-    command -q bat; and set -fx PAGER bat -p -lfish
-    set -fx BAT_PAGER "less -RFX"
-    history search --reverse --show-time="# %F %T%n" $argv
+    history search --reverse --show-time="# %F %T%n" --color=always $argv
 end
