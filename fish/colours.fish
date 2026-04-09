d574b97bb67445e2a5f4ed05a82abc6c76bdc682
Z. Grace Moreau <zgracem@users.noreply.github.com>
2026-04-09 10:37:41 -0600

    fish: fix path to dircolors Makefile

--- fish/colours.fish
+++ fish/colours.fish
@@ -24,7 +24,7 @@ if command -q eza
     set --erase LS_COLORS EXA_COLORS EZA_COLORS
 else if is-gnu ls; and path is -d $XDG_CONFIG_HOME/dircolors
     set -l ls_colors_file "$XDG_DATA_HOME/dircolors/thirty2k.ls_colors.fish"
-    make -s -C $XDG_DATA_HOME/dircolors
+    make -s -C $XDG_CONFIG_HOME/dircolors
     set -gx LS_COLORS (string match -r "(?<=')(?:[^=]+=(?:[\d;]+|target):)+" <$ls_colors_file)
 else
     # Generated at http://geoff.greer.fm/lscolors/
